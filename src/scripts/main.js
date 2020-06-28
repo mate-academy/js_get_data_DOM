79e3344242c0552be820424c551aa4145bdc6b39
Anatoliy Pokhylko <pokhilko.av@gmail.com>
2020-06-28 21:40:20 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const tagsPopulation = document.querySelectorAll('span.population');
+const arrayPopulation = [...tagsPopulation].map(item =>
+  !Number.isNaN((item.innerText).replace(/[,]/g, ''))
+    ? Number((item.innerText).replace(/[,]/g, ''))
+    : 0
+);
+const totalPopulation = arrayPopulation.reduce((acum, item) => acum + item);
+const avaragePopulation = totalPopulation / arrayPopulation.length;
+const total = document.querySelector('.total-population');
+const avarage = document.querySelector('.average-population');
+
+total.textContent = totalPopulation;
+avarage.textContent = avaragePopulation;
