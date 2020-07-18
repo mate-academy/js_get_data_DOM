a5dccbf1199287b4b465b79296ae11c81eebf4b1
Vlad Savustianenko <vladsavustianenko@gmail.com>
2020-07-18 15:01:35 +0300

    'Solution'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write your code here
+const populations = document.getElementsByClassName('population');
+
+let totalPopulation = 0;
+
+for (let i = 0; i < populations.length; i++) {
+  totalPopulation += parseInt(populations[i].innerHTML.split(',').join(''));
+}
+
+const averagePopulation = totalPopulation / populations.length;
+
+document.querySelector('.total-population')
+  .innerHTML = totalPopulation;
+
+document.querySelector('.average-population')
+  .innerHTML = averagePopulation;
