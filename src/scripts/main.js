05dcde25db30c4d590e75988bd95696a9c31b96c
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-16 14:26:48 +0300

    +

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+document.querySelector('.total-population')
+  .innerHTML = String([...document.querySelectorAll('.population')]
+    .map(pop => pop.textContent.split(',').join(''))
+    .reduce((sum, pop) => sum + +pop, 0)).split('')
+    .map((elem, i, arr) => ((arr.length - 1 - i) % 3 === 0 && i < arr
+      .length - 1) ? `${elem},` : elem).join('');
+
+document.querySelector('.average-population')
+  .innerHTML = String([...document.querySelectorAll('.population')]
+    .map(pop => pop.textContent.split(',').join(''))
+    .reduce((sum, pop) => sum + +pop, 0) / document
+    .querySelectorAll('.population').length).split('')
+    .map((elem, i, arr) => ((arr.length - 1 - i) % 3 === 0 && i < arr
+      .length - 1) ? `${elem},` : elem).join('');
