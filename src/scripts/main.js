61c8d4a1af420b918f722d0ed7894f37d642766c
Serhii Antonyshyn <serginio4422@gmail.com>
2020-08-17 13:10:30 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write your code here
+const population = document.getElementsByClassName('population');
+
+let sum = 0;
+
+for (let i = 0; i < population.length; i++) {
+  sum += Number(population[i].textContent.replace(/,/g, ''));
+}
+
+function addComma(number) {
+  const number2 = String(number);
+  const splitNumber = number2.split('');
+
+  for (let y = splitNumber.length % 3; y < splitNumber.length; y = y + 3) {
+    splitNumber[y - 1] = splitNumber[y - 1] + ',';
+  }
+
+  const finalNumber = splitNumber.join('');
+
+  return finalNumber;
+}
+
+const totalPopulation = document.getElementsByClassName('total-population');
+
+totalPopulation[0].textContent = addComma(sum);
+
+const averagePopulation = document.getElementsByClassName('average-population');
+const averageSum = sum / population.length;
+
+averagePopulation[0].textContent = addComma(averageSum);
