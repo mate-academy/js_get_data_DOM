793029c53d301e90319e63d1549bf7a2e612f834
Misha Grinko <glukym@gmail.com>
2020-08-18 06:36:51 +0300

    Update src/scripts/main.js
    Co-authored-by: Kryvytskyi Volodymyr <vovakrivickiy34@gmail.com>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 
 document.querySelector('.total-population')
   .innerHTML = String([...document.querySelectorAll('.population')]
-    .map(pop => pop.textContent.split(',').join(''))
+    .map(population => population.textContent.split(',').join(''))
     .reduce((sum, pop) => sum + +pop, 0)).split('')
     .map((elem, i, arr) => ((arr.length - 1 - i) % 3 === 0 && i < arr
       .length - 1) ? `${elem},` : elem).join('');
