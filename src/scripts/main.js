97d0f84f4fabc2f9ef82d0da8f15b801dc6ed70a
Maxym Chernyavskiy <masyatya@gmail.com>
2020-08-19 17:27:45 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const listPopulation = document.querySelectorAll('.population');
+let total = 0;
+
+for (const population of listPopulation) {
+  const num = population.textContent;
+
+  total += parseInt(num.split(',').join(''));
+}
+
+const totalSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+
+totalSpan.textContent = total;
+averageSpan.textContent = total / listPopulation.length;
