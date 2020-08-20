37c161026f2e11e70632a077e9c2abfec2558954
oggyrock <steelo.sun@gmail.com>
2020-08-20 16:00:24 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const arr = [...document.querySelectorAll('.population')]
+  .map(elem => +elem.textContent.split(',').join(''));
+
+document.querySelector('.total-population').textContent
+= addCommas(arr.reduce((a, b) => a + b));
+
+document.querySelector('.average-population').textContent
+= addCommas(arr.reduce((a, b, i) => (a * i + b) / (i + 1)));
+
+function addCommas(number) {
+  const numArr = ('' + number).split('');
+  let i = numArr.length - 3;
+
+  while (i > 0) {
+    numArr.splice(i, 0, ',');
+    i -= 3;
+  }
+
+  return numArr.join('');
+}
