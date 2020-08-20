c915968e0fb0a4b5c22321cc4e251930858c4821
oivannikov <alex0502ivannikov@gmail.com>
2020-08-20 14:20:29 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const getCountries = [...document.querySelectorAll('.population')];
+const getPopulationCountries = getCountries.map(country => {
+  return Number(country.innerHTML.split(',').join(''));
+});
+const getTotalPopulationCountries = getPopulationCountries
+  .reduce((totalPopulation, currentPopulation) =>
+    totalPopulation + currentPopulation);
+const getAveragePopulation = getTotalPopulationCountries / getCountries.length;
+
+document
+  .querySelector('.total-population').innerHTML = getTotalPopulationCountries
+    .toLocaleString()
+    .replace(/\s/g, ',');
+
+document
+  .querySelector('.average-population').innerHTML = getAveragePopulation
+    .toLocaleString()
+    .replace(/\s/g, ',');
