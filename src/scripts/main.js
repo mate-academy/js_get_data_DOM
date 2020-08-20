ae811f357973def790957a19f1ceee94aefc8dfc
nanccyy <lidiapriymak@gmail.com>
2020-08-20 21:37:47 +0800

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const items = document.querySelectorAll('.population');
+
+const numbers = [...items].map(item => +item.innerText.split(',').join(''));
+const total = numbers.reduce((acc, num) => (acc + num), 0);
+
+const totalOnPage = total.toLocaleString('en');
+const averageOnPage = (total / numbers.length).toLocaleString('en');
+
+document.querySelector('.total-population').textContent = totalOnPage;
+document.querySelector('.average-population').textContent = averageOnPage;
