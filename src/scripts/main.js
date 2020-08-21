5d3e30f4a9b347fbe6332911e956993afe39c6c6
RusTron <rusan.tron@gmail.com>
2020-08-21 11:48:15 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+let populations = document.querySelectorAll('.population');
+const reg = /,/g;
+
+populations = [...populations].map(x => Number(x.innerText.replace(reg, '')))
+  .filter(x => !isNaN(x));
+
+const totalPopulation = populations.reduce((acc, el) => acc + el);
+
+document.querySelector('.total-population').innerHTML = totalPopulation;
+
+document.querySelector('.average-population').innerHTML
+= totalPopulation / populations.length;
