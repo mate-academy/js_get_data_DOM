b5a57ad94311a83bd044f7d687c44c423e86a46f
Mikle25 <mikle2593@gmail.com>
2020-08-21 14:48:16 +0300

    population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const populationsSize = [...population].map(countryPopulation => {
+  return +countryPopulation.textContent.replace(/,/g, '');
+});
+
+const total = populationsSize.reduce((sum, value) => sum + value);
+const average = total / populationsSize.length;
+
+totalPopulation.innerHTML = total.toLocaleString('en-US');
+averagePopulation.innerHTML = average.toLocaleString('en-US');
