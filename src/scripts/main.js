6a7c63d03e2ec8d29fb8274d7f293aaafad3c74c
Anastasiia Lukina <anlukina24@gmail.com>
2020-08-21 13:34:10 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const span = document.querySelectorAll('.population');
+
+const arr = [...span]
+  .map(text => text.innerText)
+  .map(elem => elem.split(',').join(''));
+
+const totalValue = arr.reduce((accum, elem) => accum + +elem, 0);
+
+document
+  .getElementsByClassName('total-population')[0]
+  .innerText = totalValue;
+
+document
+  .getElementsByClassName('average-population')[0]
+  .innerText = totalValue / arr.length;
