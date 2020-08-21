308628dfdde1a22c0ed47663cd3d5b0dd05f2792
Zarva <zarva6596@gmail.com>
2020-08-21 17:19:19 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const people = document.querySelectorAll('span');
+const total = [...people]
+  .map(x => +x.innerText
+    .split(',')
+    .join(''))
+  .splice(0, 10)
+  .reduce((start, a) => start + a);
+
+const average = total / 10;
+
+document.querySelector('.total-population').innerText = total;
+document.querySelector('.average-population').innerText = average;
