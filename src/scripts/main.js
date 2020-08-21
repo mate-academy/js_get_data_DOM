2fe8edec621b1d23c334dcb9a62613c0e68f1b32
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-08-21 21:59:46 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+// get
+const populations = [];
+
+document.querySelectorAll('.population')
+  .forEach(elem => populations
+    .push(+elem.innerText.split(',').join('')));
+
+const total = populations.reduce((sum, elem) => sum + elem);
+const avarage = total / populations.length;
+
+// set
+document.querySelector('.total-population').innerText = total;
+document.querySelector('.average-population').innerText = avarage;
