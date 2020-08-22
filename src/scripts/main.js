d2aafe6c721f359c24740cf34272e6c856038408
Anastasiia Bortnichuk <feilanheid@gmail.com>
2020-08-22 17:44:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const items = document.querySelectorAll('.population');
+const numbers = [...items].map(item => +item.innerText.split(',').join(''));
+const total = numbers.reduce((a, b) => a + b);
+const avarage = total / numbers.length;
+
+const totalStr = total.toString().split('');
+
+totalStr.splice(-3, 0, ',');
+totalStr.splice(-7, 0, ',');
+totalStr.splice(-11, 0, ',');
+
+const totalResult = totalStr.join('');
+
+const avarageStr = avarage.toString().split('');
+
+avarageStr.splice(-3, 0, ',');
+avarageStr.splice(-7, 0, ',');
+
+const avarageResult = avarageStr.join('');
+
+const totalSpan = document.querySelector('.total-population');
+
+totalSpan.innerText = totalResult;
+
+const avarageSpan = document.querySelector('.average-population');
+
+avarageSpan.innerText = avarageResult;
