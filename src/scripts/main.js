400768a229bb8ec9efdc3c8da3e8b9d999820bee
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-08-23 20:54:20 +0300

    toLocaleString() added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,5 +11,8 @@ const total = populations.reduce((sum, elem) => sum + elem);
 const avarage = total / populations.length;
 
 // set
-document.querySelector('.total-population').innerText = total;
-document.querySelector('.average-population').innerText = avarage;
+document.querySelector('.total-population').innerText = total
+  .toLocaleString('en-AU');
+
+document.querySelector('.average-population').innerText = avarage
+  .toLocaleString('en-AU');
