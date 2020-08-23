16f9c26589df55a819ba01fb239fbca8287bf2a3
Yurii Rysyn <zerocool_cs@ukr.net>
2020-08-23 15:59:36 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write your code here
+const listOfPopulation = [...document.querySelectorAll('.population')]
+  .map(item => {
+    return +item.innerHTML.split(',').join('');
+  });
+
+const sumOfPopulation = [...listOfPopulation]
+  .reduce((sum, current) => sum + current, 0);
+
+const avaragePopulation = sumOfPopulation / listOfPopulation.length;
+
+document.querySelector('.total-population').innerHTML = sumOfPopulation;
+document.querySelector('.average-population').innerHTML = avaragePopulation;
