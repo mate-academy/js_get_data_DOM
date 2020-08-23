7716e63a6c11c93df07928d6cb0ececf9871d888
Artur <artroyal.pro@gmail.com>
2020-08-23 13:47:44 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')];
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const sum = population
+  .map(country => {
+    return Number(country.innerText.replace(/,/g, ''));
+  })
+  .reduce((a, b) => a + b);
+
+totalPopulation.innerText = sum;
+averagePopulation.innerText = sum / population.length;
