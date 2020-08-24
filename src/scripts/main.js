96c1f1c96f038a2d89b6ff855adfec1c450a7143
DmJSCourse <dimonua@gmail.com>
2020-08-24 14:07:04 +0300

    completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const popsCollection = document.getElementsByClassName('population');
+const popsArray = [];
+
+for (const element of popsCollection) {
+  popsArray.push(Number(element.textContent.split(',').join('')));
+}
+
+const total = popsArray.reduce((a, b) => a + b);
+const average = total / popsArray.length;
+
+document.querySelector('.total-population').textContent = total;
+document.querySelector('.average-population').textContent = average;
