0e1bae16afc782f409f50539bb03e0397d7c317c
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-08-25 02:26:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+
+const population = [...spans]
+  .map(item => Number(item.innerText.split(',').join('')));
+
+const total = population.reduce((sum, current) => sum + current);
+
+const average = population
+  .reduce((sum, current) => sum + current) / population.length;
+
+function separator(number) {
+  let toSeparate = number;
+  const result = [];
+
+  while (toSeparate > 1000) {
+    result.push(toSeparate % 1000);
+    toSeparate = Math.trunc(toSeparate / 1000);
+  }
+
+  result.push(toSeparate);
+
+  return result.reverse().join(',');
+}
+
+document.querySelector('.total-population').innerText = separator(total);
+
+document.querySelector('.average-population').innerText = separator(average);
