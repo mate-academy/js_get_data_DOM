4827474359b8cd3ca47d2219de5390cae28661f2
yuriimovchko <movchko.yuriy@gmail.com>
2020-08-25 01:39:42 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+let totalNum = 0;
+let averageNum = 0;
+
+for (const num of population) {
+  totalNum += +num.innerText.split(',').join('');
+}
+
+averageNum = totalNum / population.length;
+
+total.replaceWith(totalNum.toLocaleString('en-US'));
+average.replaceWith(averageNum.toLocaleString('en-US'));
