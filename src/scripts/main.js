aae2625213553ff29a24fb50afd7ad4c473cfa8f
ChrisZinch <chrispakulyak@gmail.com>
2020-08-24 15:14:16 +0300

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write your code here
+// eslint-disable-next-line no-unused-vars
+const population = document.querySelectorAll('.population');
+const arr = [...population].map(item => +item.innerText.split(',').join(''));
+const total = arr.reduce((sum, current) => sum + current, 0);
+const average = total / arr.length;
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.textContent = total.toLocaleString('en-US');
+averagePopulation.textContent = average.toLocaleString('en-US');
