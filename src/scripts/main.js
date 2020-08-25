8b3caf13bd79511e75760c25904c7e22ae177ef0
oggyrock <steelo.sun@gmail.com>
2020-08-25 12:21:59 +0300

    fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const arr = [...document.querySelectorAll('.population')]
+const populationArr = [...document.querySelectorAll('.population')]
   .map(elem => +elem.textContent.split(',').join(''));
 
 document.querySelector('.total-population').textContent
-= addCommas(arr.reduce((a, b) => a + b));
+  = addCommas(populationArr.reduce((accu, elem) => accu + elem));
 
 document.querySelector('.average-population').textContent
-= addCommas(arr.reduce((a, b, i) => (a * i + b) / (i + 1)));
+  = addCommas(populationArr.reduce((accu, elem, i) =>
+    ((accu * i) + elem) / (i + 1)));
 
 function addCommas(number) {
   const numArr = ('' + number).split('');
