10b291c0a8aacc076ccb7e6a841e73165bfd9526
nznmvs <nznmvs@gmail.com>
2020-08-25 16:15:38 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const displayTotal = document.querySelector('.total-population');
+const displayAverage = document.querySelector('.average-population');
+const populations = [...document.querySelectorAll('.population')];
+
+const total = populations.map(population => {
+  return +population.textContent.split(',').join('');
+}).reduce((sum, population) => sum + population, 0);
+
+const average = total / populations.length;
+
+function convertIt(num) {
+  return String(num).split('')
+    .map((digit, i, arr) => {
+      if ((arr.length - 1 - i) % 3 === 0
+        && i < arr.length - 1) {
+        return digit + ',';
+      } else {
+        return digit;
+      }
+    }).join('');
+}
+
+displayTotal.innerHTML = convertIt(total);
+displayAverage.innerHTML = convertIt(average);
