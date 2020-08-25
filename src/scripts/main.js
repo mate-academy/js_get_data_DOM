65b0a635c135b7744b0f5c1029bab088c588a121
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-08-25 10:23:13 +0300

    changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,20 +10,8 @@ const total = population.reduce((sum, current) => sum + current);
 const average = population
   .reduce((sum, current) => sum + current) / population.length;
 
-function separator(number) {
-  let toSeparate = number;
-  const result = [];
+document.querySelector('.total-population')
+  .replaceWith(total.toLocaleString('en-US'));
 
-  while (toSeparate > 1000) {
-    result.push(toSeparate % 1000);
-    toSeparate = Math.trunc(toSeparate / 1000);
-  }
-
-  result.push(toSeparate);
-
-  return result.reverse().join(',');
-}
-
-document.querySelector('.total-population').innerText = separator(total);
-
-document.querySelector('.average-population').innerText = separator(average);
+document.querySelector('.average-population')
+  .replaceWith(average.toLocaleString('en-US'));
