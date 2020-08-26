be3290bd7e4d6463c194fe7e71cdd52d1200368f
Anastasiia Bortnichuk <feilanheid@gmail.com>
2020-08-26 12:19:35 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,20 +5,9 @@ const numbers = [...items].map(item => +item.innerText.split(',').join(''));
 const total = numbers.reduce((a, b) => a + b);
 const avarage = total / numbers.length;
 
-const totalStr = total.toString().split('');
+const totalResult = total.toLocaleString('en-US');
 
-totalStr.splice(-3, 0, ',');
-totalStr.splice(-7, 0, ',');
-totalStr.splice(-11, 0, ',');
-
-const totalResult = totalStr.join('');
-
-const avarageStr = avarage.toString().split('');
-
-avarageStr.splice(-3, 0, ',');
-avarageStr.splice(-7, 0, ',');
-
-const avarageResult = avarageStr.join('');
+const avarageResult = avarage.toLocaleString('en-US');
 
 const totalSpan = document.querySelector('.total-population');
 
