6bd85064d293d4e1c1866fb1d8d4d5b98490dfbe
Yazheviks <vicsergeyeva@gmail.com>
2020-10-01 16:19:10 +0300

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('span.population');
+const totalPlace = document.querySelector('span.total-population');
+const averagePlace = document.querySelector('span.average-population');
+
+const numberedPopulation = [...population].map(item => {
+  let a = item.textContent.split(',');
+
+  a = a.join('');
+
+  return +a;
+});
+const totalData = numberedPopulation.reduce((sum, item) => sum + item);
+const averageData = totalData / numberedPopulation.length;
+
+totalPlace.textContent = totalData;
+averagePlace.textContent = averageData;
