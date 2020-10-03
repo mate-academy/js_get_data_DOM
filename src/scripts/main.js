7a169fa4683533bf3851b9adbe00811aafa72673
losanov <losanov@ukr.net>
2020-10-03 16:51:45 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const totalPoputationCage = document.querySelector('.total-population');
+const averageCage = document.querySelector('.average-population');
+const popArr = [];
+
+populations.forEach((elem) => popArr.push(+elem.textContent.replace(/,/g, '')));
+
+const totalPoputation = popArr.reduce((a, b) => a + b);
+
+totalPoputationCage.textContent = totalPoputation.toLocaleString('en-US');
+
+averageCage.textContent
+= (totalPoputation / populations.length).toLocaleString('en-US');
