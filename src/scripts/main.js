c0e66a8f14458c5f192cca95dbc61ae6abc8fd52
dmitry <akulinind@gmail.com>
2020-10-09 14:01:25 +0300

    add calc total func

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+
+function calcTotal() {
+  const populationCollection = document.querySelectorAll('.population');
+  const totalSpan = document.querySelector('p.total > span');
+
+  const total = [...populationCollection]
+    .reduce((acc, elem) =>
+      acc + parseInt(elem.textContent.replaceAll(',', '')),
+    0);
+
+  totalSpan.textContent = total;
+};
+
+// function calcAverage()
+
+calcTotal();
