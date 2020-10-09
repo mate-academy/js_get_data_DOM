6cc2515d7edf7508002d9ad2ef7b0be2c647a7e2
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-10-09 17:48:06 +0300

    solution get data DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const population = document.getElementsByClassName('population');
+
+const allCountry = [...population].map(item => {
+  return item.innerText.split(',').join('');
+});
+const totalPopulation = allCountry.reduce((acc, cur) => acc + +cur, 0);
+const aweragePopulation = totalPopulation / allCountry.length;
+const selectorTotal = document.querySelector('.total-population');
+const selectoraverage = document.querySelector('.average-population');
+
+selectorTotal.innerHTML = totalPopulation.toLocaleString();
+selectoraverage.innerHTML = aweragePopulation.toLocaleString();
