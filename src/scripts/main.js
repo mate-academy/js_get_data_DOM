62afae30a98a8c731183515f26404ca4ced143b3
andlap24 <andrew.laptev24@gmail.com>
2020-10-12 14:51:49 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const populationArr = [...population];
+
+const totalPopulation = populationArr
+  .map(item => +(item.innerText.replace(/[\s.,]/g, '')))
+  .reduce((a, b) => a + b);
+const averagePopulation = totalPopulation / populationArr.length;
+
+const totalElement = document.querySelector('.total-population');
+
+totalElement.innerHTML = totalPopulation;
+
+const averageElement = document.querySelector('.average-population');
+
+averageElement.innerHTML = averagePopulation;
