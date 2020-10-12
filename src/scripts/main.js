27f28cb504928094ca97dc4167f6ac4edf0fddac
Tania Omelko <tanyaomelko39@gmail.com>
2020-10-12 15:56:41 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const population = document.body.children[0].querySelectorAll('.population');
-const populationData = [...population].map(x => +x.innerText.replace(/,/g, ''));
+const population = document.querySelectorAll('.population');
+const populationData = [...population].map(i => +i.innerText.replace(/,/g, ''));
 const populationTotal = populationData.reduce((a, b) => a + b, 0);
 const populationAverage = populationTotal / population.length;
 
-document.body.children[0].querySelector('.total-population').innerText
+document.querySelector('.total-population').innerText
   = populationTotal.toLocaleString();
 
-document.body.children[0].querySelector('.average-population').innerText
+document.querySelector('.average-population').innerText
   = populationAverage.toLocaleString();
