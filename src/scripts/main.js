375aae1544acd7d61a6426fa4d2c56b547a60459
milla-romankova <milla-romankova@gmail.com>
2020-10-13 00:06:41 +0300

    add js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const totalPopulationElement = document.querySelector('.total-population');
+const averagePopulationElement = document.querySelector('.average-population');
+
+const totalPopulation = [...populations].reduce(
+  (accumulator, nextPopulation) => {
+    return accumulator + Number(nextPopulation.textContent.replace(/,/g, ''));
+  }, 0);
+
+const averagePopulation = totalPopulation / populations.length;
+
+totalPopulationElement.textContent = totalPopulation.toLocaleString();
+averagePopulationElement.textContent = averagePopulation.toLocaleString();
