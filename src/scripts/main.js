4d659e51ba9ddb16001023da0a3ecea905066767
Yuriy Putsan <putsan57@gmail.com>
2020-10-14 00:46:41 +0300

    calculate total and average population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const populationOfCountries = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+const parsedPopulations = [ ...populationOfCountries ]
+  .map(populationOfCountry => (
+    +populationOfCountry.textContent.split(',').join('')
+  ));
+const totalPopulation = parsedPopulations
+  .reduce((acc, populationOfCountry) => (
+    acc + populationOfCountry
+  ), 0);
+const averagePopulation = totalPopulation / parsedPopulations.length;
+
+total.textContent = totalPopulation
+  .toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+
+average.textContent = averagePopulation
+  .toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
