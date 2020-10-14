c4696661153d7a9fd02b1580ae982d15ddeaf783
Yurii <yurastt1@gmail.com>
2020-10-15 01:20:56 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const populationArray
+= ([...population].map(
+  item => +item.innerText.replace(/,/g, '')));
+
+const populationTotal
+= populationArray.reduce(
+  (accumulator, currentValue) => accumulator + currentValue);
+
+const avaragePopulation = populationTotal / populationArray.length;
+
+document.querySelector('.total-population').innerText = populationTotal;
+
+document.querySelector('.average-population').innerText = avaragePopulation;
