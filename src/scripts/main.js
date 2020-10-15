5ac9977605c7bdf963821eefd4e918cf290279a8
Eugene <evolokhin@gmail.com>
2020-10-15 16:14:49 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+// debugger;
+const populationSpan = document.querySelectorAll('.population');
+
+const population = [...populationSpan].map(value =>
+  (
+    +[...value.textContent].join('').split(',').join('')
+  ));
+
+const totalPopulation = population.reduce((acc, value)=> acc+value);
+const averagePopulation =  totalPopulation/ population.length;
+
+document.querySelector('.total-population').textContent = totalPopulation;
+document.querySelector('.average-population').textContent = averagePopulation;
