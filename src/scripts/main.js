c1d6eaf4685fc251f58e01e254882dc3b7c38cc6
Marina <m.gerasymova29@gmail.com>
2020-10-15 16:11:23 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const total = [...document.querySelectorAll('.population')];
+const totalPopulationElement = document.querySelector('.total-population');
+const avaragePopulationElement = document.querySelector('.average-population');
+const totalPopulation = total.reduce((sum, population) =>
+  sum + +population.innerHTML.split(',').join(''), 0);
+
+totalPopulationElement.innerHTML = totalPopulation.toLocaleString('en-EN');
+
+avaragePopulationElement.innerHTML = Math.round(
+  totalPopulation / total.length).toLocaleString('en-EN');
