80ef60b364db0ba243d235b119044620303c8b9b
Olena kuzii <okuziy@gmail.com>
2020-10-15 14:22:57 +0300

    change Data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const population = document.querySelectorAll('.population');
-const populationDate = [...population].map(item => (
+const populationData = [...population].map(item => (
   parseInt(item.innerText.split(',').join('')))
 );
-const populationSum = populationDate.reduce((sum, item) => sum + item, 0);
-const populationAverage = populationSum / populationDate.length;
+const populationSum = populationData.reduce((sum, item) => sum + item, 0);
+const populationAverage = populationSum / populationData.length;
 
-const totalPopulationEl = document.querySelector('.total-population');
-const averagePopulationEl = document.querySelector('.average-population');
+const totalPopulationElem = document.querySelector('.total-population');
+const averagePopulationElem = document.querySelector('.average-population');
 
-totalPopulationEl.textContent = populationSum.toLocaleString('en-US');
-averagePopulationEl.textContent = populationAverage.toLocaleString('en-US');
+totalPopulationElem.textContent = populationSum.toLocaleString('en-US');
+averagePopulationElem.textContent = populationAverage.toLocaleString('en-US');
