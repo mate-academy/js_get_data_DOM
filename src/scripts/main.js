6540e97a873359d4fcf0b61322e452a2eca334e9
Lancaelot <Lancaelot@gmail.com>
2020-10-15 14:48:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const populationList = document.querySelectorAll('span.population');
+const populationArr = [...populationList].map(
+  item => +item.innerText.replace(/,/g, '')
+);
+
+function calculateTotal() {
+  return populationArr.reduce((acc, country) => acc + country, 0);
+}
+
+function calculateAverage() {
+  return calculateTotal() / populationArr.length;
+}
+
+document.querySelector('p.total').innerText = `Total: ${
+  Intl.NumberFormat('en-US').format(calculateTotal())
+}`;
+
+document.querySelector('p.average').innerText = `Average: ${
+  Intl.NumberFormat('en-US').format(calculateAverage())
+}`;
