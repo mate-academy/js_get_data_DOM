7335462753d92b81acdfdc982d8b26a73f438072
Yurii <yurastt1@gmail.com>
2020-10-15 16:01:13 +0300

    changed style

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,8 +6,7 @@ const populationArray = ([...population].map(
   item => +item.innerText.replace(/,/g, ''))
 );
 
-const populationTotal
-= populationArray.reduce(
+const populationTotal = populationArray.reduce(
   (accumulator, currentValue) => accumulator + currentValue);
 
 const avaragePopulation = populationTotal / populationArray.length;
