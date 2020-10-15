8064357d5912775d6119a38cb70cdd521101dbd2
Eugene <evolokhin@gmail.com>
2020-10-15 16:16:41 +0300

    fix spaces

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,8 +8,8 @@ const population = [...populationSpan].map(value =>
     +[...value.textContent].join('').split(',').join('')
   ));
 
-const totalPopulation = population.reduce((acc, value)=> acc+value);
-const averagePopulation =  totalPopulation/ population.length;
+const totalPopulation = population.reduce((acc, value) => acc + value);
+const averagePopulation = totalPopulation / population.length;
 
 document.querySelector('.total-population').textContent = totalPopulation;
 document.querySelector('.average-population').textContent = averagePopulation;
