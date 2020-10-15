8b51de7cd3f68c824f8c33672811f78b5f5d8c77
Eugene <evolokhin@gmail.com>
2020-10-15 17:43:43 +0300

    fixed mentors comment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,8 @@
 
 const populationSpan = document.querySelectorAll('.population');
 
-const population = [...populationSpan].map(value =>
-  (
-    +value.textContent.split(',').join('')
-  ));
+const population = [...populationSpan]
+  .map(value => +value.textContent.split(',').join(''));
 
 const totalPopulation = population.reduce((acc, value) => acc + value);
 const averagePopulation = totalPopulation / population.length;
