179ad3ef6189a30826d49cb4a440ad8c3aaa7859
Dmitry Morozov <10mdv11@gmail.com>
2020-10-16 14:49:01 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,7 @@ const averagePopulationClass = document.querySelector('.average-population');
 const total = population.reduce((sum, currentPopulation) =>
   sum + +currentPopulation.innerHTML.split(',').join(''), 0);
 
-const average = total / population.length;
+const average = Math.round(total / population.length);
 
 totalPopulationClass.innerHTML = total.toLocaleString('en-EN');
-averagePopulationClass.innerHTML = Math.round(average).toLocaleString('en-EN');
+averagePopulationClass.innerHTML = average.toLocaleString('en-EN');
