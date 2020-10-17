58d37b50795b7cfcc0fff38574a5df0e36925f5e
Taras Shchadylo <tarasdidg@gmail.com>
2020-10-17 16:21:36 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+const populationNumbers = [...population].map(value => {
+  return Number(value.textContent.split(',').join(''));
+});
+
+const calculatedTotal = populationNumbers.reduce((acc, val) => acc + val);
+const calculatedAverage = calculatedTotal / populationNumbers.length;
+
+total.textContent = calculatedTotal.toLocaleString();
+
+average.textContent = calculatedAverage.toLocaleString();
