f8bd18cf7b5f4ae49184783669f312a1011a6002
Olga Rubalska <olga.anikeieva@gmail.com>
2020-11-12 23:02:34 +0200

    Calculated total population number, average number and replaced the final spans

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const [...population] = document.querySelectorAll('.population');
+const textTotal = document.querySelector('.total-population');
+const textAverage = document.querySelector('.average-population');
+
+const result = function(arr) {
+  let sum = 0;
+
+  arr.forEach(country => {
+    const populationPerCountry = country.innerHTML.replace(/,/g, '');
+
+    sum += parseInt(populationPerCountry);
+  });
+
+  return sum;
+};
+
+const total = result(population);
+const average = total / population.length;
+
+textTotal.innerHTML = total.toLocaleString();
+
+textAverage.innerHTML = average.toLocaleString();
