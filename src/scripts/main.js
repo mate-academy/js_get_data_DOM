1024747d38280c3dfe0f41ff55af95c9e835405b
Ruslan <ruslan.ogarkov88@gmail.com>
2020-12-01 12:41:46 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const propulations = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const avaragePopulation = document.querySelector('.average-population');
+
+let totalPopulationNumber = 0;
+
+for (const num of propulations) {
+  totalPopulationNumber += +num.textContent.split(',').join('');
+}
+
+const avarageValue = totalPopulationNumber / propulations.length;
+
+totalPopulation.innerHTML = totalPopulationNumber;
+avaragePopulation.innerHTML = avarageValue;
