fe172c02059d19cdccc51030194426fbf0dc1646
Oleksandr Orel <oleksandrorel@ukr.net>
2020-12-08 08:37:06 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write your code here
+const population = document.querySelectorAll(`.population`);
+let totalPopulationValue = 0;
+
+for (const el of population) {
+  totalPopulationValue += +el.innerText.split(`,`).join(``);
+}
+
+const averagePopulationValue = totalPopulationValue / population.length;
+
+const totalPopulation = document.querySelector(`.total-population`);
+const averagePopulation = document.querySelector(`.average-population`);
+
+totalPopulation.textContent = totalPopulationValue;
+averagePopulation.textContent = averagePopulationValue;
