e085d707b27bf257e0e8273c0dfe1643da0f4499
AndrewKreshchenko <mail2andyk@gmail.com>
2020-12-11 20:15:34 +0200

    Provide functionality to display total and average values of populations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const populationsElems = document.getElementsByClassName('population');
+
+  const populations = Array.prototype.map.call(populationsElems, (elem) => {
+    return parseInt(elem.innerText.replaceAll(',', ''));
+  });
+
+  const total = populations.reduce((added, curr) => added + curr);
+  const average = total / populations.length;
+
+  document.querySelector('.total-population').innerText = total;
+  document.querySelector('.average-population').innerText = average;
+});
