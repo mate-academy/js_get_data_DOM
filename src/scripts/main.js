b0a5ae47e5c03928d070170823b502c326d6c595
Rostyslav Meleshko <meleshkorostyslav@gmail.com>
2020-12-28 17:13:24 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const arr = document.getElementsByClassName('population');
+
+const countries = [...arr].map((x, i) => x.innerHTML);
+
+const countriesPop = countries.map(x => Number(x.split(',').join('')));
+
+const sumPopulation = countriesPop.reduce((a, b) => a + b);
+
+const averagePopulation = sumPopulation / countriesPop.length;
+
+const elementSum = document.getElementsByClassName('total-population');
+
+const elementAvg = document.getElementsByClassName('average-population');
+
+elementSum[0].innerHTML = `${sumPopulation}`;
+
+elementAvg[0].innerHTML = `${averagePopulation}`;
