a0c87779e8801658f9d64edbc6921792650c9924
Serhii Alieksieievych <kajot1863@gmail.com>
2020-12-28 22:17:46 +0200

    Completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('.population');
+const arr = [...list];
+let total = 0;
+let count = 0;
+const validArr = [];
+
+for (let i = 0; i < arr.length; i++) {
+  validArr.push([]);
+
+  for (let j = 0; j < arr[i].textContent.length; j++) {
+    if (arr[i].textContent[j] !== ',') {
+      validArr[i].push(arr[i].textContent[j]);
+    }
+  }
+
+  validArr[i] = validArr[i].reduce((base, str) => base + str, '');
+}
+
+for (const number of validArr) {
+  total += +(number);
+  count++;
+}
+
+const average = total / count;
+
+document.querySelector('.average-population').textContent = average;
+document.querySelector('.total-population').textContent = total;
