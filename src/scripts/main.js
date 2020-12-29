6a90c6bf37def1d3bed7b8c0a89edbca9184be87
Dmytro Tsehelnyk <34zimnii@gmail.com>
2020-12-29 23:35:09 +0200

    reduced

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,15 @@
 'use strict';
 
 const populationElements = document.querySelectorAll('.population');
-const populationNums = [];
 let totalAmount = 0;
 
 populationElements.forEach(popElement => {
   const populationNum = +popElement.innerText.split(',').join('');
 
   totalAmount += populationNum;
-  populationNums.push(populationNum);
 });
 
-const averagePopulation = Math.floor(totalAmount / populationNums.length);
+const averagePopulation = Math.floor(totalAmount / populationElements.length);
 
 const totalPopulationEl = document.querySelector('.total-population');
 const averagePopulationEl = document.querySelector('.average-population');
