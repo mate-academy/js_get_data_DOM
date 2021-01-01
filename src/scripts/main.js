4d6a1ae4a1fb418c6b87e7887fc800a30c1e8ddf
Khrulenko Vitalii <khrulenko.v@gmail.com>
2021-01-01 14:19:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+const numberFormatting = function(n) {
+  const formatted = [];
+
+  if (String(n).length % 3 > 0) {
+    formatted.push(String(n).substr(0, String(n).length % 3));
+  }
+
+  for (let i = String(n).length % 3; i < String(n).length; i += 3) {
+    formatted.push(String(n).substr([i], 3));
+  }
+
+  return formatted.join(',');
+};
+
+const classes = document.getElementsByClassName('population');
+
+const populations = [...classes].map(
+  country => +country.textContent.split(',').join('')
+);
+
+populations.length = 10;
+
+const total = populations.reduce((sum, popul) => sum + popul);
+
+const average = total / populations.length;
+
+const totalElement = document.getElementsByClassName('total-population')[0];
+
+const averageElement = document.getElementsByClassName('average-population')[0];
+
+totalElement.innerText = numberFormatting(total);
+averageElement.innerText = numberFormatting(average);
