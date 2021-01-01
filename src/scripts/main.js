b0d733a4696cb1732cc9ec6af96f02b1320bfeed
Oleksandr Stolyarov <Stolyarov_396@icloud.com>
2021-01-01 21:37:17 +0200

    Solution redone after student codereview

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,8 @@ const listPopulation = [...list].map(item => +item.innerText.replace(/,/g, ''));
 const totalPopulation = listPopulation.reduce((sum, item) => sum + item, 0);
 const averagePopulation = totalPopulation / listPopulation.length;
 
-document.querySelectorAll('span.total-population')
-  .textContent = `${new Intl.NumberFormat('en-US')
-    .format(totalPopulation)} people`;
+document.querySelector('span.total-population')
+  .textContent = totalPopulation.toLocaleString();
 
-document.querySelectorAll('span.average-population')
-  .textContent = `${new Intl.NumberFormat('en-US')
-    .format(averagePopulation)} people`;
+document.querySelector('span.average-population')
+  .textContent = averagePopulation.toLocaleString();
