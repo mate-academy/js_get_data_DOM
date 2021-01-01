989250cfe823176c6ad73cb2695681806140bcdf
Oleksandr Stolyarov <Stolyarov_396@icloud.com>
2021-01-01 07:07:31 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('span.population');
+const listPopulation = [...list].map(item => +item.innerText.replace(/,/g, ''));
+
+const totalPopulation = listPopulation.reduce((sum, item) => sum + item, 0);
+const averagePopulation = totalPopulation / listPopulation.length;
+
+document.querySelectorAll('span.average-population')
+  .textContent = averagePopulation.toLocaleString();
+
+document.querySelectorAll('span.total-population')
+  .textContent = totalPopulation.toLocaleString();
