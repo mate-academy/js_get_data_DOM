38da79a3f747b6c79e40c14a967195726c2eb547
Nazar Valchyshyn <nvalja@gmail.com>
2021-01-04 21:43:31 +0200

    made task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const listPopulation = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+const count = listPopulation.length;
+
+const allPopulation = [...listPopulation]
+  .map(item => +item.textContent.split(',').join(''))
+  .reduce((prev, people) => prev + people, 0);
+
+total.textContent = allPopulation.toLocaleString('en-GB');
+average.textContent = (allPopulation / count).toLocaleString('en-GB');
