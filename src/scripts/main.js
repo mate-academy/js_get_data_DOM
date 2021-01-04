d11d15d2fe3cb862d33467faf577d62852e8168e
Maryna Zinchenko <pandoz16@gmail.com>
2021-01-04 11:28:58 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+const textTotal = [...list].map(item => item.innerText);
+const resultTotal = textTotal
+  .map(item => Number(item.replace(/[\s,]/g, '')))
+  .reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue;
+  });
+
+total.textContent = resultTotal.toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+average.textContent = (resultTotal / list.length).toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
