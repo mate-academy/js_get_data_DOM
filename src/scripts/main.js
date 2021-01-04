da48b71b9cd56dd6fbb9595d6ba1bb67a9e4e2f1
Vitalii Siedykh <siedykh.v@gmail.com>
2021-01-04 13:52:16 +0200

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('span.population');
+const total = document.querySelector('span.total-population');
+const average = document.querySelector('span.average-population');
+
+const populationAmount = [...population].map(value => {
+  return Number(value.textContent.split(',').join(''));
+});
+
+const calculatedTotal = populationAmount.reduce((acc, val) => acc + val);
+const calculatedAverage = calculatedTotal / populationAmount.length;
+
+total.textContent = calculatedTotal.toLocaleString();
+average.textContent = calculatedAverage.toLocaleString();
