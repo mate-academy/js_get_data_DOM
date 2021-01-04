39eabf52b658854dddefe2f254c7039387803adb
5Mountains <V.Piatyhor@gmail.com>
2021-01-04 16:05:39 +0200

    updated solution's result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,10 @@ const total = document.querySelector('.total-population');
 const average = document.querySelector('.average-population');
 const populationList = document.querySelectorAll('.population');
 
+const normalize = str => str.toLocaleString().replace(/\s/g, ',');
+
 const wholePopulation = [...populationList].reduce((acc, item) =>
   acc + Number(item.innerText.split(',').join('')), 0);
 
-total.innerText = wholePopulation;
-average.innerText = wholePopulation / populationList.length;
+total.innerText = normalize(wholePopulation);
+average.innerText = normalize(wholePopulation / populationList.length);
