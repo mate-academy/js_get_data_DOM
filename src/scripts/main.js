72edfbcb7a56e0bb5e1225f9abdc19b8ce368bc4
OleksandrLisnevskyi <lisnevskyi@gmail.com>
2021-01-05 07:34:16 +0200

    Add solution js_get_data_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
+// debugger;
+
 // write your code here
+const popAtrs = document.querySelectorAll('[class="population"]');
+const popTexts = [...popAtrs].map(item => +item.innerText.replace(/,/g, ''));
+
+const total = (popTexts.reduce((sum, num) => sum + num, 0));
+const average = (total / popTexts.length);
+
+const classTotalPop = document.querySelector('.total-population');
+
+classTotalPop.innerText = total.toLocaleString();
+
+const classAveragePop = document.querySelector('.average-population');
+
+classAveragePop.innerText = average.toLocaleString();
