844d9a32ded9e701c1693085634a268122e7e4fb
Serhii Alieksieievych <kajot1863@gmail.com>
2021-01-08 23:53:56 +0200

    Code was optimized

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,11 @@
 'use strict';
 
 const list = document.querySelectorAll('.population');
-const arr = [...list];
 let total = 0;
 let count = 0;
-const validArr = [];
 
-for (let i = 0; i < arr.length; i++) {
-  validArr.push([]);
-
-  for (let j = 0; j < arr[i].textContent.length; j++) {
-    if (arr[i].textContent[j] !== ',') {
-      validArr[i].push(arr[i].textContent[j]);
-    }
-  }
-
-  validArr[i] = validArr[i].reduce((base, str) => base + str, '');
-}
-
-for (const number of validArr) {
-  total += +(number);
+for (let i = 0; i < list.length; i++) {
+  total += +list[i].textContent.split(',').join('');
   count++;
 }
 
