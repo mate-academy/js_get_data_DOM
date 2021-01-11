f7d5d08fe0fd2574d59c2a414178c8dcfeea0a68
Nazarii <nazarrishchuk@gmail.com>
2021-01-11 13:55:30 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const textData = document.querySelectorAll('.population');
+
+const numberData = [ ...textData ]
+  .map(text => +text.textContent.replace(/,/g, ''));
+
+const total = numberData.reduce((a, b) => a + b);
+
+const average = total / numberData.length;
+
+const totalPage = document.querySelector('.total-population');
+
+totalPage.textContent = total.toLocaleString();
+
+const averagePage = document.querySelector('.average-population');
+
+averagePage.textContent = average.toLocaleString();
