79813d9e2d4aa6beed9f1de865983f4d5eecf042
dinexpod <work.toxa@gmail.com>
2021-01-14 16:19:16 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const populateValues = [...document.querySelectorAll('.population')]
+  .map(e => +e.textContent.replaceAll(',', ''));
+const sum = populateValues.reduce((prev, e) => prev + e);
+const average = sum / populateValues.length;
+
+const avgPopulationSpan = document.querySelector('.average-population');
+const totalPopulationSpan = document.querySelector('.total-population');
+
+avgPopulationSpan.textContent = average.toLocaleString();
+totalPopulationSpan.textContent = sum.toLocaleString();
