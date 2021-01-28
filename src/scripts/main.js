0aff000d4e2cd20f66a9c48e1c9c51ffec7e5fcb
DominoSl <artemenko.mik@gmail.com>
2021-01-28 16:12:22 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,14 @@ const averagePopulation = document.querySelector('.average-population');
 let populationSum = 0;
 
 for (const country of countriesPopulation) {
-  if (!isNaN(parseInt(country.innerText, 10))) {
-    populationSum += parseInt(country.innerText.replace(/,/g, ''), 10);
+  const population = parseInt(country.innerText.replace(/,/g, ''), 10);
+
+  if (!isNaN(population)) {
+    populationSum += population;
   }
 }
 
-totalPopulation.innerText = populationSum;
-averagePopulation.innerText = populationSum / countriesPopulation.length;
+totalPopulation.innerText = populationSum.toLocaleString('en-US');
+
+averagePopulation.innerText
+= (populationSum / countriesPopulation.length).toLocaleString('en-US');
