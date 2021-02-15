c16491dc3b9944586467f52f3ea8674c0d6b5082
Igor Lisogor <illisogori@gmail.com>
2021-02-15 11:51:52 +0200

    Total, avarage population calculated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const populationArray = [...population].map(countryPopulation =>
+  Number.parseInt(countryPopulation.innerHTML.split(',').join('')));
+let totalPopulationValue = populationArray.reduce((sum, currentValue) =>
+  sum + currentValue);
+let averagePopulationValue = totalPopulationValue / populationArray.length;
+let totalPopulationArray = [];
+let averagePopulationArray = [];
+
+totalPopulationValue = totalPopulationValue.toString().split('');
+
+let separatorCount = 0;
+
+for (let i = totalPopulationValue.length - 1; i >= 0; i--) {
+  let separatorString = (separatorCount % 3) ? '' : ',';
+
+  if (separatorCount === 0) {
+    separatorString = '';
+  }
+  separatorCount++;
+
+  totalPopulationArray[i] = totalPopulationValue[i] + separatorString;
+};
+separatorCount = 0;
+averagePopulationValue = averagePopulationValue.toString().split('');
+
+for (let i = averagePopulationValue.length - 1; i >= 0; i--) {
+  let separatorString = (separatorCount % 3) ? '' : ',';
+
+  if (separatorCount === 0) {
+    separatorString = '';
+  }
+  separatorCount++;
+
+  averagePopulationArray[i] = averagePopulationValue[i] + separatorString;
+};
+averagePopulationArray = averagePopulationArray.join('');
+totalPopulationArray = totalPopulationArray.join('');
+totalPopulation.innerText = totalPopulationArray;
+averagePopulation.innerText = averagePopulationArray;
