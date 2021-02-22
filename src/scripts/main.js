667092980fa9e29bdbbf38f132ee0814949046ac
Bohdan Dobak <chudolipton@gmail.com>
2021-02-22 21:11:41 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const allPopulation = document.querySelectorAll('.population');
+let sum = 0;
+
+for (const value of allPopulation) {
+  sum += Number(value.outerText.split(',').join(''));
+}
+
+const averageSum = sum / allPopulation.length;
+
+const totalPopulation = document.querySelector('.total-population');
+
+totalPopulation.innerHTML
+  = sum.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+
+const averagePopulation = document.querySelector('.average-population');
+
+averagePopulation.innerHTML
+  = averageSum.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
