dbef516d0ea2df45c11f8197c4f84c3d430a9522
Bohdan Dobak <chudolipton@gmail.com>
2021-02-23 11:57:47 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,15 +4,15 @@ const allPopulation = document.querySelectorAll('.population');
 let sum = 0;
 
 for (const value of allPopulation) {
-  sum += Number(value.outerText.split(',').join(''));
+  sum += Number(value.innerText.split(',').join(''));
 }
 
 const averageSum = sum / allPopulation.length;
 
 const totalPopulation = document.querySelector('.total-population');
 
-totalPopulation.innerHTML = sum.toLocaleString('en');
+totalPopulation.innerText = sum.toLocaleString('en');
 
 const averagePopulation = document.querySelector('.average-population');
 
-averagePopulation.innerHTML = averageSum.toLocaleString('en');
+averagePopulation.innerText = averageSum.toLocaleString('en');
