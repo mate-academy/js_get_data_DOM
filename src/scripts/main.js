8c022b0949248a1ddd58cd80cf472bd617d2168a
antoniakstepan <antoniakstepan1@gmail.com>
2021-02-23 11:47:30 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write your code here
+const totalPopulation = document.querySelector('.total-population');
+const totalAvarege = document.querySelector('.average-population');
+const population = document.querySelectorAll('.population');
+
+let result = 0;
+
+for (const value of population) {
+  result += Number(value.outerText.split(',').join(''));
+}
+
+const totalAvaregeCount = result / population.length;
+
+totalPopulation.innerText = result.toLocaleString('en');
+totalAvarege.innerText = totalAvaregeCount.toLocaleString('en');
