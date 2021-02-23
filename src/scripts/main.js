d5c5b1e2bfe6c29d8c9a49dd58ddb238a946055a
morozoleh <olegmoroz93@gmail.com>
2021-02-23 13:09:24 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const reference = {
+  population: document.querySelectorAll('.population'),
+  total: document.querySelector('.total-population'),
+  average: document.querySelector('.average-population'),
+};
+
+const populationInfo = Array.from(reference.population);
+const withoutCommas = populationInfo.map(
+  a => +a.textContent.replace(/[,]/g, '')
+);
+const totalPopulation = withoutCommas.reduce(
+  (accumulator, item) => accumulator + item, 0
+);
+
+const averagePopulation = totalPopulation / populationInfo.length;
+
+function convertOutput(number) {
+  return String(number).split('').reverse()
+    .map((a, ind) => ind % 3 === 0 && ind !== 0 ? a + ',' : a)
+    .reverse().join('');
+}
+
+reference.total.innerHTML = convertOutput(totalPopulation);
+reference.average.innerHTML = convertOutput(averagePopulation);
