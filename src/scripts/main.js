d27ebc0b9f70c4e06f536dfa09eaf85a5e5a2fde
Artem Topolskyi <topolskyiav@gmail.com>
2021-02-24 16:30:45 +0200

    fixed mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,12 +7,12 @@ const average = document.querySelector('.average-population');
 let populationSum = 0;
 
 for (const oneContryPopulation of populationsList) {
-  const population = +oneContryPopulation.innerHTML.split(',').join('');
+  const population = +oneContryPopulation.innerText.split(',').join('');
 
   populationSum += population;
 }
 
 const populationAverage = Math.round(populationSum / populationsList.length);
 
-total.innerHTML = populationSum.toLocaleString('en');
-average.innerHTML = populationAverage.toLocaleString('en');
+total.innerText = populationSum.toLocaleString('en');
+average.innerText = populationAverage.toLocaleString('en');
