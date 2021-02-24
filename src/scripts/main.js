427f9ca84aa6b408d7de828f903cd86fb26b423e
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-02-24 15:26:34 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const [...counrties] = document.querySelectorAll('.population');
+const counrtiesPopulation = counrties.map(elem => elem.textContent);
+
+const converter = string => Number(string.split(',').join(''));
+
+for (let i = 0; i < counrtiesPopulation.length; i++) {
+  counrtiesPopulation[i] = converter(counrtiesPopulation[i]);
+}
+
+const counrtiesQuantity = counrtiesPopulation.length;
+const topTenPopulation = counrtiesPopulation
+  .reduce((acc, currentValue) => acc + currentValue);
+
+const totalCell = document.querySelector('.total-population');
+const averageCell = document.querySelector('.average-population');
+
+totalCell.textContent = topTenPopulation.toLocaleString('en-US');
+
+averageCell.textContent = (topTenPopulation / counrtiesQuantity)
+  .toLocaleString('en-US');
