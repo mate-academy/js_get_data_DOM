d4cff5e08b45c536308954e70e37c2d2bc432141
Taras <seman6745@gmail.com>
2021-02-25 19:51:20 +0200

    without coma-function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,31 +7,8 @@ const listWithPopulation = [...itemsWithPopulation]
 const sum = listWithPopulation.reduce((acum, value) => acum + value, 0);
 const avarage = sum / listWithPopulation.length;
 
-function coma(num) {
-  const numWithComa = [];
-  const numsInArray = num.toString().split('');
-
-  while (true) {
-    numWithComa.unshift(numsInArray.splice(numsInArray.length - 3).join(''));
-
-    if (numsInArray.length <= 0) {
-      break;
-    }
-
-    if (numsInArray.length < 3) {
-      numWithComa.unshift(numsInArray);
-      break;
-    }
-  }
-
-  return numWithComa.join(',');
-}
-
-const sumWithComas = coma(sum);
-const avarageWithComas = coma(avarage);
-
 const totalInHtml = document.querySelector('.total-population');
 const avarageInHtml = document.querySelector('.average-population');
 
-totalInHtml.innerText = `${sumWithComas}`;
-avarageInHtml.innerText = `${avarageWithComas}`;
+totalInHtml.innerText = sum.toLocaleString('en');
+avarageInHtml.innerText = avarage.toLocaleString('en');
