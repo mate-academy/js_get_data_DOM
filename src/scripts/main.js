705c023b191c152539ef200365a3ae1c1d9a7e5b
anastasiia <anastasiia.aliinyk@gmail.com>
2021-03-01 15:19:44 +0200

    get data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const searchedClass = document.querySelectorAll('.population');
+const elements = [...searchedClass].map(element =>
+  Number(element.innerText.replaceAll(',', '')));
+
+const total = elements.reduce((sum, element) => sum + element);
+const average = total / elements.length;
+
+document.querySelector('.total-population').innerText = total.toLocaleString();
+
+document.querySelector('.average-population')
+  .innerText = average.toLocaleString();
