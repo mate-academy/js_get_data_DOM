67d7da96c19e8fb211931cb18afa821ad10e5c99
Vitalii Skrynkovych <vitaliiskrynkovych@gmail.com>
2021-03-03 19:03:58 +0200

    average population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write your code here
+const populationValuesArray = [];
+const collectionOfPopulation = document.body.querySelectorAll('.population');
+
+collectionOfPopulation.forEach(item => {
+  const population = item.textContent.replaceAll(',', '');
+
+  populationValuesArray.push(+population);
+});
+
+const totalPopulation
+= populationValuesArray.reduce((sumPopulation, population) => {
+  return sumPopulation + population;
+}, 0);
+
+const averagePopulation = totalPopulation / populationValuesArray.length;
+
+const totalPopulationElement = document.querySelector('.total-population');
+
+totalPopulationElement.textContent = totalPopulation.toLocaleString();
+
+const averagePopulationElement = document.querySelector('.average-population');
+
+averagePopulationElement.textContent = averagePopulation.toLocaleString();
