5c2a0f72b6ab99bbda102633381f06529382d6ac
ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-03-03 19:16:43 +0200

    firstCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const takePopulation = document.querySelectorAll('.population');
+
+const totalPopulation = [...takePopulation]
+  .map(el => +(el.innerText.replace(/,/g, '')))
+  .reduce((sum, b) => sum + b);
+
+const averagePopulation = totalPopulation / takePopulation.length;
+
+document.querySelector('.total-population')
+  .innerText = totalPopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population')
+  .innerText = averagePopulation.toLocaleString('en-US');
