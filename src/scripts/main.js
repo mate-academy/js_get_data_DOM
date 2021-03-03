73a1ce4d5f5cd700c7e79433e226906658a92e9b
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-03 18:09:02 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const populationNum = [...population].map(
+  item => +item.innerText.replace(/[.,]/g, ''));
+
+const totalPopulation = populationNum.reduce(
+  (acc, current) => acc + current, 0
+);
+
+const averagePopulation = totalPopulation / population.length;
+
+const total = document.querySelector('.total-population');
+
+total.innerText = totalPopulation;
+
+const average = document.querySelector('.average-population');
+
+average.innerText = averagePopulation;
