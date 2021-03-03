abd631401dd9aa39a6a52191ec1faedecefb0b39
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-03 22:53:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+function calculateSum() {
+  return arr.reduce((accumulator, current) => {
+    return accumulator + current;
+  }, 0);
+}
+
+const population = document.querySelectorAll('.population');
+
+const total = document.querySelector('.total');
+
+const average = document.querySelector('.average');
+
+const arr = [...population].map((x) => {
+  return +x.innerText.replace(/,/gi, '');
+});
+
+average.innerText = (calculateSum() / arr.length).toLocaleString('en');
+
+total.innerText = calculateSum().toLocaleString('en');
