ee4391708c222ae3320a3dc0456711557e66a711
Vitalii Skrynkovych <vitaliiskrynkovych@gmail.com>
2021-03-04 14:03:37 +0200

    changes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,21 @@
 'use strict';
 
 // write your code here
-const populationValuesArray = [];
 const collectionOfPopulation = document.body.querySelectorAll('.population');
 
-collectionOfPopulation.forEach(item => {
-  const population = item.textContent.replaceAll(',', '');
-
-  populationValuesArray.push(+population);
+const populationValuesArray = [...collectionOfPopulation].map(item => {
+  return +(item.textContent.replaceAll(',', ''));
 });
 
-const totalPopulation
-= populationValuesArray.reduce((sumPopulation, population) => {
-  return sumPopulation + population;
-}, 0);
+const totalPopulation = populationValuesArray
+  .reduce((sumPopulation, population) => {
+    return sumPopulation + population;
+  }, 0);
 
 const averagePopulation = totalPopulation / populationValuesArray.length;
 
 const totalPopulationElement = document.querySelector('.total-population');
-
-totalPopulationElement.textContent = totalPopulation.toLocaleString();
-
 const averagePopulationElement = document.querySelector('.average-population');
 
+totalPopulationElement.textContent = totalPopulation.toLocaleString();
 averagePopulationElement.textContent = averagePopulation.toLocaleString();
