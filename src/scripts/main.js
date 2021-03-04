44a1874b08b374842677d56198e8d3d09b9c54ce
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-04 16:24:34 +0200

    Fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,15 +5,17 @@ const values = [...population].map(item => item.innerText);
 const valuesToNumbers = values.map(x => +x.split(',').join(''));
 
 const totalPopulation = valuesToNumbers.reduce(
-  (populationFirst, populationSecond) => populationFirst + populationSecond, 0
+  (populationFirst, populationSecond) => (
+    populationFirst + populationSecond
+  ), 0
 );
 
 const averagePopulation = totalPopulation / valuesToNumbers.length;
 
 const total = document.querySelector('.total');
 
-total.innerHTML = `Total: ${totalPopulation}`;
+total.innerText = `Total: ${totalPopulation}`;
 
 const average = document.querySelector('.average');
 
-average.innerHTML = `Average: ${averagePopulation}`;
+average.innerText = `Average: ${averagePopulation}`;
