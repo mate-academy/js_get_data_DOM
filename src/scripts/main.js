693ea7359da0b0e5e503d3660384b1f3af5c9aa1
Ping <4ivrik@ukr.net>
2021-03-04 13:54:44 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const spansWithPopulationClass = document.querySelectorAll('span.population');
+
+const allPopulation = [...spansWithPopulationClass]
+  .map(item => +(item.innerText.replace(/,/g, '')));
+
+const totalPopulation = allPopulation.reduce(
+  (accumulator, population) => accumulator + population
+);
+
+const averagePopulation = totalPopulation / allPopulation.length;
+
+document.querySelector('.total-population').innerText
+= totalPopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population').innerText
+= averagePopulation.toLocaleString('en-US');
