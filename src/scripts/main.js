bf3b69c8ec6d9d038c6ef14b68c05dbada61eb36
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-04 15:43:57 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const values = [...population].map(item => item.innerText);
+const valuesToNumbers = values.map(x => +x.split(',').join(''));
+
+const totalPopulation = valuesToNumbers.reduce(
+  (populationFirst, populationSecond) => populationFirst + populationSecond, 0
+);
+
+const averagePopulation = totalPopulation / valuesToNumbers.length;
+
+const total = document.querySelector('.total');
+
+total.innerHTML = `Total: ${totalPopulation}`;
+
+const average = document.querySelector('.average');
+
+average.innerHTML = `Average: ${averagePopulation}`;
