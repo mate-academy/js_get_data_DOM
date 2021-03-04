fc639f1fb5a789b0d7b60458adc5ba7a928dc0af
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-04 12:48:31 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const listOfDataAboutPopulation = document.querySelectorAll('.population');
+let totalPopulation = 0;
+
+for (let i = 0; i < listOfDataAboutPopulation.length; i++) {
+  totalPopulation += parseInt(
+    listOfDataAboutPopulation[i].innerText.split(',').join('')
+  );
+}
+
+document.querySelector('.total-population').innerHTML = totalPopulation;
+
+document.querySelector('.average-population').innerHTML = totalPopulation
+  / listOfDataAboutPopulation.length;
