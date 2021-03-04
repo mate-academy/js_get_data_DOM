1f0fa352e29a0bef10f12c89a41ea35f3d075b0a
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-03-04 22:35:31 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write your code here
+
+const allElementPopulation = document.getElementsByClassName('population');
+
+const arrPopulation = [...allElementPopulation].map(
+  item => item.innerText
+);
+
+const arrToNumber = arrPopulation.map(
+  item => parseInt(item.split(',').join(''))
+);
+
+const totalPopulationNumber = arrToNumber.reduce(
+  (prev, next) => prev + next
+);
+
+const avaragePopulationNumber = totalPopulationNumber / arrToNumber.length;
+
+const avaragePopulation = document.querySelector('.average-population');
+const totalPopulation = document.querySelector('.total-population');
+
+avaragePopulation.innerHTML = avaragePopulationNumber.toLocaleString('en-US');
+totalPopulation.innerHTML = totalPopulationNumber.toLocaleString('en-US');
