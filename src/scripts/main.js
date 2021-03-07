a7ea6768550f8b7bbf0b11283b499f48248a57af
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-07 14:03:35 +0200

    final result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,19 +3,17 @@
 const population = document.querySelectorAll('.population');
 
 const populationNumbers = [...population].map(
-  item => +item.innerText.replace(/[.,]/g, '')
+  item => Number(item.innerText.replace(/[,]/g, ''))
 );
 
 const totalPopulation = populationNumbers.reduce(
-  (acc, current) => acc + current, 0
+  (accumulator, currentValue) => accumulator + currentValue, 0
 );
 
-const averagePopulation = totalPopulation / population.length;
+const averagePopulation = totalPopulation / populationNumbers.length;
 
-const total = document.querySelector('.total-population');
+let total = document.querySelector('.total-population');
+let average = document.querySelector('.average-population');
 
 total.innerText = totalPopulation.toLocaleString('en-EN');
-
-const average = document.querySelector('.average-population');
-
 average.innerText = averagePopulation.toLocaleString('en-EN');
