74c7e0dacf7e1107a4ef38643138c5ecd3678c66
Onishchuk <onishchuk.danil@gmail.com>
2021-03-16 20:53:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const tags = document.querySelectorAll('.population');
+const population = [...tags].map(item => +item.textContent.replace(/,/g, ''));
+const total = population.reduce((sum, item) => sum + item, 0);
+const average = total / population.length;
+
+document.querySelector('.total').textContent = `Total: ${total}`;
+document.querySelector('.average').textContent = `Average: ${average}`;
