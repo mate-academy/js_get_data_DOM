47401e2216bc90fbb1d728120a720ad7b8034bc3
Vitalii Nepomiashchyi <nepomyaschyi1@gmail.com>
2021-03-18 20:16:53 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const numberToString = function(number) {
+  const string = number.toString();
+  const output = [];
+
+  let substring = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    substring = string[i] + substring;
+
+    if (substring.length > 2 || i === 0) {
+      output.unshift(substring);
+      substring = '';
+    }
+  }
+
+  return output.join(',');
+};
+
+let populationValues = document.querySelectorAll('.population');
+
+populationValues
+= [...populationValues].map(value => +value.innerText.split(',').join(''));
+
+const totalValue = populationValues.reduce((total, currentValue) => {
+  return total + currentValue;
+}, 0);
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.textContent = numberToString(totalValue);
+
+averagePopulation.textContent
+= numberToString(totalValue / populationValues.length);
