967a7620949509f986d6f2bfc6b5e942618d0ad1
Ivan Bilozerov <vaniabelozerov@gmail.com>
2021-03-18 16:29:43 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')]
+  .map(item => Number(item.innerText.split(',').join('')));
+
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+total.innerText = population
+  .reduce((acc, curr) => acc + curr);
+
+average.innerText = population
+  .reduce((acc, curr) => acc + curr) / population.length;
