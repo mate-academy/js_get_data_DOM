5b8473019dea1616765c830221b300730a62b10f
Taras Chaban <tarekchaban@gmail.com>
2021-03-23 20:03:39 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const dataCollection = document.querySelectorAll('.population');
+
+const PopulationCollection = [...dataCollection].map(
+  people => +people.textContent.split(',').join('')
+);
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const total = PopulationCollection.reduce((a, b) => a + b);
+const average = total / dataCollection.length;
+
+totalPopulation.textContent = total.toLocaleString('en-US');
+averagePopulation.textContent = average.toLocaleString('en-US');
