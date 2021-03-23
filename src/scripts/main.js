5bfc39570c7887768d9bcf298568d006f07ee399
Vitalii Nepomiashchyi <nepomyaschyi1@gmail.com>
2021-03-23 12:35:19 +0200

    deleted useless function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,17 @@
 'use strict';
 
-const numberToString = function(number) {
-  const string = number.toString();
-  const output = [];
-
-  let substring = '';
-
-  for (let i = string.length - 1; i >= 0; i--) {
-    substring = string[i] + substring;
-
-    if (substring.length > 2 || i === 0) {
-      output.unshift(substring);
-      substring = '';
-    }
-  }
-
-  return output.join(',');
-};
-
 let populationValues = document.querySelectorAll('.population');
 
 populationValues
 = [...populationValues].map(value => +value.innerText.split(',').join(''));
 
-const totalValue = populationValues.reduce((total, currentValue) => {
-  return total + currentValue;
-}, 0);
+const totalValue = populationValues.reduce((total, currentValue) => (
+  total + currentValue), 0);
 
 const totalPopulation = document.querySelector('.total-population');
 const averagePopulation = document.querySelector('.average-population');
 
-totalPopulation.textContent = numberToString(totalValue);
+totalPopulation.textContent = totalValue.toLocaleString();
 
 averagePopulation.textContent
-= numberToString(totalValue / populationValues.length);
+= (totalValue / populationValues.length).toLocaleString();
