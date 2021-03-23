9e15aa84c7579d0eacfe3a116d72636a613e7193
olena <melolena33@gmail.com>
2021-03-23 18:02:26 +0200

    Use toLocaleString

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,22 +6,6 @@ function convertFromString(amountString) {
   return +amountString.replace(/,/g, '');
 }
 
-function convertToString(amount) {
-  const amountString = '' + amount;
-  const numberLength = amountString.length;
-  let convertedString = '';
-
-  for (let i = numberLength - 3; i > -3; i -= 3) {
-    convertedString = amountString.substring(i, i + 3) + convertedString;
-
-    if (i > 0) {
-      convertedString = ',' + convertedString;
-    }
-  }
-
-  return convertedString;
-}
-
 let totalAmount = 0;
 
 for (const item of listItems) {
@@ -34,8 +18,8 @@ for (const item of listItems) {
 const averageAmount = totalAmount / listItems.length;
 const totalSpan = document.querySelector('.total-population');
 
-totalSpan.innerHTML = convertToString(totalAmount);
+totalSpan.innerHTML = totalAmount.toLocaleString();
 
 const averageSpan = document.querySelector('.average-population');
 
-averageSpan.innerHTML = convertToString(averageAmount);
+averageSpan.innerHTML = averageAmount.toLocaleString();
