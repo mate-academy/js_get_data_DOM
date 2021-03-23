afc540a19fa9d8db21e8922605328cd121c2c242
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-03-23 19:07:53 +0200

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+const populationItem = document.querySelectorAll('.population');
+
+const populationNumbers = [];
+
+for (const item of populationItem) {
+  populationNumbers.push(+item.innerHTML.replace(/,/g, ''));
+}
+
+const totalPopulation = populationNumbers.reduce((countryA, countryB) => {
+  return countryA + countryB;
+}, 0);
+
+const averageValue = totalPopulation / populationNumbers.length;
+
+document.querySelector('.total-population')
+  .innerHTML = totalPopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population')
+  .innerHTML = averageValue.toLocaleString('en-US');
