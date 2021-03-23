ede33724aa42a90356019a37c46c0f63406970ea
Ruslan Vasylyshyn <r.y.vasylyshyn@gmail.com>
2021-03-23 17:19:03 +0200

    First solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write your code here
+const DataFromPopulationSpan = document
+  .querySelectorAll('.population');
+const DataFromTotalPopulationSpan = document
+  .querySelector('.total-population');
+const DataFromAveragePopulationSpan = document
+  .querySelector('.average-population');
+
+const totalPopulationData = [...DataFromPopulationSpan]
+  .map(country => +country.innerHTML.split(',').join(''));
+
+let totalPopulationSum = 0;
+
+for (let i = 0; i < totalPopulationData.length; i++) {
+  totalPopulationSum = totalPopulationSum + totalPopulationData[i];
+}
+
+const totalPopulationAverage = totalPopulationSum / totalPopulationData.length;
+
+DataFromTotalPopulationSpan.innerHTML = totalPopulationSum
+  .toLocaleString('en-US');
+
+DataFromAveragePopulationSpan.innerHTML = totalPopulationAverage
+  .toLocaleString('en-US');
