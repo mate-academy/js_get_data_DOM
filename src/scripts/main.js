50ab3d0fa6212891893b968a694514db294faf6c
Vlad Dezhniuk <vladdezhniuk@gmail.com>
2021-03-23 17:26:43 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write your code here
+const countriesPopulation = document.querySelectorAll('.population');
+let total = [...countriesPopulation]
+  .map(item => +item.textContent.replace(/,/g, ''))
+  .reduce((a, b) => a + b);
+
+let average = total / countriesPopulation.length;
+
+function formatText(number) {
+  const result = number.toString().split('');
+
+  for (let a = result.length - 4; a >= 0; a -= 3) {
+    result[a] = result[a] + ',';
+  }
+
+  return result.join('');
+}
+total = formatText(total);
+average = formatText(average);
+
+document.querySelector('.total').textContent = `Total: ${total}`;
+document.querySelector('.average').textContent = `Average: ${average}`;
