565ed42fe7fc30d527e67f5f284667380b4cd4b7
Alexander Levchenko <a.levchenko603@gmail.com>
2021-03-23 18:13:45 +0200

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const arrayOfPopulationNumber = [...spans]
+  .map(content => +content.innerText.split(',').join(''));
+
+const totalPopulationResult = arrayOfPopulationNumber
+  .reduce((a, b) => a + b, 0);
+const length = spans.length;
+const averagePopulationResult = totalPopulationResult / length;
+
+totalPopulation.textContent = totalPopulationResult;
+averagePopulation.textContent = averagePopulationResult;
