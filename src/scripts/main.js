82ced5b535e2320d84e77b0554a98d3d4e02e940
Artem Maksimikhin <uanov86@gmail.com>
2021-03-24 13:57:21 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,5 +8,5 @@ const population = [...allTagsPopulation].map(value =>
 const totalPopulation = population.reduce((accum, value) => accum + value, 0);
 const averagePopulation = totalPopulation / population.length;
 
-totalPopulationSelector.innerText = totalPopulation;
-averagePopulationSelector.innerText = averagePopulation;
+totalPopulationSelector.innerText = totalPopulation.toLocaleString('en-US');
+averagePopulationSelector.innerText = averagePopulation.toLocaleString('en-US');
