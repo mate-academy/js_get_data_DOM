da1bb1fb716a174a00ea9c49ffbb91b469d9d477
Mariia Kornieva <mariiakornieva@gmail.com>
2021-04-07 21:35:59 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write your code here
+const populationSpans = document.querySelectorAll('.population');
+
+const populationsArray = [...populationSpans].map(span => {
+  return +(span.textContent.split(',').join(''));
+});
+
+const totalPopulation = populationsArray.reduce((accum, current) => {
+  return accum + current;
+}, 0);
+
+const averagePopulation = totalPopulation / populationsArray.length;
+
+const separateByCommas = (array) => {
+  const result = array;
+  const firstComma = array.length % 3 === 0
+    ? 3
+    : array.length % 3;
+
+  for (let i = firstComma; i < result.length; i += 3) {
+    result.splice(i++, 0, ',');
+  }
+
+  return result;
+};
+
+const totalSpan = document.querySelector('.total-population');
+
+totalSpan.textContent = separateByCommas(
+  totalPopulation.toString().split('')
+).join('');
+
+const averageSpan = document.querySelector('.average-population');
+
+averageSpan.textContent = separateByCommas(
+  averagePopulation.toString().split('')
+).join('');
