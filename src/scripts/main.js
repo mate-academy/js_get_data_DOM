500bf8aa0a47f289ebbb4d73c75c8f68278a3e0b
Maksym Maksymenko <m.maksymenko@smartyads.com>
2021-04-19 23:06:44 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const populationList = [...document.querySelectorAll('.population')];
+const spanElementTotal = document.querySelector('.total-population');
+const spanElementAverage = document.querySelector('.average-population');
+
+const totalPopulation = populationList
+  .map((populationItem) => {
+    return +populationItem.textContent.split(',').join('');
+  })
+  .reduce((accumulator, currentValue) => accumulator + currentValue);
+
+const averagePopulation = totalPopulation / populationList.length;
+
+spanElementTotal.textContent = totalPopulation.toLocaleString();
+spanElementAverage.textContent = averagePopulation.toLocaleString();
