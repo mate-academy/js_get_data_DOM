3b0a125b1e8f2e1c83791eb2077913426955d38d
Kostrica <romiorw@ukr.net>
2021-04-23 19:49:27 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write your code here
+const itemsText = document.querySelectorAll('.population');
+const arrayText = [...itemsText].map(item => item.innerText);
+const numbers = arrayText.map(item => +item.split(',').join(''));
+const totalNumber = numbers.reduce((sum, current) => sum + current, 0);
+const totalText = totalNumber.toFixed();
+let totalTextSort = '';
+let countTotal = 0;
+
+for (let i = totalText.length - 1; i >= 0; i--) {
+  countTotal++;
+  totalTextSort += totalText[i];
+
+  if (countTotal === 3 && i > 0) {
+    totalTextSort += ',';
+    countTotal = 0;
+  }
+}
+
+const averageNumber = totalNumber / numbers.length;
+const averageText = averageNumber.toFixed();
+let averageTextSort = '';
+let counterAverage = 0;
+
+for (let i = averageText.length - 1; i >= 0; i--) {
+  counterAverage++;
+  averageTextSort += averageText[i];
+
+  if (counterAverage === 3 && i > 0) {
+    averageTextSort += ',';
+    counterAverage = 0;
+  }
+}
+
+const totalPopulation = totalTextSort.split('').reverse().join('');
+const averagePopulation = averageTextSort.split('').reverse().join('');
+
+document.querySelectorAll('.total-population')[0]
+  .innerHTML = totalPopulation;
+
+document.querySelectorAll('.average-population')[0]
+  .innerHTML = averagePopulation;
