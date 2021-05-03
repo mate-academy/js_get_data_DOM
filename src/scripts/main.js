702fd49e5b94893d005830efe588de3f1d38951d
Vlad Trundenko <vlad.trundenko@gmail.com>
2021-05-03 12:22:07 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const sum = [...population]
+  .map(item => +item.innerText.split(',').join(''))
+  .reduce((a, b) => a + b, 0);
+
+const average = sum / population.length;
+
+function addSeparator(num) {
+  const separator = ',';
+  const resultNumber = [];
+
+  const splitNum = String(num).split('').reverse();
+
+  for (let i = 0; i < splitNum.length; i++) {
+    if (i % 3 === 0 && i !== 0) {
+      resultNumber.push(separator);
+    }
+    resultNumber.push(splitNum[i]);
+  }
+
+  return resultNumber.reverse().join('');
+}
+
+document.querySelector('.total-population').innerHTML = addSeparator(sum);
+document.querySelector('.average-population').innerHTML = addSeparator(average);
