6ab0276f09fa480422630ef0a6525c47358534b9
romandubovskyi <rdubowskij@gmail.com>
2021-05-08 20:35:41 +0300

    added calculation of total and avg populations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const rawPopulations = [...populations].map((element) => element.innerText);
+const numericPopulations = rawPopulations.map((element) => {
+  const regex = new RegExp(',', 'g');
+  const cleanValue = element.replace(regex, '');
+
+  return Number.parseInt(cleanValue);
+});
+
+const totalPopulation = numericPopulations.reduce((total, current) => {
+  return total + current;
+});
+const totalPopulationElement = document.querySelector(
+  '.total-population'
+);
+
+totalPopulationElement.innerText = totalPopulation.toLocaleString();
+
+const avgPopulation = totalPopulation / numericPopulations.length;
+const avgPopulationElement = document.querySelector(
+  '.average-population'
+);
+
+avgPopulationElement.innerText = avgPopulation.toLocaleString();
