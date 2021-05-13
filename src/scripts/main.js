62278564c93548211d7b31e80eedbf9f5de56a53
Anton Kostylov <antoniok003@gmail.com>
2021-05-13 13:03:17 +0300

    Solution according to review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,31 @@
 'use strict';
 
 const populationData = document.querySelectorAll('.population');
-const populationValue = [...populationData].map(item =>
+const populationValues = [...populationData].map(item =>
   Number(item.innerText.split(',').join('')));
-const totalPopulation = populationValue
+const totalPopulation = populationValues
   .reduce((a, b) => a + b);
-const averagePopulation = totalPopulation / populationValue.length;
-const totalText = totalPopulation
-  .toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-const averageText = averagePopulation
-  .toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-const total = document.querySelector('.total-population');
-const average = document.querySelector('.average-population');
+const averagePopulation = totalPopulation / populationValues.length;
 
-total.innerText = totalText;
-average.innerText = averageText;
+document.querySelector('.total-population')
+  .innerText = format(totalPopulation);
+
+document.querySelector('.average-population')
+  .innerText = format(averagePopulation);
+
+function format(number) {
+  const string = String(number);
+  let res = '';
+  let counter = 0;
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    counter++;
+    res += string[i];
+
+    if (counter % 3 === 0 && i !== 0) {
+      res += ',';
+    }
+  }
+
+  return res.split('').reverse().join('');
+}
