a4bae7d3c6a22ef092b35cd2ecdb5d2cf62d8dfd
Inna <inna_temis@ukr.net>
2021-05-18 17:38:07 +0300

    fixed after solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,12 @@
 const items = document.querySelectorAll('.population');
 const total = document.querySelector('.total-population');
 const aver = document.querySelector('.average-population');
-
-const arrayItems = [...items].map(item => Number(item.innerText.split(',').join('')));
-const sumNumber = arrayItems.reduce((a,b) => a + b);
-const sum = sumNumber.toLocaleString().replace(/\s/g, ',')
-const average = (sumNumber / arrayItems.length).toLocaleString().replace(/\s/g, ',');
+const arrayItems = [...items].map(item =>
+  Number(item.innerText.split(',').join('')));
+const sumNumber = arrayItems.reduce((a, b) => a + b);
+const sum = sumNumber.toLocaleString().replace(/\s/g, ',');
+const average = (sumNumber / arrayItems.length).toLocaleString()
+  .replace(/\s/g, ',');
 
 total.innerText = sum;
 aver.innerText = average;
