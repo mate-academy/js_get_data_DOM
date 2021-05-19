db9c976f812e90aff1feaffa2a242917226da445
Andriy Fesych <andriyhimself@gmail.com>
2021-05-19 16:35:25 +0300

    Solution_0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const totalPop = document.getElementsByClassName('total-population');
+
+const popul = Array.from(document.getElementsByClassName('population'))
+  .map((pop) => Number(pop.innerText.replace(/,/g, '')));
+
+Array.from(totalPop)[0].textContent = Number(popul
+  .reduce((total, amount) => total + amount))
+  .toLocaleString('en');
+
+const averPop = document.getElementsByClassName('average-population');
+
+Array.from(averPop)[0].textContent = Number((popul
+  .reduce((total, amount) => total + amount) / popul.length)
+  .toFixed(0))
+  .toLocaleString('en');
