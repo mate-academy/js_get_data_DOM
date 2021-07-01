48acd9b49fd2ce0ed5d6e940fd2f577f6a3cd38f
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-07-01 13:50:00 +0300

    Check me

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const lengthOfList = document.querySelectorAll('.population').length;
+const rechangeTotal = document.querySelector('.total-population');
+const rechangeAverage = document.querySelector('.average-population');
+let total = 0;
+let average = 0;
+
+for (let i = 0; i < lengthOfList; i++) {
+  let numb = document.querySelectorAll('.population')[i].innerText;
+
+  numb = +numb.split(',').join('');
+  total += numb;
+
+  if (i === lengthOfList - 1) {
+    average = Math.round(total / i + 1);
+  };
+};
+
+total = total.toLocaleString('en-US');
+average = average.toLocaleString('en-US');
+
+rechangeTotal.innerHTML = `${total}`;
+rechangeAverage.innerHTML = `${average}`;
