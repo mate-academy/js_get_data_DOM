498fb2bd8c61e061f3a5ee0a727f81b1189c8a28
korzh <korzh.alexey@yahoo.com>
2021-07-04 15:28:22 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+let countryPopulation = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+countryPopulation = [ ...countryPopulation ];
+
+const sumOfPopulation
+  = countryPopulation.reduce((acc, country) =>
+    acc + +country.innerText.split(',').join(''), 0);
+
+totalPopulation.innerHTML
+  = sumOfPopulation
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+averagePopulation.innerHTML
+  = Math.round((sumOfPopulation / countryPopulation.length))
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
