d89be02fcf7bb4504dde01fe82d796616d876169
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-07-05 16:56:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const list = [...document.querySelectorAll('.population')];
+
+function numberWithCommas(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+const listOfNumbers = list.map(item => +item.innerText.split(',').join(''));
+let total = listOfNumbers.reduce((accumulator, item) => accumulator + item);
+let average = total / listOfNumbers.length;
+
+total = numberWithCommas(total);
+average = numberWithCommas(average);
+
+totalPopulation.innerText = total;
+averagePopulation.innerText = average;
