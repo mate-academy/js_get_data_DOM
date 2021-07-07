4acce24a3dfb8d20dfdc118f7db9274c42c56cfc
David Dohru <tronsmartmy@gmail.com>
2021-07-07 21:49:06 +0300

    reduced the number of blank lines

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,25 +10,22 @@ const isThousandsSeparator = (str) => {
   return strRes.join('');
 };
 
+let allValueOfCountry = [];
+
 let allOfCountry = document.querySelectorAll('.population');
 
 allOfCountry = [...allOfCountry];
 
-let allValueOfCountry = [];
-
 allOfCountry.map(item => allValueOfCountry.push(item.textContent));
-
 allValueOfCountry = allValueOfCountry.map(x => +x.split(',').join(''));
 
 const sumValueOfAllCountry = allValueOfCountry.reduce((acamulate, current) =>
   acamulate + current);
-
 const avarageValueOfAllCountry = (sumValueOfAllCountry / allOfCountry.length);
 
 const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
 
 totalPopulation.textContent = isThousandsSeparator(sumValueOfAllCountry);
-
-const averagePopulation = document.querySelector('.average-population');
 
 averagePopulation.textContent = isThousandsSeparator(avarageValueOfAllCountry);
