775f6966459497c9c660f5cd86ac0d6aa767883c
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-11 16:33:04 +0300

    task is completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write your code here
+const elements = document.querySelectorAll('.population');
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+const populationList = [...elements].map(el => {
+  return +el.textContent.replace(/,/g, '');
+});
+const totalPopulation = populationList.reduce((acc, country) => {
+  return acc + country;
+}, 0);
+const averagePopulation = totalPopulation / populationList.length;
+
+totalElement.textContent = totalPopulation.toLocaleString('en-US');
+averageElement.textContent = averagePopulation.toLocaleString('en-US');
