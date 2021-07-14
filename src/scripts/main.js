bd2a2e55aeb2ba3f4714855430ea54a7a3f993c7
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2021-07-14 13:06:02 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write your code here
+const population = document.querySelectorAll('.population');
+const values = [...population].map(val => val.innerHTML);
+const formatedValue = [];
+
+for (const value of values) {
+  const str = value.replace(/,/g, '');
+
+  formatedValue.push(+str);
+}
+
+const sumOfPopulations = formatedValue.reduce((acc, val) => acc + val, 0);
+const averageOfPopulation = sumOfPopulations / formatedValue.length;
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+total.innerHTML = new Intl.NumberFormat('en-EN').format(sumOfPopulations);
+average.innerHTML = new Intl.NumberFormat('en-EN').format(averageOfPopulation);
