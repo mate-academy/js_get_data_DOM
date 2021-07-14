f7704c40c281fbff66045978754239eec651c6de
sergei-trofimov <feast.st@gmail.com>
2021-07-14 18:11:48 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const populationList = document.querySelectorAll('.population');
+
+const total = [...populationList].reduce((accum, item) => {
+  const population = +item.textContent.split(',').join('');
+  let totalValue = accum;
+
+  totalValue += population;
+
+  return totalValue;
+}, 0);
+
+const average = total / populationList.length;
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.textContent = total.toLocaleString('en');
+averagePopulation.textContent = average.toLocaleString('en');
