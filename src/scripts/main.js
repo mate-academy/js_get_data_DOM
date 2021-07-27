182b4d4b863b9aafc1f74c27e9de7b6728fd662f
Bohdan Hnatuk <hnatuk996@gmail.com>
2021-07-27 21:25:19 +0300

    solutions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const population = document.querySelectorAll('.population');
+const populationArr = [...population].map(a => a.innerText.replace(/,/g, ''));
+const totalNumber = populationArr.reduce((sum, x) => sum + +x, 0);
+const averageNumber = totalNumber / populationArr.length;
+
+function comma(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+totalPopulation.innerText = comma(totalNumber);
+averagePopulation.innerText = comma(averageNumber);
