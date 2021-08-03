62a8aaddf16295a3bcff7ea9e97e348def611fac
Nazar Kudatsky <divipi1996@icloud.com>
2021-08-03 18:57:59 +0300

    solll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const data = document.querySelectorAll('.population');
+const average = document.querySelector('.average-population');
+const totalPopulation = document.querySelector('.total-population');
+let total = 0;
+
+for (const countryPopulation of data) {
+  const number = countryPopulation.innerText.split(',').join('');
+
+  total += +number;
+}
+
+totalPopulation.innerText = total.toLocaleString('en');
+average.innerText = (total / data.length).toLocaleString('en');
