ac33acc77cb5a38df520eb5ebda5da47f34a6c75
Raz-Dva <my.poshta.goncharov@gmail.com>
2021-08-04 20:03:39 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const arrPopulation = [...population].map(el => {
+  return +el.textContent.replace(/,/gm, '');
+});
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+const totalPopulation = arrPopulation.reduce(reducer);
+const avaragePopulation = totalPopulation / arrPopulation.length;
+
+document.querySelector('.total-population')
+  .textContent = new Intl.NumberFormat('en-EN').format(totalPopulation);
+
+document.querySelector('.average-population')
+  .textContent = new Intl.NumberFormat('en-EN').format(avaragePopulation);
