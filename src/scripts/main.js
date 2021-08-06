fe5c87386d36a2fd09608d0f1692368ba21c5aaa
Larin Dmytro <dimiklarin420@gmail.com>
2021-08-06 15:00:35 +0300

    solution fixed x2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,21 @@
 'use strict';
 
+//  creating parseNumber function
+function parseNumber(item) {
+  let newItem = item.replace(/[,]/g, '');
+  return Number(newItem);
+}
+
 //  looking for span element value by class name
 const elements = document.getElementsByClassName('population');
 //  making array that filed by element value
-const data = [].map.call(elements, elem => elem.textContent);
-//  deleting comma in array items
-const newArr = data.map(stringItem => stringItem.replace(/[,]/g, ''));
-//  transforming array elements to number
-const intArr = newArr.map(item => Number(item));
+const data = [...elements].map(elem => elem.textContent);
+//  deleting comma in array items and transforming to number
+const newArr = data.map(parseNumber);
 //  calculating total
-const total = intArr.reduce((sum, current) => sum + current, 0);
+const total = newArr.reduce((sum, current) => sum + current, 0);
 //  calculating avarage
-const avarage = total / intArr.length;
+const avarage = total / newArr.length;
 
 //  creating comma separator function
 function separator(number) {
