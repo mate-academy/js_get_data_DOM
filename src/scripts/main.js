be6c7de8886a015df01d8a335365f0a5a7dc5a22
Lenur Azizov <lenur29azizov@gmail.com>
2021-08-13 09:44:31 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const collection = document.querySelectorAll('.population');
+
+let sum = 0;
+
+for (const item of collection) {
+  const y = +item.innerText.split(',').join('');
+
+  sum += y;
+}
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const finalTotalSum = new Intl.NumberFormat('en-US').format(sum);
+
+totalPopulation.innerText = finalTotalSum;
+
+const averageSum = sum / collection.length;
+const finalAverageSum = new Intl.NumberFormat('en-US').format(averageSum);
+
+averagePopulation.innerText = finalAverageSum;
