b053131dc4439d4acfaba1e569582776806f4acd
Ihor Nahnybida <igor653279@gmail.com>
2021-08-14 17:03:13 +0300

    main.js file modified. total and average ages added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const threeDigitSeparator = (number) => {
+  const stringNumber = number.toString();
+  const result = [];
+
+  for (let i = 0; i < stringNumber.length; i++) {
+    if (i % 3 === 0) {
+      result.push(',');
+    }
+    result.push(stringNumber.split('').reverse()[i]);
+  }
+
+  return result.reverse().slice(0, -1).join('');
+};
+
+const populations = [
+  ...document.querySelectorAll('.population'),
+].map(country => +country.textContent.replace(/,/g, ''));
+
+const totalPopulation = populations.reduce((total, current) => (
+  total + current
+),
+0);
+
+const averagePopulation = totalPopulation / populations.length;
+
+const totalHtml = document.querySelector('.total-population');
+const averageHtml = document.querySelector('.average-population');
+
+totalHtml.textContent = threeDigitSeparator(totalPopulation);
+averageHtml.textContent = threeDigitSeparator(averagePopulation);
