f258682c6bcd06b16c161d874a7e009f27c735d2
Olesya Parfylo <olesya.parfylo@gmail.com>
2021-08-16 19:19:21 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const elements = document.querySelectorAll('.population');
+
+const sum = [...elements].reduce((prev, element) => prev
++ (Number(element.innerText.replace(/,/g, ''))), 0);
+
+const average = sum / [...elements].length;
+
+const sumStr = sum.toLocaleString();
+const averageStr = average.toLocaleString();
+
+function getNewValue(classNameValue,
+  textContentValue, classParentElement, classPrevElement) {
+  const parentElement = document.querySelector(classParentElement);
+  const prevValue = document.querySelector(classPrevElement);
+  const item = document.createElement('span');
+
+  item.className = classNameValue;
+
+  item.textContent = textContentValue;
+
+  prevValue.remove();
+
+  parentElement.append(item);
+}
+
+getNewValue('total-population', sumStr, '.total', '.total-population');
+
+getNewValue('average-population',
+  averageStr, '.average', '.average-population');
