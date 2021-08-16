0bcd23077379bd4072d797d2c8695e031f635f10
Moroz Dmytro <dmytro101100@gmail.com>
2021-08-16 21:54:46 +0300

    The task has been done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+function thousandSeparator(number) {
+  let value = number;
+  let valueFormatted = '';
+
+  while (value > 0) {
+    valueFormatted = value % 1000 + ',' + valueFormatted;
+    value = Math.trunc(value / 1000);
+  }
+
+  return valueFormatted.slice(0, -1);
+}
+
+const allPopulation = document.querySelectorAll('.population');
+
+const population = [...allPopulation].map(
+  item => +item.textContent.split(',').join('')
+);
+
+const totalValue = population.reduce((sum, x) => sum + x, 0);
+const averageValue = totalValue / population.length;
+
+const averagePopulation = document.querySelector('.average-population');
+const totalPopulation = document.querySelector('.total-population');
+
+averagePopulation.textContent = thousandSeparator(averageValue);
+totalPopulation.textContent = thousandSeparator(totalValue);
