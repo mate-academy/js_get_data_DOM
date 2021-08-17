4d76d2c88c1f88763f7327718ce24fa4197825f5
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-08-17 17:10:26 +0300

    task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const divider = (number) => {
+  const digits = number.toString().split('');
+
+  for (let i = digits.length - 3; i >= 1; i -= 3) {
+    digits[i] = ',' + digits[i];
+  }
+
+  return digits.join('');
+};
+
+const populations = document.querySelectorAll('.population');
+
+const total = [...populations].reduce((sum, { textContent }) => (
+  sum + +textContent.split(',').join('')
+), 0);
+
+const average = total / populations.length;
+
+document.querySelector('.total-population').textContent = divider(total);
+
+document.querySelector('.average-population').textContent = divider(average);
