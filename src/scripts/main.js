34c1c9611313b1962e9cc294a2c6a70da8b3646d
Moroz Dmytro <dmytro101100@gmail.com>
2021-08-17 19:07:23 +0300

    Useless method was deleted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,16 @@
 'use strict';
 
-function thousandSeparator(number) {
-  let value = number;
-  let valueFormatted = '';
-
-  while (value > 0) {
-    valueFormatted = value % 1000 + ',' + valueFormatted;
-    value = Math.trunc(value / 1000);
-  }
-
-  return valueFormatted.slice(0, -1);
-}
-
 const allPopulation = document.querySelectorAll('.population');
 
 const population = [...allPopulation].map(
   item => +item.textContent.split(',').join('')
 );
 
-const totalValue = population.reduce((sum, x) => sum + x, 0);
+const totalValue = population.reduce((sum, x) => sum + x);
 const averageValue = totalValue / population.length;
 
 const averagePopulation = document.querySelector('.average-population');
 const totalPopulation = document.querySelector('.total-population');
 
-averagePopulation.textContent = thousandSeparator(averageValue);
-totalPopulation.textContent = thousandSeparator(totalValue);
+averagePopulation.textContent = averageValue.toLocaleString('en-US');
+totalPopulation.textContent = totalValue.toLocaleString('en-US');
