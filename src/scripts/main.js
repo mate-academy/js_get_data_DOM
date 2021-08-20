65fc7b0139b3c8fe08adb0d8c38346cc45ca3bdd
Viktormaz <vitecc91@gmail.com>
2021-08-20 12:13:26 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write your code here
+const arr = [];
+
+const popul = document.querySelectorAll('.population');
+const numb = [...popul].map(item => item.innerText);
+
+for (let i = 0; i < numb.length; i++) {
+  const str = numb[i].split(',').join('');
+
+  arr.push(Number(str));
+}
+
+const total = arr.reduce((tot, amount) => tot + amount);
+
+const average = total / arr.length;
+
+const totale = document.querySelector('.total-population');
+
+totale.innerHTML = total.toLocaleString('en-US');
+
+const averagee = document.querySelector('.average-population');
+
+averagee.innerHTML = average.toLocaleString('en-US');
