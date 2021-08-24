751ebe92a757c4545c340a27cf10b5999b9f3c8f
Roman Minchenko <minchenko.poma@gmail.com>
2021-08-24 15:34:47 +0300

    get_data_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const populars = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const averange = document.querySelector('.average-population');
+let sum = 0;
+
+[...populars].forEach(item => {
+  const number = item.textContent.replace(/,/g, '');
+
+  sum += +number;
+
+  return sum;
+});
+
+total.textContent = new Intl.NumberFormat('en-US').format(sum);
+
+averange.textContent
+= new Intl.NumberFormat('en-US').format(sum / [...populars].length);
