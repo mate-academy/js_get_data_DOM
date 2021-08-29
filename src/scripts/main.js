5c7c053da0628ad6ab1ec0e1de183caa9572bf5d
darynakravchuk <darinakravchuk@gmail.com>
2021-08-29 19:36:15 +0300

    js get data dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const arr = [];
+const getData = document.querySelectorAll('.population');
+const texts = [...getData].map(item => item.innerText);
+
+for (let i = 0; i < texts.length; i++) {
+  const toNumber = texts[i].split(',').join('');
+
+  arr.push(Number(toNumber));
+}
+
+const calculateTotal = arr.reduce((x, y) => x + y);
+
+const calculateAverage = calculateTotal / arr.length;
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+total.innerHTML = new Intl.NumberFormat('en-EN').format(calculateTotal);
+average.innerHTML = new Intl.NumberFormat('en-EN').format(calculateAverage);
