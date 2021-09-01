1152f7bc68a7aa79d76f24384289c482ad6bd0d1
Roman <romakelemen@gmail.com>
2021-09-01 12:42:55 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const items = document.querySelectorAll('.population');
+
+const population = [...items].map((item) => parseInt(item.textContent.replace(/,/g, '')));
+const totalValue = population.reduce((prev, curr) => prev + curr);
+const averageValue = totalValue/ population.length;
+
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+total.textContent = value(totalValue);
+average.textContent = value(averageValue);
+
+function value(number) {
+  let result = number.toString();
+  result = Array.from(result).reverse();
+  for (let i = 3; i < result.length; i++) {
+    if (i % 3 === 0) {
+      result[i] = `${result[i]},`;
+    }
+  }
+  result = result.reverse().join('');
+
+  return result;
+}
