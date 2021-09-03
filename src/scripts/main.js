b582ffed67f9aa8f3109f56b1ad06b0706c10125
Roman <romakelemen@gmail.com>
2021-09-03 10:44:51 +0300

    Solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,25 +2,19 @@
 
 const items = document.querySelectorAll('.population');
 
-const population = [...items].map((item) => parseInt(item.textContent.replace(/,/g, '')));
+const population = [...items].map((item) =>
+  parseInt(item.textContent.replace(/,/g, '')));
 const totalValue = population.reduce((prev, curr) => prev + curr);
-const averageValue = totalValue/ population.length;
+const averageValue = totalValue / population.length;
 
 const total = document.querySelector('.total-population');
 const average = document.querySelector('.average-population');
 
-total.textContent = value(totalValue);
-average.textContent = value(averageValue);
+total.textContent = getStringifiedPopulation(totalValue);
+average.textContent = getStringifiedPopulation(averageValue);
 
-function value(number) {
-  let result = number.toString();
-  result = Array.from(result).reverse();
-  for (let i = 3; i < result.length; i++) {
-    if (i % 3 === 0) {
-      result[i] = `${result[i]},`;
-    }
-  }
-  result = result.reverse().join('');
+function getStringifiedPopulation(number) {
+  const result = number.toLocaleString('en');
 
   return result;
 }
