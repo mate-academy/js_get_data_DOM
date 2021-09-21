6a41e9cb9afc6c1d41f6a17c3a2aba91f2660c12
Anton Forzun <anton_forzun@ukr.net>
2021-09-21 14:30:08 +0300

    get_data_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const obj = [];
+
+const population = document.querySelectorAll('.population');
+const number = [...population].map(item => item.innerText);
+
+for (let i = 0; i < number.length; i++) {
+  const string = number[i].split(',').join('');
+
+  obj.push(Number(string));
+}
+
+const total = obj.reduce((tot, amount) => tot + amount);
+
+const average = total / obj.length;
+
+const totale = document.querySelector('.total-population');
+
+totale.innerHTML = total.toLocaleString('en-US');
+
+const averagee = document.querySelector('.average-population');
+
+averagee.innerHTML = average.toLocaleString('en-US');
