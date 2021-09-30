6b268c613f1aecabb7d7787b16a720cbfce4e6d8
Roman Prokopiv <rprokopiv@gmail.com>
2021-09-30 19:38:20 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+let i = 0;
+let total = 0;
+
+for (const oneCountryPopulation of population) {
+  const peopleStr = oneCountryPopulation.outerText;
+  const peopleNum = +peopleStr.split(',').join('');
+
+  total += peopleNum;
+  i++;
+};
+
+const average = total / i;
+
+totalPopulation.textContent = bitRate(total);
+averagePopulation.textContent = bitRate(average);
+
+function bitRate(sum) {
+  const arr = [];
+
+  for (let j = sum.toString().length; j > 0; j = j - 3) {
+    let step = 3;
+
+    if (j < 3) {
+      step = j;
+    };
+
+    arr.unshift(sum.toString().substr(j - step, step));
+  };
+
+  return arr.join(',');
+}
