7ab8128d3d8758cad6240cac9484bb57d3cd25d4
Aleksandr Maltsev <0po4ta@gmail.com>
2021-10-02 00:56:14 +0400

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+let sum = 0;
+let average = 0;
+
+const listPopulation = document.querySelectorAll('.population');
+const calcSum = document.querySelector('.total-population');
+const calcAverege = document.querySelector('.average-population');
+
+[...listPopulation].forEach(el => {
+  sum += +el.textContent.replace(/,/g, '');
+
+  return sum;
+});
+
+average = sum / listPopulation.length;
+
+calcSum.textContent = sum.toLocaleString();
+calcAverege.textContent = average.toLocaleString();
