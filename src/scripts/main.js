88b06dd13b6cb9c31f534449e78d07af11b471aa
Andrii Malyshev <andrey.malyshev.97@gmail.com>
2021-10-02 19:04:40 +0300

    js_get_data_DOM done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const populationsCalc = [...populations]
+  .map(x => x.innerText)
+  .map(x => +x.replace(/,/g, ''))
+  .reduce((a, b) => a + b);
+const averagePopulationCalc = populationsCalc / [...populations].length;
+
+const populationsCalcArr = String(populationsCalc).split('');
+const averagePopulationCalcArr = String(averagePopulationCalc).split('');
+
+for (let i = populationsCalcArr.length - 3; i > 0; i = i - 3) {
+  populationsCalcArr.splice(i, 0, ',');
+};
+
+for (let i = averagePopulationCalcArr.length - 3; i > 0; i = i - 3) {
+  averagePopulationCalcArr.splice(i, 0, ',');
+};
+
+totalPopulation.innerText = (`${populationsCalcArr.join('')}`);
+averagePopulation.innerText = (`${averagePopulationCalcArr.join('')}`);
