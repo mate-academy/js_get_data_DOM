ea46c78f756c571c3552d31d0dfbe2e2e8576946
Oleksij Lyashenko <oleksijlyashenko@MacBook-Air-Mary.local>
2021-10-07 21:04:38 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const seach = document.querySelectorAll('.population');
+const strSum = document.querySelector('.total-population');
+const strAverage = document.querySelector('.average-population');
+
+const arr = [...seach].map(number => {
+  return Number(number.innerText.replace(/,/g, ''));
+});
+
+const sum = arr.reduce((x, y) => x + y).toLocaleString('en-EN');
+const avrg = (arr.reduce((x, y) => x + y) / arr.length).toLocaleString('en-EN');
+
+strSum.replaceWith(sum);
+strAverage.replaceWith(avrg);
