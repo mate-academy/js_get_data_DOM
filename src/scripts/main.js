f9b9e575a4ce8f60ca60849380265d2b1c5d2236
Dina Samoilova <s13.dina@gmail.com>
2021-10-27 21:46:34 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+
+const texts = [...populations].map(item => +item.innerText.split(',').join(''));
+
+const totalArr = String(texts.reduce((sum, num) =>
+  sum + num, 0)).split('').reverse();
+
+const averageArr = String(Math.round(texts.reduce((sum, num) =>
+  (sum + num), 0) / texts.length)).split('').reverse();
+
+for (let i = 0; i < totalArr.length; i++) {
+  if (((i + 1) % 3) === 0 && i > 0) {
+    totalArr[i + 1] = `${totalArr[i + 1]},`;
+  }
+}
+
+for (let i = 1; i <= averageArr.length; i++) {
+  if (i % 3 === 0 && i > 0 && i + 1 <= averageArr.length) {
+    averageArr[i] = `${averageArr[i]},`;
+  }
+}
+
+const spanTotal = document.querySelector('.total-population');
+const spanAverage = document.querySelector('.average-population');
+
+spanTotal.innerText = totalArr.reverse().join('');
+spanAverage.innerText = averageArr.reverse().join('');
