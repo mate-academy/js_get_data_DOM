ffabd5a8853906516115c81f0fbc697a4dc0a9a5
Dina Samoilova <s13.dina@gmail.com>
2021-10-28 21:08:15 +0300

    Solution after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,26 +4,10 @@ const populations = document.querySelectorAll('.population');
 
 const texts = [...populations].map(item => +item.innerText.split(',').join(''));
 
-const totalArr = String(texts.reduce((sum, num) =>
-  sum + num, 0)).split('').reverse();
-
-const averageArr = String(Math.round(texts.reduce((sum, num) =>
-  (sum + num), 0) / texts.length)).split('').reverse();
-
-for (let i = 0; i < totalArr.length; i++) {
-  if (((i + 1) % 3) === 0 && i > 0) {
-    totalArr[i + 1] = `${totalArr[i + 1]},`;
-  }
-}
-
-for (let i = 1; i <= averageArr.length; i++) {
-  if (i % 3 === 0 && i > 0 && i + 1 <= averageArr.length) {
-    averageArr[i] = `${averageArr[i]},`;
-  }
-}
+const total = texts.reduce((sum, num) => sum + num, 0);
 
 const spanTotal = document.querySelector('.total-population');
 const spanAverage = document.querySelector('.average-population');
 
-spanTotal.innerText = totalArr.reverse().join('');
-spanAverage.innerText = averageArr.reverse().join('');
+spanTotal.innerText = total.toLocaleString('en-US');
+spanAverage.innerText = (total / texts.length).toLocaleString('en-US');
