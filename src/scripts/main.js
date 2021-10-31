bcb472ed997f3ec40cac660769ac0bf7902ebf63
boduk <popovychbb@gmail.com>
2021-10-31 11:36:42 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const spanEl = document.querySelectorAll('span.population');
+let totalPop = 0;
+
+for (const value of spanEl) {
+  totalPop += +value.innerHTML.split(',').join('');
+}
+
+const averegePop = totalPop / spanEl.length;
+
+document.querySelector('span.total-population')
+  .innerHTML = totalPop.toLocaleString('en-US');
+
+document.querySelector('span.average-population')
+  .innerHTML = averegePop.toLocaleString('en-US');
