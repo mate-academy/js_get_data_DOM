81273604aaf70e8d0f2cf28f80cc8bd500a630df
Yuriy Titov <yuranus@gmail.com>
2021-11-05 18:03:03 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write your code here
+const parsed = [
+  ...document
+    .body
+    .querySelectorAll('.population')]
+  .map(el =>
+    Number(
+      el
+        .innerText
+        .split(',')
+        .join('')));
+
+const total = parsed
+  .reduce((prev, curr) =>
+    prev + curr
+  , 0);
+
+const average = total / parsed.length;
+
+document
+  .body
+  .querySelectorAll('.total-population')[0]
+  .innerText = `${total.toLocaleString()}`;
+
+document
+  .body
+  .querySelectorAll('.average-population')[0]
+  .innerText = `${average.toLocaleString()}`;
