c538de38e4d74a5ece79250f571766877e2164ef
Yuriy Titov <yuranus@gmail.com>
2021-11-08 14:54:54 +0200

    Update src/scripts/main.js
    Co-authored-by: Kryvytskyi Volodymyr <vovakrivickiy34@gmail.com>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,8 @@
 /* eslint-disable no-console */
 'use strict';
 
-const parsed = [
-  ...document
-    .body
-    .querySelectorAll('.population')]
-  .map(el =>
-    Number(
-      el
-        .innerText
-        .split(',')
-        .join('')));
+const parsed = [...document.body.querySelectorAll('.population')]
+  .map(el => Number(el.innerText.split(',').join('')));
 
 const total = parsed.reduce((prev, curr) => prev + curr, 0);
 
