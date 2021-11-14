178f4947e49157bde3b31b924495454f573cf5f1
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-14 09:42:52 +0200

    Parse population data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+function parsePopulation() {
+  const allPopulations = document.querySelectorAll('.population');
+  const populationsCountList = [];
+
+  for (const population of allPopulations) {
+    const populationNumber = +population.textContent.replace(/,/g, '');
+
+    populationsCountList.push(populationNumber);
+  }
+
+  return populationsCountList;
+}
+
+const populationsCount = parsePopulation();
+
+const totalPopulationStamp = document.querySelector('.total-population');
+const totalPopulation = populationsCount.reduce(
+  (total, populationNumber) => total + populationNumber,
+  0
+);
+
+totalPopulationStamp.textContent = totalPopulation.toLocaleString();
+
+const averagePopulationStamp = document.querySelector('.average-population');
+const averagePopulation = totalPopulation / populationsCount.length;
+
+averagePopulationStamp.textContent = averagePopulation.toLocaleString();
