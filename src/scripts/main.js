d1de91ed64683245218ecdf63787199d9208df9c
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2021-11-25 19:18:10 +0200

    Solution js_get_data_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const textData = document.querySelectorAll('.population');
+let totalPopulation = 0;
+let averagePopulation = 0;
+
+for (const value of textData) {
+  totalPopulation += +(value.firstChild.textContent).replaceAll(',', '');
+}
+
+averagePopulation = totalPopulation / textData.length;
+
+document.querySelector('.average-population').innerHTML
+  = averagePopulation.toLocaleString();
+
+document.querySelector('.total-population').innerHTML
+  = totalPopulation.toLocaleString();
