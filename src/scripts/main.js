ea6340b13e3382c26aaba1ebe4272c84583f9c5c
Marina Gerasimenko <marigera1917@gmail.com>
2021-12-06 21:41:19 +0200

    after coding review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,14 +7,9 @@ const total = document.querySelector('.total-population');
 const average = document.querySelector('.average-population');
 
 const nums = [...popul].map(people => people.innerHTML.split(',').join(''));
+const result = nums.reduce((a, b) => +a + +b);
 
-let sum = 0;
+const midl = result / nums.length;
 
-for (const num of nums) {
-  sum += +num;
-};
-
-const midl = sum / 10;
-
-total.innerHTML = sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-average.innerHTML = midl.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+total.innerHTML = result.toLocaleString('en-US');
+average.innerHTML = midl.toLocaleString('en-US');
