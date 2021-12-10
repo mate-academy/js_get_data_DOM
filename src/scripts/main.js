f90435c8d7b316dfd477e3f628576ef6ff2cbb03
Yuliia Dovbysh <juli3stepura@gmail.com>
2021-12-11 01:09:33 +0200

    js_get_data_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('.population');
+const total = [...list].reduce(
+  (acc, item) => acc + Number(item.innerText.replace(/,/g, '')),
+  0
+);
+
+const average = total / [...list].length;
+
+const totalString = total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+const averageString = average.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+document.querySelector('.total-population').innerHTML = totalString;
+
+document.querySelector('.average-population').innerHTML = averageString;
