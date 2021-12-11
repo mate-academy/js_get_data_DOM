35cf132a52261f3ffc394fbac7cc0b25fce3fae4
Artem Bolshak <fcdnipro20152705@gmail.com>
2021-12-11 13:55:29 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+function transformData(data) {
+  const arrOfLetters = String(data).split('');
+
+  for (let i = arrOfLetters.length - 3; i > 0; i = i - 3) {
+    arrOfLetters.splice(i, 0, ',');
+  }
+
+  return arrOfLetters.join('');
+}
+
+const population = document.querySelectorAll('.population');
+const allPopulation = [...population]
+  .map(el => +el.innerText.replace(/,/g, ''));
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const total = allPopulation.reduce((prev, next) => prev + next, 0);
+const average = total / allPopulation.length;
+
+const totalString = transformData(total);
+const averageString = transformData(average);
+
+totalPopulation.innerText = totalString;
+averagePopulation.innerText = averageString;
