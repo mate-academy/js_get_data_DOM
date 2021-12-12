c2554f9221f8b288e0580547b8c4d52469c3eeee
Elina Kovalenko <kovalenkoelin@gmail.com>
2021-12-12 22:46:55 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,8 @@ const numbers = [...collection].map(el => +el.innerText.split(',').join(''));
 
 const total = numbers.reduce((prev, curr) => prev + curr);
 
-const average = total / numbers.length;
+const aver = total / numbers.length;
 
 document.querySelector('.total-population').innerText = total.toLocaleString();
 
-document.querySelector('.average-population').innerText = average.toLocaleString();
+document.querySelector('.average-population').innerText = aver.toLocaleString();
