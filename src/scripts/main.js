dac07ebb403d69205b386b4a10813a1a2cc26067
Vova Alputov <alputov1998@gmail.com>
2021-12-20 16:29:17 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('.population');
+const numbers = [...list].map(item => item.innerText);
+let total = 0;
+const nf = new Intl.NumberFormat('en-US');
+
+for (const num of numbers) {
+  total += parseFloat(num.replace(/,/g, ''));
+}
+
+const average = total / numbers.length;
+
+document.querySelector('.total-population').textContent = nf.format(total);
+document.querySelector('.average-population').textContent = nf.format(average);
