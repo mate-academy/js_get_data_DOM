2c64c531905455ea7a2119a251078fc70c62e424
Robert Krukau <robert.grodno@mail.ru>
2021-12-26 13:52:36 +0100

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const totalLine = document.querySelector('.total-population');
+const averageLine = document.querySelector('.average-population');
+
+const sum = [];
+
+for (const key of population) {
+  sum.push(Number(key.textContent.split(',').join('')));
+}
+
+const total = sum.reduce((a, b) => a + b);
+const average = total / sum.length;
+
+totalLine.textContent = total.toLocaleString('en-US');
+averageLine.textContent = average.toLocaleString('en-US');
