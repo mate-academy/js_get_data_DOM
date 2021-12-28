4c6f914727a6b4661f367a1d194774d83eae443a
Jana Tretjak <tretiachechka@gmail.com>
2021-12-28 17:30:46 +0100

    ver01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+let total = 0;
+let average = 0;
+let count = 0;
+
+const span = document.querySelectorAll('.population');
+// const arr = span.map(elem => elem.innerText.split(',').join('.'))
+
+for (let elem of span) {
+  elem = elem.innerText.split(',').join('');
+  total += +elem;
+  count++;
+}
+
+average = (total / count);
+
+document.querySelector('.total-population').textContent
+= total.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent
+= average.toLocaleString('en-US');
