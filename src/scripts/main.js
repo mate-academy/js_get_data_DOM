038c999265c1e9c8f64bd907ff85997b4422a702
Daria Veretyak <dariaveretyak@gmail.com>
2022-01-09 21:21:44 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const listPopulation = document.querySelectorAll('.population');
+const arrayPopulation = [...listPopulation].map(
+  item => item.innerText.replace(/,/g, '')
+);
+const numberPopulation = arrayPopulation.map(item => Number(item));
+
+const totalAmount = numberPopulation.reduce((sum, current) => sum + current);
+const total = String(totalAmount).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1,');
+const totalElement = document.querySelector('.total-population');
+
+totalElement.innerText = total;
+
+const average = totalAmount / numberPopulation.length;
+const averageResult = String(average)
+  .replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1,');
+const averageElement = document.querySelector('.average-population');
+
+averageElement.innerText = averageResult;
