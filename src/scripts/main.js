dbae19a3a774af753116a3fff48de6891a25cdce
Yulia Shabelnagit config --global user.name Yulia <shabelna28@ukr.net>
2022-01-10 19:43:33 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const values = [...population]
+.map(number => parseInt(number.textContent.split(',').join('')));
+
+const total = values.reduce((acc, val) => acc + val);
+const average = total / values.length;
+
+totalPopulation.textContent = total.toLocaleString();
+averagePopulation.textContent = average.toLocaleString();
