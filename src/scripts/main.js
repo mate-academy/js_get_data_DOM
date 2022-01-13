779da05c806708a66f83b8b64fc63037a1ea1d1d
Victor Hrygorash <viktor12041991@i.ua>
2022-01-13 13:21:26 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const countries = [...document.querySelectorAll('.population')];
+
+const totalPopulation = countries
+  .map(country => country.innerText);
+
+const arrOfNum = totalPopulation.map(el => el.split(',').join(''));
+
+let sum = 0;
+
+for (let i = 0; i < arrOfNum.length; i++) {
+  sum += +arrOfNum[i];
+}
+
+const aver = sum / arrOfNum.length;
+
+document.querySelector('.total-population').textContent = `
+${sum.toLocaleString('en-US')}`;
+
+document.querySelector('.average-population').textContent = `
+${aver.toLocaleString('en-US')}`;
