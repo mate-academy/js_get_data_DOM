aa1fb7537f644bf83304c21896dee435c2c9b276
Dima Cherednyk <dicherednyk@gmail.com>
2022-01-21 18:30:41 +0200

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const texts = [...population].map(amount => amount.innerHTML);
+
+const numbers = texts.map(x => +x.replace(/,/g, ''));
+
+const totalAmount = numbers.reduce((sum, x) => sum + x, 0);
+
+const totalToString = totalAmount.toLocaleString('en-US');
+
+const averageToString = (totalAmount / numbers.length).toLocaleString('en-US');
+
+const total = document.querySelector('.total-population');
+
+const average = document.querySelector('.average-population');
+
+total.textContent = totalToString;
+
+average.textContent = averageToString;
+
+// console.log(total, average);
