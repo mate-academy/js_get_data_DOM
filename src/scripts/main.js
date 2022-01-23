57c64822b15e022aade0dfe36165a62d9e5e9380
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-01-23 23:32:12 +0200

    done!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
+const dataSpan = document.querySelectorAll('.population');
+
+let sum = 0;
+
+for (const item of dataSpan) {
+  const number = +item.textContent.replace(/[^0-9]/g, '');
+
+  sum += number;
+}
+
+const average = sum / dataSpan.length;
+
+const htmlTotal = document.querySelector('.total-population');
+const htmlAverage = document.querySelector('.average-population');
+
+htmlTotal.textContent = sum;
+htmlAverage.textContent = average;
+
 // write your code here
