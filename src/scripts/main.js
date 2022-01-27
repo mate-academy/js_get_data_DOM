5a309fceaee5221f9decd1f9403e5470626e030d
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-01-27 18:20:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
+const populationsDocuments = document.querySelectorAll('.population');
+const totalPopulationDocument = document.querySelector('.total-population');
+const avaragePopulationDocument = document.querySelector('.average-population');
+const totalPopulationText = [];
+
+for (const population of populationsDocuments) {
+  totalPopulationText.push(population.innerText);
+}
+
+const totalPopulation = totalPopulationText
+  .map(population => Number(population.split(',').join('')))
+  .reduce((a, b) => a + b);
+
+const avaragePopulation = totalPopulation / populationsDocuments.length;
+
+totalPopulationDocument.innerText = numberWithCommas(totalPopulation);
+avaragePopulationDocument.innerText = numberWithCommas(avaragePopulation);
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 // write your code here
