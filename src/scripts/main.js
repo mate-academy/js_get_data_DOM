e0efefab0853adb024c7bd8a935ba9b1aae9de08
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-01-27 19:29:24 +0200

    code refactor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,16 @@
 'use strict';
 
+function numberWithCommas(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 const populationsDocuments = document.querySelectorAll('.population');
 const totalPopulationDocument = document.querySelector('.total-population');
 const avaragePopulationDocument = document.querySelector('.average-population');
-const totalPopulationText = [];
-
-for (const population of populationsDocuments) {
-  totalPopulationText.push(population.innerText);
-}
-
-const totalPopulation = totalPopulationText
-  .map(population => Number(population.split(',').join('')))
+const totalPopulation = [...populationsDocuments]
+  .map(number => +number.innerText.split(',').join(''))
   .reduce((a, b) => a + b);
-
 const avaragePopulation = totalPopulation / populationsDocuments.length;
 
 totalPopulationDocument.innerText = numberWithCommas(totalPopulation);
 avaragePopulationDocument.innerText = numberWithCommas(avaragePopulation);
-
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
-
-// write your code here
