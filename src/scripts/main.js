8055a8b7b67d7211a8284f332fd4fcb2baedea3a
Roman Kovalchuk <roma220702@gmail.com>
2022-02-09 12:35:58 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('.population');
+const texts = [...list].map(item => item.innerText);
+let sum = 0;
+
+for (let i = 0; i < texts.length; i++) {
+  sum += Number(texts[i]);
+}
+
+const ave = sum / (texts.length - 1);
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+total.textContent = sum;
+average.textContent = ave;
