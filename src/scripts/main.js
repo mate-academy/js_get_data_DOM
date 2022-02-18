5ffad3b9126292948fa51abef3429265c70c71d1
Dmytro Semenenko <dmitriy.semenenko@gmail.com>
2022-02-18 18:48:46 +0200

    Solution Get Data DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const popColl = document.querySelectorAll('.population');
+const popArr = [...popColl].map((item) => {
+  return +item.innerText.replace(/,/g, '');
+});
+
+const popTotal = popArr.reduce((a, b) => a + b);
+const popTotalUS = popTotal.toLocaleString('en-US');
+
+const popAverage = popTotal / popArr.length;
+const popAverageUS = popAverage.toLocaleString('en-US');
+
+const totalPopulation = document.querySelector('.total-population');
+
+totalPopulation.innerText = popTotalUS;
+
+const averagePopulation = document.querySelector('.average-population');
+
+averagePopulation.innerText = popAverageUS;
