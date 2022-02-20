7a84d3f2491b0eac98f8c64dcc01c2800070087a
Mykhailo Molnar <mmolnar@atlassian.com>
2022-02-20 15:50:57 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
+const allPopulations = document.querySelectorAll('.population');
+
+const total = document.querySelector('.total-population');
+
+const average = document.querySelector('.average-population');
+
+var totalPopulation = 0;
+
+allPopulations.forEach(element => {
+  totalPopulation += parseInt(element.innerText.split(',').join(''));
+});
+
+total.textContent = totalPopulation.toLocaleString('en-US');
+
+average.textContent = (totalPopulation / allPopulations.length).toLocaleString('en-US');
 // write your code here
