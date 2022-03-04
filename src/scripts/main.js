9d05e43bb91dd8a3c650dbb64e904984ccd14711
Misha Novikov <novikov1807@gmail.com>
2022-03-04 11:31:55 +0200

    all complite

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,8 @@
 
 const populationElement = document.querySelectorAll('.population');
 
-const populationValue = [];
-
-// arr = populationsValue.map(el => el.textContent);
-
-for (let i = 0; i < populationElement.length; i++) {
-  populationValue.push(+populationElement[i].textContent.split(',').join(''));
-}
+const populationValue = [...populationElement]
+  .map(el => +el.textContent.split(',').join(''));
 
 const correctTotalValue = populationValue.reduce((sum, n) => sum + n, 0);
 
