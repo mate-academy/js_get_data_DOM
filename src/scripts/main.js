68db337caf1377d4da2a4f695e754970167383a9
Hlib Schebalkov <glebusha228@gmail.com>
2022-03-26 22:06:31 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const numbers = document.querySelectorAll('.population');
+const text = [...numbers].map(item => {
+  const num = item.innerHTML;
+  const newNum = num.split(',').join('');
+
+  return Number(newNum);
+})
+
+
+const sum = text.reduce((pre, cur) => pre + cur, 0);
+const numberFormatter = Intl.NumberFormat('en-US');
+const formattedSum = numberFormatter.format(sum);
+const average = sum / text.length;
+const formattedAver = numberFormatter.format(average);
+
+document.querySelector('.total-population').innerHTML = formattedSum;
+document.querySelector('.average-population').innerHTML = formattedAver;
