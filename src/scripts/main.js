43d7491dd48b504c3b25b2b0b00910b13ce6c915
Batick3737 <antonandriy@ukr.net>
2022-03-29 13:44:22 +0300

    write code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const masive = document.querySelectorAll('.population');
+const masiveNumber = [...masive].map(
+  ke => Number(ke.textContent.replace(/,/g, '')));
+const resultTotal = masiveNumber.reduce((sum, x) => sum + x, 0);
+const resultAverage = resultTotal / masiveNumber.length;
+
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+total.innerHTML = `${resultTotal.toLocaleString().replace(/\s/g, ',')}`;
+average.innerHTML = `${resultAverage.toLocaleString().replace(/\s/g, ',')}`;
