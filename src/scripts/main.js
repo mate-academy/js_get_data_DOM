3e7574444b110aa35099933f951b645501fc4d4c
Yevgeniy Savchuk <savchukevgeniy1@gmail.com>
2022-03-30 19:17:14 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const populationCollection = document.querySelectorAll('span.population');
+let total = 0;
+let average = 0;
+
+for (const people of populationCollection) {
+  const contryPopulation = +people.innerHTML.split(',').join('');
+
+  total += contryPopulation;
+  average = total / populationCollection.length;
+}
+
+function thousandsSeparator(number, separator = ',') {
+  const str = number.toString();
+  let result = '';
+
+  for (let i = 0; i < str.length / 3; i++) {
+    result = separator + str.slice(-((i + 1) * 3), str.length - i * 3) + result;
+  }
+
+  result = result.slice(1, result.length);
+
+  return result;
+}
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.innerHTML = thousandsSeparator(total);
+averagePopulation.innerHTML = thousandsSeparator(average);
