d9456aba372599d6a3e7b5780d902778081e3def
Alexander Skidan <xpromt@gmail.com>
2022-04-02 11:26:38 +0300

    Result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write your code here
+
+const average = document.querySelector('.average-population');
+
+let totalPopulation = 0;
+
+allPopulations.forEach(element => {
+  totalPopulation += parseInt(element.innerText.split(',').join(''));
+});
+
+total.textContent = totalPopulation.toLocaleString('en-US');
+
+average.textContent = (totalPopulation / allPopulations.length).toLocaleString('en-US');
+average.textContent = (totalPopulation / allPopulations.length)
+  .toLocaleString('en-US');
