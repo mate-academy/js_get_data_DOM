776c63b165fc35811ea81a47aa876dcb309f746b
Denys Lipin <lipin.transstream@gmail.com>
2022-04-10 22:40:21 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const textDataList = document.querySelectorAll('.population');
+const textList = [ ...textDataList ].map(item => item.innerText);
+const numbersList = textList.map(item => +item.split(',').join(''));
+
+let totalValue = numbersList.reduce((a, b) => a + b, 0);
+let averageValue = totalValue / numbersList.length;
+
+totalValue = totalValue.toLocaleString('en-US');
+averageValue = averageValue.toLocaleString('en-US');
+
+document.querySelector('.total-population').innerText = totalValue;
+document.querySelector('.average-population').innerText = averageValue;
