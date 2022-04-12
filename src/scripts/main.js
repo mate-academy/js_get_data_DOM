9b2084b211cca7cf1a3ec4212e8355e531ac8314
Max Shylenko <maxshylenko@gmail.com>
2022-04-12 15:07:29 +0200

    get_data_DOM is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const stringOfPopulation = [...population].map(num => (num.textContent));
+
+const numbersFromString = stringOfPopulation.map(num => {
+  let correctNum = '';
+
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] !== ',') {
+      correctNum += num[i];
+    }
+  }
+
+  return +correctNum;
+});
+
+const summ = numbersFromString.reduce((sum, x) => sum + x, 0);
+const average = summ / numbersFromString.length;
+
+const total = document.querySelector('.total-population');
+const averageSum = document.querySelector('.average-population');
+
+total.textContent = summ.toLocaleString('en-US');
+averageSum.textContent = average.toLocaleString('en-US');
