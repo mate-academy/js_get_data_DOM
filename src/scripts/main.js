ae20a7d8b6dba95a14140c66f7d75cee1f6b27cc
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-04-16 18:02:52 +0300

    js-get-data-dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')];
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const resultPopulation = population
+  .map(populations => +populations.innerText.split(',').join(''))
+  .reduce((sum, people) => sum + people, 0);
+
+const averageResult = resultPopulation / population.length;
+
+totalPopulation.innerText = resultPopulation.toLocaleString('en');
+averagePopulation.innerText = averageResult.toLocaleString('en');
