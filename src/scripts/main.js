c856ca219ee162c4e0ee622800457688f6a87b16
Ivan Kadykalo <ivan.kadykalo@gmail.com>
2022-04-18 16:04:07 +0300

    Create solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const values = [...population].map(item => +item.innerText.split(',').join(''));
+const total = values.reduce((sum, num) => sum + num);
+const avarege = total / values.length;
+
+const totalPopulation = document.querySelector('.total-population');
+const avaragePopulation = document.querySelector('.average-population');
+
+totalPopulation.innerText = total.toLocaleString('ja-JP');
+avaragePopulation.innerText = avarege.toLocaleString('ja-JP');
