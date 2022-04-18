b809f3afd543122eea8356db7138eca1e3649627
Anton Vakoliuk <antonvakolyuk2001@gmail.com>
2022-04-18 23:02:41 +0200

    Optimized

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,5 @@
 'use strict';
 
-function adderThousandsSeparator(number) {
-  const res = [];
-  const strNum = `${number}`;
-  let count = 0;
-
-  for (let i = strNum.length - 1; i >= 0; i--) {
-    count++;
-    res.push(strNum[i]);
-
-    if (count % 3 === 0 && count !== strNum.length) {
-      res.push(',');
-    }
-  }
-
-  return res.reverse().join('');
-}
-
 const values = document.querySelectorAll('.population');
 const numbers = [...values].map(value => +value.innerText.split(',').join(''));
 
@@ -26,5 +9,5 @@ const averageValue = totalValue / numbers.length;
 const total = document.querySelector('.total-population');
 const average = document.querySelector('.average-population');
 
-total.textContent = adderThousandsSeparator(totalValue);
-average.textContent = adderThousandsSeparator(averageValue);
+total.textContent = totalValue.toLocaleString('en-US');
+average.textContent = averageValue.toLocaleString('en-US');
