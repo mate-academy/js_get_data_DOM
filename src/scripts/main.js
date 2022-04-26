02b2209e33496f1d86cbc9076c950167b0a72fc5
Alina Yermolenko <alina.yermolenko27@gmail.com>
2022-04-26 21:29:09 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const spa = document.querySelectorAll('span.population');
+const strings = [...spa].map(one => one.textContent);
+const number = strings.map(el => {
+  return Number(el.split(',').join(''));
+}
+);
+
+const resultTotal = number.reduce((a, b) => a + b);
+const resultAverage = resultTotal / strings.length;
+
+const total = document.querySelector('.total-population');
+
+total.innerHTML = resultTotal.toLocaleString('en-US');
+
+const average = document.querySelector('.average-population');
+
+average.innerHTML = Math.floor(resultAverage).toLocaleString('en-US');
