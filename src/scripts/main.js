1c70d9e4892ce154f53938fb178630e7622620c3
Nikita Prepelytsia <prep.nikita@gmail.com>
2022-04-30 19:15:46 +0300

    dataDOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+let result = 0;
+let amount = 0;
+let resultTotal = '';
+let resultAverage = '';
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+const allParams = document.querySelectorAll('.population');
+
+for (let i = 0; i < [...allParams].length; i++) {
+  result += +[...allParams][i].textContent.split(',').join('');
+  amount++;
+}
+
+resultTotal = result.toLocaleString('en-US');
+resultAverage = (result / amount).toLocaleString('en-US');
+
+total.textContent = resultTotal;
+average.textContent = resultAverage;
