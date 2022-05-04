c2ac4642bcdc0baca8500499e47f8c9205c9a30c
Bohdan Dubravin <dubravin111@gmail.com>
2022-05-04 20:15:40 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const select = document.body.querySelectorAll('.population');
+
+const listPopulation = [...select].reduce((acc, val) => {
+  const num = +val.textContent.replaceAll(',', '');
+
+  acc.push(num);
+
+  return acc;
+}, []);
+
+const totalPopulation = listPopulation.reduce((acc, val) => acc + val);
+
+const averagePopulation = totalPopulation / listPopulation.length;
+
+document.querySelector('.total-population').innerHTML = totalPopulation;
+document.querySelector('.average-population').innerHTML = averagePopulation;
