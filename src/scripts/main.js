26ea2bda120f6819058488e6c8a8b3b801526a26
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-05-09 15:44:51 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+function strToDigitArr(string_){
+    const arrString = string_.split('');
+    const arrDigit = arrString.filter(letter => /^[0-9]$/.test(letter))
+    return arrDigit;
+}
+
+const span_ = document.querySelectorAll('span');
+const populationOnly = [...span_].filter(item =>item.matches('.population'))
+const len_ = populationOnly.length
+const digitArrs = populationOnly.map(item => strToDigitArr(item.innerText))
+const arrDigits = digitArrs.map(item => Number(item.join('')))
+const total = arrDigits.reduce((a, b) => a + b, 0)
+const aver = total / len_
+const totalD = total.toLocaleString()
+const averD = aver.toLocaleString()
+const totalResult = document.querySelector('.total-population')
+const averagResult = document.querySelector('.average-population')
+totalResult.innerHTML = totalD
+averagResult.innerHTML = averD
