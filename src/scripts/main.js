ee87eedf1f979496a5293b7c8496b016acd1cd88
Kostiantyn <havryshkostya@gmail.com>
2022-05-12 15:17:22 +0400

    vyihav na traktori povernuvsya s tankam ya

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const elements = document.querySelectorAll('.population');
+
+const allPopulationsNum = [...elements].map(el =>
+  +(el.innerText).split(',').join(''));
+
+const total = allPopulationsNum.reduce((x, y) => x + y, 0);
+const average = total / allPopulationsNum.length;
+
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+
+totalElement.innerText = total.toLocaleString();
+averageElement.innerText = average.toLocaleString();
