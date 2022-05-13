60d9b1848b1c3eab0cd4b743079836626e39b791
ShapovalDenys <denys.igorevich@gmail.com>
2022-05-13 14:39:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const arrOfEl = [...document.querySelectorAll('.population')];
+const arrOfNum = arrOfEl.map(e => +e.textContent.split(',').join(''));
+const totalSum = arrOfNum.reduce((a, b) => a + b);
+const averageSum = totalSum / arrOfNum.length;
+
+const totalPopulationSpan = document.querySelector('.total-population');
+
+totalPopulationSpan.textContent = totalSum.toLocaleString('en-US');
+
+const averagePopulationSpan = document.querySelector('.average-population');
+
+averagePopulationSpan.textContent = averageSum.toLocaleString('en-US');
