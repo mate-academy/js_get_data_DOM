7ebc87ceb766cb7b75ce2adb5a4ccb91746bb5dc
Katya Suvorova <katerinasuvorova97@gmail.com>
2022-05-18 19:07:15 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const olList = document.querySelector('ol');
+
+const result = [];
+
+for (const i of olList.childNodes) {
+  if (i.children !== undefined) {
+    let spanPopulationValue = i.children[1].textContent;
+
+    spanPopulationValue = spanPopulationValue.split(',').join('');
+
+    const num = Number(spanPopulationValue);
+
+    result.push(num);
+  }
+}
+
+const length = result.length;
+
+let total = 0;
+
+for (const number of result) {
+  total += number;
+}
+
+const average = total / length;
+
+const totalPopulation = document.querySelector('p').children[0];
+const averagePopulation = document.querySelector('[class="average"]')
+  .children[0];
+
+averagePopulation.textContent = average;
+
+totalPopulation.textContent = total;
