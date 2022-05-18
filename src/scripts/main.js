186d710d712d0c30c368d87b9d40da93ffe9d86e
Roman Lipatov <96875973+roman-lipatov@users.noreply.github.com>
2022-05-18 14:13:03 +0300

    get-data-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+const spanCollection = document.querySelectorAll('.population');
+const totalEl = document.querySelector('.total-population');
+const averageEl = document.querySelector('.average-population');
+
+const totalPopulation = [...spanCollection].reduce(
+  (total, { textContent }) => {
+    const number = Number(textContent.split(',').join(''));
+
+    return total + number;
+  }, 0);
+
+averageEl.textContent = (totalPopulation / spanCollection.length)
+  .toLocaleString('en');
+
+totalEl.textContent = (totalPopulation / spanCollection.length)
+  .toLocaleString('en');
