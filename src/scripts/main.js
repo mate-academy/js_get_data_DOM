08d7c3ffd7fb49744deba32ee2ef707721f07ebb
Alina Yermolenko <alina.yermolenko27@gmail.com>
2022-05-27 18:14:45 +0300

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,12 @@
 
 const spa = document.querySelectorAll('span.population');
 const strings = [...spa].map(one => one.textContent);
-const number = strings.map(el => {
-  return Number(el.split(',').join(''));
+const populations = strings.map(el => {
+  Number(el.split(',').join(''));
 }
 );
 
-const resultTotal = number.reduce((a, b) => a + b);
+const resultTotal = populations.reduce((a, b) => a + b);
 const resultAverage = resultTotal / strings.length;
 
 const total = document.querySelector('.total-population');
