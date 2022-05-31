deab8048e6f7748a2dee740eab42ccc337aa6d99
Katruk Evgeny <zaiaz_@i.ua>
2022-05-31 20:35:03 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write your code here
+const colectionPopulation = document.querySelectorAll('.population');
+const arrPopulation = [...colectionPopulation]
+  .map(item => +item.innerHTML.replace(/,/g, ''));
+const total = arrPopulation.reduce((a, b) => a + b);
+const totalAverage = total / arrPopulation.length;
+const totalTxt = document.querySelector('.total-population');
+
+totalTxt.textContent = total;
+
+const totalTxtAverage = document.querySelector('.average-population');
+
+totalTxtAverage.textContent = totalAverage;
