792245c2f4a25e22a894285e0b44e9964d3cef5d
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-06-09 10:41:07 +0800

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+// spans.length === 10
+
+let countTotal = 0;
+
+for (const item of spans) {
+  countTotal += +(item.textContent.replace(/\D/g, ''));
+}
+
+const countAverage = countTotal / spans.length;
+
+// const text = [...spans].map(item => item.innerText);
+
+document.querySelector('.total-population').innerText = countTotal;
+document.querySelector('.average-population').innerText = countAverage;
