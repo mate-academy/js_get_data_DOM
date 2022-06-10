faab5ec05495a4170ef549b40f2a33d2a13d6b5c
Volodumur Vitinskiy <vovahunter1989@gmail.com>
2022-06-10 11:29:18 +0300

    first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const items = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+let totalPop = 0;
+let averagePop = 0;
+
+for (const item of items) {
+  totalPop += +(item.innerText.split(',').join(''));
+}
+
+averagePop = totalPop / items.length;
+
+total.innerText = totalPop.toLocaleString('en-US');
+average.innerText = averagePop.toLocaleString('en-US');
