66413335667edd163c9979cc870866f838683c9a
Max <59259022+WhiteAshDc@users.noreply.github.com>
2022-06-14 16:40:46 +0200

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
+// Получаем елемент за класом
 
-// write your code here
+const getSpanClass = document.querySelectorAll('.population');
+// з колекція => масив => сума
+
+const totalAmount = [...getSpanClass]
+  .map(item => item.innerText.replaceAll(',', ''))
+  .map(el => Number(el)).reduce((acc, el) => acc + el);
+
+// отрмуємо елем за класом
+const newEl = document.querySelector('.total-population');
+
+// присвоюю значення
+newEl.innerText = totalAmount.toLocaleString('en-US');
+
+// отрмую елем за тегом
+
+const itemNew = document.querySelector('ol').children;
+// зберігаю довжину на випадок якщо додадуться інші країни
+const itemLen = itemNew.length;
+
+// вираховую середнє значення загальна на кількість країн
+const averageAm = totalAmount / itemLen;
+
+// доступ за класом => змінити контенет
+const aver = document.querySelector('.average-population');
+
+aver.innerText = averageAm.toLocaleString('en-Us');
