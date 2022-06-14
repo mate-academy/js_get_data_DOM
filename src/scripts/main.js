b7756150af998456270e8f09bb799b78d8b1fa24
Artem Furdela <furdela.artem@gmail.com>
2022-06-14 22:39:51 +0300

    Get_data_DOM first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const popArray = [...populations].map(pop =>
+  Number(pop.textContent.split(',').join('')));
+
+const popTotal = popArray.reduce((pop1, pop2) => pop1 + pop2);
+const popAverage = Math.round(popTotal / popArray.length);
+
+document.querySelector('.total-population').innerText
+  = popTotal.toLocaleString('en');
+
+document.querySelector('.average-population').innerText
+  = popAverage.toLocaleString('en');
