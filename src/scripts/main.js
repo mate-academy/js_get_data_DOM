d2de221ad274e1482f4bb9c7af4ff077c195dd72
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-06-15 19:42:21 +0300

    Top 10 largest countries population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const totalPopulationValue = [...population].reduce((prev, current) => {
+  return prev + Number(current.textContent.split(',').join(''));
+}, 0);
+
+const averagePopulationValue = totalPopulationValue / population.length;
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.innerHTML = totalPopulationValue.toLocaleString();
+averagePopulation.innerHTML = averagePopulationValue.toLocaleString();
