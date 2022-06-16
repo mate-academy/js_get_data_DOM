07fb2977c07bfaefaa4703672dd75b23bf5a98e4
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-06-16 17:49:12 +0300

    js get data DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write your code here
+let sum = 0;
+const list = [...document.querySelectorAll('.population')];
+
+const arr = list.map(item => +item.innerText.replace(/,/g, ''));
+
+for (const number of arr) {
+  sum += number;
+}
+
+const average = sum / arr.length;
+
+const itemTotal = document.querySelector('.total-population');
+const itemAverage = document.querySelector('.average-population');
+
+itemTotal.innerHTML = sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+itemAverage.innerHTML
+  = average.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
