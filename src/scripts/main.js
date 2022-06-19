d1fe49a307b917dfea8a3308a6dd76f2534b1ba2
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-06-19 14:31:01 +0300

    Second Run

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const list = [...document.querySelectorAll('.population')];
+
+const arrNew = list.map(item => +item.innerText.replace(/,/g, ''));
+
+const total = arrNew.reduce((sum, current) => sum + current, 0);
+
+const average = total / arrNew.length;
+
+const itemTotal = document.querySelector('.total-population');
+const itemAverage = document.querySelector('.average-population');
+
+itemTotal.innerHTML = total.toLocaleString('en');
+
+itemAverage.innerHTML
+  = average.toLocaleString('en');
