3613a5ce5adf921ce2d0b63155bb4dd1fb0e8995
Kirill Polozenko <kir.polozenko@icloud.com>
2022-06-20 21:28:01 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const total = [];
+
+for (let i = 0; i < population.length; i++) {
+  const populationToNumber = Number(population[i].innerHTML
+    .split(',').join(''));
+
+  total.push(populationToNumber);
+}
+
+const sum = total.reduce((a, b) => a + b, 0);
+const average = sum / total.length;
+
+document.querySelector('.total-population')
+  .textContent = sum;
+
+document.querySelector('.average-population')
+  .textContent = average;
