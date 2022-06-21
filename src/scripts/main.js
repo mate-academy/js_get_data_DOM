da4e914d874dd47c132442bb59b9577a647c07fe
Ihor Kuchin <vedunigor@gmail.com>
2022-06-21 13:20:30 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const CountriesPopulation = document.querySelectorAll('.population');
+const populationData = [...CountriesPopulation].map(number => number.innerText);
+
+function getTotalPopulation(populations) {
+  return populations
+    .map(el => +el.replaceAll(',', ''))
+    .reduce((total, population) => total + population);
+}
+
+function getAveragePopulation(populations) {
+  return getTotalPopulation(populations) / populations.length;
+}
+
+function getCommaFormat(num) {
+  return new Intl.NumberFormat('en-US', { style: 'decimal' }).format(num);
+}
+
+function showTotalPopulation(populations) {
+  const totalPopulationField = document.querySelector('.total-population');
+  const totalPopulation = getTotalPopulation(populations);
+
+  totalPopulationField.textContent = getCommaFormat(totalPopulation);
+}
+
+function showAveragePopulation(populations) {
+  const averagePopulationField = document.querySelector('.average-population');
+  const averagePopulation = getAveragePopulation(populations);
+
+  averagePopulationField.textContent = getCommaFormat(averagePopulation);
+}
+
+showTotalPopulation(populationData);
+showAveragePopulation(populationData);
