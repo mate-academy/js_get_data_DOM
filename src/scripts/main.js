a0fa022794526c094aea2ce918c4e72512ec038f
Konstantin <ivanchenkomypost@gmail.com>
2022-06-26 17:38:32 +0300

    Task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const populations = Array.from(document.querySelectorAll('.population'))
+  .map(el => el.textContent)
+  .map(el => +el.split(',').join(''));
+
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+total.textContent = populations.reduce((a, b) => a + b).toLocaleString('en-US');
+
+average.textContent = (populations.reduce((a, b) => a + b) / populations.length)
+  .toLocaleString('en-US');
