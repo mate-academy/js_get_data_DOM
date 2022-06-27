78bd33257628f35fd47b362df4227ad8f1fce643
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-27 19:32:51 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
 // write your code here
+
+const population = document.querySelectorAll(`.population`);
+const totalPop = document.querySelector(`.total-population`);
+const avaragePop = document.querySelector(`.average-population`);
+const array = [];
+
+function separetor(number) {
+  const string = `${number}`;
+  const newString = [];
+
+  for (let i = 1; i <= string.length; i++) {
+    newString.unshift(string[string.length - i]);
+
+    if (i % 3 === 0 && i !== 0 && i !== string.length) {
+      newString.unshift(',');
+    }
+  }
+
+  return newString.join('');
+}
+
+population.forEach(number => {
+  const str = number.innerHTML.replaceAll(',', '');
+
+  if (!isNaN(Number(str))) {
+    array.push(Number(str));
+  }
+});
+
+const total = array.reduce((prev, curr) => prev + curr);
+const avarage = total / array.length;
+
+totalPop.innerHTML = separetor(total);
+avaragePop.innerHTML = separetor(avarage);
