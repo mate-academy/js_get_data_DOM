7c5a74da90ac8ec116c7b6856706a6410219a787
Kostya Berehoviy <kostyaber3@gmail.com>
2022-06-28 00:37:39 +0300

    Get DOM Data Solution (sorry for some HTML changes)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const findSpan = document.querySelectorAll('.population');
+
+const numbers = [...findSpan].map(numb =>
++(numb.innerText.split(',').join('')));
+
+const totalPopulation = Math.round(numbers.reduce((c1, c2) => c1 + c2, 0));
+const averagePopulation = totalPopulation / numbers.length;
+
+const aveInHtml = document.querySelector('.average-population');
+const totalInHtml = document.querySelector('.total-population');
+
+aveInHtml.textContent = averagePopulation.toLocaleString('en');
+totalInHtml.textContent = totalPopulation.toLocaleString('en');
