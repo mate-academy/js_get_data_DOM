30ef4ae3a91010f6cca7215760f4863d0f6406a2
Stanislav Bzhezinskyi <stasiva28@gmail.com>
2022-06-28 18:46:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const items = document.querySelectorAll('.population');
+const num = [...items]
+  .map(item => item.innerText)
+  .map(item => Number(item.replaceAll(',', '')));
+const total = num.reduce((num1, num2) => num1 + num2).toLocaleString();
+const average = Math.round(
+  num.reduce((num1, num2) => (num1 + num2) / num.length)
+).toLocaleString();
+const totalString = document.querySelector('.total-population');
+const averageString = document.querySelector('.average-population');
+
+totalString.innerText = total;
+averageString.innerText = average;
