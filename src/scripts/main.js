ad1d2f81e358ccfd1f2a97ed4784a6b588b32c9e
DimaShm <shmagindmitry@gmail.com>
2022-06-29 00:34:18 +0300

    Solution was improved by the method toLocaleString

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,35 +9,5 @@ const numberArr = [...list].map(item =>
 const total = numberArr.reduce((a, b) => a + b, 0);
 const average = total / numberArr.length;
 
-const strTotal = String(total).split('');
-const strAverage = String(average).split('');
-
-let reversResultTotal = '';
-let reversResultAverage = '';
-let count = 0;
-
-for (let i = strTotal.length - 1; i >= 0; i--) {
-  count++;
-  reversResultTotal += strTotal[i];
-
-  if (count % 3 === 0 && strTotal.length !== count) {
-    reversResultTotal += ',';
-  }
-}
-
-const resultTotal = reversResultTotal.split('').reverse().join('');
-totalElement.innerText = resultTotal;
-
-count = 0;
-
-for (let i = strAverage.length - 1; i >= 0; i--) {
-  count++;
-  reversResultAverage += strAverage[i];
-
-  if (count % 3 === 0 && strAverage.length !== count) {
-    reversResultAverage += ',';
-  }
-}
-
-const resultAverage = reversResultAverage.split('').reverse().join('');
-averageElement.innerText = resultAverage;
+totalElement.innerText = total.toLocaleString('en-US');
+averageElement.innerText = average.toLocaleString('en-US');
