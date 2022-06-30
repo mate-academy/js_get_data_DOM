436f0f88d6451c0e51e25d25e74b87c7b88c871d
Alyona Pisotska <pisotska.o@gmail.com>
2022-06-30 15:34:07 +0300

    JS_get_data_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+
+const populationValues = [...populationElements].map(item =>
+  +(item.innerText.split(',').join(''))
+);
+
+const totalPopulation = populationValues.reduce((sum, n) => sum + n, 0);
+const avgPopulation = totalPopulation / populationValues.length;
+
+const totalPopulationElement = document.querySelector('.total-population');
+const avgPopulationElement = document.querySelector('.average-population');
+
+totalPopulationElement.textContent = totalPopulation.toLocaleString('en-US');
+avgPopulationElement.textContent = avgPopulation.toLocaleString('en-US');
