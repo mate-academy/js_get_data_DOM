b9ce9d1ac71e5efcf00250696d647c0d7bd1d30e
silchyn <ivan.silchin@gmail.com>
2022-07-05 16:08:57 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const countriesCount = populations.length;
+let totalPopulation = 0;
+
+for (const population of populations) {
+  const populationNumber = +population.innerHTML.replace(/,/g, '');
+
+  totalPopulation += populationNumber;
+}
+
+document.querySelector('.total-population').innerHTML
+  = totalPopulation.toLocaleString('en-us');
+
+document.querySelector('.average-population').innerHTML
+  = (totalPopulation / countriesCount).toLocaleString('en-US');
