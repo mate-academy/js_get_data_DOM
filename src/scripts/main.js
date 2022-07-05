0a6aa54f59d227e5a7a6cd604a8fd6b44c30fea1
silchyn <ivan.silchin@gmail.com>
2022-07-05 16:49:28 +0300

    changed innerHTML to innerText

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,13 @@ const countriesCount = populations.length;
 let totalPopulation = 0;
 
 for (const population of populations) {
-  const populationNumber = +population.innerHTML.replace(/,/g, '');
+  const populationNumber = +population.innerText.replace(/,/g, '');
 
   totalPopulation += populationNumber;
 }
 
-document.querySelector('.total-population').innerHTML
+document.querySelector('.total-population').innerText
   = totalPopulation.toLocaleString('en-us');
 
-document.querySelector('.average-population').innerHTML
+document.querySelector('.average-population').innerText
   = (totalPopulation / countriesCount).toLocaleString('en-US');
