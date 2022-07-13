292534ac307b70acf7bf6ed2bcfef0f6b4bf772e
Olha Arama <arama_o_f@ukr.net>
2022-07-13 15:42:38 +0300

    Total and average were calculated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const numberFormat = new Intl.NumberFormat('en-US');
+
+const arr = [];
+
+for (const item of population) {
+  arr.push(Number(item.innerHTML.split(',').join('')));
+}
+
+const total = arr.reduce((prev, current) => prev + current, 0);
+const average = total / arr.length;
+
+const formattedTotal = numberFormat.format(total);
+const formattedAverage = numberFormat.format(average);
+
+document.querySelector('.total-population').textContent = formattedTotal;
+document.querySelector('.average-population').textContent = formattedAverage;
