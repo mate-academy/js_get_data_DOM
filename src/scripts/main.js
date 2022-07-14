97196f6a9fb99dd74f3c164744698ce10507e461
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-07-14 15:17:08 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+const arrPopulation = [...population].map(el =>
+  +el.innerHTML.split(',').join(''));
+const averPopulation = (arrPopulation.reduce((a, b) => a + b));
+const resultTotal = averPopulation / arrPopulation.length;
+
+const numberFormatter = Intl.NumberFormat('en-US');
+
+total.textContent = numberFormatter.format(averPopulation);
+average.textContent = numberFormatter.format(resultTotal);
