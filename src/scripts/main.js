c8673444b80344bde559d3fa9890106549e3d600
Oleksìi <bardyshev@gmail.com>
2022-07-18 12:16:13 +0300

    Corrected solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,17 +4,10 @@ const populationsEls = document.querySelectorAll('.population');
 const totalPopulationEl = document.querySelector('.total-population');
 const averagePopulationEl = document.querySelector('.average-population');
 
-// let total = 0;
-
-const popArray = [];
-
-for (let i = 0; i < populationsEls.length; i++) {
-  popArray.push(Number(populationsEls[i].textContent.split(',').join('')));
-}
-
-const total = popArray.reduce((acc, curr) => acc + curr);
-
-const avarage = total / populationsEls.length;
+const populations = [...populationsEls]
+  .map(elem => +elem.innerText.split(',').join(''));
+const total = populations.reduce((acc, curr) => acc + curr, 0);
+const average = total / populations.length;
 
 totalPopulationEl.textContent = total.toLocaleString('en-US');
-averagePopulationEl.textContent = avarage.toLocaleString('en-US');
+averagePopulationEl.textContent = average.toLocaleString('en-US');
