68bbbcc0e919d0a9617a533c0e4b869cfefdd90e
lentyaeev <daniilshaliuk@gmail.com>
2022-07-18 14:54:33 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const allSpans = document.getElementsByTagName('span');
+let total = 0;
+let count = 0;
+
+for (const element of allSpans) {
+  if (element.classList.contains('population')) {
+    total += +element.textContent.replace(/,/gi, '');
+    count++;
+  }
+}
+
+const average = total / count;
+
+document.querySelector('.total-population')
+  .textContent = total.toLocaleString('en-Us');
+
+document.querySelector('.average-population')
+  .textContent = average.toLocaleString('en-Us');
