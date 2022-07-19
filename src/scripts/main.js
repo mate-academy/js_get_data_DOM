b249dfcb173ae00f1bd3d1864c2c066de37f2be1
Oleksii Zasiedka <alex.zasedka@gmail.com>
2022-07-19 19:23:10 +0300

    code simplification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,6 @@ const avgElem = document.querySelector('.average-population');
 const total = [...countriesPopulation].reduce((a, b) => (
   a + +b.textContent.split(',').join('')
 ), 0);
-
 const avg = Math.floor(total / countriesPopulation.length);
 
 totalElem.textContent = total.toLocaleString('en-US');
