55fc46cacff47b845faf135c58369223cdb17ef0
Oleksii Zasiedka <alex.zasedka@gmail.com>
2022-07-19 19:21:46 +0300

    code simplification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,9 @@
 const countriesPopulation = document.querySelectorAll('.population');
 const totalElem = document.querySelector('.total-population');
 const avgElem = document.querySelector('.average-population');
-let total = 0;
-
-countriesPopulation.forEach(x => {
-  total += +x.textContent.split(',').join('');
-});
+const total = [...countriesPopulation].reduce((a, b) => (
+  a + +b.textContent.split(',').join('')
+), 0);
 
 const avg = Math.floor(total / countriesPopulation.length);
 
