6a6506195a5231c7ebfe76bd6eb66651dc355fcd
Denys Dmytruk <dmytrukdenysan@gmail.com>
2022-07-19 10:53:51 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ const averageElement = document.querySelector('.average-population');
 
 const populationValues = [...allCountriesInfo]
   .map(item => parseInt(item.innerText.split(',').join('')));
-const totalAmount = populationValues.reduce((a, b) => a + b);
+const totalAmount = populationValues.reduce((a, b) => a + b, 0);
 const averageAmount = totalAmount / populationValues.length;
 
 totalElement.textContent = totalAmount.toLocaleString('en-US');
