98c06cd2b37c05669b14eb1bd816bd7af4859983
Oleg Vetrov <oleg94038@gmail.com>
2022-07-21 19:34:05 +0300

    Added total and average population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const population = document.getElementsByClassName('population');
+
+const totalPopulationElement = document
+  .getElementsByClassName('total-population')[0];
+const averagePopulationElement = document
+  .getElementsByClassName('average-population')[0];
+
+const totalPopulation = [...population].reduce((acc, country) => {
+  return acc + Number(country.innerText.split(',').join(''));
+}, 0);
+
+totalPopulationElement.innerHTML = totalPopulation.toLocaleString('en');
+
+averagePopulationElement.innerHTML = (totalPopulation / population.length)
+  .toLocaleString('en');
