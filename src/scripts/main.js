94c272a87a51f1f02d3a9c7d14b9e4ec0862525a
Denys Korotkoruchko <denenergii@gmail.com>
2022-07-22 13:55:49 +0300

    ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+let sum = 0;
+const arr = [];
+
+population.forEach(element => (arr.push(element.innerHTML
+  .replace(/,/g, ''))));
+
+sum = arr.reduce((x, y) => x + +y, 0);
+
+total.innerText = new Intl.NumberFormat('en-US').format(sum);
+average.innerText = new Intl.NumberFormat('en-US').format(sum / arr.length);
