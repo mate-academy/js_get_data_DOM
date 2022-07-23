08bea6c6f488d1683bf3f834e5ab58302caf8bbd
PavloMarkov <r9markovpn@gmail.com>
2022-07-23 11:30:51 +0300

    total & avarage population were colculated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const numbers = document.querySelectorAll('.population');
+
+const list = [];
+
+numbers.forEach(item => list.push(item.innerText));
+
+const population = list.map(item => {
+  return +item.split(',').join('');
+});
+const totalPopulation = population.reduce((prev, num) => prev + num, 0);
+const avaragePopulation = totalPopulation / population.length;
+
+const totalToIncert = totalPopulation.toLocaleString('en-US');
+const avarageToIncert = avaragePopulation.toLocaleString('en-US');
+
+const elementTotal = document.querySelectorAll('.total-population');
+const elementAvarage = document.querySelectorAll('.average-population');
+
+elementTotal[0].innerHTML = totalToIncert;
+elementAvarage[0].innerHTML = avarageToIncert;
