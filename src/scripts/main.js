efe88069c335e31def83d4d469032adaab867174
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2022-07-25 18:11:52 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+
+let totalPopulation = 0;
+
+const population = document.querySelectorAll('.population');
+
+population.forEach(country => {
+  console.log(country.innerText.split(',').join(''))
+  const normalizeNimber = +country.innerText.split(',').join('')
+  totalPopulation += normalizeNimber;
+});
+
+console.log(totalPopulation)
+
+const populationHtml = document.querySelector('.total-population');
+const avarageHtml = document.querySelector('.average-population');
+
+
+populationHtml.innerText = totalPopulation.toLocaleString('en-US');
+avarageHtml.innerText = (totalPopulation / 10).toLocaleString('en-US');
