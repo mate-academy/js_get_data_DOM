637d259dd485e713df3f411a4a5cadf2db58f860
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2022-07-27 10:45:49 +0300

    without consols

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,13 @@
 'use strict';
 
-// write your code here
-
 let totalPopulation = 0;
 
 const population = document.querySelectorAll('.population');
 
 population.forEach(country => {
-  console.log(country.innerText.split(',').join(''))
   const normalizeNimber = +country.innerText.split(',').join('')
   totalPopulation += normalizeNimber;
 });
-
-console.log(totalPopulation)
 
 const populationHtml = document.querySelector('.total-population');
 const avarageHtml = document.querySelector('.average-population');
