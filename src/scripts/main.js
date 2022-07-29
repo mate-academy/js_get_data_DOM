3f108cbdbd1cef325245037be9252df7c7e4a077
Yura Atamaniuk <atam3905@gmail.com>
2022-07-29 16:22:55 +0300

    getData

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write your code here
+
+const populationtEl = document.querySelectorAll('.population');
+const totalPopulationEl = document.querySelector('.total-population');
+const avaragePop = document.querySelector('.average-population');
+
+const populations = [...populationtEl]
+  .map((item) => +item.innerText.split(',').join(''));
+const total = populations.reduce((a, b) => a + b, 0);
+const average = total / populations.length;
+
+totalPopulationEl.textContent = total.toLocaleString('en-US');
+avaragePop.textContent = average.toLocaleString('en-US');
