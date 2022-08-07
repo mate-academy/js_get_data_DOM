ef9e514de591baeae78f439fbe3a38cb6b77c6de
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2022-08-07 17:01:49 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write your code here
+const popSpans = document.querySelectorAll('.population');
+const totalSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+// get str inner text; change comma to ''; str convert to num
+const dataNums = [...popSpans].map(span => {
+  return parseFloat(span.innerText.replace(/,/g, ''));
+});
+// calculate the sum of numerical values using the .reduce()
+const totalPopulation = dataNums.reduce((sum, value) => sum + value, 0);
+const totalCommasFormat = thousandsSeparator(totalPopulation);
+const averagePopulation = thousandsSeparator(totalPopulation / dataNums.length);
+
+function thousandsSeparator(num) {
+  return num.toFixed(1).replace(/\d(?=(\d{3})+\.)/g, '$&,').replace('.0', '');
+}
+
+totalSpan.textContent = totalCommasFormat;
+averageSpan.textContent = averagePopulation;
