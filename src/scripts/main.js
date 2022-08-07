8b06437c9e28c7172f1711ed211f18296830fffb
Oksana Shvets <ksenyasvatko1993@gmail.com>
2022-08-07 11:21:35 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write your code here
+
+const matches = document.getElementsByClassName('population');
+const newMatches = Array.from(matches, x =>
+  Number(x.innerHTML.replace(/,/g, '')));
+const total = newMatches.reduce((a, b) => a + b);
+const average = total / newMatches.length;
+const totalV = total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+const averageV = average.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+document.getElementsByClassName('total-population')[0].innerText = totalV;
+document.getElementsByClassName('average-population')[0].innerText = averageV;
