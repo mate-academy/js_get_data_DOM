81d5407fbe4b0866c1a183d168c784edf906a9f6
Yevhen Veselov <veselovyevhen@gmail.com>
2022-08-11 16:09:22 +0300

    1st commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const avaragePopulation = document.querySelector('.average-population');
+
+const arrPopulation = [...population]
+  .map(item => +item.innerHTML
+    .split(',')
+    .join(''));
+const resulTotalPop = arrPopulation.reduce((a, b) => a + b, 0);
+const resultAveragePop = Math.round(resulTotalPop / arrPopulation.length);
+
+totalPopulation.innerHTML = resulTotalPop.toLocaleString('en-US');
+avaragePopulation.innerHTML = resultAveragePop.toLocaleString('en-US');
