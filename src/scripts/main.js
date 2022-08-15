6b1799f02e04c369b2aa9ec61f145a16af29c7a0
Sasha Kolomiiets <kinbean444@gmail.com>
2022-08-15 19:36:50 +0300

    better solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,25 +6,21 @@ const population = document.querySelectorAll('.population');
 const totalPopulation = [...population].map(country => +country.innerHTML
   .replace(/,/g, '')).reduce((a, b) => a + b, 0);
 const averagePopulation = totalPopulation / population.length;
-let totalWithCommas = '';
-let averageWithCommas = '';
 
-for (let i = 0; i < String(totalPopulation).length; i++) {
-  if (i % 3 === 0 && i !== 0) {
-    totalWithCommas += ',';
+function addCommas(number) {
+  let result = '';
+  const num = String(number).split('').reverse().join('');
+
+  for (let i = 0; i < String(number).length; i++) {
+    if (i % 3 === 0 && i !== 0) {
+      result += ',';
+    }
+
+    result += num[i];
   }
 
-  totalWithCommas += String(totalPopulation).split('').reverse().join('')[i];
+  return result;
 }
 
-for (let i = 0; i < String(averagePopulation).length; i++) {
-  if (i % 3 === 0 && i !== 0) {
-    averageWithCommas += ',';
-  }
-
-  averageWithCommas += String(averagePopulation)
-    .split('').reverse().join('')[i];
-}
-
-total.textContent = totalWithCommas.split('').reverse().join('');
-average.textContent = averageWithCommas.split('').reverse().join('');
+total.textContent = addCommas(totalPopulation).split('').reverse().join('');
+average.textContent = addCommas(averagePopulation).split('').reverse().join('');
