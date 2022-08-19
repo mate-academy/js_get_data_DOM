383802f51015dab052436a8b5be52bb5f39f1138
Nazar Kulchevych <gamebandit38@gmail.com>
2022-08-19 11:09:39 +0300

    added default value for reduce & used toLocaleString()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,14 @@
 'use strict';
 
-function normalizeNumber(str, separator) {
-  let normalizeStr = '';
-  const reversedStr = str.split('').reverse().join('');
-
-  for (let i = 0; i < reversedStr.length; i++) {
-    if (i % 3 === 0 && i !== 0) {
-      normalizeStr += separator;
-    }
-    normalizeStr += reversedStr[i];
-  }
-
-  return normalizeStr.split('').reverse().join('');
-}
-
 const countriesPopulation = document.querySelectorAll('.population');
 const total = [...countriesPopulation]
   .map(item => +item.textContent.split(',').join(''))
-  .reduce((a, b) => a + b)
-  .toString();
+  .reduce((a, b) => a + b, 0);
 
-const average = (total / countriesPopulation.length).toString();
+const average = (total / countriesPopulation.length);
 
 document.querySelector('.total-population')
-  .textContent = normalizeNumber(total, ',');
+  .textContent = total.toLocaleString('en-US');
 
 document.querySelector('.average-population')
-  .textContent = normalizeNumber(average, ',');
+  .textContent = average.toLocaleString('en-US');
