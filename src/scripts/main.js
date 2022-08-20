8d695071dbcfb2b250eac10d6ce835b715936489
Dima <dimak119988@gmail.com>
2022-08-20 21:19:46 +0300

    First Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+// const collectionOfSpan = document.querySelectorAll('.population');
+
+const arrayOfSpan
+= [...document.querySelectorAll('.population')].map(span => span.innerText);
+
+const arrayOfNumbers = arrayOfSpan.map(string => {
+  const number = string.split(',').join('');
+
+  if (typeof (+number) === 'number') {
+    return +number;
+  }
+});
+
+const total = arrayOfNumbers.reduce((prev, curret) => prev + curret);
+const average = total / arrayOfNumbers.length;
+
+const totalParagraph = document.querySelector('.total-population');
+const averageParagraph = document.querySelector('.average-population');
+
+totalParagraph.innerText = total.toLocaleString('en-US');
+averageParagraph.innerText = average.toLocaleString('en-US');
