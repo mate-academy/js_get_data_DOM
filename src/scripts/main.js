b9462319af8a2671cb21f33a89a37ab8dc7f0b1b
Yurii Kozlov <yurka22870@gmail.com>
2022-08-21 13:11:20 +0300

    largest countries statistics

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const populationField = document.querySelectorAll('.population');
+
+const totalField = document.querySelector('.total-population');
+const averageField = document.querySelector('.average-population');
+
+const numbers = [...populationField]
+  .map(number => +number.textContent.replaceAll(',', ''));
+
+const totalAmount = numbers.reduce((sum, number) => sum + number);
+const averageAmount = totalAmount / numbers.length;
+
+totalField.textContent = totalAmount.toLocaleString('en');
+averageField.textContent = averageAmount.toLocaleString('en');
