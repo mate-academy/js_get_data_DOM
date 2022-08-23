10553db224fa30485c9ede5b35153af1256d8102
Bohdan Tron <ikwiczzts@gmail.com>
2022-08-23 14:36:45 +0300

    Parsed and calculated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+let sum = 0;
+let count = 0;
+
+document.querySelectorAll('.population').forEach((item) => {
+  sum += parseInt(item.innerHTML.split(',').join(''));
+  count++;
+});
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+document.querySelector('.total-population').innerHTML = numberWithCommas(sum);
+
+document
+  .querySelector('.average-population')
+  .innerHTML = numberWithCommas(sum / count);
