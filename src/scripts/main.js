e1463c86b2e74dfe1f109261c48b156f7e261294
Elina Bahirova <eli4ka.bagirova@gmail.com>
2022-08-24 12:16:25 +0300

    Task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
 // write your code here
+
+let populations = document.querySelectorAll('span.population');
+
+populations
+  = [...populations].map(
+    population => +population.innerText.split(',')
+      .join(''));
+
+const total = populations.reduce((sum, n) => sum + n, 0);
+
+const average = total / populations.length;
+
+const averagePopulation = document.querySelector('.average-population');
+const totalPopulation = document.querySelector('.total-population');
+
+function decimalFormat(number) {
+  let n = number;
+  const result = [];
+
+  do {
+    const part = Math.trunc(n / 1000);
+
+    result.push(n - part * 1000);
+    n = part;
+  } while (n > 1000);
+
+  result.push(n);
+
+  return result.reverse().join(',');
+}
+
+totalPopulation.innerHTML = decimalFormat(total);
+averagePopulation.innerHTML = decimalFormat(average);
