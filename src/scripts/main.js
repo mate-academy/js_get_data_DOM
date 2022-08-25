eea1f3c01294521d2196e08b6ac091d7c1d7c1b2
Dmytro Matsehora <ninjawithoutface@gmail.com>
2022-08-25 13:25:22 +0300

    code style fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const arrSelectors = [...document.querySelectorAll('.population')];
-const populations = arrSelectors.map(el => +el.outerText.split(',').join(''));
+const populations = arrSelectors.map(el => (
+  +el.outerText
+    .split(',')
+    .join('')));
 
 const totalPopulations = populations.reduce((prev, current) => prev + current);
 const averagePopulations = totalPopulations / populations.length;
@@ -10,7 +13,11 @@ const resultTotal = document.getElementsByClassName('total-population');
 const resultAverage = document.getElementsByClassName('average-population');
 
 resultTotal[0].outerText = totalPopulations
-  .toLocaleString('de-DE').split('.').join(',');
+  .toLocaleString('de-DE')
+  .split('.')
+  .join(',');
 
 resultAverage[0].outerText = averagePopulations
-  .toLocaleString('de-DE').split('.').join(',');
+  .toLocaleString('de-DE')
+  .split('.')
+  .join(',');
