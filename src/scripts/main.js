907c41f3feadb4110f3a5f8343f0a5264b9f7de6
Shahmatov David <davidsahmatov4@gmail.com>
2022-08-26 14:14:24 +0300

    Version 0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const populationList = document.querySelectorAll('.population');
+
+const total = [...populationList]
+  .map(element => Number(element.textContent.split(',').join('')))
+  .reduce((sum, current) => sum += current);
+
+const average = total / populationList.length;
+
+document.querySelector('.total-population')
+  .textContent = total.toLocaleString('en');
+
+document.querySelector('.average-population')
+  .textContent = average.toLocaleString('en');
