59cd6f1e5845206b1cac11d8ce42e246e4087153
Andrii Stiebalov <stiebalov@gmail.com>
2022-08-28 13:21:51 +0300

    added population calculation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const arrOfPopulations = [ ...populations ];
+
+const sumPopulations = arrOfPopulations
+  .reduce((prev, elem) => prev + +elem.innerText.split(',').join(''), 0);
+
+const totalPopulation = document.querySelector('.total-population');
+
+totalPopulation.innerHTML = thousandsSeparate(sumPopulations);
+
+const averagePopulation = document.querySelector('.average-population');
+const avergeSum = sumPopulations / arrOfPopulations.length;
+
+averagePopulation.innerHTML = thousandsSeparate(avergeSum);
+
+function thousandsSeparate(num) {
+  return num.toString()
+    .split('')
+    .reverse()
+    .map((char, i) => char + (i % 3 ? '' : ','))
+    .reverse()
+    .join('');
+}
