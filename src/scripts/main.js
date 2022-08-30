1ca3ed98414490d772a210be72ef19eef8d9c430
Sheremet Oleksandra <sandasheremet@gmail.com>
2022-08-30 16:24:35 +0300

    Solution the first attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+const populations = document.querySelectorAll('span.population');
+const numbersArray = [...populations].map(item => item.innerText);
+let total = 0;
+
+for (const number of numbersArray) {
+  const temp = number.innerHTML.split(',').join('');
+
+  total += +temp;
+}
+
+const average = total / numbersArray.length;
+
+const textTotalPopulation = document.querySelectorAll('total-population');
+
+textTotalPopulation.textContent = total;
+
+const textAveragePopulation = document.querySelectorAll('average-population');
+
+textAveragePopulation.textContent = average;
