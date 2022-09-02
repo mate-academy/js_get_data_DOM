3f93db0cd10a6cb153cf5b73ab0d5c68209c4268
Mykhailo Oblat <moblat890@gmail.com>
2022-09-02 15:49:57 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const arrayOfPopulation = [...populations].map(item =>
+  +item.innerText.split(',').join(''));
+
+const sumPopulations = arrayOfPopulation.reduce((a, b) => a + b, 0);
+const averagePopulations = sumPopulations / arrayOfPopulation.length;
+
+function separator(number) {
+  const res = String(number);
+  let str = '';
+  let count = 0;
+
+  for (let i = res.length - 1; i >= 0; i--) {
+    str += res[i];
+
+    count++;
+
+    if (count % 3 === 0 & i !== 0) {
+      str += ',';
+    }
+  };
+
+  return str.split('').reverse().join('');
+};
+
+const resultSumPopulations = separator(sumPopulations);
+const resultAveragePopulations = separator(averagePopulations);
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.textContent = resultSumPopulations;
+averagePopulation.textContent = resultAveragePopulations;
