0ff1ac6060edf72d4ff2184bea4d22b14d2043b0
p-omogit-e <polinakotyrlo@gmail.com>
2022-09-04 17:59:25 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const container = document.querySelectorAll('span.population');
+const numbs = [...container].map(item => +(item.innerText.split(',').join('')));
+
+const sum = numbs.reduce((x, y) => {
+  return x + y;
+});
+
+const avg = Math.round(sum / numbs.length);
+const sumEl = document.querySelector('span.total-population');
+const avgEl = document.querySelector('span.average-population');
+
+sumEl.textContent = `${sum}`;
+avgEl.textContent = `${avg}`;
