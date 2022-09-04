bcc74cb3817b77c47c5c12c2e51ab1269e2f29dc
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-09-04 17:42:49 +0800

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const allData = document.querySelectorAll('span.population');
+const numbers = [...allData].map(elem => {
+  return +elem.innerText.split(',').join('');
+});
+const total = numbers.reduce((acc, next) => {
+  return acc + next;
+}, 0);
+const average = total / numbers.length;
+
+const totalHtml = document.querySelector('span.total-population');
+const averageHtml = document.querySelector('span.average-population');
+
+totalHtml.innerHTML = total.toLocaleString('en-US');
+averageHtml.innerHTML = average.toLocaleString('en-US');
