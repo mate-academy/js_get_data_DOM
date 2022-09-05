92b9db9b20aa37d1302e60a646325171cf41f204
Dima Korol <korovaidima@gmail.com>
2022-09-05 10:28:02 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const popul = document.querySelectorAll('.population');
+
+const dates = [...popul].map(date => date.innerText);
+
+const numbersOfDates = dates.map(elem => parseFloat(elem.replace(/,/g, '')));
+
+const total = numbersOfDates.reduce((a, b) => a + b);
+const averageAges = total / numbersOfDates.length;
+
+const totalAgesInWeb = document.querySelector('.total-population');
+const averagelAgesInWeb = document.querySelector('.average-population');
+
+totalAgesInWeb.innerText = total.toLocaleString('en-US');
+averagelAgesInWeb.innerText = averageAges.toLocaleString('en-US');
