68334f735568bd16f68522a084ade54dac3491b1
fenderukr <priststoreua@gmail.com>
2022-09-06 13:22:07 +0300

    top10countries0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write your code here
+const elements = document.querySelectorAll('.population');
+const arr = [];
+
+for (const elem of elements) {
+  const num = (Number(elem.innerHTML.split(',').join('')));
+
+  arr.push(num);
+}
+
+const totalValue = arr.reduce((accum, current) =>
+  accum + current).toLocaleString();
+
+const average = arr.reduce((a, b) => (a + b) / arr.length).toLocaleString();
+
+const tPopulationClass = document.querySelector('.total-population');
+
+tPopulationClass.textContent = totalValue;
+
+const averagePopulationClass = document.querySelector('.average-population');
+
+averagePopulationClass.textContent = average;
