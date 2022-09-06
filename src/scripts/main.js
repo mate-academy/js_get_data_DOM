4d7f14bddd1a6a098e7a900d55cb3254b10bd9d2
Misha Kostov <kostov.mihail321@gmail.com>
2022-09-06 15:04:11 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const separator = new Intl.NumberFormat('en-US');
+let total = 0;
+
+for (let i = 0; i < list.length; i++) {
+  total += Number(list[i].innerHTML.split(',').join(''));
+}
+
+totalPopulation.innerHTML = String(separator.format(total));
+averagePopulation.innerHTML = String(separator.format(total / list.length));
