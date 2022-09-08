4b215b13a99c4dbcae39838019369cb052c8d61c
Oleksii <oleksii@it.com>
2022-09-08 12:51:36 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const text = [...population].map(data =>
+  parseInt(data.textContent
+    .split(',')
+    .join('')));
+
+const total = text.reduce((prev, sum) => prev + sum, 0);
+
+const average = total / text.length;
+
+const totalResult = document.querySelector('.total-population');
+
+const averageResult = document.getElementsByClassName('.average-population');
+
+totalResult.textContent = total.toLocaleString('en-Us');
+
+averageResult.textContent = average.toLocaleString('en-Us');
