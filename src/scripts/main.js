a56927e1b0cc544724fe32913b0d2c4d099048ac
Yevheniia Danylchuk <shenijad1@gmail.com>
2022-09-10 15:28:58 +0200

    canculated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write your code here
+const items = document.querySelectorAll('span');
+
+const populationArray = [...items].map(item => item.innerText);
+
+populationArray.length = populationArray.length - 2;
+
+for (const num in populationArray) {
+  populationArray[num] = +populationArray[num].replaceAll(',', '');
+}
+
+const sum = populationArray.reduce((prev, current) => prev + current, 0);
+const formatedSum = sum.toLocaleString('en-US');
+
+const average = sum / populationArray.length;
+const formatedAverage = average.toLocaleString('en-US');
+
+const sumValue = document.querySelector('.total-population');
+
+sumValue.textContent = formatedSum;
+
+const averageValue = document.querySelector('.average-population');
+
+averageValue.textContent = formatedAverage;
