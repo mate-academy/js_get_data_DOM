1bd9d100feee555e2ff26633fd7da999ccf16d81
Oleh Savchuk <sav4uk.oleg@gmail.com>
2022-09-13 23:54:39 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,55 +1,16 @@
 'use strict';
 
-function transformNumber(n) {
-  let bilions = Math.floor(n / 1000000000);
-  let milions = Math.floor((n - bilions * 1000000000) / 1000000);
-  let thousands
-    = Math.floor((n - bilions * 1000000000 - milions * 1000000) / 1000);
-  let hundreds
-    = n - bilions * 1000000000 - milions * 1000000 - thousands * 1000;
+const populations = document.querySelectorAll('.population');
 
-  if (bilions >= 1) {
-    bilions += ',';
-  } else {
-    bilions = '';
-  }
+const numbers = [...populations].map(span =>
+  +span.innerText.split(',').join(''));
 
-  if (milions >= 1) {
-    milions += ',';
-  } else {
-    milions = '';
-  }
-
-  if (thousands >= 1) {
-    thousands += ',';
-  } else {
-    thousands = '';
-  }
-
-  if (hundreds < 10) {
-    hundreds = '00' + hundreds;
-  }
-
-  if (hundreds < 100) {
-    hundreds = '0' + hundreds;
-  }
-
-  return `${bilions}${milions}${thousands}${hundreds}`;
-}
-
-const spans = document.querySelectorAll('.population');
-
-const numbers = [...spans].map(span => span.innerText);
-
-let total = 0;
-
-for (const number of numbers) {
-  total += +number.split(',').join('');
-};
+const total = numbers.reduce((a, b) => a + b);
 
 const average = total / numbers.length;
 
-document.querySelector('.total-population').innerText = transformNumber(total);
+document.querySelector('.total-population').innerText 
+  = new Intl.NumberFormat('en-GB').format(total);
 
 document.querySelector('.average-population').innerText
-    = transformNumber(average);
+    = new Intl.NumberFormat('en-GB').format(average);
