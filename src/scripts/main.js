57c2c532d914b2e0cbeb7d15f38d0c4cd710d15f
Anastasiia Mahalias <anastasiabatychenko@gmail.com>
2022-09-13 10:01:00 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const numbers = document.querySelectorAll('.population');
+let totalNumber = 0;
+
+for (const number of numbers) {
+  const correctNumber = +(number.innerHTML.split('')
+    .filter(item => item !== ',').join(''));
+
+  totalNumber += correctNumber;
+}
+
+const averageNumber = totalNumber / numbers.length;
+
+document.querySelector('.total-population').innerHTML = totalNumber
+  .toLocaleString('en-US');
+
+document.querySelector('.average-population').innerHTML = averageNumber
+  .toLocaleString('en-US');
