48be0a770266b941d999aeaabb459743f4255a55
Volodymir Tymtsias <vovanuch2007@gmail.com>
2022-09-14 14:39:03 +0300

    Solution get data DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const population = [...document.body.querySelectorAll('.population')]
+  .map(item => +item.textContent.replace(/,/g, ''));
+const totalPopulation = population.reduce((sum, current) => sum + current, 0);
+const averagePopulation = totalPopulation / population.length;
+
+document.body.querySelector('.total-population').textContent
+  = getThousandsSeparator(totalPopulation, ',');
+
+document.body.querySelector('.average-population').textContent
+  = getThousandsSeparator(averagePopulation, ',');
+
+function getThousandsSeparator(number, separator) {
+  let numberString = number.toString();
+  const thousands = [];
+
+  while (numberString.length > 0) {
+    thousands.unshift(numberString.slice(-3));
+    numberString = numberString.slice(0, -3);
+  }
+
+  return thousands.join(separator);
+}
