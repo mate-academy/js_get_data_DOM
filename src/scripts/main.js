f6224c6b4f3b0ccdde5a17507fda9442b2f82959
Paul Voronin <voron.paul@gmail.com>
2022-09-14 23:04:23 +0300

    ver.1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const nation = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+let total = 0;
+let count = 0;
+
+for (const population of nation) {
+  total += +population.textContent.replace(/,/g, '');
+  count++;
+}
+
+totalPopulation.textContent = total.toLocaleString('en-ES');
+averagePopulation.textContent = (total / count).toLocaleString('en-ES');
