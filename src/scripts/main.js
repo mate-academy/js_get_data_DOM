60a00e4843b5ea81f37dd5b9c5bbe5725b68e870
Volodymir Tymtsias <vovanuch2007@gmail.com>
2022-09-15 16:15:06 +0300

    used toLocaleString

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,19 +6,7 @@ const totalPopulation = population.reduce((sum, current) => sum + current, 0);
 const averagePopulation = totalPopulation / population.length;
 
 document.body.querySelector('.total-population').textContent
-  = getThousandsSeparator(totalPopulation, ',');
+  = totalPopulation.toLocaleString('en-US');
 
 document.body.querySelector('.average-population').textContent
-  = getThousandsSeparator(averagePopulation, ',');
-
-function getThousandsSeparator(number, separator) {
-  let numberString = number.toString();
-  const thousands = [];
-
-  while (numberString.length > 0) {
-    thousands.unshift(numberString.slice(-3));
-    numberString = numberString.slice(0, -3);
-  }
-
-  return thousands.join(separator);
-}
+  = averagePopulation.toLocaleString('en-US');
