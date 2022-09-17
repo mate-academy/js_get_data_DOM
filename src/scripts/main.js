6c9f6f5e304b037ad284b67552e045f7b708f19a
Max Bovtun <maxbovtun@gmail.com>
2022-09-17 18:46:33 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write your code here
+const population = document.querySelectorAll('span.population');
+const totalElement = document.querySelector('span.total-population');
+const averageElement = document.querySelector('span.average-population');
+const populationNums = [];
+
+for (const populationElement of population) {
+  populationNums.push(+populationElement.innerText.split(',').join(''));
+}
+
+const total = populationNums.reduce((a, b) => a + b);
+const average = total / populationNums.length;
+
+totalElement.innerText = total.toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+averageElement.innerText = average.toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
