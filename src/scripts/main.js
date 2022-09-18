7f6a6fcbf82b841d8b6becb8afe5b2c1f9b99c64
Paul Voronin <voron.paul@gmail.com>
2022-09-18 17:03:43 +0300

    ver.2.0-FINAL

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,12 @@
 const nation = document.querySelectorAll('.population');
 const totalPopulation = document.querySelector('.total-population');
 const averagePopulation = document.querySelector('.average-population');
-let total = 0;
 
 const newNation = Array.from(nation, x =>
   Number(x.innerHTML.replace(/,/g, '')));
 
-total = newNation.reduce((sum, x) => sum + x);
+const total = newNation.reduce((sum, x) => sum + x);
+
 totalPopulation.textContent = total.toLocaleString('en-ES');
 
 averagePopulation.textContent = (total / newNation.length)
