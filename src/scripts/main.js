02713ae42e21c04320f5898600026d000f99acea
grafit-off <graf.off.it@gmail.com>
2022-09-20 13:44:50 +0300

    feat: 10 largest countries task was completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write your code here
+const populationArr = [...document.querySelectorAll('.population')];
+const population = populationArr.map(el => +el.textContent.replace(/,/g, ''));
+
+const totalPopulation = population.reduce((acc, cur) => acc + cur, 0);
+const averagePopulation = totalPopulation / population.length;
+
+const totalSelector = document.querySelector('.total-population');
+const avarageSelector = document.querySelector('.average-population');
+
+const formatNum = num => num.toLocaleString('en');
+
+totalSelector.textContent = formatNum(totalPopulation);
+avarageSelector.textContent = formatNum(averagePopulation);
