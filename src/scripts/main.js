139e42aadc334379658099d32d6ed74e22a8882e
Max Bovtun <maxbovtun@gmail.com>
2022-09-20 11:29:28 +0300

    fixed after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
 // write your code here
-const population = document.querySelectorAll('span.population');
-const totalElement = document.querySelector('span.total-population');
-const averageElement = document.querySelector('span.average-population');
+const population = document.querySelectorAll('.population');
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
 const populationNums = [...population]
   .map(populationElement => (
     Number(populationElement.innerText.split(',').join(''))
   ));
 
-const total = populationNums.reduce((prev, curr) => prev + curr);
+const total = populationNums.reduce((acc, curr) => acc + curr);
 const average = total / populationNums.length;
 
 totalElement.innerText = total.toString()
