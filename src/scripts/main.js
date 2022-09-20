6b73486ec59f1ab8e18f88b6030f4a56195369d2
grafit-off <graf.off.it@gmail.com>
2022-09-20 19:20:27 +0300

    fix: replaced variables names to more clear

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,16 @@
 'use strict';
 
 // write your code here
-const populationArr = [...document.querySelectorAll('.population')];
-const population = populationArr.map(el => +el.textContent.replace(/,/g, ''));
+const populationNodes = [...document.querySelectorAll('.population')];
+const population = populationNodes.map(el => +el.textContent.replace(/,/g, ''));
 
 const totalPopulation = population.reduce((acc, cur) => acc + cur, 0);
 const averagePopulation = totalPopulation / population.length;
 
-const totalSelector = document.querySelector('.total-population');
-const avarageSelector = document.querySelector('.average-population');
+const totalNode = document.querySelector('.total-population');
+const avarageNode = document.querySelector('.average-population');
 
 const formatNum = num => num.toLocaleString('en');
 
-totalSelector.textContent = formatNum(totalPopulation);
-avarageSelector.textContent = formatNum(averagePopulation);
+totalNode.textContent = formatNum(totalPopulation);
+avarageNode.textContent = formatNum(averagePopulation);
