846a7ba2ab20042182a356f7104e848a7d03d1e1
Vitalii Derlemenko <vitaliidox@gmail.com>
2022-09-25 20:26:52 +0300

    Refactor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,16 +4,11 @@ const list = document.querySelectorAll('.population');
 const items = [...list].map(el => +el.innerText.split(',').join(''));
 
 const total = items.reduce((prev, a) => prev + a, 0);
-const average = total / items.length;
-
-const totalArray = total.toString().split('');
-const averageArray = average.toString().split('');
-
-const totalSeparated = (+totalArray.join('')).toLocaleString();
-const averageSeparated = (+averageArray.join('')).toLocaleString();
+const totalString = total.toLocaleString();
+const average = (total / items.length).toLocaleString();
 
 document.querySelector('.total-population').textContent
-= totalSeparated;
+= totalString;
 
 document.querySelector('.average-population').textContent
-= averageSeparated;
+= average;
