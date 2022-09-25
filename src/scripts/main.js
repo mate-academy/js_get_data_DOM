9234e0176699f9df18c43407181acef7aefce96c
Vitalii Derlemenko <vitaliidox@gmail.com>
2022-09-25 19:20:55 +0300

    Replace "Calculate it!" with total and average numbers of population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('.population');
+const items = [...list].map(el => +el.innerText.split(',').join(''));
+
+const total = items.reduce((prev, a) => prev + a, 0);
+const average = total / items.length;
+
+const totalArray = total.toString().split('');
+const averageArray = average.toString().split('');
+
+const totalSeparated = (+totalArray.join('')).toLocaleString();
+const averageSeparated = (+averageArray.join('')).toLocaleString();
+
+document.querySelector('.total-population').textContent
+= totalSeparated;
+
+document.querySelector('.average-population').textContent
+= averageSeparated;
