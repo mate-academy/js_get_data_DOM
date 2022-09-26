561035e9e4ea842f0ee34b0e62c0086014930c51
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-26 19:23:06 +0300

    average and total populations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populations = [...document.querySelectorAll('.population')];
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const populationNumbers = populations
+  .map(country => +country.innerHTML.split(',').join(''));
+const populationTotal = populationNumbers
+  .reduce((acc, curr) => acc + curr, 0);
+const populationAverage = populationTotal / populationNumbers.length;
+
+totalPopulation.innerText = separate(populationTotal);
+averagePopulation.innerText = separate(populationAverage);
+
+function separate(number) {
+  return number.toLocaleString('en');
+}
