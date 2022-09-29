b0c83bdbb34166da7c95eda5af9175476261a3af
Killdex <kdcompany2018@gmail.com>
2022-09-29 19:58:49 +0300

    top list created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const populationValue = [...populations].map(item => (
+  +item.textContent.replace(/,/g, '')
+));
+
+const total = populationValue.reduce((acc, prev) => acc + prev, 0);
+const average = total / populationValue.length;
+
+const separate = value => value.toLocaleString('en');
+
+totalPopulation.innerText = separate(total);
+averagePopulation.innerText = separate(average);
