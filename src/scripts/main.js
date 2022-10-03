d7ec441e1c2764f81d2b71498d6bb2fa2e6fa245
Andriy Hladkevych <andriyglad@gmail.com>
2022-10-03 16:18:06 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')];
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+const countries = document.querySelectorAll('.list__item');
+
+const totalPopulation = population
+  .map(country => +country.innerHTML.split(',').join(''))
+  .reduce((a, b) => a + b, 0);
+
+const averagePopulation = totalPopulation / countries.length;
+
+function numToValidString(num) {
+  const sep = (str) => {
+    return str.length
+      ? [str.slice(0, 3), ...sep(str.slice(3))]
+      : [];
+  };
+
+  const validNum = sep(String(num)
+    .split('')
+    .reverse()
+    .join(''))
+    .reverse()
+    .join(',');
+
+  return validNum;
+}
+
+total.innerHTML = numToValidString(totalPopulation);
+average.innerHTML = numToValidString(averagePopulation);
