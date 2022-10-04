6ec03b1ed6856430c82a55c467e63ae41e874316
Danil <dasha8854567@gmail.com>
2022-10-04 22:15:18 +0300

    commit1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const populations = [...document.querySelectorAll('.population')]
+  .map(x => +x.innerText.split(',').join(''));
+const averagePopulation = (populations.reduce((sum, x) => sum + x, 0)
+/ populations.length);
+const totalPopulation = populations.reduce((sum, x) => sum + x, 0);
+
+document.querySelector('.total-population').innerText
+  = totalPopulation.toLocaleString();
+
+document.querySelector('.average-population').innerText
+  = averagePopulation.toLocaleString();
