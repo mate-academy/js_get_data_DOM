9d9a0b4e8673ee9146d8b7af2d5a689bc9a985dd
Maksym Storozhuk <hitthemax111@gmail.com>
2022-10-08 20:58:36 +0300

    Solution top Js_get_data_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write your code here
+const span = document.querySelectorAll('.population');
+
+const newFormat = new Intl.NumberFormat('en-Us');
+
+const ourList = [...span].map(item => +(item.innerText.split(',').join('')));
+
+const totalPopulation = ourList.reduce((item, sum) => sum + item, 0);
+
+const totalSpan = document.querySelector('.total-population');
+const avarageSpan = document.querySelector('.average-population');
+
+totalSpan.textContent = newFormat.format(totalPopulation);
+avarageSpan.textContent = newFormat.format(totalPopulation / ourList.length);
