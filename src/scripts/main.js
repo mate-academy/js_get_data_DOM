d4258412628f9223d757ab341b5e9adc121d25a5
Oleksandr <Khabailiuk@gmail.com>
2022-10-08 13:37:38 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const numbers = [].map.call(document.querySelectorAll('.population'),
+  function(el) {
+    return el.innerHTML;
+  });
+
+const noComa = numbers.map(function minusOne(el) {
+  return el.replaceAll(',', '');
+});
+
+const arrOfNum = noComa.map(str => {
+  return Number(str);
+});
+
+const sum = arrOfNum.reduce((accumulator, value) => {
+  return accumulator + value;
+}, 0);
+
+const average = sum / arrOfNum.length;
+
+document.querySelector('.total-population').innerHTML
+ = `${sum.toLocaleString('en-US')}`;
+
+document.querySelector('.average-population').innerHTML
+ = `${average.toLocaleString('en-US')}`;
