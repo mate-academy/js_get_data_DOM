9fb03e3936bfb23973bcf30e606352abdfaf2b6f
Dima <ddikey1@gmail.com>
2022-10-10 01:43:13 +0300

    Solution of 'Get Data DOM' task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
-
 // write your code here
+
+const populations = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const totalAverage = document.querySelector('.average-population');
+const populationSum = [...populations]
+  .map(item => item.innerText.split(',').join(''))
+  .reduce((sum, currentPopulation) => (
+    (+sum) + (+currentPopulation)
+  ), 0);
+const averagePopulation = populationSum / populations.length;
+
+totalPopulation.innerHTML = `${populationSum.toLocaleString('en-GB')}`;
+
+totalAverage.innerHTML = `${averagePopulation.toLocaleString('en-GB')}`;
