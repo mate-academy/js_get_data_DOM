c4802b226db67e950de32b1fbf90024c3fc67b1b
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-10-13 17:18:49 +0300

    Calculate everage and total data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const populationData = document.querySelectorAll('.population');
+const populations = [...populationData].map(el => (
+  +el.innerText.split(',').join('')
+));
+
+const total = populations.reduce((prev, curr) => prev + curr, 0);
+const totalData = splitWithCommas(total);
+
+const everage = total / populations.length;
+const everageData = splitWithCommas(everage);
+
+document.querySelector('.total-population').innerText = totalData;
+document.querySelector('.average-population').innerText = everageData;
+
+function splitWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
