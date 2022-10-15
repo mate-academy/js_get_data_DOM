03b5f102b5029f086cf6d8d852a40021016430b0
Maksym Storozhuk <hitthemax111@gmail.com>
2022-10-15 13:40:05 +0300

    Requested changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,16 @@ const span = document.querySelectorAll('.population');
 
 const newFormat = new Intl.NumberFormat('en-Us');
 
-const ourList = [...span].map(item => +(item.innerText.split(',').join('')));
+const countryList = [...span]
+  .map(item => +(item.innerText.split(',').join('')));
 
-const totalPopulation = ourList.reduce((item, sum) => sum + item, 0);
+const totalPopulation = countryList
+  .reduce((sum, population) => sum + population, 0);
 
 const totalSpan = document.querySelector('.total-population');
 const avarageSpan = document.querySelector('.average-population');
 
 totalSpan.textContent = newFormat.format(totalPopulation);
-avarageSpan.textContent = newFormat.format(totalPopulation / ourList.length);
+
+avarageSpan.textContent = newFormat
+  .format(totalPopulation / countryList.length);
