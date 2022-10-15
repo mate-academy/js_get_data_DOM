0e92b7e8ffe6ffac7982ccc0c3742f17849d89a1
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-10-15 16:35:44 +0300

    Fix spelling errors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,11 +8,11 @@ const populations = [...populationData].map(el => (
 const total = populations.reduce((prev, curr) => prev + curr, 0);
 const totalData = splitWithCommas(total);
 
-const everage = total / populations.length;
-const everageData = splitWithCommas(everage);
+const average = total / populations.length;
+const averageData = splitWithCommas(average);
 
 document.querySelector('.total-population').innerText = totalData;
-document.querySelector('.average-population').innerText = everageData;
+document.querySelector('.average-population').innerText = averageData;
 
 function splitWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
