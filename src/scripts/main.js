ad5732225ec2aea396802a87e39938c6f44c0f7a
Nazar Vorona <voron1765@gmail.com>
2022-10-17 11:21:00 +0300

    Need to do thousands

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const popu = document.querySelectorAll('.population');
+const arrPopu = [...popu];
+let total = 0;
+
+for (const chap of arrPopu) {
+  const country = chap.textContent.split('');
+
+  let temporaryStr = '';
+
+  for (const numb of country) {
+    if (numb !== ',') {
+      temporaryStr += numb;
+    }
+  }
+  total += +temporaryStr;
+}
+
+const average = total / arrPopu.length;
+const addTotal = document.querySelector('.total-population');
+
+addTotal.innerHTML = `${total}`;
+
+const addAverage = document.querySelector('.average-population');
+
+addAverage.innerHTML = `${average}`;
