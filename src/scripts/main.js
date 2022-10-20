0e8ba735dc56277e6f3401c9c3980692a5f9130e
Mykola Yakymiv <mykola8426@gmail.com>
2022-10-20 22:41:11 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const spanWithPopulation = document.querySelectorAll('.population');
+
+let totalPopulation = 0;
+let averagePopulation = 0;
+const countOfCountries = [...spanWithPopulation].length;
+
+for (let i = 0; i < countOfCountries; i++) {
+    totalPopulation+= +spanWithPopulation[i].childNodes[0].textContent.replaceAll(',', '')
+}
+
+averagePopulation = totalPopulation / countOfCountries;
+document.querySelector('.average-population').innerHTML = `${averagePopulation.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`
+document.querySelector('.total-population').innerHTML = `${totalPopulation.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`
