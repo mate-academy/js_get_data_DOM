5241352b77673e6101bd15baa997ffea7647081c
Yana <yanamelnychuk10@gmail.com>
2022-10-20 13:21:34 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const populationsArr = [...document.querySelectorAll('.population')];
+
+const sumPopulations = populationsArr
+  .reduce((prev, elem) => prev + +elem.innerText.split(',').join(''), 0);
+
+document.querySelector('.total-population')
+  .innerHTML = thousandsSeparate(sumPopulations);
+
+const avergePopulation = sumPopulations / populationsArr.length;
+
+document.querySelector('.average-population')
+  .innerHTML = thousandsSeparate(avergePopulation);
+
+function thousandsSeparate(num) {
+  return num.toString()
+    .split('')
+    .reverse()
+    .map((char, i) => char + (i % 3 === 0 && i !== 0 ? ',' : ''))
+    .reverse()
+    .join('');
+}
