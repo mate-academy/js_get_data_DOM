ed919b80d00b1dd4bf103444fafb90e5a8b4a914
Anastasia Babko <hr.anastasia.t@gmail.com>
2022-10-21 18:51:26 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+let total = 0;
+let populationCount = 0;
+
+population.forEach(country => {
+  const normalizedPopulationNum = +country.innerText.split(',').join('');
+
+  total += normalizedPopulationNum;
+  populationCount++;
+});
+
+const totalOnPage = document.querySelector('.total-population');
+const averageOnPage = document.querySelector('.average-population');
+
+totalOnPage.innerText = total.toLocaleString('en-US');
+averageOnPage.innerText = (total / populationCount).toLocaleString('en-US');
