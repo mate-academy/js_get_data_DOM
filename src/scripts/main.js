43655622602bd2426ad6b2d0a43c3d47b3d49419
John Rust <johnrust12@gmail.com>
2022-10-22 14:18:42 +0300

    Solution6

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,36 +12,3 @@ document.querySelector('.total-population')
 
 document.querySelector('.average-population')
   .innerHTML = averagePop.toLocaleString('en-US');
-
-// const totalPopArr = String(totalPop).split('').reverse();
-// let totalPopComas = [];
-// const averagePopArr = String(averagePop).split('').reverse();
-// let averagePopComas = [];
-
-// for (let n = 0; n < totalPopArr.length; n++) {
-//   totalPopComas.push(totalPopArr[n]);
-
-//   if ((n + 1) % 3 === 0 && n !== totalPopArr.length - 1) {
-//     totalPopComas.push(',');
-//   }
-// };
-
-// for (let i = 0; i < averagePopArr.length; i++) {
-//   averagePopComas.push(averagePopArr[i]);
-
-//   if ((i + 1) % 3 === 0 && i !== averagePopArr.length - 1) {
-//     averagePopComas.push(',');
-//   }
-// };
-
-// totalPopComas = totalPopComas.reverse().join('');
-
-// const elementTotal = document.querySelector('.total-population');
-
-// elementTotal.innerText = totalPopComas;
-
-// averagePopComas = averagePopComas.reverse().join('');
-
-// const elementAverage = document.querySelector('.average-population');
-
-// elementAverage.innerText = averagePopComas;
