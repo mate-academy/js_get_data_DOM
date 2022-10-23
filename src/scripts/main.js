f95d327bd5b59280bb8112a423076f4f383561c2
Valentyn Ivanenko <Ivanenko1402@gmail.com>
2022-10-23 17:58:38 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const numbers = [...document.querySelectorAll('.population')];
+const arrs = numbers.map(item => +item.textContent.split(',').join(''));
+const total = arrs.reduce((a, b) => a + b);
+const average = total / numbers.length;
+
+document.querySelector(
+  '.total-population').innerHTML = total.toLocaleString('en-US');
+
+document.querySelector(
+  '.average-population').innerHTML = average.toLocaleString('en-US');
