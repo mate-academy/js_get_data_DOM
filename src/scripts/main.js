74035b60a988b4868558e489aba85e1289baf7dd
Oleh Bazyla <olegbazila@gmail.com>
2022-10-23 21:02:10 +0300

    10 largest countries task was completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const populationList = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const totalAmount = [...populationList].reduce((acc, curr) => (
+  acc + parseInt(curr.innerText.replace(/,/g, ''))
+), 0);
+
+const average = totalAmount / populationList.length;
+
+totalPopulation.innerText = totalAmount.toLocaleString('en');
+averagePopulation.innerText = average.toLocaleString('en');
