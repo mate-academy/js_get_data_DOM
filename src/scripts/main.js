1fbc14840b3c43d2592c86e0fab944cab1a3fc9e
Yurii Teslia <teslya.yuriy@gmail.com>
2022-10-24 18:20:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const doc = document.querySelectorAll('.population')
+
+const sum = [...doc].map(x => +x.innerHTML.replaceAll(',', '')).reduce((a, b) => (a + b), 0);
+const avg = sum / doc.length;
+
+const ttl = document.getElementsByClassName('total-population')[0];
+const average = document.getElementsByClassName('average-population')[0];
+
+ttl.innerHTML = sum.toLocaleString('en-US');
+average.innerHTML = avg.toLocaleString('en-US');
