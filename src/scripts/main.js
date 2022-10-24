2ce5ab3cd6e53d4fcfc1bc81a77b4d36cd2529d1
Mykyta Krainik <nikinta2003@gmail.com>
2022-10-24 22:12:39 +0300

    Total and average populations were calculated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const populations = [...document.querySelectorAll('.population')]
+  .map(population => {
+    const parsedPopulation = parseInt(population
+      .textContent
+      .replaceAll(',', '')
+    );
+
+    if (!isFinite(parsedPopulation)) {
+      return 0;
+    }
+
+    return parsedPopulation;
+  });
+
+const populationSum = populations.reduce((sum, population) => {
+  return sum + population;
+}, 0);
+
+const totalPopulation = document.querySelector('.total-population');
+
+totalPopulation.textContent = populationSum.toLocaleString();
+
+const averagePopulation = document.querySelector('.average-population');
+
+averagePopulation.textContent = (populationSum / populations.length)
+  .toLocaleString();
