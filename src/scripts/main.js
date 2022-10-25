aad9c1d89f901602da147c4b0a79856e6792ea98
Anastasiya145 <asiva@ukr.net>
2022-10-25 15:48:35 +0200

    the task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const items = document.getElementsByClassName('population');
+const numbers = [...items].map(item => +item.innerText.split(',').join(''));
+
+const total = numbers.reduce((a, b) => a + b, 0);
+const average = total / numbers.length;
+
+document.querySelector(
+  '.total-population').innerHTML = total.toLocaleString('en-US');
+document.querySelector(
+  '.average-population').innerHTML = average.toLocaleString('en-US');
