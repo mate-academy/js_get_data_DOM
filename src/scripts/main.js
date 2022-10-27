ee3cb03a4f3286c8ebedc50f71d6583191eb38da
Kate Riazantseva <katyarezvova96@gmail.com>
2022-10-27 16:44:23 +0300

    Update solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,16 @@
 'use strict';
 
 const population = document.querySelectorAll('span.population');
-let arrOfPopulation = [...population].map(num => Number(num.innerText.replace(/[,]/g, '')));
-let totalPopulation = arrOfPopulation.reduce((num, sum) => num + sum);
-let averagePopulation = totalPopulation / arrOfPopulation.length;
 
-document.querySelector('span.average-population').innerHTML = averagePopulation.toLocaleString('en-US');
-document.querySelector('span.total-population').innerHTML = totalPopulation.toLocaleString('en-US');
+const arrOfPopulation = [...population].map(num =>
+  Number(num.innerText.replace(/[,]/g, '')));
+
+const totalPopulation = arrOfPopulation.reduce((num, sum) => num + sum);
+
+const averagePopulation = totalPopulation / arrOfPopulation.length;
+
+document.querySelector('span.average-population').innerHTML
+    = averagePopulation.toLocaleString('en-US');
+
+document.querySelector('span.total-population').innerHTML
+    = totalPopulation.toLocaleString('en-US');
