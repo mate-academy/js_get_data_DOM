996eb80f42c5260f58ad7894ba842f81a215f8c8
ovsiannikov <ovsiannikov@ucu.edu.ua>
2022-11-01 17:29:32 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const population = document.getElementsByClassName('population');
+const total = [...population]
+ .map(item => parseFloat(item.innerHTML.replaceAll(',', '')))
+  .reduce((a, b) => a + b);
+const average = total / [...population].length
+document.getElementsByClassName('total-population')[0].innerHTML = total.toLocaleString('en-US');
+document.getElementsByClassName('average-population')[0].innerHTML = average.toLocaleString('en-US');
