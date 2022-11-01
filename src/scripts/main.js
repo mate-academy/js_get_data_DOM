11a0e0e016d0fe8cf8a2ca0e821656996641de00
Max Fedak <max.fedak@gmail.com>
2022-11-01 16:04:28 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+function makeNumber(val) {
+  if (!val.match(/a-zA-z/)) {
+    return +val.replace(/,/g, '');
+  } else {
+    throw new Error('Wrong data');
+  }
+}
+
+const totalPopElement = document.querySelector('.total-population');
+const avgPopElement = document.querySelector('.average-population');
+const populationElements = [...document.querySelectorAll('.population')];
+const countriesAmount = populationElements.length;
+
+const populationsNumbers = populationElements.map(
+  item => makeNumber(item.innerText)
+);
+
+const populationTotal = populationsNumbers.reduce((sum, cur) => sum + cur, 0);
+
+const populationAvg = populationTotal / countriesAmount;
+
+totalPopElement.innerText = populationTotal.toLocaleString('en-EN');
+avgPopElement.innerText = populationAvg.toLocaleString('en-EN');
