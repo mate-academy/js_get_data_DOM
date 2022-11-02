40a26ea1206223a94569c0a6e81c968722341e42
Denis Cherevaschenko <deniska.cherevaschenko@gmail.com>
2022-11-02 19:15:44 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+function getThousSep(number) {
+  return number.toLocaleString('en-Us');
+}
+
+const totalHtml = document.querySelector('.total-population');
+const averageHtml = document.querySelector('.average-population');
+let allNum = document.querySelectorAll('.population');
+
+allNum = [...allNum].map(num => num.textContent)
+  .map(num => num.replaceAll(',', ''))
+  .map(num => +num);
+
+const total = allNum.reduce((sum, x) => sum + x, 0);
+const aver = total / allNum.length;
+
+totalHtml.textContent = getThousSep(total);
+averageHtml.textContent = getThousSep(aver);
