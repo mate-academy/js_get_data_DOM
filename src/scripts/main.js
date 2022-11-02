69b1bca3258ee088e5e43bb93b2a2cafd2721d2f
ovsiannikov <ovsiannikov@ucu.edu.ua>
2022-11-02 13:15:01 +0200

    fixed issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,9 @@ const total = population
  .map(item => parseFloat(item.innerHTML.replaceAll(',', '')))
   .reduce((a, b) => a + b);
 const average = total / population.length;
-const totalPopulation = getElementsByClassName('total-population')
+const totalPopulation = document.getElementsByClassName('total-population')
 .innerHTML;
-const averagePopulation = getElementsByClassName('average-population')
+const averagePopulation = document.getElementsByClassName('average-population')
 .innerHTML;
 
 totalPopulation[0] = total.toLocaleString('en-US');
