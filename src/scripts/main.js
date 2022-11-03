6f8ba48ff2db68ce76ba2f29a54c7356303e9435
Elena Ver <hellena84@i.ua>
2022-11-03 17:43:09 +0100

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write your code here
+/* const list = document.querySelector('.list'); */
+const population = [...document.querySelectorAll('span.population')];
+const populationValue = population.map(item =>
+  Number(item.innerText.split(',').join('')));
+
+const totalValue = populationValue.reduce(
+  (sum, num) => sum + num, 0);
+
+const average = (totalValue / population.length);
+
+document.querySelector('.total-population').innerText
+  = new Intl.NumberFormat('en-GB').format(totalValue);
+
+document.querySelector('.average-population').innerText
+    = new Intl.NumberFormat('en-GB').format(average);
