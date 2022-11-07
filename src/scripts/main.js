5f22995d4f970ff2a5e1bd243c9488cd38e65459
Dmytro Legerda <terrain1984@gmail.com>
2022-11-07 13:50:54 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const population = document.getElementsByClassName('population');
+const averPopulation = document.getElementsByClassName('average-population');
+const totalPopulation = document.getElementsByClassName('total-population');
+
+const text = [];
+
+for (let i = 0; i < population.length; i++) {
+  const newAmount = population[i].innerText.split(',').join('');
+
+  text.push(+newAmount);
+}
+
+const total = text.reduce((acc, el) => {
+  return acc + el;
+});
+
+totalPopulation[0].innerText = total.toLocaleString(); ;
+
+const average = total / population.length;
+
+averPopulation[0].textContent = average.toLocaleString();
