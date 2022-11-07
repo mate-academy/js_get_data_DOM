c8c8d7d0c3bf43ff99ad2371367e0372d02f6caf
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-11-07 17:14:30 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const nodes = document.querySelectorAll('.population');
+
+const arr = [];
+let arr2 = [];
+for (let i = 0; i < nodes.length; i++) {
+ arr.push(nodes[i].innerHTML);
+}
+
+
+for (let i = 0; i < arr.length; i++) {
+  const str = arr[i].split('').map(el => {
+    if (el === ',') {
+      return '';
+    } else {
+      return el;
+    }
+  });
+
+  arr2.push(str.join(''));
+}
+
+const total = arr2.reduce((a, b) => +a + +b);
+const average = total / 10;
+
+const totalHtml = document.querySelector('.total');
+const averageHtml = document.querySelector('.average');
+
+totalHtml.innerHTML = total.toString();
+averageHtml.innerHTML = average.toString();
