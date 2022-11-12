e265818b35eaddac864d73d07d3cde92956926e7
Artem66 <melodpi@gmail.com>
2022-11-12 18:54:58 +0000

    added average-population and total population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write your code here
+const population = document.querySelectorAll('.population');
+
+const transformToNumber = [...population]
+  .map(item => +item.innerText.replace(/,/g, ''));
+
+const total = transformToNumber.reduceRight((a, b) => a + b);
+
+document.getElementsByClassName('total-population')[0]
+  .textContent = total.toLocaleString('en-US');
+
+document.getElementsByClassName('average-population')[0]
+  .textContent = (total / transformToNumber.length).toLocaleString('en-US');
