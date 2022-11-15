b10135b257c3c548d4fa69517305a038d458564b
Bohdan Rudenko <RudBeee@gmail.com>
2022-11-15 13:43:47 +0200

    vol_2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,8 @@ const text = [...list].map(item => item.innerText);
 const number = text.map(elem => +elem.split(',').join(''));
 const totalSumm = number.reduce((a, b) => a + b);
 const desima = new Intl.NumberFormat('en-GB');
-const correctSumm = desima.format(totalSumm);
+const total = desima.format(totalSumm);
 const average = desima.format(totalSumm / number.length);
 
-document.querySelector('.total-population').innerHTML = correctSumm;
-document.querySelector('.average-population').innerHTML = average;
+document.querySelector('.total-population').textContent = total;
+document.querySelector('.average-population').textContent = average;
