c4a4198e8ca90dd0c445a3f9dd06624365021019
Oksana Kyryienko <kseniyals1609@gmail.com>
2022-11-15 15:01:41 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write your code here
+const element = document.querySelectorAll('.population');
+
+const arr = [...element].map(item => item.innerHTML);
+
+const str = arr.join(' ');
+
+let newStr = '';
+
+for (let i = 0; i < str.length; i++) {
+  if (str[i] !== ',') {
+    newStr += str[i];
+  }
+}
+
+const arrRes = newStr.split(' ');
+
+const res = arrRes.map(item => parseInt(item));
+
+const total = res.reduce((acc, item) => acc + item);
+
+// eslint-disable-next-line no-unused-vars
+const average = total / res.length;
