8e83e01094b9bebf3d600836c0b61b1e7e59e15f
Tetiana Melnyk <uasmiykaua@gmail.com>
2022-11-17 08:19:51 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const elements = document.querySelectorAll('span.population');
+const arr = [...elements].map(item =>
+  +item.textContent.split(',').join(''));
+
+const total = arr.reduce((sum, x) => sum + x, 0);
+
+const totalPopulation = document.querySelector('.total-population');
+
+totalPopulation.innerText = (arr.reduce((sum, x) =>
+  sum + x, 0)).toLocaleString('en-US');
+
+const averagePopulation = document.querySelector('.average-population');
+
+averagePopulation.innerText = (total / arr.length).toLocaleString('en-US');
