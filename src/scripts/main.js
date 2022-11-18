ed50a0f2a80d722c52b4ffe06d6c0e9e3f899ac8
Yevhen Petrovskyi <petrovskyi.yevhen@gmail.com>
2022-11-18 19:30:23 +0200

    solution for Get data DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const convertToNomber = [...population].map(elem => elem.innerText);
+const numbers = convertToNomber.map(el => +el.split(',').join(''));
+const totalPopulation = numbers.reduce((a, b) => a + b);
+const averagePopulation = totalPopulation / numbers.length;
+
+document.querySelector('.total-population').innerHTML
+  = totalPopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population').innerHTML
+  = averagePopulation.toLocaleString('en-US');
