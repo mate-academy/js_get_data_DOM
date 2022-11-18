24e246af43a13ed4432d31819138b2d26924d307
Tetiana Melnyk <uasmiykaua@gmail.com>
2022-11-18 14:26:07 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,17 @@
 'use strict';
 
-const elements = document.querySelectorAll('span.population');
-const arr = [...elements].map(item =>
-  +item.textContent.split(',').join(''));
+const population = document.querySelectorAll('span.population');
+const normalizePopulation = [...population]
+  .map(item => +item.textContent.split(',').join(''));
 
-const total = arr.reduce((sum, x) => sum + x, 0);
+const total = normalizePopulation.reduce((sum, x) => sum + x, 0);
 
 const totalPopulation = document.querySelector('.total-population');
 
-totalPopulation.innerText = (arr.reduce((sum, x) =>
-  sum + x, 0)).toLocaleString('en-US');
+totalPopulation.innerText = (normalizePopulation
+  .reduce((sum, x) => sum + x, 0)).toLocaleString('en-US');
 
 const averagePopulation = document.querySelector('.average-population');
 
-averagePopulation.innerText = (total / arr.length).toLocaleString('en-US');
+averagePopulation.innerText
+  = (total / normalizePopulation.length).toLocaleString('en-US');
