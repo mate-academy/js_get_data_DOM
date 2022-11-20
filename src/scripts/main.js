95c33b0762c249b0b4c1640244fd56f52b2df34f
Sergey Mironenko <serg1prime@gmail.com>
2022-11-20 21:30:12 +0200

    Task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+let total = 0;
+let average = 0;
+const items = document.querySelectorAll('.population');
+
+for (const node of [...items]) {
+  total += Number(node.innerText.split(',').join(''));
+}
+
+average = total / [...items].length;
+
+total = total.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+average = average.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+
+document.querySelector('.total-population').innerText = total;
+document.querySelector('.average-population').innerText = average;
