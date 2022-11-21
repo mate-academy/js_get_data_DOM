72b238aad04569f60fa7e11727c46955a828a6a0
Sergey Mironenko <serg1prime@gmail.com>
2022-11-21 17:43:38 +0200

    Task was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,14 +4,15 @@ let total = 0;
 let average = 0;
 const items = document.querySelectorAll('.population');
 
+function toThousands(number) {
+  return number.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+}
+
 for (const node of [...items]) {
   total += Number(node.innerText.split(',').join(''));
 }
 
 average = total / [...items].length;
 
-total = total.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
-average = average.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
-
-document.querySelector('.total-population').innerText = total;
-document.querySelector('.average-population').innerText = average;
+document.querySelector('.total-population').innerText = toThousands(total);
+document.querySelector('.average-population').innerText = toThousands(average);
