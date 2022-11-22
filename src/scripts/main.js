fbf68eebba30441a3fccb6a90886a9038ef44bdb
Anna Kolmakova <annakolmakova@gmail.com>
2022-11-22 18:00:45 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const populationByCountry = document.querySelectorAll('.population');
+const populationTotal = document.querySelector('.total-population');
+const populationAverage = document.querySelector('.average-population');
+let worldPopulation = 0;
+
+populationByCountry.forEach(countryPopulation => {
+  worldPopulation += Number(countryPopulation.innerHTML.split(',').join(''));
+});
+
+const newFormat = new Intl.NumberFormat('en-US');
+
+populationTotal.innerHTML = newFormat.format(worldPopulation);
+
+populationAverage.innerHTML = newFormat.format(worldPopulation
+   / populationByCountry.length);
