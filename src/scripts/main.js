97c6a0bafab9c63c8a1f6c758c95ea1a5c83de2a
Anastasiia Smirnova <anastasia.smirnova96@gmail.com>
2022-11-24 20:13:56 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,6 @@ const totalPopClass = document.querySelector('.total-population');
 const averagePopClass = document.querySelector('.average-population');
 
 const populationSpans = [...document.querySelectorAll('.population')];
-
 const popSpansToNum = populationSpans.map(num => convertToNum(num.innerText));
 const totalPopulation = popSpansToNum.reduce((a, b) => a + b);
 const averagePopulation = totalPopulation / populationSpans.length;
