2576f9af4913c117f84b9c2b439303c9f037e22b
Panianchuk M <panianchuk@gmail.com>
2022-11-28 20:31:49 +0200

    total/average

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const thousandsSeparator = (n) => n.toLocaleString('en-Us');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+const pop = document.querySelectorAll('.population');
+const arrPop = [...pop].map(el => el.innerText);
+const arrPopToNum = arrPop.map(el => +el.split(',').join(''));
+const len = arrPopToNum.length;
+const totPop = arrPopToNum.reduce((acc, el) => acc + el);
+const averagePop = totPop / len;
+
+total.textContent = thousandsSeparator(totPop);
+average.textContent = thousandsSeparator(averagePop);
