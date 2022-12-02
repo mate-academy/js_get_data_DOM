21f85e030b69acd8fa1e0dad10a4edc2aa9900d1
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2022-12-02 17:39:26 +0200

    Get data solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const populationList = [...document.querySelectorAll('.population')]
+  .map(population => +(population.textContent.split(',').join(''))
+  );
+
+const total = populationList.reduce((prev, curr) => prev + curr, 0);
+const totalNode = document.querySelector('.total-population');
+
+totalNode.textContent = `${total.toLocaleString('en-US')}`;
+
+const average = total / populationList.length;
+const averageNode = document.querySelector('.average-population');
+
+averageNode.textContent = `${average.toLocaleString('en-US')}`;
