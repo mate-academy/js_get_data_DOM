a49c6faca1f333c949459434b96d277ea2b92577
Artem Hirzhev <a.hirzhev@gmail.com>
2022-12-03 16:44:59 +0200

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const numbers = [...population].map(
+  country => Number(country.textContent.split(',').join(''))
+);
+
+const total = numbers.reduce((sum, current) => sum + current, 0);
+
+const average = total / numbers.length;
+
+document.querySelector('.total-population').textContent
+  = total.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent
+  = average.toLocaleString('en-US');
