60d62ff2e8965f002f325ef678c7d03dbb4bea8d
Serhii Vovchenko <seriivigas@gmail.com>
2022-12-06 18:37:00 +0200

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const textFromPopulation = document.querySelectorAll('.population');
-const numberOfPopulation = [...textFromPopulation].map(number =>
+const population = document.querySelectorAll('.population');
+const populationNumber = [...population].map(number =>
   Number(number.innerHTML.split(',').join('')));
 
-const totalSumPopulation = numberOfPopulation.reduce((sum, value) =>
+const totalSumPopulation = populationNumber.reduce((sum, value) =>
   sum + value);
 
 const averagePopulation = document.querySelector('.average-population');
 const totalPopulation = document.querySelector('.total-population');
 
 averagePopulation.innerHTML = (totalSumPopulation
-  / numberOfPopulation.length).toLocaleString('en-US');
+  / populationNumber.length).toLocaleString('en-US');
 totalPopulation.innerHTML = totalSumPopulation.toLocaleString('en-US');
