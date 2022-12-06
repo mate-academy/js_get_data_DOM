84ada60e45fe3639e31d95be69e5d489a7f0b584
Vadym Kimpaiev <kivabi.uk@gmail.com>
2022-12-06 09:28:51 +0200

    Fixed after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,62 +1,22 @@
 'use strict';
 
 const collection = document.querySelectorAll('.population');
-const arrayOfPopulation = [];
-let populationNum;
+
+let totalValue = 0;
 
 for (let i = 0; i < collection.length; i++) {
-  const bufferArray = (collection[i].textContent).split(',');
+  const populationStr = (collection[i].textContent);
+  const stringWithoutCommas = populationStr.replaceAll(',', '');
 
-  populationNum = +(bufferArray.join(''));
-
-  if (!isNaN(populationNum)) {
-    arrayOfPopulation[i] = populationNum;
-  } else {
-    arrayOfPopulation[i] = 0;
+  if (!isNaN(stringWithoutCommas)) {
+    totalValue += Number(stringWithoutCommas);
   }
 }
 
-const total = arrayOfPopulation.reduce((sum, value) => {
-  return sum + value;
-});
+const avg = totalValue / collection.length;
 
-const totalElem = document.querySelector('.total-population');
+document.querySelector('.total-population')
+  .textContent = totalValue.toLocaleString('en-US');
 
-totalElem.textContent = formatter('' + total);
-
-const averagePopulation = total / collection.length;
-const averageElem = document.querySelector('.average-population');
-
-averageElem.textContent = formatter('' + averagePopulation);
-
-function formatter(str = '') {
-  let len; // amount of digits that is exposed to formattig
-  let loopCounter;
-  const arr = Array.from(str);
-
-  if (str.includes('.')) {
-    len = str.indexOf('.');
-  } else {
-    len = str.length;
-  }
-
-  if (len < 4) {
-    return str;
-  }
-
-  /* calculate start point of "for"-loop === place for
-   first comma (loopCounter); */
-  const reminder = len % 3;
-
-  if (reminder === 0) {
-    loopCounter = 3;
-  } else {
-    loopCounter = reminder;
-  }
-
-  for (loopCounter; loopCounter < len; loopCounter += 4) {
-    arr.splice(loopCounter, 0, ',');
-  }
-
-  return arr.join('');
-}
+document.querySelector('.average-population')
+  .textContent = avg.toLocaleString('en-US');
