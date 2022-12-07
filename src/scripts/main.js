38acc90cc80a3119d5788cf2426dd0e36ddc970b
Yevhenii Stanchenko <galagystew@gmail.com>
2022-12-07 14:01:46 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const popul = [...document.querySelectorAll('.population')].map(population =>
+  Number(population.textContent.split(',').join('')));
+
+const total = popul.reduce((prev, current) => prev + current, 0);
+const totalNode = document.querySelector('.total');
+
+totalNode.textContent = `Total: ${total.toLocaleString('en-US')}`;
+
+const avarage = total / popul.length;
+const averageNode = document.querySelector('.average');
+
+averageNode.textContent = `Avarage: ${avarage.toLocaleString('en-US')}`;
