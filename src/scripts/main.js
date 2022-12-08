d220d1d198efa971f23c131733943f102992e997
Taras Vorobets <vorobetst777@gmail.com>
2022-12-08 21:45:35 +0200

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populationData = document.querySelectorAll('.population');
+
+const newArr = [...populationData].map(
+  el => Number(el.textContent.split(',').join('')));
+
+const sumAll = newArr.reduce((prev, current) => prev + current);
+
+const average = Math.trunc(sumAll / newArr.length);
+
+const totalForReplace = document.querySelector('.total');
+
+totalForReplace.textContent = `Total: ${sumAll.toLocaleString(`en-US`)}`;
+
+const averageForReplace = document.querySelector('.average');
+
+averageForReplace.textContent = `Average: ${average.toLocaleString(`en-Us`)}`;
