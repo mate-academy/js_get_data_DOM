f8963093f38bad131332575cc33c64105242e0ba
Ihor Chaikovskyi <ihorchaikovskyiweb@gmail.com>
2022-12-09 20:04:15 +0200

    First attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const totalPopulationClass = document.querySelector('.total-population');
+const averagePopulationClass = document.querySelector('.average-population');
+
+const arrayPopulations = [...populations].map(one =>
+  +one.textContent.split(',').reduce((x, y) => x + y,)
+);
+
+const totalPopulation = arrayPopulations.reduce((x, y) => x + y);
+const averagePopulation = totalPopulation / arrayPopulations.length;
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+totalPopulationClass.textContent = numberWithCommas(totalPopulation);
+averagePopulationClass.textContent = numberWithCommas(averagePopulation);
