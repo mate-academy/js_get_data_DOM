71b1fa1a6cb2de828268cba11c0245c91ffdac5e
Sergey Kovyazin <mr.roland1989@gmail.com>
2022-12-11 06:36:01 +0200

    Get data DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const population = document.body.querySelectorAll('span.population');
+
+const numbers = [...population].map(item => +item.innerHTML
+  .split(',').join(''));
+
+const total = numbers.reduce((a, b) => a + b).toLocaleString('en-US');
+
+const average = (numbers.reduce((a, b) => a + b) / numbers.length)
+  .toLocaleString('en-US');
+
+document.querySelector('span.total-population').innerHTML = total;
+document.querySelector('span.average-population').innerHTML = average;
