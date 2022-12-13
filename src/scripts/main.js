a6c01d947df3e9e4a790cb7564ccc032cc4053bb
Oselskyi Oleksandr <oselskyi3@gmail.com>
2022-12-13 14:14:18 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write your code here
+
+const items = document.getElementsByClassName('population');
+const population = [...items].map(item => item.innerText.split(',').join(''));
+const totalPopulation = population.reduce((a, b) => a + Number(b), 0);
+const averagePopulation = totalPopulation / population.length;
+
+document.querySelector('.total-population').innerText = totalPopulation;
+document.querySelector('.average-population').innerHTML = averagePopulation;
