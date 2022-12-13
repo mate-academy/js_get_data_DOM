02469860d13cfcaccc5d3962231e1d52b804bc58
Roman Malysh <romanmalysh@romans-macbook-air.home>
2022-12-13 22:19:00 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write your code here
+
+
+const [...ppl] = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+const totalPpl = [...ppl]
+.map(el => Number(el.textContent.split(',').join('')))
+.reduce((sum, elem) => sum + elem);
+
+const avgPpl = totalPpl / ppl.length;
+
+total.textContent = totalPpl.toLocaleString();
+average.textContent = avgPpl.toLocaleString();
