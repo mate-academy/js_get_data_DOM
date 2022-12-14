09649ca3d87f9a0a37b37f1b4a7e98f30b75fe41
Konstantin Burnazaki <kostas.burnazaki@gmail.com>
2022-12-14 20:26:23 +0200

    initial

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populationNodes = document.querySelectorAll('span.population');
+const populationArray
+= Array.from(populationNodes).map(e => parseFloat(e.innerHTML));
+
+const total = populationArray.reduce((a, b) => a + b, 0);
+const avg = (total / populationArray.length);
+
+const totalNode = document.querySelector('.total-population');
+const aveNode = document.querySelector('.average-population');
+
+totalNode.replaceWith(thousandsSeparator(total));
+aveNode.replaceWith(thousandsSeparator(avg));
+
+function thousandsSeparator(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
