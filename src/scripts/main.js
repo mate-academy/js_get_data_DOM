06e9631ec509526e8b9eba70a5b6f781fec482f7
Dmitriy Sporysh <dopmoneyd@gmail.com>
2022-12-14 18:26:56 +0200

    maybe Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const textData = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const numbers = [...textData].map((elem) => +elem.innerHTML.replace(/,/gi, ''));
+
+const total = numbers.reduce(
+  (accumulator, current) => accumulator + current,
+  0
+);
+
+const average = total / numbers.length;
+
+totalPopulation.textContent = total;
+averagePopulation.textContent = average;
