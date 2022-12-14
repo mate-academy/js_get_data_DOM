4930906b010a1622a9c53f60930a727d4cee11a2
Oselskyi Oleksandr <oselskyi3@gmail.com>
2022-12-14 16:35:58 +0200

    fix remarks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,14 @@
 
 const items = document.getElementsByClassName('population');
 const population = [...items].map(item => item.innerText.split(',').join(''));
-const totalPopulation = population.reduce((a, b) => a + Number(b), 0);
-const averagePopulation = totalPopulation / population.length;
+let totalPopulation = population.reduce((a, b) => a + Number(b), 0);
+let averagePopulation = totalPopulation / population.length;
+
+totalPopulation = String(totalPopulation)
+  .replace(/(?=(\d\d\d)+([^\d]|$))/g, ',');
+
+averagePopulation = String(averagePopulation)
+  .replace(/(?=(\d\d\d)+([^\d]|$))/g, ',').slice(1);
 
 document.querySelector('.total-population').innerText = totalPopulation;
 document.querySelector('.average-population').innerHTML = averagePopulation;
