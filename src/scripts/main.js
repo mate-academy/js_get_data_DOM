f5e5d32f22e7eeb674a1418fd8cddfd73587ea99
Vasiliy22 <hitretsukrnet@gmail.com>
2022-12-15 23:27:59 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const element = document.querySelectorAll('.population');
+const elementTotal = document.querySelector('.total');
+const elementAverage = document.querySelector('.average');
+const text = [...element].map(item => item.innerText);
+let numbersStr = '';
+const convertString = text.join(' ');
+
+for (let i = 0; i < convertString.length; i++) {
+  numbersStr += convertString[i].replace(',', '');
+}
+
+const arr = numbersStr.split(' ');
+const numbersArr = [];
+
+for (let i = 0; i < arr.length; i++) {
+  numbersArr.push(+arr[i]);
+}
+
+const total = numbersArr.reduce((x, y) => {
+  return x + y;
+}, 0);
+
+const average = numbersArr.reduce((x, y) => {
+  return Math.round((x + y) / numbersArr.length);
+}, 0);
+
+elementTotal.innerHTML = `
+<p class="total">Total: ${total.toLocaleString('en-US')}</p>
+`;
+
+elementAverage.innerHTML = `
+<p class="average">Average: ${average.toLocaleString('en-US')}</p>
+`;
