8e5e65e692a2de171f52ab801397fe3ed2b4dc04
Nick_Radchuk <mukola.radchuk@gmail.com>
2022-12-17 20:49:55 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+let totalNumber = 0;
+let count = 0;
+
+for (const span of spans) {
+  totalNumber += (+span.textContent.split(',').join(''));
+  count++;
+}
+
+const total = document.querySelector('.total-population');
+const avrg = document.querySelector('.average-population');
+
+total.textContent = totalNumber.toLocaleString();
+avrg.textContent = (totalNumber / count).toLocaleString();
