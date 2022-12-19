4353f267ce7a0021e95c7f25fea9e31147eb31ed
Evgen Maximov <evgen@debian-BULLSEYE-live-builder-AMD64>
2022-12-19 22:28:50 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const textData = [];
+
+population.map(item => {
+  textData.push(item.textContent);
+});
+
+let total = 0;
+
+textData.map(item => {
+  const number = item.split(',').join('');
+
+  total += Number(number);
+});
+
+const average = total / textData.length;
+const averageField = document.querySelector('.average-population');
+const totalField = document.querySelector('.total-population');
+
+totalField.textContent = total;
+averageField.textContent = average;
