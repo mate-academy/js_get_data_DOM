3961625e7b7ad3e82115fcefe8bc46069cbabcb1
Denis Bokov <denisbokov1703@gmail.com>
2022-12-20 16:59:45 +0100

    Solution Demo link fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+
+const element = document.querySelectorAll('.population');
+
+let total = 0;
+let count = 0;
+
+for (let el of element) {
+  el = +el.textContent.split(',').join('');
+
+  total += el;
+
+  count++;
+}
+
+const full = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+full.textContent = total.toLocaleString();
+average.textContent = (total / count).toLocaleString();
