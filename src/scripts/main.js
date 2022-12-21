5160276d4a77b1deb03d25019c61c9ce421ecfbe
Rafael Mamedov <rafmamedov@gmail.com>
2022-12-21 13:05:42 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const populationValue = [...population].map(item => item.innerText);
+const total = populationValue
+  .map(item => +(item.split(',').join('')))
+  .reduce((sum, x) => sum + x, 0);
+
+const average = (total / populationValue.length);
+
+document.querySelector('.total-population').innerHTML
+  = total.toLocaleString('en-US');
+
+document.querySelector('.average-population').innerHTML
+  = average.toLocaleString('en-US');
