165927c087da6653b93ea9693c828cd36c0f47ee
Kukharuk <its.up.to.you.dk@gmail.com>
2022-12-22 16:27:33 +0200

    dataCalculationsDK

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')].map(
+  el => el.innerText);
+const populationWoComa = population.map(
+  el => +(el.split('').filter(els => els !== ',').join('')));
+
+const total = populationWoComa.reduce((sum, x) => sum + x, 0);
+const average = total / populationWoComa.length;
+
+const totalHTML = document.querySelector('.total-population');
+const averageHTML = document.querySelector('.average-population');
+
+totalHTML.innerText = new Intl.NumberFormat('en-US').format(total);
+averageHTML.innerText = new Intl.NumberFormat('en-US').format(average);
