b90c1681a998e6a6080b5ee0783d529deba3d036
Roman <romanmalysh@romans-macbook-air.home>
2022-12-23 17:22:49 +0100

    someChanges

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,6 @@ const people = document.querySelectorAll('.population');
 const total = document.querySelector('.total-population');
 const average = document.querySelector('.average-population');
 
-console.log(people);
-
 const totalPeople = [...people]
 .map(el => Number(el.textContent.split(',').join('')))
 .reduce((sum, elem) => sum + elem);
