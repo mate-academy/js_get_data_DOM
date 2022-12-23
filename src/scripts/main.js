91b4bf392b49bcc017202cb5fb2c0fe2c26b92b4
Oleksii Mishchenko <alex.mishch@gmail.com>
2022-12-24 00:23:46 +0200

    Count total and average population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const makeThousands = num => num.toLocaleString('en-US');
+
+const populations = document.querySelectorAll('.population');
+const totalPopHTML = document.querySelector('.total-population');
+const avgPopHTML = document.querySelector('.average-population');
+const populationAmounts = [...populations].map(
+  population => +population.textContent.split(',').join('')
+);
+const populationsSum = populationAmounts.reduce(
+  (curr, prev) => curr + prev
+);
+const averagePopulation = populationsSum / populationAmounts.length;
+
+totalPopHTML.textContent = makeThousands(populationsSum);
+avgPopHTML.textContent = makeThousands(averagePopulation);
