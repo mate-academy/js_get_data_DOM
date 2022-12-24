8853824124ebf2a3cd61ed6e94a7c8eb35549c7e
Aleks <offsset@gmail.com>
2022-12-24 16:07:02 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+let summa = 0;
+
+const result = document.querySelectorAll('li');
+
+for (const key of result) {
+  const sum = key.querySelector('.population');
+
+  const number = +sum.innerText.split(',').join('');
+
+  summa += number;
+}
+
+const total = document.querySelector('.total-population');
+
+total.innerText = new Intl.NumberFormat('en-us').format(summa);
+
+const average = document.querySelector('.average-population ');
+
+average.innerText = new Intl.NumberFormat('en-us')
+  .format(summa / result.length);
