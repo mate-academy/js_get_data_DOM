70c13d734125a3c313b831bd7755b6b3e0056689
Nick Busarhin <busarginkolia@gmail.com>
2022-12-25 20:48:55 +0200

    top countries

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const numbers = document.querySelectorAll('.population');
+
+const populations = [...numbers].map((el) => el.innerText);
+
+const separatedNumbers = populations.join('/');
+let modified = '';
+
+for (const number of separatedNumbers) {
+  if (number !== ',') {
+    modified += number;
+  }
+}
+
+const newData = modified.split('/');
+
+const parsed = newData.map((el) => parseInt(el));
+
+const total = parsed.reduce((sum, x) => sum + x, 0);
+
+const formattedTotal = total.toLocaleString('en-Us');
+
+const average = (total / parsed.length).toLocaleString('en-Us');
+
+const totalCalculate = document.querySelector('.total-population');
+const totalAverage = document.querySelector('.average-population');
+
+totalCalculate.innerText = formattedTotal;
+totalAverage.innerText = average;
