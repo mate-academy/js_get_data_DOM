1616084e3101ccb158e9424218a3009dbb3eb233
Aleks <offsset@gmail.com>
2022-12-26 14:57:12 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,20 @@
 'use strict';
 
-let summa = 0;
+let population = 0;
 
 const result = document.querySelectorAll('li');
 
 for (const key of result) {
-  const sum = key.querySelector('.population');
+  const countrysPopulation = key.querySelector('.population');
 
-  const number = +sum.innerText.split(',').join('');
-
-  summa += number;
+  population += +countrysPopulation.innerText.split(',').join('');
 }
 
 const total = document.querySelector('.total-population');
 
-total.innerText = new Intl.NumberFormat('en-us').format(summa);
+total.innerText = new Intl.NumberFormat('en-us').format(population);
 
-const average = document.querySelector('.average-population ');
+const averagePopulation = document.querySelector('.average-population ');
 
-average.innerText = new Intl.NumberFormat('en-us')
-  .format(summa / result.length);
+averagePopulation.innerText = new Intl.NumberFormat('en-us')
+  .format(population / result.length);
