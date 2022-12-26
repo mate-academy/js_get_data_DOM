80358700cea28a26587f6bf93659e2d097ac4016
Kristina Makarovska <kris.makarovska@gmail.com>
2022-12-26 13:35:03 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+let total = 0;
+
+population.forEach(item => {
+  const numberPopulation = +(item.innerText.split(',').join(''));
+
+  total += numberPopulation;
+});
+
+const averageNumberPop = total / population.length;
+
+function thousandSeparator(num) {
+  return String(num).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+totalPopulation.innerText = thousandSeparator(total);
+averagePopulation.innerText = thousandSeparator(averageNumberPop);
