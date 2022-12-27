3a06cd120fb4a401e79aaf75095e8df04efbf876
EhorSaprykin <astralik1993@gmail.com>
2022-12-27 12:39:53 +0200

    All calculates done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+function calculatePopulation() {
+  const items = document.querySelectorAll('.population');
+
+  const populations = [...items].map(item => item.textContent);
+
+  const totalPopulation
+  = populations
+    .map(population => +population.split(',').join(''))
+    .reduce((a, b) => a + b, 0);
+
+  const averagePopulation = totalPopulation / populations.length;
+
+  document.querySelector('.total-population').textContent
+    = totalPopulation.toLocaleString('en-US');
+
+  document.querySelector('.average-population').textContent
+  = averagePopulation.toLocaleString('en-US');
+};
+
+calculatePopulation();
