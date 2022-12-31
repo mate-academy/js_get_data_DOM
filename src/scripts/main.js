a5766980004ba1ffc90cc026df170b637af2b8de
Nazarii Kyselevych <nazarkysel@gmail.com>
2022-12-31 13:57:24 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const populationSpans = document.querySelectorAll('span.population');
+const totalPopSpan = document.querySelector('span.total-population');
+const averagePopSpan = document.querySelector('span.average-population');
+
+const totalPopulation = [ ...populationSpans ]
+  .map(span => Number(span.innerHTML.split(',').join('')) || 0)
+  .reduce((sum, population) => sum + population);
+
+const averagelPopulation = totalPopulation / populationSpans.length;
+
+totalPopSpan.innerHTML = totalPopulation.toLocaleString();
+averagePopSpan.innerHTML = averagelPopulation.toLocaleString();
