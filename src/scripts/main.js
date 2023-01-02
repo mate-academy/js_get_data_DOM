042d17f331bf56489fa50c354c3c8a376fc9b73a
Anna Koval <anny.koval@icloud.com>
2023-01-02 19:42:32 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const thousandsSeparator = (number) => number.toLocaleString('en-Us');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+const population = document.querySelectorAll('.population');
+const arrayPopulations = [...population].map(el => el.innerText);
+const arrayToNumPopulations = arrayPopulations.map(el =>
+  +el.split(',').join(''));
+const numberOfElementsArray = arrayToNumPopulations.length;
+const totalPopulation = arrayToNumPopulations.reduce((acc, el) => acc + el);
+const averagePopulation = totalPopulation / numberOfElementsArray;
+
+total.textContent = thousandsSeparator(totalPopulation);
+average.textContent = thousandsSeparator(averagePopulation);
