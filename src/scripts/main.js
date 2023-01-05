9d6c2e4d6b8d86f088070aa2cead498efe68e499
yana-kotsulym <yanak7170@gmail.com>
2023-01-05 20:42:17 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+let numericPopulation = [...population].map(number => number.textContent);
+
+numericPopulation = numericPopulation.map(number => {
+  const arr = number.split(',');
+
+  return arr.join('');
+})
+  .filter(number => number.trim() && isFinite(number))
+  .map(number => +number);
+
+const totalValue = (numericPopulation.reduce((sum, x) => sum + x, 0));
+const averageValue = totalValue / numericPopulation.length;
+
+total.innerHTML
+  = totalValue.toLocaleString('en-US', { maximumFractionDigits: 2 });
+
+average.innerHTML
+  = averageValue.toLocaleString('en-US', { maximumFractionDigits: 2 });
