e5033ecc6eb4ede8a0fc61b1949e24c94f8a25b1
Vitalii Skomskiy <skomskiy@ukr.net>
2023-01-06 13:45:19 +0200

    population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write your code here
+const content = document.querySelectorAll('.population');
+let count = 0;
+let total = 0;
+let avarage = 0;
+
+for (const child of content) {
+  const re = /,/gi;
+
+  total += parseInt(child.textContent.replace(re, ''));
+  count++;
+};
+
+avarage = total / count;
+
+const numFor = Intl.NumberFormat('en-US');
+const newForTotal = numFor.format(total);
+const newForAvarage = numFor.format(avarage);
+
+const totalFinal = document.querySelector('.total');
+const averageFinal = document.querySelector('.average');
+
+document.querySelector('.total-population').remove();
+
+totalFinal.insertAdjacentHTML('beforeend', `
+  <span class="total-population">
+    ${newForTotal}
+  </span>
+`);
+
+document.querySelector('.average-population').remove();
+
+averageFinal.insertAdjacentHTML('beforeend', `
+  <span class="average-population">
+    ${newForAvarage}
+  </span>
+`);
