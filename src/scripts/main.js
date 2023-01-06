e998767e8c59d499ba50d3838e87d64eaa611e28
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-01-06 13:34:57 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+// Variables
+const populationData = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+// Calculation
+const populationList = [...populationData].map(data =>
+  Number(data.innerText.replaceAll(',', '')));
+const total = populationList.reduce((amount, population) =>
+  amount + population, 0);
+
+const average = total / populationList.length;
+
+// Add the calculated data to the page
+totalPopulation.innerText = total.toLocaleString('en-GB');
+averagePopulation.innerText = average.toLocaleString('en-GB');
