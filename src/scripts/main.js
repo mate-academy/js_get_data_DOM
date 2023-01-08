6dff677781588b72923bf53ea82b936cf0750488
Vitalii Skomskiy <skomskiy@ukr.net>
2023-01-08 13:58:59 +0200

    new commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,23 +3,23 @@
 const content = document.querySelectorAll('.population');
 let total = 0;
 let avarage = 0;
-const arr = [];
+const populationArray = [];
 
 for (const child of content) {
   const re = /,/gi;
 
-  arr.push(+child.textContent.replace(re, ''));
-
-  total = arr.reduce((a, b) => {
-    return a + b;
-  });
+  populationArray.push(+child.textContent.replace(re, ''));
 };
+
+total = populationArray.reduce((a, b) => {
+  return a + b;
+});
 
 avarage = total / content.length;
 
-const numFor = Intl.NumberFormat('en-US');
-const newForTotal = numFor.format(total);
-const newForAvarage = numFor.format(avarage);
+const numberFormat = Intl.NumberFormat('en-US');
+const formattedTotal = numberFormat.format(total);
+const formattedAvarage = numberFormat.format(avarage);
 
 const totalFinal = document.querySelector('.total');
 const averageFinal = document.querySelector('.average');
@@ -28,7 +28,7 @@ document.querySelector('.total-population').remove();
 
 totalFinal.insertAdjacentHTML('beforeend', `
   <span class="total-population">
-    ${newForTotal}
+    ${formattedTotal}
   </span>
 `);
 
@@ -36,6 +36,6 @@ document.querySelector('.average-population').remove();
 
 averageFinal.insertAdjacentHTML('beforeend', `
   <span class="average-population">
-    ${newForAvarage}
+    ${formattedAvarage}
   </span>
 `);
