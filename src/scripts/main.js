a58746aa53e0b67da1f1e1005c4c449ef899d999
Sofiia Triasko <sofia13tryasko@gmail.com>
2023-01-09 20:55:44 +0200

    commit 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const countOfPopulation = document.querySelectorAll('span.population');
+
+const countofPopulationArray = [...countOfPopulation].map(item =>
+  Number(item.innerText.split(',').join('')));
+
+const sumOfPopulation = countofPopulationArray.reduce(
+  function(a, b) {
+    return a + b;
+  },
+);
+
+const averageOfPopulation = sumOfPopulation / countofPopulationArray.length;
+
+const total = document.querySelectorAll('span.total-population');
+
+total[0].textContent = `${sumOfPopulation.toLocaleString()}`;
+
+const average = document.querySelectorAll('span.average-population');
+
+average[0].textContent = `${averageOfPopulation.toLocaleString()}`;
