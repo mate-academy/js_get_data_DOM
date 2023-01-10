f8bccac43282c30f61ca5144a39ac46d169d0d64
Anna Inozemets <inoznetta@gmail.com>
2023-01-10 17:22:58 +0200

    Total and average population values were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+function thousandsSeparator(number) {
+  const splitedNum = String(number).split('');
+  const modificatedNum = [];
+  let counter = 0;
+
+  for (let i = splitedNum.length - 1; i >= 0; i--) {
+    counter++;
+    modificatedNum.push(splitedNum[i]);
+
+    if (counter % 3 === 0 && i !== 0) {
+      modificatedNum.push(',');
+    }
+  }
+
+  return modificatedNum.reverse().join('');
+}
+
+const populationsList = document.querySelectorAll('.population');
+const populationsInfo = [...populationsList];
+
+const populationSum = populationsInfo
+  .map(value => +value.innerText.split(',').join(''))
+  .reduce((previous, current) => previous + current, 0);
+const populationAverage = populationSum / populationsInfo.length;
+
+const populationSumElement = document.querySelector('.total-population');
+const populationAverageElement = document.querySelector('.average-population');
+
+populationSumElement.textContent = thousandsSeparator(populationSum);
+populationAverageElement.textContent = thousandsSeparator(populationAverage);
