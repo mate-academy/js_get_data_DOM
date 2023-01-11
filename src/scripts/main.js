a84caf0250fd35402a9a3a52f8a3a859796eef6b
haduigon <haduigon@gmail.com>
2023-01-11 21:31:51 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // write your code here
+
+const countries = document.querySelectorAll('.population');
+
+const totalPeople = [];
+
+for (let i = 0; i < countries.length; i++) {
+  const temp = [countries[i]].map(country => country.innerText);
+
+  totalPeople.push(temp[0]);
+}
+
+const total = totalPeople.map(string => string.replaceAll(',', ''));
+
+const totalNumber = total.map(string2 => +string2);
+
+const totalSum = totalNumber.reduce((a, b) => a + b, 0);
+
+const totalHtml = document.querySelector('.total-population');
+
+totalHtml.innerText = totalSum;
+
+const average = document.querySelector('.average-population');
+
+average.innerHTML = totalSum / totalNumber.length;
