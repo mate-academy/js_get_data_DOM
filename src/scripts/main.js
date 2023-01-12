ae5d7bfa7185068c1cc3746b9f721206ea155ba8
Vadym Prydatok <vadymprydatok@gmail.com>
2023-01-12 19:09:38 +0200

    population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const totalPopulation = [...population].reduce((sum, value) => {
+  return sum + +value.textContent.split(',').join('');
+}, 0);
+
+const averagePopulation = totalPopulation / population.length;
+
+document.querySelector('.average-population').textContent
+= averagePopulation.toLocaleString('en-US');
+
+document.querySelector('.total-population').textContent
+= totalPopulation.toLocaleString('en-US');
