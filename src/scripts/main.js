14e9951408ec10a2b833aaf3bea367098a887fb9
Yulia Borshovska <yulya.borshovska@gmail.com>
2023-01-11 22:23:51 -0800

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const avarage = document.querySelector('.avarage-population');
+
+const populationList = [...population].map(
+  item => Number(item.innerText.split(',').join(''))
+);
+
+const totalPopulation = populationList.reduce((sum, prev) => sum + prev, 0);
+const avaragePopulation = totalPopulation / populationList.length;
+
+total.innerText = totalPopulation.toLocaleString('en-Us');
+avarage.innerText = avaragePopulation.toLocaleString('en-Us');
