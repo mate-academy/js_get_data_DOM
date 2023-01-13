087ab001067274e32709d8f198ee4af4d019f830
Anastasia Radchenko <radchenckooo@gmail.com>
2023-01-13 11:44:01 +0200

    Population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write your code here
+const populationList = [...document.querySelectorAll('.population')]
+  .map(population => +(population.textContent.split(',').join(''))
+  );
+
+const totalResult = populationList.reduce((prev, cur) => prev + cur, 0);
+const averageResult = totalResult / populationList.length;
+
+const totalNode = document.querySelector('.total-population');
+const averageNode = document.querySelector('.average-population');
+
+totalNode.textContent = `${totalResult.toLocaleString('en-US')}`;
+averageNode.textContent = `${averageResult.toLocaleString('en-US')}`;
