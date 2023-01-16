530c8a1dc9047ae2f658af9a242ef80d237e19cb
Alex Vahnovan <sonyaxent@gmail.com>
2023-01-16 14:05:59 +0100

    New solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const populationText = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+const textArray = [...populationText].map(country =>
+  Number(country.innerText.split(',').join(''))
+);
+const totalPopulation = textArray.reduce((x, y) => x + y, 0);
+const averagePopulation = totalPopulation / textArray.length;
+
+total.innerText = totalPopulation.toLocaleString('en-US');
+average.innerText = averagePopulation.toLocaleString('en-US');
