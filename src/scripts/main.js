1e7b1f8ef8942c2d652be27b70ec2c9245527620
Olena Hapon <olenagapon90gmail.com>
2023-01-17 19:44:33 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const populationArrow = [...population].map(item => item.innerText);
+
+const arrow = populationArrow.map(string => +string.split(',').join(''));
+
+let total = arrow.reduce((sum, number) => sum + number, 0);
+
+let average = total / arrow.length;
+
+total = total.toLocaleString('en');
+average = average.toLocaleString('en');
+
+const totalSpan = document.querySelector('.total-population');
+
+totalSpan.innerText = total;
+
+const averageSpan = document.querySelector('.average-population');
+
+averageSpan.innerText = average;
