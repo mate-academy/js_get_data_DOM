f06fa38a4ad5ff0f5ae58a6a5f65ea7fa5dcb3ab
ihorivna007 <emmakorovnik@gmail.com>
2023-01-18 15:30:29 +0200

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const inputData = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+let total = 0;
+
+for (const data of inputData) {
+  const numericData = Number((data.innerHTML).replaceAll(',', ''));
+
+  total += numericData;
+}
+
+const average = new Intl.NumberFormat().format(total / inputData.length);
+
+total = new Intl.NumberFormat().format(total);
+totalPopulation.innerHTML = total;
+averagePopulation.innerHTML = average;
