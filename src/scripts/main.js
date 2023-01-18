00295abf7422648c83c50a37d7c3317cceda953e
Aleksey-Aleksey <l0734062646@gmail.com>
2023-01-18 19:17:47 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const allArrInOne = [...population].map(el => el.textContent);
+
+const convToNum = allArrInOne.map(el => +el.split(',').join(''));
+
+const totalPopulation = convToNum.reduce((sum, el) => sum + el, 0);
+
+const averagePopulation = totalPopulation / allArrInOne.length;
+
+const allAmount = document.querySelector('.total-population');
+
+allAmount.innerText = totalPopulation.toLocaleString('en-US');
+
+const averageAmount = document.querySelector('.average-population');
+
+averageAmount.innerText = averagePopulation.toLocaleString('en-US');
