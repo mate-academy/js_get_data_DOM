a36fa05b5bd862de5b3b35265ad60043695bcf3a
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-20 15:40:58 +0200

    Finish task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+const populationArray = [...document.querySelectorAll('.population')];
+
+const totalPopulation = populationArray.reduce(
+  (acc, p) =>
+    isNaN(parseInt(p.textContent.replace(/,/g, '')))
+      ? 0
+      : acc + parseInt(p.textContent.replace(/,/g, '')),
+  0
+);
+const averagePopulation = totalPopulation / populationArray.length;
+
+document.querySelector('.average-population').textContent
+  = averagePopulation.toLocaleString();
+
+document.querySelector('.total-population').textContent
+  = totalPopulation.toLocaleString();
