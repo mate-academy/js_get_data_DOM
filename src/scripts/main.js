c1408bf443a36a6ceaade4febbd5c52e7ad4fb27
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-21 09:54:38 +0200

    Fix reduce callback1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
 // write your code here
-const populationArray = [...document.querySelectorAll(".population")];
+const populationArray = [...document.querySelectorAll('.population')];
 
 const totalPopulation = populationArray.reduce(
-  (acc, p) => acc + +p.textContent.split(",").join(""),
+  (acc, p) => acc + +p.textContent.split(',').join(''),
   0
 );
 const averagePopulation = totalPopulation / populationArray.length;
 
-document.querySelector(".average-population").textContent =
-  averagePopulation.toLocaleString("en-US");
+document.querySelector('.average-population').textContent
+  = averagePopulation.toLocaleString('en-US');
 
-document.querySelector(".total-population").textContent =
-  totalPopulation.toLocaleString("en-US");
+document.querySelector('.total-population').textContent
+  = totalPopulation.toLocaleString('en-US');
