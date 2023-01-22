94ca42828caf7f2fff8d26eca859b586212911d2
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-01-22 19:24:33 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
+const spanTags = document.querySelectorAll('.population');
+const sumSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+
+let sum = 0;
+let count = 0;
+
+for (const span of spanTags) {
+  const number = span.textContent.split(',').join('');
+
+  sum += +number;
+  count++;
+};
+
+const totalAverage = sum / count;
+
+sumSpan.innerText = `${sum.toLocaleString('en-US')}`;
+averageSpan.innerText = `${totalAverage.toLocaleString('en-US')}`;
+
 // write your code here
