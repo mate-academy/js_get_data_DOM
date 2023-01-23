e60f575238dac60a86a91949394b12dace54b781
ihorivna007 <emmakorovnik@gmail.com>
2023-01-23 18:39:20 +0200

    fixed thousands separator

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const inputData = document.querySelectorAll('.population');
+const population = document.querySelectorAll('.population');
 const totalPopulation = document.querySelector('.total-population');
 const averagePopulation = document.querySelector('.average-population');
 let total = 0;
 
-for (const data of inputData) {
+for (const data of population) {
   const numericData = Number((data.innerHTML).replaceAll(',', ''));
 
   total += numericData;
 }
 
-const average = new Intl.NumberFormat().format(total / inputData.length);
+const average = (total / population.length).toLocaleString();
 
-total = new Intl.NumberFormat().format(total);
+total = total.toLocaleString();
 totalPopulation.innerHTML = total;
 averagePopulation.innerHTML = average;
