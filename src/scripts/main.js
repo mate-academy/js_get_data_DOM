c2d7287118102521e0c740a08c4ed96d91ca78ed
Kate <kate@Kates-MacBook-Pro-16.local>
2023-02-03 14:46:12 +0100

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const data = [...document.getElementsByClassName("population")].map(it => +it.innerText.replaceAll(',', ''));
+
+const total = data.reduce((a, b) => a + b);
+const totalFormated = total.toLocaleString();
+
+const average = total / data.length;
+const averageFormated = average.toLocaleString();
+
+document.getElementsByClassName("total-population")[0].innerText = totalFormated;
+document.getElementsByClassName("average-population")[0].innerText = averageFormated;
