2c0fcc41310b18d85164c25dca2cbbf95799f44e
Kate <kate@Kates-MacBook-Pro-16.local>
2023-02-03 19:38:54 +0100

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const data = [...document.getElementsByClassName("population")].map(it => +it.innerText.replaceAll(',', ''));
+const data = [...document.getElementsByClassName('population')]
+  .map(it => +it.innerText.replaceAll(',', ''));
 
 const total = data.reduce((a, b) => a + b);
 const totalFormated = total.toLocaleString();
@@ -8,5 +9,8 @@ const totalFormated = total.toLocaleString();
 const average = total / data.length;
 const averageFormated = average.toLocaleString();
 
-document.getElementsByClassName("total-population")[0].innerText = totalFormated;
-document.getElementsByClassName("average-population")[0].innerText = averageFormated;
+document.getElementsByClassName('total-population')[0]
+  .innerText = totalFormated;
+
+document.getElementsByClassName('average-population')[0]
+  .innerText = averageFormated;
