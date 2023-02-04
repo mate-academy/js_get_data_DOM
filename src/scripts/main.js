5194cdb4151c39d5cf650c198b37d4fbcf7a5b1b
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-02-04 02:58:48 +0200

    Work done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const element = document.body.getElementsByClassName('population');
+let total = 0;
+let counter = 0;
+
+for (const node of element) {
+  counter++;
+
+  const fromNumberToText = parseInt(node.innerText.replace(/\D/g, ''));
+
+  total += fromNumberToText;
+}
+
+const average = total / counter;
+
+const aver = document.body.querySelector('.average-population');
+
+aver.textContent = average.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+const tot = document.body.querySelector('.total-population');
+
+tot.textContent = total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
