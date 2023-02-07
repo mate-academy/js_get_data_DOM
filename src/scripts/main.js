cb604131e5ade09d99439d805ec377d51d692a86
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-02-07 19:59:38 +0200

    total and average population calculated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const array = [];
+const spans = document.querySelectorAll('.population');
+
+for (const population of spans) {
+  array.push(population.innerText);
+}
+
+const numbersArray = array.map(elem => +elem.replace(/,/g, ''));
+
+const total = numbersArray.reduce((a, b) => a + b, 0);
+const average = total / 10;
+
+const totalSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+
+totalSpan.innerText = total.toLocaleString('en-US');
+averageSpan.innerText = average.toLocaleString('en-US');
