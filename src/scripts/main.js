678a95dcd9a83323967b5c4c51b655be2b3a2b6d
Jaxxxon78 <jaxxxon78@ukr.net>
2023-02-07 20:03:43 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const res = [];
+const elements = document.querySelectorAll('.population');
+
+for (const item of elements) {
+  res.push(+(item.innerHTML.split(',').join('')));
+}
+
+const total = res.reduce((el, sum) => el + sum, 0);
+const average = (total / res.length).toLocaleString('en-US');
+
+document.querySelector('.total-population').innerHTML = total.toLocaleString('en-US');
+document.querySelector('.average-population').innerHTML = average;
