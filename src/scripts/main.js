0b68f4f557abfc27946618b52376b29c484b0e0f
Nikitin Evgeny <ngenya81@gmail.com>
2023-02-07 09:50:39 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,34 +15,8 @@ for (const ch of numberPeople) {
 
 const averValuePeople = totalPeople / numberPeople.length;
 
-const resultAver = [];
-const srtingAverPeople = String(averValuePeople).split('');
-let countOne = 0;
-
-for (let i = srtingAverPeople.length - 1; i >= 0; i--) {
-  if (countOne === 3 || countOne === 6 || countOne === 9) {
-    resultAver.unshift(',');
-  }
-  resultAver.unshift(srtingAverPeople[i]);
-  countOne++;
-}
-
-const resultTotal = [];
-const srtingTotalPeople = String(totalPeople).split('');
-let count = 0;
-
-for (let i = srtingTotalPeople.length - 1; i >= 0; i--) {
-  if (count === 3 || count === 6 || count === 9) {
-    resultTotal.unshift(',');
-  }
-  resultTotal.unshift(srtingTotalPeople[i]);
-  count++;
-}
-
 const calcAver = document.querySelector('.average-population');
 const calcTotal = document.querySelector('.total-population');
 
-calcAver.textContent = `${resultAver.join('')}`;
-calcTotal.textContent = resultTotal.join('');
-
-// return numberPeople;
+calcAver.textContent = `${averValuePeople.toLocaleString('en-US')}`;
+calcTotal.textContent = totalPeople.toLocaleString('en-US');
