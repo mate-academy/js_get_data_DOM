5d008175d367765080b5b6690b87ea244221610e
Vasyl Pavlenko <genlus@ukr.net>
2023-02-08 14:44:02 +0200

    Main.js has been written

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+let total = 0;
+
+list.forEach((item) => {
+  const population = item.innerText.split(',').join('');
+
+  if (!isNaN(population)) {
+    total += parseInt(population);
+  }
+});
+
+const average = (total / list.length).toLocaleString('en-US');
+
+total = total.toLocaleString('en-US');
+
+totalPopulation.innerText = total;
+averagePopulation.innerText = average;
