7c41b82f5e54be6c23be1be92eb68cce959c0052
Oleksandr <98667890az@gmail.com>
2023-02-09 16:57:57 +0200

    Fixed naming

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const count = document.querySelectorAll('.population');
-const countMassive
-  = [...count].map(item => +item.innerText.replaceAll(',', ''));
+const populations = document.querySelectorAll('.population');
+const normalizedPopulations
+  = [...populations].map(item => +item.innerText.replaceAll(',', ''));
 
-const totalCount = document.querySelector('.total-population');
-const averageCount = document.querySelector('.average-population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
 
-const total = countMassive.reduce((sum, num) => sum + num);
+const calculatedTotal = normalizedPopulations.reduce((sum, num) => sum + num);
+const calculatedAverage = (calculatedTotal / normalizedPopulations.length);
 
-totalCount.innerHTML = total.toLocaleString('en');
-averageCount.innerHTML
-  = (total / countMassive.length).toLocaleString('en');
+total.innerHTML = calculatedTotal.toLocaleString('en');
+average.innerHTML = calculatedAverage.toLocaleString('en');
