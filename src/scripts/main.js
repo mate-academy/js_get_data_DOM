b4a717ed2eb47d272babffcfaffe89c66c2ebd4e
Oleksandr <98667890az@gmail.com>
2023-02-09 16:22:18 +0200

    get_data_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const count = document.querySelectorAll('.population');
+const countMassive
+  = [...count].map(item => +item.innerText.replaceAll(',', ''));
+
+const totalCount = document.querySelector('.total-population');
+const averageCount = document.querySelector('.average-population');
+
+const total = countMassive.reduce((sum, num) => sum + num);
+
+totalCount.innerHTML = total.toLocaleString('en');
+averageCount.innerHTML
+  = (total / countMassive.length).toLocaleString('en');
