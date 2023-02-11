e480bea6a6a7dd4691fdecd8194794434467fea6
Nick <tortickk8475@gmail.com>
2023-02-11 11:18:23 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const populationList = document.querySelectorAll('.population');
+const populationArrey = [];
+
+for (let i = 0; i < populationList.length; i++) {
+  let item = document.querySelectorAll('.population')[i].textContent;
+
+  populationArrey.push(item = +item.replaceAll(',', ''));
+}
+
+let totalPopulation = 0;
+
+populationArrey.map(sum => {
+  totalPopulation += sum;
+});
+
+const formattedTotal = totalPopulation.toLocaleString('en-US');
+
+const avaragePopulation
+  = (totalPopulation / populationList.length).toLocaleString('en-US');
+
+const classTotal = document.querySelector('.total-population');
+const classAverage = document.querySelector('.average-population');
+
+classTotal.innerHTML = `
+  <span class="total-population">
+    ${formattedTotal}
+  </span>
+`;
+
+classAverage.innerHTML = `
+  <span class="average-population">
+  ${avaragePopulation}
+  </span>
+`;
