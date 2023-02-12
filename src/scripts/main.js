ba504c21c74fb632f9e2cbdfdb031ce706be788f
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-02-12 15:31:03 +0200

    Solution Top 10 largest countries

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const valueOfpopulation = [...population].map(
+  country => +country.innerText.replaceAll(',', '')
+);
+const total = valueOfpopulation.reduce((acc, value) => acc + value);
+const average = total / valueOfpopulation.length;
+
+document.querySelector('.total-population').innerText = total;
+document.querySelector('.average-population').innerText = average;
