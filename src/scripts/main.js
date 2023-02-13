30f4a92f77c290dc56992bc885595bcf9071753e
KseniyaGonchar <kseniyadf888@gmail.com>
2023-02-13 21:40:05 +0200

    sollution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const items = document.querySelectorAll('.population');
+
+const numbers = [...items].map(item => item.innerText);
+
+const total = numbers.map(el =>
+  Number(el.split(',').join(''))).reduce((sum, item) => sum + item);
+
+const result = total.toLocaleString('en-US');
+
+const average = total / 10;
+
+const resultAver = average.toLocaleString('en-US');
+
+const totalPop = document.querySelector('.total-population');
+
+totalPop.textContent = result;
+
+const averagePop = document.querySelector('.average-population');
+
+averagePop.textContent = resultAver;
