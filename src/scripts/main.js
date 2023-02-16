276e0f1e131fd496a61c6a744e84f1e4842ee637
ivanm2706 <ivanmorhun664@gmail.com>
2023-02-16 07:58:37 +0200

    added total && average number

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const totalPeople = document.querySelector('.total-population');
+const averagePeople = document.querySelector('.average-population');
+
+const { total, acount } = [...population].reduce((acc, elem) => {
+  const populationContry = +elem.innerText.replaceAll(',', '');
+
+  acc.acount++;
+  acc.total += populationContry;
+
+  return acc;
+}, {
+  total: 0, acount: 0,
+});
+
+totalPeople.innerText = getNumber(total);
+averagePeople.innerText = getNumber(total / acount);
+
+function getNumber(num) {
+  const arr = num.toString().split('');
+  const arrNum = [];
+
+  for (let i = arr.length - 1, a = 0; i >= 0; i--) {
+    if (a < 3) {
+      arrNum.unshift(arr[i]);
+      a++;
+      continue;
+    }
+
+    arrNum.unshift(',');
+    arrNum.unshift(arr[i]);
+    a = 1;
+  }
+
+  return arrNum.join('');
+}
