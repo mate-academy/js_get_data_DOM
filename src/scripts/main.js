d5f0509bcef0d5854429795315fa9f79d1dc5d17
tarasnakonechnyi <nakonechnyi.taras@gmail.com>
2023-02-19 21:38:12 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const pops = [...document.querySelectorAll('.population')]
+  .map(el => parseInt(el.textContent.replace(/,/g, '')))
+  .filter(num => !isNaN(num));
+
+const total = pops.reduce((acc, num) => acc + num, 0);
+const avg = total / pops.length;
+
+const formattedTotal = total.toLocaleString();
+const formattedAvg = avg.toLocaleString(undefined, {
+  maximumFractionDigits: 2,
+});
+
+document.querySelector('.total-population').textContent = formattedTotal;
+document.querySelector('.average-population').textContent = formattedAvg;
