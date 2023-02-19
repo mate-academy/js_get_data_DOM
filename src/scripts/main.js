969f8595196bfadd44c8dc0f1e00250454bfdb1e
Vlad <bojko.vladislav16@gmail.com>
2023-02-19 13:56:50 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const textData = document.getElementsByClassName('population');
+const totalValue = document.getElementsByClassName('total-population')[0];
+const averageValue = document.getElementsByClassName('average-population')[0];
+
+const list = [...textData]
+  .map(text => Number(text.textContent.split(',').join('')));
+
+const numberFormatter = Intl.NumberFormat('en-US');
+
+totalValue.textContent = list.reduce((a, b) => a + b, 0);
+averageValue.textContent = totalValue.textContent / list.length;
+
+totalValue.textContent = numberFormatter.format(totalValue.textContent);
+averageValue.textContent = numberFormatter.format(averageValue.textContent);
