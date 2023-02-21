1f6ccc7041e351d8786d2af6213bfa90e927f153
Mykola Butylkov <kokos.ibo.58@gmail.com>
2023-02-21 10:55:01 +0200

    solution-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,51 @@
 'use strict';
-
 // write your code here
+
+const population = document.querySelectorAll('.population');
+
+function populationNum(currentPupulation) {
+  return +currentPupulation.innerText.replaceAll(',', '');
+}
+
+const populationArray = [...population].map(populationNum);
+const totalPopulation = populationArray.reduce(
+  (sum, currentValue) => sum + currentValue);
+const avgAmountPopulation = totalPopulation / populationArray.length;
+
+const spanCalculateItTotal = document.querySelector('.total-population');
+const spanCalculateItAverage = document.querySelector('.average-population');
+
+function populationStr(num) {
+  const str = num.toString();
+
+  let result = '';
+  let start;
+
+  if (str.length % 3 === 0) {
+    start = 1;
+  };
+
+  if (str.length % 3 === 1) {
+    start = 3;
+  };
+
+  if (str.length % 3 === 2) {
+    start = 2;
+  };
+
+  for (let i = 0; i < str.length; i++) {
+    result += str[i];
+
+    if ((i + start) % 3 === 0 && i + 1 < str.length) {
+      result += ',';
+    }
+  }
+
+  return result;
+};
+
+const totalPopulationStr = populationStr(totalPopulation);
+const avgPopulationStr = populationStr(avgAmountPopulation);
+
+spanCalculateItTotal.replaceWith(totalPopulationStr);
+spanCalculateItAverage.replaceWith(avgPopulationStr);
