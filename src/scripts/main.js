1b05b379fbaff47a9b241ca8cbc99c19954469f2
Mykola Butylkov <kokos.ibo.58@gmail.com>
2023-02-21 16:16:00 +0200

    solution-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,37 +15,5 @@ const avgAmountPopulation = totalPopulation / populationArray.length;
 const spanCalculateItTotal = document.querySelector('.total-population');
 const spanCalculateItAverage = document.querySelector('.average-population');
 
-function populationStr(num) {
-  const str = num.toString();
-
-  let result = '';
-  let start;
-
-  if (str.length % 3 === 0) {
-    start = 1;
-  };
-
-  if (str.length % 3 === 1) {
-    start = 3;
-  };
-
-  if (str.length % 3 === 2) {
-    start = 2;
-  };
-
-  for (let i = 0; i < str.length; i++) {
-    result += str[i];
-
-    if ((i + start) % 3 === 0 && i + 1 < str.length) {
-      result += ',';
-    }
-  }
-
-  return result;
-};
-
-const totalPopulationStr = populationStr(totalPopulation);
-const avgPopulationStr = populationStr(avgAmountPopulation);
-
-spanCalculateItTotal.replaceWith(totalPopulationStr);
-spanCalculateItAverage.replaceWith(avgPopulationStr);
+spanCalculateItTotal.replaceWith(totalPopulation.toLocaleString('en-US'));
+spanCalculateItAverage.replaceWith(avgAmountPopulation.toLocaleString('en-US'));
