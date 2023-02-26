d0104a201d8bfb8b5609685394120423f3f25c4c
Yelisieiev Kim <rbv271@outlook.com>
2023-02-26 15:41:09 +0200

    total and average population counted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const populationList = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const totalPop = [...populationList].reduce((sum, pop) => (
+  sum + +pop.innerText.split(',').join('')
+), 0);
+const averagePop = totalPop / populationList.length;
+
+totalPopulation.innerText = totalPop.toLocaleString('en-US');
+averagePopulation.innerText = averagePop.toLocaleString('en-US');
