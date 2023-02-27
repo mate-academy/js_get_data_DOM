40979132105ea5d453b6ef515747bfa60115746a
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-02-27 18:24:33 +0200

    Task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const countries = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+let total = 0;
+let average = 0;
+
+for (const country of countries) {
+  total += +country.innerText.split(',').join('');
+}
+
+average = total / countries.length;
+
+totalPopulation.textContent = total.toLocaleString('en-US');
+averagePopulation.textContent = average.toLocaleString('en-US');
