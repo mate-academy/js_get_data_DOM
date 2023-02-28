99e6604ba3a30e5241289bf99816412518727256
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-02-28 18:24:42 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const totalField = document.querySelector('.total-population');
+const averageField = document.querySelector('.average-population');
+const populations = [...document.querySelectorAll('.population')];
+
+const sumOfPopulations = populations.reduce((sum, current) => (
+  sum + Number(current.innerText.replaceAll(',', ''))
+), 0);
+
+const averagePopulation = (sumOfPopulations / populations.length);
+
+totalField.textContent = sumOfPopulations.toLocaleString('en-US');
+averageField.textContent = averagePopulation.toLocaleString('en-US');
