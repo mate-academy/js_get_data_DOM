53b28815ddbf19dab65a51b2a3a073b49da93e80
Ilona Romaniuk <ilona09041983@gmail.com>
2023-02-28 17:52:41 +0200

    Get data DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const texts = document.querySelectorAll('.population');
+const digits = [...texts].map(text => Number(text.innerText.replace(/,/g, '')));
+const totalAmount = digits.reduce((sum, num) => sum + num);
+const averageAmount = Math.round(totalAmount / digits.length);
+const numberFormat = new Intl.NumberFormat('en-US');
+const averagePopulation = numberFormat.format(averageAmount);
+const totalPopulation = numberFormat.format(totalAmount);
+
+document.querySelector('.total-population').textContent = totalPopulation;
+document.querySelector('.average-population').textContent = averagePopulation;
