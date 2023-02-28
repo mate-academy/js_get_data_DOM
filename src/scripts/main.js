214c5886fabeea899200cf222eaf207c6df7fd2f
Sergey Mikhalskiy <msa1991work@gmail.com>
2023-02-28 15:08:46 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const total = [...population].reduce((acc, el) => {
+  return acc + +(el.textContent.split(',').join(''));
+}, 0);
+
+const average = total / population.length;
+
+const getSeparatedNumbers = number => number.toLocaleString('en-US');
+
+totalPopulation.textContent = getSeparatedNumbers(total);
+averagePopulation.textContent = getSeparatedNumbers(average);
