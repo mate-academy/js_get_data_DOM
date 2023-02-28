30e260f2ac25dbd47d6cfdc1703eae7a5dd09d3a
Serhii Nikolin <sergeynikolin@gmail.com>
2023-02-28 20:45:32 +0200

    Code edited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,14 @@
 'use strict';
 
 const popArr = document.querySelectorAll('.population');
-let total = 0;
+const totalInfo = document.querySelector('.total-population');
+const averageInfo = document.querySelector('.average-population');
 
-for (const item of popArr) {
-  total += +item.textContent.split(',').join('');
-}
+const total = [...popArr].reduce((prev, curr) => {
+  return prev + +curr.textContent.split(',').join('');
+}, 0);
 
 const average = total / popArr.length;
 
-function DecimalFormat(number) {
-  const stringArr = `${number}`.split('');
-  const numbArr = [];
-
-  while (stringArr.length >= 3) {
-    numbArr.unshift(stringArr.splice(-3, 3).join(''));
-  }
-
-  if (stringArr.length > 0) {
-    const rest = stringArr.length;
-
-    numbArr.unshift(stringArr.splice(-rest, rest).join(''));
-  }
-
-  return numbArr.join(',');
-}
-
-const totalForm = DecimalFormat(total);
-const averageForm = DecimalFormat(average);
-
-document.querySelector('.total-population').textContent = totalForm;
-document.querySelector('.average-population').textContent = averageForm;
+totalInfo.textContent = total.toLocaleString('en-us');
+averageInfo.textContent = average.toLocaleString('en-us');
