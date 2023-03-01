2176d43330bd15401e6c4ea0f01118037933d653
Vadim Drobyazko <volandemortuwka@gmail.com>
2023-03-01 17:16:14 +0200

    data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const item = document.querySelectorAll('.population');
+const newStorke = [...item]
+  .map(type => type.innerHTML)
+  .map(type => parseInt(type.split(',').join('')));
+
+const itemTotal = newStorke.reduce((a, b) => a + b);
+const itemAvarage = itemTotal / newStorke.length;
+
+const newTotal = document.querySelector('.total-population');
+const newAvarage = document.querySelector('.average-population');
+
+newTotal.textContent = itemTotal.toLocaleString('en-US');
+newAvarage.textContent = itemAvarage.toLocaleString('en-US');
