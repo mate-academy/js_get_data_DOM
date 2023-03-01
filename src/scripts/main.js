2ea0ce87b61465d7c577a4dabb5f4e7f80b045b1
Vladyslav Ashurov <ashurovvladislav@gmail.com>
2023-03-01 23:00:24 +0200

    Calculator finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const numbers = Array.from(document.querySelectorAll('.population')).map(item =>
+  item.innerHTML.split(',').join('')
+);
+
+const total = numbers.reduce((sum, item) => sum + +item, 0);
+const average = total / numbers.length;
+
+const totalHtml = document.querySelector('.total-population');
+const averageHtml = document.querySelector('.average-population');
+
+totalHtml.innerHTML = total.toLocaleString('en-US');
+averageHtml.innerHTML = average.toLocaleString('en-US');
