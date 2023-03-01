fc406f3a4ef34c1e9e9a9fb4e47ed9893f35c707
Vadim Drobyazko <volandemortuwka@gmail.com>
2023-03-01 19:02:57 +0200

    JS Get Data v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,13 @@
 
 const item = document.querySelectorAll('.population');
 const newStorke = [...item]
-  .map(type => type.innerHTML)
-  .map(type => parseInt(type.split(',').join('')));
+  .map(type => parseInt(type.innerHTML.split(',').join('')));
 
 const itemTotal = newStorke.reduce((a, b) => a + b);
 const itemAvarage = itemTotal / newStorke.length;
 
-const newTotal = document.querySelector('.total-population');
-const newAvarage = document.querySelector('.average-population');
+const TotalElement = document.querySelector('.total-population');
+const avarageElement = document.querySelector('.average-population');
 
-newTotal.textContent = itemTotal.toLocaleString('en-US');
-newAvarage.textContent = itemAvarage.toLocaleString('en-US');
+TotalElement.textContent = itemTotal.toLocaleString('en-US');
+avarageElement.textContent = itemAvarage.toLocaleString('en-US');
