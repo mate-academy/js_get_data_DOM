63880292931526e015bb8985070b099f5f0f8bbc
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-03-02 00:02:12 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,16 +4,11 @@ const populationItem = document.querySelectorAll('.population');
 
 const arrayOfNumbers = [...populationItem].map(item =>
   strToNum(item.innerText)).filter(num => !isNaN(num));
-// console.log(arrayOfNumbers);
 
 const total = arrayOfNumbers.reduce((accumulator, currentValue) =>
   accumulator + currentValue);
-// console.log(totalSum);
 
 const result = total.toLocaleString('en-US');
-// для форматирования числового значения total в строку
-// такое форматирование включает в себя разделение чисел запятым
-// console.log(result);
 
 const average = total / arrayOfNumbers.length;
 
@@ -25,13 +20,7 @@ function strToNum(str) {
 
 const totalPop = document.querySelector('.total-population');
 
-// выбор элемента на веб-странице с классом total-population
-// Элемент, найденный с помощью этого метода, сохраняется в переменной totalPop.
-// console.dir(totalPop);
-
 totalPop.textContent = result;
-// значение, содержащееся в переменной result,
-// будет присвоено для свойства textContent в объект totalPop
 
 const averagePop = document.querySelector('.average-population');
 
