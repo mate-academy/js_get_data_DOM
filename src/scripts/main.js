ac267b37a0cc31fb1330a134f335678374a59219
Tolik Petras <toliketras4@gmail.com>
2023-03-02 19:36:04 +0200

    solution fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,30 +5,13 @@ const sources = document.getElementsByClassName('population');
 const allPopulations = [...sources].map(country =>
   +country.innerText.split(',').join(''));
 
-const totalNumbers = allPopulations.reduce((accum, current) =>
-  accum + current, 0).toString().split('');
+const total = allPopulations.reduce((accum, current) =>
+  accum + current, 0);
 
-const averageNumbers = (totalNumbers.join('') / allPopulations.length)
-  .toString().split('');
-
-function separateByThousand(arr) {
-  let str = '';
-
-  for (let i = arr.length - 1; i >= 0; i--) {
-    if ((arr.length - i) % 3 === 0 && i !== 0) {
-      str += arr[i];
-
-      str += ',';
-    } else {
-      str += arr[i];
-    }
-  }
-
-  return str.split('').reverse().join('');
-}
+const average = (total / allPopulations.length);
 
 document.getElementsByClassName('total-population')[0].innerText
-  = separateByThousand(totalNumbers);
+  = total.toLocaleString('en-US');
 
 document.getElementsByClassName('average-population')[0].innerText
-  = separateByThousand(averageNumbers);
+  = average.toLocaleString('en-US');
