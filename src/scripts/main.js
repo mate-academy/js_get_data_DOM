8ec93ab59a56d8c7a0b7b1f502fc4376aa01bba7
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-03-03 16:34:14 +0200

    Refiniments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,13 @@
 'use strict';
 
 const list = document.querySelectorAll('.population');
+const listArray = [...list];
 
-let sum = 0;
-let count = 0;
-
-for (const item of list) {
-  sum += Number(item.innerText.replaceAll(',', ''));
-  count++;
-}
+const total = listArray.reduce((sum, e) =>
+  Number(e.innerText.replaceAll(',', '')) + sum, 0);
 
 const totalElement = document.querySelector('.total-population');
 const average = document.querySelector('.average-population');
 
-totalElement.innerText = sum.toLocaleString();
-average.innerText = (sum / count).toLocaleString();
+totalElement.innerText = total.toLocaleString();
+average.innerText = (total / list.length).toLocaleString();
