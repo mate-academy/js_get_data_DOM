ccb62e2d92964c7c0a7e02a5a12b983d336df0c9
Maindloll <valeriy.mikheyev@gmail.com>
2023-03-06 17:54:30 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const allSpan = document.querySelectorAll('.population');
+const sumSpan = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+let sum = 0;
+let count = 0;
+
+for (const span of allSpan) {
+  const number = span.innerHTML.split(',').join('');
+
+  sum += +number;
+  count++;
+};
+
+const totalAverage = sum / count;
+
+sumSpan.innerText = `${sum.toLocaleString('en-US')}`;
+average.innerText = `${totalAverage.toLocaleString('en-US')}`;
