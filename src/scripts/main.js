5be1d9ef432e2bd24bd821866e49762f2253dde5
anton_chornobai <antonchornobajj@gmail.com>
2023-03-11 10:21:50 +0100

    task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // write your code here
+
+const population = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const avarage = document.querySelector('.average-population');
+
+function totalPopulation(collection) {
+  const arrayPopulation = [...collection];
+  let sum = 0;
+
+  const numbers = arrayPopulation.map(elem => elem.innerHTML);
+
+  for (const number of numbers) {
+    const parsedNumber = parseFloat(number.replace(/,/g, ''));
+
+    sum += parsedNumber;
+  }
+
+  return sum;
+}
+
+const avarageAge = totalPopulation(population) / population.length;
+
+total.textContent = totalPopulation(population);
+avarage.innerHTML = avarageAge;
