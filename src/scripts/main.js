3d18959cd551920152378aca27132317e474878a
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-13 16:11:23 +0100

    fix task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,17 @@
 'use strict';
 
-const allNum = document.querySelectorAll('.population');
+const population = document.querySelectorAll('.population');
 const spanTotal = document.querySelector('.total-population');
 const spanAverage = document.querySelector('.average-population');
-const arrNum = [];
+const arrWithPopulation = [...population]
+  .map(element => +element.innerHTML.replace(/,/g, ''));
 
-for (const value of allNum) {
-  const trueValue = +value.innerHTML.replace(/,/g, '');
-
-  arrNum.push(trueValue);
-}
-
-const total = arrNum.reduce((a, b) => a + b, 0);
+const total = arrWithPopulation.reduce((a, b) => a + b, 0);
 const average = total / 10;
 
 function getFormatNum(num) {
-  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+  return num.toLocaleString('en-US');
 }
 
-const rightTotal = getFormatNum(total);
-const rightAverage = getFormatNum(average);
-
-spanTotal.textContent = rightTotal;
-spanAverage.textContent = rightAverage;
+spanTotal.textContent = getFormatNum(total);
+spanAverage.textContent = getFormatNum(average);
