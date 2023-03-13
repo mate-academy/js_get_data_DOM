95f261145ee771fc96ea5d0deaf120bb3b827dff
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-13 15:02:32 +0100

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const allNum = document.querySelectorAll('.population');
+const spanTotal = document.querySelector('.total-population');
+const spanAverage = document.querySelector('.average-population');
+const arrNum = [];
+
+for (const value of allNum) {
+  const trueValue = +value.innerHTML.replace(/,/g, '');
+
+  arrNum.push(trueValue);
+}
+
+const total = arrNum.reduce((a, b) => a + b, 0);
+const average = total / 10;
+
+function getFormatNum(num) {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+}
+
+const rightTotal = getFormatNum(total);
+const rightAverage = getFormatNum(average);
+
+spanTotal.textContent = rightTotal;
+spanAverage.textContent = rightAverage;
