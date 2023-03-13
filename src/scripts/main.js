0e5eef372919d74a70476d512321f111d424f8ba
Nazar <nazar378378379379@gmail.com>
2023-03-13 10:34:38 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
+const populations = [...document.querySelectorAll('.population')];
+const averageSpan = document.querySelector('.average-population');
+const totalSpan = document.querySelector('.total-population');
+
+const totalPop = populations.reduce((sum, pop) => (
+  sum + +pop.innerText.split(',').join('')
+), 0);
+
+const averagePop = totalPop / populations.length;
+
+averageSpan.innerText = averagePop.toLocaleString('en-US');
+totalSpan.innerText = totalPop.toLocaleString('en-US');
+
 // write your code here
