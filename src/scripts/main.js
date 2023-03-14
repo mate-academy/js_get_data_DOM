7540c2fa378d2889c1a0b423ebeb7548e08ef574
Kocherha Roman <f.your.d.2017@gmail.com>
2023-03-14 14:10:07 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const contry = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+const transformToNum = [...contry]
+  .map(item => +item.innerText.split(',').join(''));
+
+const sumCount = transformToNum
+  .reduce((acc, num) => acc + num, 0);
+
+const averageCount = sumCount / transformToNum.length;
+
+total.innerText = sumCount;
+average.innerText = averageCount;
