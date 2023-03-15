10734771ecb115c2810065e958c5536c01816283
Serhii Melnychenko <8melya8@gmail.com>
2023-03-15 18:57:10 +0200

    all fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,8 +6,8 @@ const populations = [...items].map(item => item.innerText.split(',').join(''));
 
 const result = populations.reduce((sum, item) => sum + +item, 0);
 
-const averagePop = document.querySelector('.average-population');
-const totalPop = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+const total = document.querySelector('.total-population');
 
-totalPop.textContent = result.toLocaleString('en-US');
-averagePop.textContent = (result / populations.length).toLocaleString('en-US');
+total.textContent = result.toLocaleString('en-US');
+average.textContent = (result / populations.length).toLocaleString('en-US');
