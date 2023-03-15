ffbe213fb516230612ba7fe2f2e613bd780814e4
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2023-03-15 13:23:38 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const population = document.querySelectorAll('.population');
+
+function totalSum(numbers) {
+  let sum = 0;
+
+  for (const pop of numbers) {
+    const countryPopulation = pop.textContent.split(',').join('');
+
+    sum += +countryPopulation;
+  }
+
+  return +sum;
+}
+totalSum(population);
+
+function numberWithCommas(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+const averageSum = totalSum(population) / population.length;
+
+totalPopulation.textContent = numberWithCommas(totalSum(population));
+averagePopulation.textContent = numberWithCommas(averageSum);
