e475a9b538e62d55fc51b9d71a12780ec115ac58
Sergey Lipikhin <football3@ukr.net>
2023-03-19 19:44:11 +0200

    Complete solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+function convertPopulationToNumber(populationString) {
+  return Number(populationString.split(',').join(''));
+}
+
+const populationElements = document.querySelectorAll('span.population');
+const populationList = [...populationElements]
+  .map(({ textContent }) => convertPopulationToNumber(textContent));
+
+const totalPopulation = populationList
+  .reduce((sum, value) => sum + value);
+const averagePopulation = totalPopulation / populationList.length;
+
+const totalPopulationElement = document.querySelector('span.total-population');
+const averagePopulationElement
+  = document.querySelector('span.average-population');
+
+totalPopulationElement.textContent = totalPopulation.toLocaleString('en');
+averagePopulationElement.textContent = averagePopulation.toLocaleString('en');
