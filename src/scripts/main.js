4187c92346214c685d55443de851a1586b423a14
Nazar Vorona <voron1765@gmail.com>
2023-03-22 02:57:42 +0200

    Task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+function numberWithCommas(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+const population = document.querySelectorAll('.population');
+
+const arrPopulation = [...population];
+let total = 0;
+
+for (const chap of arrPopulation) {
+  const country = chap.textContent.split('');
+
+  let temporaryStr = '';
+
+  for (const numb of country) {
+    if (numb !== ',') {
+      temporaryStr += numb;
+    }
+  }
+  total += Number(temporaryStr);
+}
+
+const average = total / arrPopulation.length;
+const addTotal = document.querySelector('.total-population');
+
+addTotal.innerHTML = `${numberWithCommas(total)}`;
+
+const addAverage = document.querySelector('.average-population');
+
+addAverage.innerHTML = `${numberWithCommas(average)}`;
