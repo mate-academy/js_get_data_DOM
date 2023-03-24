191e60b84f103a8f18270a1ba518729097aaeafa
Daria <hamdsad8@gmail.com>
2023-03-24 13:10:36 +0200

    created average and total data calculator

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('span.population');
+const textData = [...populations].map(item => item.innerText);
+const numsWithoutCommas = [];
+
+for (const country of textData) {
+  const currentCountry = country.split('');
+
+  numsWithoutCommas.push(currentCountry
+    .filter(item => item !== ',')
+    .join('')
+  );
+}
+
+const textDataToNumber = numsWithoutCommas.map(item => Number(item));
+
+const totalPopulationNumber = textDataToNumber
+  .reduce((acc, item) => acc + item, 0)
+  .toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+const averagePopulationNumber = (textDataToNumber
+  .reduce((acc, item) => acc + item, 0)
+  / textDataToNumber.length)
+  .toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+const total = document.querySelector('span.total-population');
+
+total.innerText = totalPopulationNumber;
+
+const avarage = document.querySelector('span.average-population');
+
+avarage.innerText = averagePopulationNumber;
