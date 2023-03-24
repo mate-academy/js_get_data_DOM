512f6fcf14e588e8e8e4282525eaad2b28770299
Lilia Obushenko <liliaobushenko@gmail.com>
2023-03-24 18:51:07 +0200

    reduce was used

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,12 @@ const populations = document.querySelectorAll('.population');
 const totalNumber = document.querySelector('.total-population');
 const averageNumber = document.querySelector('.average-population');
 
-const populationNumbers = [...populations].map(element =>
-  Number(element.innerText.split(',').join('')));
+const totalPopulation = [...populations].reduce((sum, element) =>
+  sum + Number(element.innerText.split(',').join('')), 0);
 
-const totalPopulation = populationNumbers.reduce(
-  (sum, element) => sum + element, 0);
-
-const averagePopulation = totalPopulation / populations.length;
+const averagePopulation = populations.length === 0
+  ? 0
+  : totalPopulation / populations.length;
 
 totalNumber.innerText = totalPopulation.toLocaleString('en-US');
 averageNumber.innerText = averagePopulation.toLocaleString('en-US');
