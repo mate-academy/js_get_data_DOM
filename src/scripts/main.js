935ab3fc2302dd51dafbc6874d618ada0390d044
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-03-25 19:01:51 +0200

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const textCollection = document.querySelectorAll('.population');
+
+const arr = [...textCollection];
+const textArray = [];
+
+for (const item of arr) {
+  const textData = item.textContent;
+  const digitData = Number(textData.split(',').join(''));
+
+  textArray.push(digitData);
+}
+
+const total = textArray.reduce((start, acc) => (start + acc), 0);
+const average = total / textArray.length;
+
+const modifiedTotal = total.toLocaleString('en-Us');
+const modifiedAverage = average.toLocaleString('en-Us');
+
+const totalCalculate = document.querySelector('.total-population');
+const averageCalculate = document.querySelector('.average-population');
+
+totalCalculate.textContent = modifiedTotal;
+averageCalculate.textContent = modifiedAverage;
