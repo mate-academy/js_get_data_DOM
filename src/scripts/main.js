de6fbab2527d1a9fd8346e6502210d6630a886bd
Anna Grechana <grechanaanna@gmial.com>
2023-03-28 00:21:55 +0300

    1st version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
+const populationsAll = document.body.children[0].children[1].children
+
+let total = 0
+let amountOfCountries=0
+
+for(let el of populationsAll){
+    let population = (el.children[1].textContent)
+    let populationNum = +(population.replace(/,/g, ''))
+    total= total+populationNum
+    amountOfCountries +=1
+}
+
+let average = (total/amountOfCountries).toLocaleString('en-US')
+let totalWithSeparator = total.toLocaleString('en-US')
+
+
+let totalPopulation = document.querySelector('.total-population')
+totalPopulation.innerHTML = totalWithSeparator;
+let averagePopulation = document.querySelector('.average-population')
+averagePopulation.innerHTML = average;
+
+
 
 // write your code here
