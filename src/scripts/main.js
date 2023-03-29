d2f942a617d5e92d9ded82ac19a9c7c05eb4f101
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-03-29 19:54:14 +0300

    one variable was removed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,19 @@
 'use strict';
 
 let total = 0;
-let number = 0;
 const data = document.querySelectorAll('.population');
 
 for (const country of data) {
   const numbers = country.textContent.replace(/,/g, '');
 
   total += Number(numbers);
-  number++;
 }
 
 const totalPopulation = document.querySelector('.total-population');
 const avaragePopulation = document.querySelector('.average-population');
 
 totalPopulation.innerHTML = formatNumber(total);
-avaragePopulation.innerHTML = formatNumber(total / number);
+avaragePopulation.innerHTML = formatNumber(total / data.length);
 
 function formatNumber(startNumber) {
   return startNumber.toLocaleString('en-US');
