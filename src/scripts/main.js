a5773ccb4ffefd98e40cd632d33610b5be368f86
Anna Grechana <grechanaanna@gmial.com>
2023-03-29 20:57:09 +0300

    add task solutio1111111n

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,26 @@
 'use strict';
-const populationsAll = document.body.children[0].children[1].children
 
-let total = 0
-let amountOfCountries=0
+const populationsAll = document.body.children[0].children[1].children;
 
+let total = 0;
+let amountOfCountries = 0;
 
-for(let el of populationsAll){
-    let population = (el.children[1].textContent)
-    let populationNum = +(population.replace(/,/g, ''))
-    total= total+populationNum
-    amountOfCountries +=1
+for (const el of populationsAll) {
+  const population = (el.children[1].textContent);
+  const populationNum = +(population.replace(/,/g, ''));
+
+  total += populationNum;
+  amountOfCountries += 1;
 }
 
-let average = (total/amountOfCountries).toLocaleString('en-US')
-let totalWithSeparator = total.toLocaleString('en-US')
+const average = (total / amountOfCountries).toLocaleString('en-US');
+const totalWithSeparator = total.toLocaleString('en-US');
 
+const totalPopulation = document.querySelector('.total-population');
 
-let totalPopulation = document.querySelector('.total-population')
 totalPopulation.innerHTML = totalWithSeparator;
-let averagePopulation = document.querySelector('.average-population')
+
+const averagePopulation = document.querySelector('.average-population');
+
 averagePopulation.innerHTML = average;
 // write your code here
