57f5abaffaee22d35aa958669d724ba11046501e
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-03-29 18:46:51 +0300

    task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+let total = 0;
+let number = 0;
+const data = document.querySelectorAll('.population');
+
+for (const country of data) {
+  const numbers = country.textContent.replace(/,/g, '');
+
+  total += Number(numbers);
+  number++;
+}
+
+const totalPopulation = document.querySelector('.total-population');
+const avaragePopulation = document.querySelector('.average-population');
+
+totalPopulation.innerHTML = formatNumber(total);
+avaragePopulation.innerHTML = formatNumber(total / number);
+
+function formatNumber(startNumber) {
+  return startNumber.toLocaleString('en-US');
+}
