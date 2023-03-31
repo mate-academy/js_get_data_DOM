593d668d0b5fc2c70c678982083cdea765acf481
metja <mikolapenzanik@gmail.com>
2023-03-31 11:57:19 +0300

    Comleted task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+const totalPopulation = [...population].reduce((acc, item) => {
+  const valuePopulation = +item.textContent.split(',').join('');
+
+  // eslint-disable-next-line no-param-reassign
+  return (acc += valuePopulation);
+}, 0);
+
+total.textContent = totalPopulation.toLocaleString('en-US');
+
+average.textContent
+  = (totalPopulation / population.length).toLocaleString('en-US');
