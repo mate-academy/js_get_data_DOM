4519840d4086183643a6f77864a982bedb51503f
Anna Peretiatko <tobossanna@gmail.com>
2023-04-01 16:32:23 +0200

    Solution v.1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write your code here
+
+const spanCollection = document.querySelectorAll('.population');
+const allData = [];
+let finalNumbersData;
+
+for (const number of spanCollection) {
+  const textData = number.textContent;
+
+  allData.push(textData);
+  finalNumbersData = allData.map(num => Number(num.replace(/,/g, '')));
+}
+
+const total = finalNumbersData.reduce((sum, x) => sum + x, 0);
+const average = total / finalNumbersData.length;
+
+const totalValue = document.querySelector('.total-population');
+const avrgValue = document.querySelector('.average-population');
+
+totalValue.textContent = total.toLocaleString();
+avrgValue.textContent = average.toLocaleString();
