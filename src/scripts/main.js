6a4726ef77262da2ab78bf2130d500648cfc0213
Anna Peretiatko <tobossanna@gmail.com>
2023-04-01 12:16:06 +0200

    GetDataDOM solution v.2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,5 @@
 'use strict';
 
-// Steps to do this challenge:
-// 1. Get all text data from `span` with class `population`
-// 2. Make sure that the given string can be converted to a number
-// and convert it to number.
-// 3. Calculate average and total value-based to parsed numbers.
-// 4. Replace `Calculate it!` with calculated average numbers in
-// `span` with classes `average-population` and `total-population`.
-// 5. Calculated `average` and `total` numbers must be separated
-// by a [thousands separator]
-// (https://chortle.ccsu.edu/java5/Notes/chap24B/ch24B_9.html)
-// (same numeric style as populations presented).
-// 6. Congrats, mate! You did it!
-
 // write your code here
 
 const spanCollection = document.querySelectorAll('.population');
