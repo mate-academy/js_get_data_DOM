ef80694b7096c7693d5db447c6abe6f8d9297a9f
Oksana Tytanych <oksanatytanych@gmail.com>
2023-04-03 20:42:09 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const populationArray = document.querySelectorAll('[class=population]');
+const populationInCountries = [...populationArray].map(el =>
+  el.innerText.split(',').join(''));
+
+const sum = populationInCountries.reduce((res, el) => +el + res, 0);
+const average = sum / populationInCountries.length;
+
+document.querySelector('[class=total-population]').textContent
+  = new Intl.NumberFormat('ja-JP').format(sum);
+
+document.querySelector('[class=average-population]').textContent
+  = new Intl.NumberFormat('ja-JP').format(average);
