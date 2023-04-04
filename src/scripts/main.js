d7aa08ffee45517bccb3e95fdd137008c6050696
Yurii Hlushenko <yauglushenko@gmail.com>
2023-04-05 00:07:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+const populations = [...spans].map(span =>
+  +span.innerText.replace(/[^\d]/g, ''));
+const spanTotal = document.querySelector('.total-population');
+const spanAverage = document.querySelector('.average-population');
+const total = populations.reduce((sum, population) => sum + population);
+
+spanTotal.innerText = addComa(total);
+spanAverage.innerText = addComa(total / populations.length);
+
+function addComa(num) {
+  const x = num.toString().split('').reduce((sum, n, index) =>
+    (num.toString().length - index) % 3 === 0 ? sum + ',' + n : sum + n);
+
+  return x;
+}
