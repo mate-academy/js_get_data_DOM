8518c93f5edcffde44415664186fad9c1996db04
popeye88 <aleksei.taran@gmail.com>
2023-04-06 15:57:15 +0300

    fix: lint errors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const items = document.querySelectorAll('.population');
-const populations = [...items].map(item => +(item.innerText.replaceAll(',', '')));
+const populations = [...items]
+  .map(item => +(item.innerText.replaceAll(',', '')));
 const total = populations.reduce((prev, curr) => prev + curr, 0);
 const average = total / populations.length;
 
 document.querySelector('.total-population').innerText = total.toLocaleString();
-document.querySelector('.average-population').innerText = average.toLocaleString();
+
+(document
+  .querySelector('.average-population').innerText
+) = average.toLocaleString();
