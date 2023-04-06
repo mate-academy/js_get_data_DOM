ae80c7227be05bfd018a459a457ce3f5098c1f26
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-06 22:14:04 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
+const [...spanPop] = document.querySelectorAll('.population');
+
+let totalText = document.querySelector('.total-population') ;
+let averageText = document.querySelector('.average-population');
+
+const arrNumbers = spanPop.map(people => Number(people.innerText.split(',').join('')));//
+
+const totalPeople = arrNumbers.reduce((a, b) => a + b);
+
+const average = totalPeople / spanPop.length;
+
+function addSeparator(num) {
+  const qwe = String(num).split('');
+ 
+  for (let i = qwe.length - 1; i >= 0 ; i -= 3) {
+    qwe[i - 3] += ',';
+
+  }
+ 
+  return qwe.join(''); 
+}
+
+totalText.innerText = addSeparator(totalPeople);
+
+averageText.innerText = addSeparator(average);
+
+
+
 // write your code here
