8ecf2969116c7ae73a58a8895fb189fbe178736e
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-06 22:18:54 +0300

    Fix readme.md

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,12 @@
 
 const [...spanPop] = document.querySelectorAll('.population');
 
-let totalText = document.querySelector('.total-population') ;
-let averageText = document.querySelector('.average-population');
+const totalText = document.querySelector('.total-population');
+const averageText = document.querySelector('.average-population');
 
-const arrNumbers = spanPop.map(people => Number(people.innerText.split(',').join('')));//
+const arrNumbers = spanPop.map(people =>
+  Number(people.innerText.split(',').join(''))
+);//
 
 const totalPeople = arrNumbers.reduce((a, b) => a + b);
 
@@ -13,19 +15,16 @@ const average = totalPeople / spanPop.length;
 
 function addSeparator(num) {
   const qwe = String(num).split('');
- 
-  for (let i = qwe.length - 1; i >= 0 ; i -= 3) {
-    qwe[i - 3] += ',';
 
+  for (let i = qwe.length - 1; i >= 0; i -= 3) {
+    qwe[i - 3] += ',';
   }
- 
-  return qwe.join(''); 
+
+  return qwe.join('');
 }
 
 totalText.innerText = addSeparator(totalPeople);
 
 averageText.innerText = addSeparator(average);
-
-
 
 // write your code here
