9a42f83d6e3f2244f657934d9bf6989470e02f4f
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-07 10:25:10 +0300

    Fix total and average

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,24 +7,12 @@ const averageText = document.querySelector('.average-population');
 
 const arrNumbers = spanPop.map(people =>
   Number(people.innerText.split(',').join(''))
-);//
+);
 
 const totalPeople = arrNumbers.reduce((a, b) => a + b);
 
 const average = totalPeople / spanPop.length;
 
-function addSeparator(num) {
-  const qwe = String(num).split('');
+totalText.innerText = totalPeople.toLocaleString();
 
-  for (let i = qwe.length - 1; i >= 0; i -= 3) {
-    qwe[i - 3] += ',';
-  }
-
-  return qwe.join('');
-}
-
-totalText.innerText = addSeparator(totalPeople);
-
-averageText.innerText = addSeparator(average);
-
-// write your code here
+averageText.innerText = average.toLocaleString();
