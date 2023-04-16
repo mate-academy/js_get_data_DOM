a40a599c561f51434ad1571934e8805ada35b993
адмін <admin@Mac-Max.local>
2023-04-16 16:32:30 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+
+const populations = [];
+
+spans.forEach(span => {
+  populations.push(span.textContent);
+});
+
+const populationValue = [];
+
+populations.forEach(population => {
+  populationValue.push(parseInt(population.replace(/,/g, '')));
+});
+
+const totalPopulation = populationValue.reduce((a, b) => a + b, 0);
+
+const numberOfCountries = populationValue.length;
+const averagePopulation = totalPopulation / numberOfCountries;
+
+const formattedTotalPopulation = totalPopulation.toLocaleString('en-US');
+const formattedAveragePopulation = averagePopulation.toLocaleString('en-US');
+
+document.querySelector('.total-population')
+  .textContent = formattedTotalPopulation;
+
+document.querySelector('.average-population')
+  .textContent = formattedAveragePopulation;
