1a6344e8d12270a0ba640086fb271330cdd6855c
Zibi95 <sylwester.sob95@gmail.com>
2023-04-16 14:22:49 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
+/* eslint-disable max-len */
 'use strict';
 
-// write your code here
+function getNumbersFromNodes(nodeList) {
+  const numbersArray = [];
+
+  for (const node of nodeList) {
+    const number = +node.textContent.split(',').join('');
+
+    numbersArray.push(number);
+  }
+
+  return numbersArray;
+}
+
+function getTotalAndAverage(numbersArray) {
+  const total = numbersArray.reduce((acc, curr) => acc + curr);
+  const average = total / numbersArray.length;
+
+  return [total, average];
+}
+
+function displayAverageAndTotal() {
+  const populationList = document.querySelectorAll('.population');
+  const totalPopulationElement = document.querySelector('.total-population');
+  const averagePopulationElement = document.querySelector('.average-population');
+
+  const numbersArray = getNumbersFromNodes(populationList);
+  const [total, average] = getTotalAndAverage(numbersArray);
+
+  totalPopulationElement.textContent = total.toLocaleString('en-us');
+  averagePopulationElement.textContent = average.toLocaleString('en-us');
+}
+
+displayAverageAndTotal();
