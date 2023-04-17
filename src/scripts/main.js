bc23412485c1c685d172b9eeccaa5433359bf667
Mikołaj <ostermi@wp.pl>
2023-04-17 17:51:00 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+function thousandsSeparator(number) {
+  const numberStringified = String(number);
+  const numberParts = [];
+  let currentEnd = numberStringified.length % 3
+    ? numberStringified.length % 3 : 3;
+  let currentStart = 0;
+
+  while (currentStart <= numberStringified.length - 3) {
+    numberParts.push(numberStringified.slice(currentStart, currentEnd));
+    currentStart = currentEnd;
+    currentEnd += 3;
+  }
+
+  return numberParts.join(',');
+}
+
+const populationAmounts = document.querySelectorAll('.population');
+const totalPopulationAmount = document.querySelector('.total');
+const averagePopulationAmount = document.querySelector('.average');
+
+let populationAmountSum = 0;
+
+populationAmounts.forEach(populationAmount => {
+  populationAmountSum += Number(populationAmount.textContent
+    .split(',').join(''));
+});
+
+totalPopulationAmount.textContent
+= `Total: ${thousandsSeparator(populationAmountSum)}`;
+
+averagePopulationAmount.textContent
+  = `Average: ${thousandsSeparator(populationAmountSum
+  / populationAmounts.length)}`;
