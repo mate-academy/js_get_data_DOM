4d9f49fb1987f1a894167b5aa9fccd9ad3b3c038
Alla Gutsul <allagutsul1990@gmail.com>
2023-04-18 23:27:58 +0300

    add total and average population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+
+const arrayOfPopulations = [...populations].map(
+  population => +population.textContent.split(',').join(''));
+
+const totalPopulation = arrayOfPopulations.reduce(
+  (acc, population) => acc + population, 0);
+const totalNumOfPopulation = document.querySelector('.total-population');
+
+totalNumOfPopulation.textContent = formatNumber(totalPopulation);
+
+const averagePopulation = document.querySelector('.average-population');
+const averageNumOfPopulation = totalPopulation / arrayOfPopulations.length;
+
+averagePopulation.textContent = formatNumber(averageNumOfPopulation);
+
+function formatNumber(num) {
+  const reversedNum = String(num).split('').reverse();
+  let count = 0;
+  let formattedNum = '';
+
+  for (let i = 0; i < reversedNum.length; i++) {
+    if (count > 2) {
+      formattedNum += ',';
+      count = 0;
+    }
+
+    formattedNum += reversedNum[i];
+    count++;
+  }
+
+  return formattedNum.split('').reverse().join('');
+}
