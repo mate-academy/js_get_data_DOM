2026cfda9f7500f50b7192a218692609935f768a
Antonio S <soi4.toxa@ukr.net>
2023-04-25 17:07:35 +0300

    Populations-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const populations = [...document.querySelectorAll('span.population')]
+  .map(span => +span.textContent
+    .split(',')
+    .join(''));
+
+const totalPop = populations.reduce((sum, pop) => sum + pop);
+const averagePop = totalPop / populations.length;
+
+const spanTotalPop = document.querySelector('span.total-population');
+const spanAveragePop = document.querySelector('span.average-population');
+
+spanTotalPop.innerHTML = totalPop.toLocaleString();
+spanAveragePop.innerHTML = averagePop.toLocaleString();
