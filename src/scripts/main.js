cf7955f2623abe636e09013cf30848b65e9b7a9b
Maria Shmakova <masha.shmakova@gmail.com>
2023-04-26 13:31:48 +0300

    Calculation were created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('.population');
+const texts = [...list].map(item => item.innerText);
+const numbers = texts.map(str => +str.split(',').join(''));
+const total = numbers.reduce((sum, n) => sum + n, 0);
+const everage = total / numbers.length;
+
+document.querySelector('.total-population').textContent
+  = total.toLocaleString();
+
+document.querySelector('.average-population').textContent
+  = everage.toLocaleString();
