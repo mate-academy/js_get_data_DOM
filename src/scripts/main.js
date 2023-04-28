e05c93af7dd182b69dbe7123953475303628ea4d
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-04-28 22:54:53 +0200

    after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,12 @@
+/* eslint-disable max-len */
 'use strict';
 
 const totalOutput = document.querySelector('.total-population');
 const averageOutput = document.querySelector('.average-population');
 const populationCollection = document.querySelectorAll('.population');
 
-const populationArray = [...populationCollection].map(el => {
-  return +el.textContent.replaceAll(',', '');
-});
-
-const totalPopulation = populationArray.reduce((total, next) => total + next);
-const averagePopulation = totalPopulation / populationArray.length;
+const totalPopulation = [...populationCollection].reduce((total, next) => total + Number(next.innerText.replaceAll(',', '')), 0);
+const averagePopulation = totalPopulation / populationCollection.length;
 
 totalOutput.textContent = totalPopulation.toLocaleString('en-GB');
 averageOutput.textContent = averagePopulation.toLocaleString('en-GB');
