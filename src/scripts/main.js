97f015fc92794c6e1f1f88605a57c81694af9af8
Anton <crossnaders4949@gmail.com>
2023-05-08 22:33:20 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+let count = 0;
+const populationsConv = [ ...populations ];
+
+for (const elem of populationsConv) {
+  const convArr = elem.textContent.split(',').join('');
+
+  count = count + +convArr;
+}
+
+const average = count / populationsConv.length;
+
+totalPopulation.textContent = count;
+averagePopulation.textContent = average;
