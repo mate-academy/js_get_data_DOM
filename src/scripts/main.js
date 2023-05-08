3daa7b8491f763f98f4eb68deda285cb28cf57ec
Anton <crossnaders4949@gmail.com>
2023-05-08 23:39:46 +0200

    Changed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,34 @@
 'use strict';
 
-const populations = document.querySelectorAll('.population');
+function addCommas(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 3 === 0 && i !== 0) {
+      arr[i] = arr[i] + ',';
+    }
+  }
+
+  return arr;
+}
+
+const populationsCollection = document.querySelectorAll('.population');
 const totalPopulation = document.querySelector('.total-population');
 const averagePopulation = document.querySelector('.average-population');
 let count = 0;
-const populationsConv = [ ...populations ];
+const populationsArray = [ ...populationsCollection ];
 
-for (const elem of populationsConv) {
+for (const elem of populationsArray) {
   const convArr = elem.textContent.split(',').join('');
 
   count = count + +convArr;
 }
 
-const average = count / populationsConv.length;
+const average = count / populationsArray.length;
+const reversePopulationArray = String(count).split('').reverse();
+const reverseAverageArray = String(average).split('').reverse();
+const resultFunctPopulation = addCommas(reversePopulationArray);
+const resultFunctAverage = addCommas(reverseAverageArray);
+const resultPopulation = resultFunctPopulation.reverse().join('');
+const resultAverage = resultFunctAverage.reverse().join('');
 
-totalPopulation.textContent = count;
-averagePopulation.textContent = average;
+totalPopulation.textContent = resultPopulation;
+averagePopulation.textContent = resultAverage;
