7b25746e59c9c07bc3888053d8f74338adf2f931
Antonina Klishch <tonya91t@gmail.com>
2023-05-12 17:23:22 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const avaragePopulation = document.querySelector('.average-population');
+
+let total = 0;
+let average = 0;
+
+for (const count of population) {
+  total += Number(count.innerHTML.split(',').join(''));
+  average = total / population.length;
+}
+
+totalPopulation.textContent = total;
+avaragePopulation.textContent = average;
