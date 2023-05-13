693a2f3218bb80d57e5d5277b594720550feed28
Vitaliy Zviriuk <zvirvitalik1991@gmail.com>
2023-05-13 13:10:06 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const items = document.querySelectorAll('.population');
+const texts = [...items].map(
+  item => parseFloat(item.innerText.replace(/,/g, ''))
+);
+
+const totalValue = texts.reduce((sum, item) => sum + item, 0);
+const averageValue = totalValue / texts.length;
+const formattedTotal = totalValue.toLocaleString('en-US');
+const formattedAverage = averageValue.toLocaleString('en-US');
+
+document.querySelector('.average-population').innerHTML = formattedAverage;
+document.querySelector('.total-population').innerHTML = formattedTotal;
