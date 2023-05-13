df04a7f577f99cdb12afc29a3c4808405d773a4f
Bohdan Shumylo <injobs.kharkiv@gmail.com>
2023-05-13 11:25:20 +0300

    Fixed Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const populations = [];
+
+document.querySelectorAll('span.population').forEach(
+  spanElement => !Number.isNaN(spanElement.textContent)
+    ? populations.push(Number(spanElement.textContent.split(',').join('')))
+    : undefined
+);
+
+const totalPopulation = populations.length > 0
+  ? populations.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0)
+  : 0;
+
+const averagePopulation = totalPopulation / populations.length;
+
+document.querySelector('span.average-population').innerHTML = averagePopulation
+  .toLocaleString();
+
+document.querySelector('span.total-population').innerHTML = totalPopulation
+  .toLocaleString();
