bd6283aa3cb41d119966f9a85fbe0da3b69f1db2
Anton <crossnaders4949@gmail.com>
2023-05-15 21:38:22 +0200

    Fixed mistake

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,5 @@
 'use strict';
 
-function addCommas(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (i % 3 === 0 && i !== 0) {
-      arr[i] = arr[i] + ',';
-    }
-  }
-
-  return arr;
-}
-
 const populationsCollection = document.querySelectorAll('.population');
 const totalPopulation = document.querySelector('.total-population');
 const averagePopulation = document.querySelector('.average-population');
@@ -23,12 +13,8 @@ for (const elem of populationsArray) {
 }
 
 const average = count / populationsArray.length;
-const reversePopulationArray = String(count).split('').reverse();
-const reverseAverageArray = String(average).split('').reverse();
-const resultFunctPopulation = addCommas(reversePopulationArray);
-const resultFunctAverage = addCommas(reverseAverageArray);
-const resultPopulation = resultFunctPopulation.reverse().join('');
-const resultAverage = resultFunctAverage.reverse().join('');
+const resultPopulation = count.toLocaleString('en-US');
+const resultAverage = average.toLocaleString('en-US');
 
 totalPopulation.textContent = resultPopulation;
 averagePopulation.textContent = resultAverage;
