5fcee9f54ea77aec6ebf99b4b80931d77279512f
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-05-16 11:12:50 +0300

    added total, avg population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+const populationsHtml
+  = document.getElementsByClassName('population');
+const totalPopultaionHtml
+  = document.getElementsByClassName('total-population')[0];
+const avaragePopultaionHtml
+  = document.getElementsByClassName('average-population')[0];
+
+let totalPopulation = 0;
+let avgPopulation = 0;
+let i = 0;
+
+for (const population of populationsHtml) {
+  i++;
+  totalPopulation += parseInt(population.innerText.replaceAll(',', ''));
+}
+
+avgPopulation = totalPopulation / i;
+totalPopultaionHtml.innerText = totalPopulation;
+avaragePopultaionHtml.innerHTML = avgPopulation;
