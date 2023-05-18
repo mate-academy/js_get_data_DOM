3cfc643a7f2e67b34132eddd2075e1325cc017d7
AndriiYelieva <andrii.yelieva@gmail.com>
2023-05-18 16:58:21 +0300

    Changes made

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,22 +2,19 @@
 
 const listing = document.querySelectorAll('.population');
 
-const listArry = [...listing].map(list => list.innerHTML);
+const listArry = [...listing].map((list) => list.innerHTML);
 
-let totalPop = 0;
-
-for (const newlist of listArry) {
-  const popState = newlist.split(',').join('');
-
-  totalPop += +popState;
-}
+const totalPop = listArry.reduce(
+  (sum, currentPeople) => sum + +currentPeople.split(',').join(''),
+  0
+);
 
 const averagePop = totalPop / listArry.length;
 
 const elementTotal = document.body.children[0].children[2].children[0];
 
-elementTotal.innerHTML = totalPop.toLocaleString('en');
+elementTotal.innerHTML = totalPop.toLocaleString('en-US');
 
 const elementAveg = document.body.children[0].children[3].children[0];
 
-elementAveg.innerHTML = averagePop.toLocaleString('en');
+elementAveg.innerHTML = averagePop.toLocaleString('en-US');
