a2803cd3313bad3c488cb225b2c54cb5cf662ac7
AndriiYelieva <andrii.yelieva@gmail.com>
2023-05-18 15:51:30 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const listing = document.querySelectorAll('.population');
+
+const listArry = [...listing].map(list => list.innerHTML);
+
+let totalPop = 0;
+
+for (const newlist of listArry) {
+  const popState = newlist.split(',').join('');
+
+  totalPop += +popState;
+}
+
+const averagePop = totalPop / listArry.length;
+
+const elementTotal = document.body.children[0].children[2].children[0];
+
+elementTotal.innerHTML = totalPop.toLocaleString('en');
+
+const elementAveg = document.body.children[0].children[3].children[0];
+
+elementAveg.innerHTML = averagePop.toLocaleString('en');
