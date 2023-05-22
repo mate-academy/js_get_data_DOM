141ac65711294f064df873fe105cf2f2d5d6f50a
Rostyslav Sobchyshyn <rosta2604@gmail.com>
2023-05-22 11:12:33 +0100

    Solution v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+function separator(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+const populationSpans = document.getElementsByClassName('population');
+
+const populationData = [];
+
+for (let i = 0; i < populationSpans.length; i++) {
+  const text = populationSpans[i].textContent;
+
+  populationData.push(parseFloat(text.replace(/,/g, '')));
+}
+
+// eslint-disable-next-line max-len
+const total = (populationData.reduce((a, b) => a + b, 0));
+const avg = total / populationData.length;
+
+const totalSpans = document.getElementsByClassName('total-population')[0];
+const avgSpans = document.getElementsByClassName('average-population')[0];
+
+totalSpans.innerText = separator(total);
+avgSpans.innerText = separator(avg);
