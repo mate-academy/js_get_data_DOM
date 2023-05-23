daa11d9e5185aafa62b5501f271d124cd42a5c6b
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-05-23 14:05:06 +0300

    'done'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const elements = document.querySelectorAll('.population');
+
+const countries = [...elements].map(item => item.innerText);
+
+const newArray = countries.map(str => str.split(',').join(''));
+
+const newNumber = newArray.map(string => parseFloat(string));
+
+const sum = newNumber.reduce(
+  (result, currentValue) => result + currentValue, 0
+);
+
+const total = document.querySelector('.total-population');
+
+const newText = sum;
+
+total.textContent = newText.toLocaleString();
+
+const average = document.querySelector('.average-population');
+
+const newData = sum / newNumber.length;
+
+average.textContent = newData.toLocaleString();
