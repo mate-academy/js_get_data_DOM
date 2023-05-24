71ee870e191247e8fab1c0981a80a8025814a544
Jane Shavrukova <eva.shavrukova@gmail.com>
2023-05-24 14:21:51 -0700

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populationArray = [...document.querySelectorAll('.population')];
+
+const numbersOfPopulation = populationArray
+  .map(population => Number(population.innerText.split(',').join('')));
+
+const totalPopulation = numbersOfPopulation
+  .reduce((sum, population) => sum + population, 0);
+
+const averagePopulation = totalPopulation / numbersOfPopulation.length;
+
+const average = document.querySelector('.average-population');
+
+const total = document.querySelector('.total-population');
+
+average.innerHTML = averagePopulation.toLocaleString('en-US');
+total.innerHTML = totalPopulation.toLocaleString('en-US');
