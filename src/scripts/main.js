becb20b54fcdbad5786ce98b719918284d8fd0ed
Volodymyr Alieksieiev <alexvova88@gmail.com>
2023-05-25 00:01:06 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const populationArr = document.getElementsByClassName('population');
+const totalPopulationValue = document.querySelector('.total-population');
+const averagePopulationValue = document.querySelector('.average-population');
+const populationValue = [...populationArr].map(amount => amount.innerText);
+const populationValueTrue
+= populationValue.map(people => +(people.replaceAll(',', '')));
+
+totalPopulationValue.innerText
+ = populationValueTrue.reduce((sumOfPopulations, currentPopulation) =>
+    sumOfPopulations + currentPopulation, 0);
+
+averagePopulationValue.innerText
+= totalPopulationValue.innerText / populationValueTrue.length;
+
+totalPopulationValue.innerText
+= totalPopulationValue.innerText.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1,');
+
+averagePopulationValue.innerText
+= averagePopulationValue.innerText.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1,');
