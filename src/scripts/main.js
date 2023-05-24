72cbc89d8d7fbf7fd3406dd9925f633983c9253b
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-05-25 00:36:36 +0300

    js_get_data_DOM - first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const populationArray = [];
+
+population.forEach(string => {
+  const text = string.textContent;
+  const commasRemoved = text.replaceAll(',', '');
+  const number = parseInt(commasRemoved);
+
+  populationArray.push(number);
+});
+
+function calculateTotal(array) {
+  return array
+    .reduce((acc, item) => acc + item, 0)
+    .toLocaleString();
+}
+
+function calculateAverage(array) {
+  const averagePopulation = array
+    .reduce((acc, item) => acc + item, 0) / (populationArray.length - 1);
+
+  return parseInt(averagePopulation).toLocaleString();
+}
+
+const total = calculateTotal(populationArray);
+const average = calculateAverage(populationArray);
+
+document.querySelector('.total-population').innerHTML = total;
+document.querySelector('.average-population').innerHTML = average;
