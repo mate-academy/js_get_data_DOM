d5504900ce6da9df8c6db35625b4ded854718cec
Vasyl Kozak <sasdaiv@gmail.com>
2023-05-31 00:33:33 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const popTags = document.querySelectorAll('.population');
+let items = [];
+
+popTags.forEach(element => {
+  const text = element.innerText;
+
+  items.push(text);
+});
+
+items.map(n => n.toLocaleString('en-Us'));
+items = items.map(str => parseFloat(str.replace(/,/g, '')));
+
+const totalPop = items.reduce((sum, currentVal) => sum + currentVal, 0);
+const averagePop = totalPop / items.length;
+
+const totalPopTag = document.querySelector('.total-population');
+const averagePopTag = document.querySelector('.average-population');
+
+totalPopTag.innerText = totalPop.toLocaleString('en-US');
+averagePopTag.innerText = averagePop.toLocaleString('en-US');
