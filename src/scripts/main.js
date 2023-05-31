3500faa4c8baae2805848e7642fefd99c3d57243
Alla Nebozhenko <111634874+AllaNebozhenko@users.noreply.github.com>
2023-05-31 10:46:58 +0200

    Get data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')];
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const totalValue = population.reduce((acc, el) => {
+  return acc + +el.innerText.split(',').join('');
+}, 0);
+const averageValue = totalValue / population.length;
+
+totalPopulation.textContent = totalValue.toLocaleString('en-US');
+averagePopulation.textContent = averageValue.toLocaleString('en-US');
