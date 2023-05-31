9eaf7001fce45ca58ce7cbfbe143f2fee70e4e24
vuyko23 <mykolasakulych@gmail.com>
2023-05-31 21:34:56 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+let count = 0;
+let total = 0;
+
+const populationClassList = document.getElementsByClassName('population');
+
+for (let countryPopulation of populationClassList) {
+  let number = parseInt(countryPopulation.textContent.replaceAll(',', ''));
+  console.log(number);
+  if (number !== NaN) {
+    count++;
+    total += number;
+  }
+}
+
+document.querySelector('.total-population').textContent = total.toLocaleString('en-US');
+document.querySelector('.average-population').textContent = (total / count).toLocaleString('en-US');
