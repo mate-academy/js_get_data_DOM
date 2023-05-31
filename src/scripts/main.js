33091797393e5ac1aa133ad6de521d07fd591ed7
Adrian Yevchuk <adrianyevchuk@MacBook-Air-Adrian.local>
2023-06-01 00:07:32 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const listOfPopulations = document.querySelectorAll('.population');
+
+const arrayOfNumbers = [...listOfPopulations].map(el =>
+  Number.parseInt(el.textContent.split(',').join(''))
+);
+
+const totalPopulation = document.querySelector('.total-population');
+
+totalPopulation.innerText = arrayOfNumbers.reduce((sum, el) => sum + el, 0)
+  .toLocaleString('en-US');
+
+const avgPopulation = document.querySelector('.average-population');
+
+avgPopulation.innerText = (arrayOfNumbers.reduce((sum, el) => sum + el, 0)
+  / arrayOfNumbers.length)
+  .toLocaleString('en-US');
