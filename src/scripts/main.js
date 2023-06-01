d232ee5e91c0d758b0029daaefd1b9fbb1940b28
Nazar Hotko <nazar.hotko@outlook.com>
2023-06-01 22:11:15 +0200

    task solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write your code here
+const list = document.getElementsByClassName('list')[0];
+
+const countryList = [];
+
+for (const el of list.children) {
+  const population = (el.children[1].innerHTML).split(',').join('');
+
+  countryList.push(+population);
+}
+
+const total = document.querySelector('.total');
+
+const average = document.querySelector('.average');
+
+total.children[0].textContent = countryList.reduce((a, b) => {
+  return +a + +b;
+}, 0).toLocaleString('en-US');
+
+average.children[0].textContent = (countryList.reduce((a, b) => {
+  return +a + +b;
+}, 0) / countryList.length).toLocaleString('en-US');
