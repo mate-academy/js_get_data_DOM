678d8e238ff7da87f8170e76942b5c2d97111f98
domkab <dominykas.bobinas@gmail.com>
2023-06-03 11:30:55 +0300

    1.0.0 added calculations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const textData = document.querySelectorAll('.population');
+const items = document.querySelectorAll('.list__item');
+let total = 0;
+
+textData.forEach(node => {
+  const numberStr = node.textContent.replace(/,/g, '');
+  const number = Number(numberStr);
+
+  total += number;
+});
+
+const average = total / items.length;
+
+const totalPopulation = document.querySelector('.total-population');
+
+totalPopulation.textContent = total.toLocaleString();
+
+const averagePopulation = document.querySelector('.average-population');
+
+averagePopulation.textContent = average.toLocaleString();
