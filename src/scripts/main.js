542429d25aa4c5aa804fb01499647e5e181f2e80
Hishchak Tetiana <tanaiosered@ukr.net>
2023-06-04 19:50:38 +0300

    getDataDom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const items = document.getElementsByClassName('population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+const arr = [...items].map(item => item.innerText);
+let sum = 0;
+
+for (const item of arr) {
+  const value = +item.split(',').join('');
+
+  sum += value;
+}
+
+total.innerText = sum.toLocaleString('en-US') + `!`;
+average.innerText = (sum / arr.length).toLocaleString('en-US') + `!`;
