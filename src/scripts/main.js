804068965d3f446bb3a6b41653b33eb40c3f2ce8
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-06-04 21:37:33 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populationClass = [...document.querySelectorAll('.population')];
+let total = 0;
+
+populationClass.forEach(item => {
+  total += +item.innerText.replaceAll(',', '');
+});
+
+const average = total / populationClass.length;
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.innerText
+= total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+averagePopulation.innerText
+= average.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
