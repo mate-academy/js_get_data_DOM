a66402cfd8fb51087b4800edf6a4cb67ed739198
Nataliia Koreniuk <koreniuknataliia@gmail.com>
2023-06-05 14:19:59 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('.population');
+
+const population = [];
+
+for (let i = 0; i < list.length; i++) {
+  population.push(list[i].innerHTML.replaceAll(',', ''));
+}
+
+const total = population.reduce(function(sum, current) {
+  return sum + +current;
+}, 0);
+
+const average = total / 10;
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.innerHTML = total.toLocaleString('en-US');
+averagePopulation.innerHTML = average.toLocaleString('en-US');
