7a0da46ff39155ce5054dbe962f88493fe0591b5
domkab <dominykas.bobinas@gmail.com>
2023-06-06 18:08:21 +0300

    1.1.0 const total with reduce method, Array.from

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,22 @@
 'use strict';
 
 const textData = document.querySelectorAll('.population');
+const textDataArray = Array.from(textData);
 const items = document.querySelectorAll('.list__item');
-let total = 0;
 
-textData.forEach(node => {
+const total = textDataArray.reduce((sum, node) => {
   const numberStr = node.textContent.replace(/,/g, '');
   const number = Number(numberStr);
 
-  total += number;
-});
+  return sum + number;
+}, 0);
 
 const average = total / items.length;
 
 const totalPopulation = document.querySelector('.total-population');
 
-totalPopulation.textContent = total.toLocaleString();
+totalPopulation.textContent = total.toLocaleString().split(',').join(' ');
 
 const averagePopulation = document.querySelector('.average-population');
 
-averagePopulation.textContent = average.toLocaleString();
+averagePopulation.textContent = average.toLocaleString().split(',').join(' ');
