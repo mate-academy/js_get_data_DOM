4ef7c8399634d01bed59cfdea4a998c02b1ab487
Misha Siv <0Sivcov0@gmail.com>
2023-06-06 23:46:28 +0300

    get data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const getData = document.querySelectorAll('.population');
+
+let totalCalck = 0;
+
+for (const i of getData) {
+  const item = Number(i.textContent.replace(/,/g, ''));
+
+  totalCalck += item;
+}
+
+const averageCalck = totalCalck / getData.length;
+
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+total.append(totalCalck.toLocaleString('en-US'));
+average.append(averageCalck.toLocaleString('en-US'));
