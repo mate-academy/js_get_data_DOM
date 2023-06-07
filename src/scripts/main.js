5c600567b9e77d0932b6c195b21b6c07f947b4e2
Vo7kov <jegor.volkov.a@gmail.com>
2023-06-07 12:38:50 -0700

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-let population = document.querySelectorAll('.population');
+const population = document.querySelectorAll('.population');
 const totalText = document.querySelector('.total-population');
 const averageText = document.querySelector('.average-population');
 
-[...population] = [...population]
+const numbersArray = [...population]
   .map(number => +number.innerText.split(',').join(''));
 
-const sum = population.reduce((a, b) => a + b);
+const sum = numbersArray.reduce((a, b) => a + b);
 
 totalText.innerText = sum.toLocaleString('en-US');
-averageText.innerText = (sum / population.length).toLocaleString('en-US');
+averageText.innerText = (sum / numbersArray.length).toLocaleString('en-US');
