431f2e3287f6d34106c9a054e837eebd9d8ec9fb
domkab <dominykas.bobinas@gmail.com>
2023-06-08 12:21:49 +0300

    1.1.1 added thousand separator again?

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,8 +15,8 @@ const average = total / items.length;
 
 const totalPopulation = document.querySelector('.total-population');
 
-totalPopulation.textContent = total.toLocaleString().split(',').join(' ');
+totalPopulation.textContent = total.toLocaleString();
 
 const averagePopulation = document.querySelector('.average-population');
 
-averagePopulation.textContent = average.toLocaleString().split(',').join(' ');
+averagePopulation.textContent = average.toLocaleString();
