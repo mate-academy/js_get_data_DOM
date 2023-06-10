60b5f17c27530f6c5d61b84eedd3a960e3d40743
Stepan Haida <stepanhaida@gmail.com>
2023-06-10 22:23:12 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+const pTotalElem = document.querySelector('.total');
+const pAverageElem = document.querySelector('.average');
+
+const populationData = [...populationElements].map(element => (
+  +element
+    .innerText
+    .replaceAll(',', '')
+));
+
+const populationTotal = populationData
+  .reduce((a, b) => a + b);
+
+const populationAverage = Math.floor(
+  populationTotal / populationData.length
+);
+
+pTotalElem.innerText = `
+  Total: ${populationTotal.toLocaleString('en-US')}
+`;
+
+pAverageElem.innerText = `
+  Average: ${populationAverage.toLocaleString('en-US')}
+`;
