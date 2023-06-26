7beca79a92c6156f4d919021fa89511edd61cb6d
spencerspe <spencerspe@gmail.com>
2023-06-26 15:44:22 +0100

    First solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+const populations = document.getElementsByClassName('population');
+
+let totalPopulation = 0;
+
+for (let i = 0; i < populations.length; i++) {
+  const commaGone = Number(populations[i].textContent.replaceAll(',', ''));
+
+  totalPopulation += commaGone;
+}
+
+const htmlTotalPop = document.getElementsByClassName('total-population');
+
+htmlTotalPop[0].textContent = totalPopulation.toLocaleString();
+
+const populationAverage = totalPopulation / populations.length;
+
+const popAve = document.getElementsByClassName('average-population');
+
+popAve[0].textContent = populationAverage.toLocaleString();
