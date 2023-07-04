0369c37b4353bdd0b02850ad1cabe6fe6f6d638d
Vitaly Maistrenko <maystrenko977@gmail.com>
2023-07-04 14:37:25 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+// eslint-disable-next-line max-len
+const populationValues = Array.from(populationElements).map(span => span.textContent.trim());
+
+const parsedPopulations = populationValues.map(population => {
+  const numericValue = parseInt(population.replace(/,/g, ''));
+
+  return isNaN(numericValue) ? 0 : numericValue;
+});
+
+// eslint-disable-next-line max-len
+const totalPopulation = parsedPopulations.reduce((total, population) => total + population, 0);
+const averagePopulation = totalPopulation / parsedPopulations.length;
+
+const averagePopulationSpan = document.querySelector('.average-population');
+
+averagePopulationSpan.textContent = formatNumberWithCommas(averagePopulation);
+
+const totalPopulationSpan = document.querySelector('.total-population');
+
+totalPopulationSpan.textContent = formatNumberWithCommas(totalPopulation);
+
+function formatNumberWithCommas(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
