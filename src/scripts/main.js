032b9ad55968b36ec3bf0238915f3a2594602943
Artem Velychko <avelychko@icloud.com>
2023-07-19 14:00:09 -0400

    Changes made

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,19 +4,14 @@ const population = document.querySelectorAll('.population');
 const averagePopulation = document.querySelector('.average-population');
 const totalPopulation = document.querySelector('.total-population');
 
-let count = 0;
-let totalResult = 0;
+const populationArr = [...population].map((element) =>
+  +element.innerText.split(',').join(''));
 
-for (let i = 0; i < population.length; i++) {
-  const populationConverted = +population[i].innerText.split(',').join('');
+const populationTotal = populationArr.reduce((sum, current) => sum + current);
+const averageResult = populationTotal / population.length;
 
-  totalResult += populationConverted;
-  count++;
-}
-
-const averageResult = totalResult / count;
 const formattedAverage = averageResult.toLocaleString('en-US');
-const formattedTotal = totalResult.toLocaleString('en-US');
+const formattedTotal = populationTotal.toLocaleString('en-US');
 
 averagePopulation.innerText = formattedAverage;
 totalPopulation.innerText = formattedTotal;
