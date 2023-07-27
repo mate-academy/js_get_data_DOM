a0387501ffa223f3606509ddd5289cc4b1272bca
Volodymyr Biletskyi <volodymyrbiletskyi92@gmail.com>
2023-07-27 20:26:16 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+
+const totalPopulation = [...populationElements]
+  .reduce((acc, element) =>
+    acc + Number(element.textContent.trim().replace(/,/g, '')), 0);
+
+const averagePopulation = totalPopulation / populationElements.length;
+
+function formatNumberWithCommas(number) {
+  return number.toLocaleString('en-US');
+}
+
+const averagePopulationElement = document.querySelector('.average-population');
+const totalPopulationElement = document.querySelector('.total-population');
+
+averagePopulationElement.textContent
+= formatNumberWithCommas(averagePopulation);
+
+totalPopulationElement.textContent
+= formatNumberWithCommas(totalPopulation);
