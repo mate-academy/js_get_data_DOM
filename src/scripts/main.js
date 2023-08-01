1b2a8545b4e116402cc63a7fc1fe7ab1e5be8098
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-08-01 16:01:58 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const populationValues = [...document.querySelectorAll('.population')]
+  .map(element => {
+    const populationText = element.textContent;
+
+    try {
+      return parseInt(populationText.replace(/,/g, ''), 10);
+    } catch (error) {
+      return 0;
+    }
+  });
+
+const totalPopulation = populationValues
+  .reduce((total, population) => total + population, 0);
+const averagePopulation = totalPopulation / populationValues.length || 0;
+
+const averagePopulationElement = document.querySelector('.average-population');
+const totalPopulationElement = document.querySelector('.total-population');
+
+averagePopulationElement.innerHTML = averagePopulation.toLocaleString('en-US');
+totalPopulationElement.innerHTML = totalPopulation.toLocaleString('en-US');
