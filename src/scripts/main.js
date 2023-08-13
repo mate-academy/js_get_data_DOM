41592e72d4c5511f53f4c2251a7f96a737938032
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-08-13 20:04:11 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,18 +2,16 @@
 
 const populationValues = [...document.querySelectorAll('.population')]
   .map(element => {
-    const populationText = element.textContent;
+    const populationText = element.textContent || '';
 
-    try {
-      return parseInt(populationText.replace(/,/g, ''), 10);
-    } catch (error) {
-      return 0;
-    }
+    return parseInt(populationText.replace(/,/g, ''), 10) || 0;
   });
 
 const totalPopulation = populationValues
   .reduce((total, population) => total + population, 0);
-const averagePopulation = totalPopulation / populationValues.length || 0;
+const averagePopulation = populationValues.length === 0
+  ? 0
+  : totalPopulation / populationValues.length;
 
 const averagePopulationElement = document.querySelector('.average-population');
 const totalPopulationElement = document.querySelector('.total-population');
