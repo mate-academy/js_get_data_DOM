844fe0c22bb99b478595123e85035f3426e541c4
Dmytro Lazarenko <lazarenkodmytro@gmail.com>
2023-08-19 23:04:35 +0200

    Solution/js_get_data_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', function() {
+  const populationSpans = document.querySelectorAll('.population');
+  const averageSpan = document.querySelector('.average-population');
+  const totalSpan = document.querySelector('.total-population');
+
+  let totalPopulation = 0;
+  let validCount = 0;
+
+  populationSpans.forEach(span => {
+    const populationText = span.textContent.trim();
+    const populationNumber = parseInt(populationText.replace(/,/g, ''));
+
+    if (!isNaN(populationNumber)) {
+      totalPopulation += populationNumber;
+      validCount++;
+    }
+  });
+
+  const averagePopulation = totalPopulation / validCount;
+
+  averageSpan.textContent = formatNumberWithCommas(averagePopulation.toFixed(0)
+  );
+  totalSpan.textContent = formatNumberWithCommas(totalPopulation);
+});
+
+function formatNumberWithCommas(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
