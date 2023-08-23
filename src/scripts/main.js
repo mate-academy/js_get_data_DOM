7c1d5820ea7e162823be02d8ff559bd614fabe3d
Dmytro Loboda <dmytro.loboda@gmail.com>
2023-08-24 00:44:06 +0800

    Population task solve

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const populationSpans = document.querySelectorAll('.population');
+
+let totalPopulation = 0;
+
+populationSpans.forEach(populationSpan => {
+  const populationValue
+    = parseInt(populationSpan.textContent.replace(/,/g, ''));
+
+  if (!isNaN(populationValue)) {
+    totalPopulation += populationValue;
+  }
+});
+
+const averagePopulation = totalPopulation / populationSpans.length;
+
+function addThousandsSeparator(number) {
+  return number.toLocaleString('en-Us');
+}
+
+const totalPopulationSpan = document.querySelector('.total-population');
+const averagePopulationSpan = document.querySelector('.average-population');
+
+totalPopulationSpan.textContent = addThousandsSeparator(totalPopulation);
+averagePopulationSpan.textContent = addThousandsSeparator(averagePopulation);
