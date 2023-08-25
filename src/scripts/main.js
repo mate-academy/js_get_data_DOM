99161d176452374991f4c604dce938672d212ed6
Andrzej Pietkiewicz <pietan@me.com>
2023-08-25 19:55:18 +0200

    top10 largest countries solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const elements = document.getElementsByTagName('span');
+const countriesPopulation = [];
+
+for (const element of elements) {
+  if (element.classList.contains('population')
+  && typeof +element.innerText === 'number') {
+    countriesPopulation.push(+element.innerText.split(',').join(''));
+  }
+}
+
+const total = countriesPopulation.reduce((a, b) => a + b, 0);
+const avg = total / countriesPopulation.length;
+
+const totalPopulationElement = document.querySelector('.total-population');
+
+if (totalPopulationElement) {
+  totalPopulationElement.textContent = total.toLocaleString('en');
+}
+
+const avgPopulationElement = document.querySelector('.average-population');
+
+if (avgPopulationElement) {
+  avgPopulationElement.textContent = avg.toLocaleString('en');
+}
