1fcc1e031ffa1c4d62c975d156cf30b41788cb63
Eugene Kulish <genekulish13@gmail.com>
2023-09-12 15:31:05 +0300

    solve the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populationSpans = document.querySelectorAll('.population');
+const populationValues = [...populationSpans].map(
+  span => span.firstChild.nodeValue
+);
+const populationCounts = populationValues.map(
+  value => +value.split(',').join('')
+);
+
+const total = populationCounts.reduce((totalCount, count) => totalCount + count, 0);
+const average = total / populationCounts.length;
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.textContent = total.toLocaleString('en-US');
+averagePopulation.textContent = average.toLocaleString('en-US');
