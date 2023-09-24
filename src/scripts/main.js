1f6300ba28b696e8e6169cc8c81e01ba415792d2
AndruhaMan <andreysut12@gmail.com>
2023-09-24 14:26:31 +0300

    The task is completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const populations = document.querySelectorAll('.population');
+let total = 0;
+
+for (const population of populations) {
+  const number = parseInt(population.innerText.replace(/,/g, ''));
+
+  total += number;
+}
+
+totalPopulation.innerText = total.toLocaleString('en-US');
+
+averagePopulation.innerText
+  = (total / populations.length).toLocaleString('en-US');
