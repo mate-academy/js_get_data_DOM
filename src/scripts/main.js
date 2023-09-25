466fdc769736393ebd43d8188c0d364bc5b8e2fe
Volodymyr <psn22782@gmail.com>
2023-09-26 00:31:18 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const totalPopulation = document.querySelector('.total-population');
+const avaragePopulation = document.querySelector('.average-population');
+const population = document.querySelectorAll('.population');
+
+let calc = 0;
+
+for (const countPopulation of population) {
+  calc += parseInt(countPopulation.textContent.trim().replace(/,/g, ''));
+};
+
+const format = (num) => num.toLocaleString('en-US');
+
+totalPopulation.textContent = format(calc);
+avaragePopulation.textContent = format(calc / population.length);
