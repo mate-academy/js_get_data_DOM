d069322757166d79ea042c973b91fa97d7d5182c
alex <cebanoleksandr@gmail.com>
2023-10-01 15:58:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const populationNode = document.querySelectorAll('.population');
+const population = [...populationNode].map(p => +p.innerText.split(',').join(''));
+let total = population.reduce((a, b) => a + b, 0);
+let average = total / population.length;
+const totalEl = document.querySelector('.total-population');
+const avgEl = document.querySelector('.average-population');
+const pattern = /(\d)(?=(\d{3})+(?!\d))/g;
+
+total = total.toString().replace(pattern, '$1,');
+average = average.toString().replace(pattern, '$1,');
+
+totalEl.innerText = total;
+avgEl.innerText = average;
