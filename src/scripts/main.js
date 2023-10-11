0d1941d4a59fc0a012da8e28d9b07a22502fcdff
Dima Astapenko <bonum_taurus@ukr.net>
2023-10-11 16:07:03 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const countrys = document.querySelectorAll('.population');
+
+let countPopulation = 0;
+
+for (const count of [...countrys]) {
+  countPopulation += +count.textContent.split(',').join('');
+}
+
+const average = countPopulation / [...countrys].length;
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.textContent = countPopulation.toLocaleString('en-US');
+averagePopulation.textContent = average.toLocaleString('en-US');
