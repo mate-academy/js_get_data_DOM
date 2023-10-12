770745e973b7eaf1391129321aa6ca879412da64
AndriiYelieva <andrew.yelieva@gmail.com>
2023-10-12 14:18:50 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const nodeListState = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+const states = [...nodeListState].map(state => {
+  const conversionToNumber = state.innerHTML.split(',').join('');
+
+  return +conversionToNumber;
+});
+
+const totalPopulation = states.reduce((accumulator, currentState) => {
+  return accumulator + currentState;
+}, 0);
+
+total.innerHTML = totalPopulation.toLocaleString('en-US');
+average.innerHTML = (totalPopulation / states.length).toLocaleString('en-US');
