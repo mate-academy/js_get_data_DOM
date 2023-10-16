7c26c41db724687ced3cc1fcebbd050b49c6d9a7
Kristina Bekher <bekherkr@gmail.com>
2023-10-16 19:56:57 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write your code here
+function formatNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+const elements = [...document.querySelectorAll('.population')];
+
+const numbers = [];
+
+elements.forEach(element => {
+  numbers.push(parseInt(element.innerHTML.replace(/,/g, '')));
+});
+
+const total = numbers.reduce((sum, number) => sum + number, 0);
+
+const average = total / numbers.length;
+
+document.querySelector('.total-population').innerHTML = formatNumber(total);
+document.querySelector('.average-population').innerHTML = formatNumber(average);
