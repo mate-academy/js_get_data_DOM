4cb913f91f04071ae6d2a6488db7dd8053c9e7dd
Kristina Bekher <bekherkr@gmail.com>
2023-10-17 17:29:05 +0200

    update solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,19 @@
 'use strict';
 
-// write your code here
-function formatNumber(number) {
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
-
 const elements = [...document.querySelectorAll('.population')];
 
 const numbers = [];
 
 elements.forEach(element => {
-  numbers.push(parseInt(element.innerHTML.replace(/,/g, '')));
+  numbers.push(parseInt(element.textContent.replace(/,/g, '')));
 });
 
 const total = numbers.reduce((sum, number) => sum + number, 0);
 
 const average = total / numbers.length;
 
-document.querySelector('.total-population').innerHTML = formatNumber(total);
-document.querySelector('.average-population').innerHTML = formatNumber(average);
+document.querySelector('.total-population')
+  .innerHTML = total.toLocaleString('en-US');
+
+document.querySelector('.average-population')
+  .innerHTML = average.toLocaleString('en-US');
