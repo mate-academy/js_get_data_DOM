a5ff1588d49e85ea8e69728c4f579c72cc165873
Damian <damian.adamczewski92@gmail.com>
2023-11-08 10:50:05 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const totalPopulationElem = document.querySelector('.total-population');
+const avgPopulationElem = document.querySelector('.average-population');
+
+const populationElements = document.querySelectorAll('.population');
+
+const populationList = [...populationElements].map(population => {
+  return population.innerText;
+});
+
+const totalPopulation = populationList.reduce((sum, population) => {
+  const currPopulation = +population.split(',').join('');
+
+  return sum + currPopulation;
+}, 0);
+
+const avgPopulation = totalPopulation / populationList.length;
+
+totalPopulationElem.innerText = totalPopulation.toLocaleString('en-US');
+avgPopulationElem.innerText = avgPopulation.toLocaleString('en-US');
