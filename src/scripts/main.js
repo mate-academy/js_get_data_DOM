37d7adfcecc48b35ead05828cd8a288928ae9c76
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-11-24 00:43:43 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const countryPopulation = document.getElementsByClassName('population');
+const countryPopulationArr = [];
+
+for (const el of countryPopulation) {
+  const tmp = el.innerText.split(',').join('');
+
+  if (!isNaN(tmp)) {
+    countryPopulationArr.push(tmp);
+  }
+}
+
+const total = countryPopulationArr.reduce((el, sum) => el + sum, 0);
+const average = total / countryPopulationArr.length;
+
+document.getElementsByClassName('total-population')[0].innerHTML
+= addComma(total);
+
+document.getElementsByClassName('average-population')[0].innerHTML
+= addComma(average);
+
+function addComma(val) {
+  const tmp = val.toString();
+  const thousands = /\B(?=(\d{3})+(?!\d))/g;
+
+  return tmp.replace(thousands, ',');
+}
