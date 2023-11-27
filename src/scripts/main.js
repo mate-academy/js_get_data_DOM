d1a4ef9129fc8c9fedfe5f441b9c01b5b200db58
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-11-27 15:19:19 +0200

    add task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const data = document.querySelectorAll('.population');
+const totalSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+
+const successData = [];
+
+data.forEach(amount => successData.push(+amount.innerHTML.replaceAll(',', '')));
+
+const total = successData.reduce((prev, acc) => prev + acc, 0);
+const average = total / successData.length;
+
+totalSpan.innerHTML = total.toLocaleString('en-US');
+averageSpan.innerHTML = average.toLocaleString('en-US');
