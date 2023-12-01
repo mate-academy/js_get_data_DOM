98d0eedb1193dc25e74f9856bac566b9f7e0197f
Sergey Arnautov <sirius9312@ukr.net>
2023-12-01 12:37:43 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const arrPopulations = [...populations]
+  .map((population) => +((population.textContent).split(',').join('')));
+
+const totalCountPeople = arrPopulations
+  .reduce((prevPeople, people) => prevPeople + people, 0);
+
+const averageCountPeople = Math.ceil((arrPopulations
+  .reduce((prevPeople, people) => prevPeople + people, 0))
+  / arrPopulations.length);
+
+const numberFormatter = Intl.NumberFormat('en-US');
+const formattedTotalCountPeople = numberFormatter.format(totalCountPeople);
+const formattedAverageCountPeople = numberFormatter.format(averageCountPeople);
+
+totalPopulation.textContent = formattedTotalCountPeople;
+averagePopulation.textContent = formattedAverageCountPeople;
