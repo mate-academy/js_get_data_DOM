286c307299a954251d6c426661eb2e8650425141
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-12-07 23:48:05 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', function() {
+  const populationElements = document.querySelectorAll('.population');
+
+  let totalPopulation = 0;
+  let populationCount = 0;
+
+  populationElements.forEach(function(element) {
+    const populationNumber = parseInt(
+      element.textContent.replace(/,/g, ''),
+      10,
+    );
+
+    if (!isNaN(populationNumber)) {
+      totalPopulation += populationNumber;
+      populationCount++;
+    }
+  });
+
+  const averagePopulation
+    = populationCount > 0 ? totalPopulation / populationCount : 0;
+
+  const formattedTotal = numberWithCommas(totalPopulation);
+  const formattedAverage = numberWithCommas(averagePopulation.toFixed(2));
+
+  document.querySelector('.total-population').textContent = formattedTotal;
+  document.querySelector('.average-population').textContent = formattedAverage;
+
+  function numberWithCommas(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+});
