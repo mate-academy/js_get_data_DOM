ae92e2da9aa563ece6ee70fe91384745b5cd7133
Robert Szałapak <robsza1212@gmail.com>
2024-01-03 16:47:55 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write your code here
+let totalPopulation = 0;
+let averagePopulation = 0;
+
+function thousandsSeperator(number) {
+  const numberWithCommas
+   = number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+  return numberWithCommas;
+}
+
+const totalPopulationBlock = document.querySelector('.total-population');
+
+const averagePopulationBlock = document.querySelector('.average-population');
+
+const populations = document.querySelectorAll('.population');
+
+populations.forEach(population => {
+  const count = population.textContent.split(',').join('');
+
+  totalPopulation += +count;
+});
+
+console.log(populations);
+
+averagePopulation = totalPopulation / populations.length;
+
+totalPopulationBlock.textContent = thousandsSeperator(totalPopulation);
+
+averagePopulationBlock.textContent = thousandsSeperator(averagePopulation);
+
+console.log([...populations]);
