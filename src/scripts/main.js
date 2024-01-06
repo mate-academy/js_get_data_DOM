23afb8f8010ef548fed2ad8e50f763c518277334
LabPetro <lamppetro30@outlook.com>
2024-01-06 03:00:01 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const populationList = document.querySelectorAll('.population');
+const populations = [...populationList];
+
+let total = 0;
+
+populations.forEach((population) => {
+  const popText = population.textContent.replace(/,/g, '');
+  const numericPop = Number(popText);
+
+  if (!isNaN(numericPop)) {
+    total += numericPop;
+  }
+});
+
+const average = total / populations.length;
+
+document.querySelector('.total-population').textContent
+= total.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent
+= average.toLocaleString('en-US');
