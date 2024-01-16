0c67cc0a82c8ad4c6afc0585032d8505d4cee94f
Lubomur Chuchvara <lubomircucvara@gmail.com>
2024-01-16 13:50:22 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const nodeList = document.querySelectorAll('.population');
+const elemenTotal = document.querySelector('.total-population');
+const elemAverage = document.querySelector('.average-population');
+const population = [...nodeList]
+  .map(elem => +elem.innerText.replaceAll(',', ''));
+const total = population.reduce((a, b) => a + b, 0);
+const avarage = (total / population.length).toLocaleString();
+
+elemenTotal.innerText = total.toLocaleString();
+elemAverage.innerText = avarage;
