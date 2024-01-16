a910aba8d2cb6c7d698b6c2a8cb38946a771e94a
Siarhei Tsishkavets <kenny.sergey@gmail.com>
2024-01-16 15:58:36 +0100

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const populationsList = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+let tempTotalPopulation = 0;
+
+for (const element of populationsList) {
+  let tempValue = element.textContent;
+
+  tempValue = tempValue.replaceAll(',', '');
+  tempValue = +tempValue;
+
+  tempTotalPopulation += tempValue;
+}
+
+totalPopulation.innerHTML = tempTotalPopulation.toLocaleString('en-US');
+
+averagePopulation.innerHTML = (tempTotalPopulation / populationsList.length)
+  .toLocaleString('en-US');
