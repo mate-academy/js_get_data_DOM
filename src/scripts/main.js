34465c3f9a21bfa25350e00c183686b62099850c
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-01-17 17:22:27 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+
+const populationsNumber = [...populations].map(population =>
+  +(population.innerText.replaceAll(',', '')));
+
+let totalPopulation = 0;
+
+for (const ch of populationsNumber) {
+  totalPopulation += ch;
+}
+
+const averagePopulation = (totalPopulation / populationsNumber.length);
+
+const total = document.querySelector('.total-population');
+
+const average = document.querySelector('.average-population');
+
+total.innerText = `${totalPopulation.toLocaleString('en-US')}`;
+average.innerText = `${averagePopulation.toLocaleString('en-US')}`;
