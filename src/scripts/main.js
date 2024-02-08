952292347c26a618c3bb13dca44cdf199bab4b62
Wita-Shchurko <vitaliya.mememe@gmail.com>
2024-02-08 21:22:00 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,27 +4,12 @@ const population = document.querySelectorAll('.population');
 const total = document.querySelector('.total-population');
 const average = document.querySelector('.average-population');
 
-function splitNumber(number) {
-  const reversedNumber = number.split('').reverse();
-  let result = '';
-
-  for (let i = 0; i < reversedNumber.length; i++) {
-    if (i % 3 === 0 && i !== 0) {
-      result = reversedNumber[i] + ',' + result;
-    } else {
-      result = reversedNumber[i] + result;
-    }
-  }
-
-  return result;
-}
-
 const numbers = [...population].map((i) => {
   return +i.textContent.split(',').join('');
 });
 
-const sum = String(numbers.reduce((acc, cur) => acc + cur));
-const avg = String(sum / numbers.length);
+const sum = numbers.reduce((acc, cur) => acc + cur);
+const avg = sum / numbers.length;
 
-total.textContent = splitNumber(sum);
-average.textContent = splitNumber(avg);
+total.textContent = sum.toLocaleString('en-US');
+average.textContent = avg.toLocaleString('en-US');
