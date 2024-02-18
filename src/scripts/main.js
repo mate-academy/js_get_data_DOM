8a5ff1241983e8d5d9e63bc3c925f9210a4be80e
Horizon-git <apbreloaded947@gmail.com>
2024-02-18 23:52:05 +0200

    task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll(".population");
+const populationArray = [...population].map(
+  (element) => +element.innerText.split(',').join('')
+);
+
+const sumElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+
+const sum = populationArray.reduce((acc, curr) => acc + curr, 0);
+const avg = sum / populationArray.length;
+
+sumElement.textContent = sum.toLocaleString('en-US');
+averageElement.textContent = avg.toLocaleString('en-US');
+
