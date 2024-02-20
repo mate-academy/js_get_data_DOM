f68a0a870ad47c980cdbb83c0110dc127c3d64be
Dariusz Bydolek <darek021982@gmail.com>
2024-02-20 10:00:44 +0100

    Solution v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
-const populationElements = document.querySelectorAll(".population");
+const populationElements = document.querySelectorAll('.population');
 
 const totalPopulation = [...populationElements]
   .map((el) => el.textContent)
-  .map((item) => +item.replaceAll(",", ""))
+  .map((item) => +item.replaceAll(',', ''))
   .reduce((acc, item) => acc + item, 0);
 
 const averagePopulation = totalPopulation / populationElements.length;
 
-const totalElement = document.querySelector(".total-population");
-const averageElement = document.querySelector(".average-population");
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
 
-totalElement.innerText = totalPopulation.toLocaleString("en-US");
-averageElement.innerText = averagePopulation.toLocaleString("en-US");
+totalElement.innerText = totalPopulation.toLocaleString('en-US');
+averageElement.innerText = averagePopulation.toLocaleString('en-US');
