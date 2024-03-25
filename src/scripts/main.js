0920848a26c4478f678de48733ecc5ff6a2608a8
Semen Sokolovskyi <anori.fjord@gmail.com>
2024-03-25 15:30:06 +0200

    calculated population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+const data = document.querySelectorAll('.population');
+let sum = 0;
+const count = data.length;
+
+data.forEach(function (el) {
+  const population = parseInt(el.textContent.replaceAll(',', ''));
+
+  sum += population;
+});
+
+const avg = sum / count;
+
+const formatter = new Intl.NumberFormat('en-US');
+
+const formattedSum = formatter.format(sum);
+const formattedAvg = formatter.format(avg);
+
+document.querySelector('.total-population').textContent = formattedSum;
+document.querySelector('.average-population').textContent = formattedAvg;
