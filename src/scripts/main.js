f59d134d258ed86b648f90a1ddd8669b1c85e9c5
Valeria Oleynik <oleynik.valeria27@gmail.com>
2024-03-26 14:15:26 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const items = document.querySelectorAll('.population');
+const spanTotal = document.querySelector('.total-population');
+const spanAverage = document.querySelector('.average-population');
+
+const thousandsSeparator = (number) => {
+  return number.toLocaleString('en-US');
+};
+
+const population = [...items].map((item) => {
+  return Number(item.innerText.split(',').join(''));
+});
+const total = population.reduce((sum, n) => sum + n, 0);
+const average = Math.floor(total / population.length);
+
+const formattedTotal = thousandsSeparator(total);
+const formatteAverage = thousandsSeparator(average);
+
+spanAverage.textContent = formatteAverage;
+spanTotal.textContent = formattedTotal;
