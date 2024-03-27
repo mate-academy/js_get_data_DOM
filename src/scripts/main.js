046b82b51b6d100abdfafbe17ee9321b2462983a
Emil Sukiennik <ewilemil@gmail.com>
2024-03-27 20:00:49 +0100

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const collection = document.getElementsByClassName('population');
+
+const totalPopulation = [].map
+  .call(collection, (item) => +item.textContent.replaceAll(',', ''))
+  .reduce((a, b) => a + b, 0);
+
+document.getElementsByClassName('average-population')[0].textContent = (
+  totalPopulation / collection.length
+).toLocaleString('en-US');
+
+document.getElementsByClassName('total-population')[0].textContent =
+  totalPopulation.toLocaleString('en-US');
