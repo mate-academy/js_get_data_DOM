e45f1afa2a8eb294e078f2700448f190a6472b75
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-04-09 12:17:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+const countriesPopulation = [...population].map((country) => {
+  return +country.textContent.split(',').join('');
+});
+
+const totalPopulation = countriesPopulation.reduce((sum, i) => sum + i);
+const averagePopulation = totalPopulation / countriesPopulation.length;
+
+total.textContent = totalPopulation.toLocaleString('en-US');
+average.textContent = averagePopulation.toLocaleString('en-US');
