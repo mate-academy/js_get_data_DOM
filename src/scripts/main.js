492fe6c33d06157e1a3b26e3f72b53999943e38e
Pavlo Mykhalov <pavlomihaliov@gmail.com>
2024-04-12 12:00:23 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const populationCount = document.querySelectorAll('span.population');
+const averagePopulation = document.querySelector('span.average-population');
+const totalPopulation = document.querySelector('span.total-population');
+
+function removeCommas(string) {
+  return string.replace(/,/g, '');
+}
+
+function getTotalPopulation() {
+  let total = 0;
+
+  populationCount.forEach((span) => {
+    total += parseInt(removeCommas(span.textContent.trim(), 10));
+  });
+
+  return total.toLocaleString('en-US');
+}
+
+function getAveragePopulation() {
+  const total = +removeCommas(getTotalPopulation());
+  const count = populationCount.length;
+
+  if (count === 0) {
+    return 0;
+  }
+
+  const averageCount = total / count;
+
+  return averageCount.toLocaleString('en-US');
+}
+
+totalPopulation.textContent = getTotalPopulation();
+averagePopulation.textContent = getAveragePopulation();
