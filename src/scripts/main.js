92cf235953632232b940db07a8da0e6d20ba9933
Bohdan Liutyi <liutyibohdan13@gmail.com>
2024-04-15 21:55:54 +0300

    get the average and total value

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
-'use strict';
+'use stript';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const numbers = [...population].map((i) => {
+  return +i.textContent.split(',').join('');
+});
+
+const sum = numbers.reduce((acc, cur) => acc + cur);
+
+const average = sum / numbers.length;
+
+totalPopulation.textContent = sum.toLocaleString('en-US');
+
+averagePopulation.textContent = average.toLocaleString('en-Us');
