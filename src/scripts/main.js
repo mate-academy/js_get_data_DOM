5d0c2c541eab5b658dc9c7718f045670cf146c5d
Dmytro Manankin <dmitry.manankin@gmail.com>
2024-04-15 18:22:29 +0300

    task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write your code here
+const totalPopulation = document.querySelector('span.total-population');
+const averagePopulation = document.querySelector('span.average-population');
+const populationByCountries = document.querySelectorAll('span.population');
+
+function removeCommas(string) {
+  return string.replaceAll(',', '');
+}
+
+function getTotalPopulation() {
+  let total = 0;
+
+  for (const countryPopulation of populationByCountries) {
+    const data = countryPopulation.textContent.trim();
+    const population = +parseInt(removeCommas(data));
+
+    total += population;
+  }
+
+  return total.toLocaleString('en-US');
+}
+
+function getAveragePopulation() {
+  const total = +removeCommas(getTotalPopulation());
+  const countryAmount = populationByCountries.length;
+
+  if (countryAmount === 0) {
+    return 0;
+  }
+
+  const average = Math.round(total / countryAmount);
+
+  return average.toLocaleString('en-US');
+}
+
+totalPopulation.textContent = getTotalPopulation();
+averagePopulation.textContent = getAveragePopulation();
