d0e6f2bc0f81db3d24808281a0760fb251a59976
Orest Kozak <orestkozak17@gmail.com>
2024-04-27 13:30:52 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const populationSpans = document.querySelectorAll('.population');
+
+const populationList = Array.from(populationSpans).map((span) => {
+  const populationString = span.textContent.replace(/,/g, '');
+
+  return parseInt(populationString);
+});
+
+const totalPopulation = populationList.reduce(
+  (total, population) => total + population,
+  0,
+);
+const averagePopulation = totalPopulation / populationList.length;
+
+function addThousandsSeparator(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+const totalSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+
+totalSpan.textContent = addThousandsSeparator(totalPopulation);
+averageSpan.textContent = addThousandsSeparator(averagePopulation);
