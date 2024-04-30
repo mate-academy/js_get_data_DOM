ea41e46b5e100020889b6908b2de79efc252023c
Dmytro_Krylov <dmutrokrulov@gmail.com>
2024-04-30 12:52:15 +0300

    js_get_data_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
-'use strict';
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+"use strict";
 
-// write your code here
+const spanElements = [...document.querySelectorAll(".population")];
+const totalElement = document.querySelector(".total-population");
+const averageElement = document.querySelector(".average-population");
+
+const arrTexts = spanElements.map(
+  (element) => +element.textContent.replaceAll(",", "")
+);
+
+let total = 0;
+let average = 0;
+
+for (const number of arrTexts) {
+  total += number;
+  average = total / arrTexts.length;
+}
+
+totalElement.textContent = total.toLocaleString("en-US");
+averageElement.textContent = average.toLocaleString("en-US");
