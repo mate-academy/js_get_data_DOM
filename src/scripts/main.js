375d1d89f9c347aa3b91c51f4c24ff6bbe7c9175
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2024-05-02 16:40:27 +0200

    fix linter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,22 @@
 'use strict';
+
 const population = [...document.querySelectorAll('.population')];
 const totalTag = document.querySelector('.total');
 const averageTag = document.querySelector('.average');
 
 const valuesArray = [];
 
-population.forEach(country => valuesArray
-  .push(parseInt(country.textContent.replaceAll(',', '')))
-)
+population.forEach((country) => {
+  const value = parseInt(country.textContent.replaceAll(',', ''));
 
-let total = valuesArray.reduce((acc, curr) => acc + curr);
-let average = total / valuesArray.length;
+  valuesArray.push(value);
+});
+
+const total = valuesArray.reduce((acc, curr) => acc + curr);
+const average = total / valuesArray.length;
 
 function preparePopulation(param) {
-  let prepared = ('' + param).split('').reverse();
+  const prepared = ('' + param).split('').reverse();
   let res = '';
 
   for (let i = 0; i < prepared.length; i++) {
