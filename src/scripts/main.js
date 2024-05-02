ef618d6de9b51b3b7b4964daf85c4d1b9a262a84
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2024-05-02 15:42:07 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
+const population = [...document.querySelectorAll('.population')];
+const totalTag = document.querySelector('.total');
+const averageTag = document.querySelector('.average');
 
-// write your code here
+const valuesArray = [];
+
+population.forEach(country => valuesArray
+  .push(parseInt(country.textContent.replaceAll(',', '')))
+)
+
+let total = valuesArray.reduce((acc, curr) => acc + curr);
+let average = total / valuesArray.length;
+
+function preparePopulation(param) {
+  let prepared = ('' + param).split('').reverse();
+  let res = '';
+
+  for (let i = 0; i < prepared.length; i++) {
+    if (i !== 0 && i % 3 === 0) {
+      res += ',';
+    }
+    res += prepared[i];
+  }
+
+  return res.split('').reverse().join('');
+}
+
+totalTag.textContent = `Total: ${preparePopulation(total)}`;
+averageTag.textContent = `Average: ${preparePopulation(average)}`;
