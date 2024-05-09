c95366ef7229e003952cb9a39a710294c141a49f
katya-sn <kattya.sagun@gmail.com>
2024-05-09 23:14:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populationCollection = document.querySelectorAll('span.population');
+const populationArray = [...populationCollection];
+
+const total = populationArray.reduce((sum, element) => {
+  return sum + Number(element.innerHTML.replace('/,/g', ''), 0);
+});
+
+const average = total / populationArray.length;
+
+const numberFormatter = Intl.NumberFormat('en-US');
+
+const totalSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+
+totalSpan.textContent = numberFormatter.format(total);
+averageSpan.textContent = numberFormatter.format(average);
