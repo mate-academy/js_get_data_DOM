34eedbfe10a66ad0530a39d536ff5f9654480cb1
katya-sn <kattya.sagun@gmail.com>
2024-05-10 10:43:05 +0300

    change total

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
-const populationCollection = document.querySelectorAll('span.population');
+const populationCollection = document.querySelectorAll('.population');
 const populationArray = [...populationCollection];
 
-const total = populationArray.reduce((sum, element) => {
-  return sum + Number(element.innerHTML.replace('/,/g', ''), 0);
+let total = 0;
+
+populationArray.forEach((population) => {
+  const populationText = population.textContent.replace(/,/g, '');
+  const populationNumber = parseInt(populationText);
+
+  total += populationNumber;
 });
 
 const average = total / populationArray.length;
@@ -14,5 +19,5 @@ const numberFormatter = Intl.NumberFormat('en-US');
 const totalSpan = document.querySelector('.total-population');
 const averageSpan = document.querySelector('.average-population');
 
-totalSpan.textContent = numberFormatter.format(total);
+totalSpan.textContent = total.toLocaleString('en-US');
 averageSpan.textContent = numberFormatter.format(average);
