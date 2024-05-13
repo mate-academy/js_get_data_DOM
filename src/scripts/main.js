2a1566dd3766c0320f9c52ae3571403be94caf65
ArtyWhit <julwer1k@gmail.com>
2024-05-13 17:41:01 +0300

    finished task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const populations = document.querySelectorAll('.population');
+
+const lengthCountry = populations.length;
+
+let total = 0;
+
+populations.forEach((population) => {
+  const populationText = population.textContent.split(',').join('');
+  const populationNumber = +populationText;
+
+  total += populationNumber;
+});
+
+const average = total / lengthCountry;
+
+totalPopulation.textContent = total.toLocaleString('en-US');
+averagePopulation.textContent = average.toLocaleString('en-US');
