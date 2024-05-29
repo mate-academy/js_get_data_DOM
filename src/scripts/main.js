bc7b813f4a449cc36ffc2e090b7ad30dea0f0b42
ordila <ordila99@gmail.com>
2024-05-29 22:36:42 +0300

    add solving

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const allPopulationArray = [...document.querySelectorAll('.population')].map(
+  (el) => el.innerHTML.replaceAll(',', ''),
+);
+
+const allPopulation = allPopulationArray.reduce((prevValue, currentValue) => {
+  return prevValue + Number(currentValue);
+}, 0);
+
+const totalPopulationTag = document.querySelector('.total-population');
+
+totalPopulationTag.innerHTML = allPopulation.toLocaleString('en-US');
+
+const averagePopulationTag = document.querySelector('.average-population');
+
+averagePopulationTag.innerHTML = (
+  allPopulation / allPopulationArray.length
+).toLocaleString('en-US');
