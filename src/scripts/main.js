b313f29a16476556d285b2330e659edd8ead84dd
Roma <roma150269@gmail.com>
2024-05-31 11:05:39 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const totalPopulatio = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+
+const numArry = [];
+
+population.forEach((item) => {
+  const num = +item.innerHTML.split(',').join('');
+
+  if (!isNaN(num)) {
+    numArry.push(num);
+  }
+});
+
+const total = numArry.reduce((acc, item) => acc + item);
+const average = total / numArry.length;
+
+totalPopulatio.innerHTML = total.toLocaleString('en-US');
+averageElement.innerHTML = average.toLocaleString('en-US');
