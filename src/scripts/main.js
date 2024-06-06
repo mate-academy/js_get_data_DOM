8a2592bd7cf6615f13a98dacb3728bfed648c0f8
SrTrace <levashov04sergey@gmail.com>
2024-06-06 21:00:01 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const populationElements = [...document.querySelectorAll('.population')];
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+
+function parseNumber(str) {
+  const clearComaStr = str.replace(/,/g, '');
+  const number = Number(clearComaStr);
+
+  return isNaN(number) ? 0 : number;
+}
+
+const totalPopulation = populationElements.reduce((sum, el) => {
+  const numberOfPeople = parseNumber(el.innerText);
+
+  return sum + numberOfPeople;
+}, 0);
+
+const numberFormatter = Intl.NumberFormat('en-US');
+const formattedTotal = numberFormatter.format(totalPopulation);
+
+const averagePopulation = totalPopulation / populationElements.length;
+const formattedAverage = numberFormatter.format(averagePopulation);
+
+totalElement.innerText = formattedTotal;
+averageElement.innerText = formattedAverage;
