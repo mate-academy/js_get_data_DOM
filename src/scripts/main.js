6a2aa9efd7fd4ba58e8979a916f733ef001adb82
Dmytro Bashlai <imaxs2016@gmail.com>
2024-06-07 00:05:38 +0300

    Average and total populations have been calculated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const populations = [...document.querySelectorAll('.population')];
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+
+const populationsTotal = populations.reduce((total, population) => {
+  return total + +population.textContent.split(',').join('');
+}, 0);
+
+totalElement.textContent = populationsTotal.toLocaleString('en-US');
+
+averageElement.textContent = (
+  populationsTotal / populations.length
+).toLocaleString('en-US');
