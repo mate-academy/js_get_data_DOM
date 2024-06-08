a8914be49e186e42ff3804ad44eb4274ea2992fd
Yevhen Protasov <protasovyevhen@gmail.com>
2024-06-08 19:31:05 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write your code here
+let population = [...document.querySelectorAll('.population')];
+
+population = population.map((v) => convertToNumber(v.innerHTML));
+
+function convertToNumber(value) {
+  return parseInt(value.replace(/,/g, ''), 10);
+}
+
+const totalPopulation = population.reduce((v1, v2) => v1 + v2);
+
+const avgPopulation = totalPopulation / population.length;
+
+const totalContainer = document.querySelector('.total-population');
+const avgContainer = document.querySelector('.average-population');
+
+function formatTheNumberForDisplay(number) {
+  return number.toLocaleString('en-US');
+}
+
+totalContainer.innerHTML = formatTheNumberForDisplay(totalPopulation);
+avgContainer.innerHTML = formatTheNumberForDisplay(avgPopulation);
