e1d86684550fe0799d36b7097a5a11ea37c9d545
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2024-06-08 11:29:50 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const populationList = document.querySelectorAll('span.population');
+const total = document.querySelector('span.total-population');
+const average = document.querySelector('span.average-population');
+
+const quantityCounter = populationList.length;
+let totalCount = 0;
+
+for (const span of populationList) {
+  const nowNum = Number(span.innerText.split(',').join(''));
+
+  totalCount += nowNum;
+}
+
+total.innerHTML = totalCount;
+average.innerHTML = totalCount / quantityCounter;
