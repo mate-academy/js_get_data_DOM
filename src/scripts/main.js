7298afb7a589b0929d995f9003a88b001f853a84
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-06-10 12:28:40 +0300

    Parsing countries population.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+const populationData = [];
+
+populationElements.forEach((item) => {
+  populationData.push(parseInt(item.textContent.replaceAll(',', '')));
+});
+
+const totalPopulation = populationData.reduce(
+  (accumulator, population) => accumulator + population,
+  0,
+);
+const averagePopulation = totalPopulation / populationData.length;
+
+document.querySelector('.average-population').textContent =
+  averagePopulation.toLocaleString();
+
+document.querySelector('.total-population').textContent =
+  totalPopulation.toLocaleString();
