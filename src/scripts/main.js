8ac53f75b6319f4d3e7f3dc4798b63cf18004782
katya havriuk <0303gavkat@gmail.com>
2024-06-10 13:10:09 -0600

    task solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+const populations = Array.from(populationElements).map((element) => {
+  const text = element.innerText;
+  let numberString = '';
+
+  for (const char of text) {
+    if (!isNaN(char) && char !== ' ') {
+      numberString += char;
+    }
+  }
+
+  return parseInt(numberString, 10);
+});
+
+const total = populations.reduce((prev, item) => {
+  return prev + item;
+}, 0);
+
+const average = total / populations.length;
+
+document.querySelector('.total-population').innerText =
+  total.toLocaleString('en-US');
+
+document.querySelector('.average-population').innerText = Number(
+  average.toFixed(0),
+).toLocaleString('en-US');
