adb7dd7442d5264d6c28a98971dd281d3eb0d09d
Voloshynd <Voloshynd13@gmail.com>
2024-06-10 22:40:13 +0200

    add solution of task Top 10 largest countries

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const populationArr = [...document.querySelectorAll('.population')].map(
+  (amount) => +amount.innerText.split(',').join(''),
+);
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const total = populationArr.reduce((acc, next) => acc + next, 0);
+const average =
+  populationArr.reduce((acc, next) => acc + next, 0) / populationArr.length;
+
+function getNumericStyle(num) {
+  return Number(num).toLocaleString().replace(/\s/g, ',');
+}
+
+totalPopulation.innerText = getNumericStyle(total);
+
+averagePopulation.innerText = getNumericStyle(average);
