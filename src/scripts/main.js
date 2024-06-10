32156d0786331ae7f4793da3e69bb521d9ac3fdc
Vadym Bryvka <vadym.bryvkaa@gmail.com>
2024-06-09 20:45:43 -0500

    population-calculatogit add src

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
+const populationElements = [...document.querySelectorAll('.population')];
+
+function calculateAverage(elements) {
+  let totalPopulation = 0;
+  const count = elements.length;
+
+  elements.forEach((element) => {
+    const population = parseInt(element.textContent.replace(/,/g, ''), 10);
+
+    totalPopulation += population;
+  });
+
+  const averagePopulation = totalPopulation / count;
+
+  return { totalPopulation, averagePopulation };
+}
+
+const { sum, average } = calculateAverage(populationElements);
+
+document.querySelector('.total-population').textContent = sum.toLocaleString();
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString();
+
 // write your code here
