2b4100343ee2a9521c60b3b90d64d2eaf78efe1d
Tatiana <tanya_potapenko@bigmir.net>
2024-06-11 12:32:28 +0200

    js_get-data-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const populationSpans = document.querySelectorAll('span.population');
+
+const populationNumbers = Array.from(populationSpans).map((span) => {
+  const number = parseInt(span.textContent.replace(/,/g, ''));
+
+  return isNaN(number) ? 0 : number;
+});
+
+const totalPopulation = populationNumbers.reduce((acc, num) => acc + num, 0);
+const averagePopulation = totalPopulation / populationNumbers.length;
+
+function formatNumber(num) {
+  return num.toLocaleString('en-US');
+}
+
+const formattedTotal = formatNumber(totalPopulation);
+const formattedAverage = formatNumber(averagePopulation.toFixed(2));
+
+document.querySelector('span.average-population').textContent =
+  formattedAverage;
+document.querySelector('span.total-population').textContent = formattedTotal;
