c80daf6d8fa0f6876beb696eabb505aaf56814e6
Alina Ovod <alina.ipatova98@gmail.com>
2024-06-11 20:02:23 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+
+const populations = [];
+
+populationElements.forEach((element) => {
+  const populationNumber = Number(element.textContent.split(',').join(''));
+
+  if (!isNaN(populationNumber)) {
+    populations.push(populationNumber);
+  }
+});
+
+const totalPopulation = populations.reduce(
+  (acc, population) => acc + population,
+  0,
+);
+const averagePopulation = totalPopulation / populations.length;
+
+document.querySelector('.total-population').textContent =
+  totalPopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  averagePopulation.toLocaleString('en-US');
