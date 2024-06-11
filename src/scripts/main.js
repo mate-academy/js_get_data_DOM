9e5a0b912a8b4578adbda70237b1766dd886fa22
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-06-11 16:58:48 +0300

    task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const populationElems = document.querySelectorAll('.population');
+
+const sumAllPeople = [...populationElems]
+  .map((country) => country.innerHTML.split(',').join(' '))
+  .map((people) => Number(people.split(' ').join('')))
+  .reduce((sum, people) => sum + people, 0);
+
+document.querySelector('.total-population').innerHTML =
+  sumAllPeople.toLocaleString('en-US');
+
+document.querySelector('.average-population').innerHTML = (
+  sumAllPeople / populationElems.length
+).toLocaleString('en-US');
