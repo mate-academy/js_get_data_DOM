0ed19602c502cadc87f90e1ae4e2ba1e49ed90a3
Misha Vynnyk <vinnikmisha94@gmail.com>
2024-06-12 20:01:06 +0200

    solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,18 @@
 'use strict';
 
 const allElementPopulation = document.querySelectorAll('.population');
-let totalElement = document.querySelector('.total-population');
-let averageElement = document.querySelector('.average-population');
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
 let totalNumberPopulation = 0;
 
-for(let i = 0; i < allElementPopulation.length; i++) {
-  let population = allElementPopulation[i].textContent;
-  let populationNumber = Number(population.split(',').join(''));
+for (let i = 0; i < allElementPopulation.length; i++) {
+  const population = allElementPopulation[i].textContent;
+  const populationNumber = Number(population.split(',').join(''));
 
   totalNumberPopulation += populationNumber;
 }
 
-averageElement.textContent = (totalNumberPopulation / allElementPopulation.length).toLocaleString('en-US');
+averageElement.textContent = (
+  totalNumberPopulation / allElementPopulation.length
+).toLocaleString('en-US');
 totalElement.textContent = totalNumberPopulation.toLocaleString('en-US');
-
