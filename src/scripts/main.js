28e8fbd5df3dfb8d8cba84a01e89713a5650d7ed
Pavlo Svinarchuk <shokkontent2004@gmail.com>
2024-06-12 21:44:13 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const parsePopulation = (text) => Number(text.replace(/,/g, ''));
+const container = document.querySelector('.container');
+const arrayOfCountryPeople = [...container.querySelectorAll('.population')];
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const sum = arrayOfCountryPeople
+  .map((pop) => parsePopulation(pop.textContent))
+  .reduce((acc, num) => acc + num, 0);
+
+totalPopulation.textContent = sum;
+averagePopulation.textContent = sum / arrayOfCountryPeople.length;
