3b17dfa0612c872e6f28e807f4c0f9c743f7b84f
Den <clixqqk@gmail.com>
2024-06-13 01:37:58 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const spanNumber = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+let total = 0;
+
+spanNumber.forEach((num) => {
+  total += parseInt(num.textContent);
+});
+
+const average = total / spanNumber.length;
+
+totalPopulation.textContent = total;
+averagePopulation.textContent = average.toFixed(2);
