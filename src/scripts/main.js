45a35101eee45386315563659fb4c74080301901
ZakZh <38009936+ZakZh@users.noreply.github.com>
2024-06-13 17:15:20 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const countriesPopulationElements = [
+  ...document.querySelectorAll('span.population'),
+];
+const totalPopulationElement = document.querySelector('span.total-population');
+const averagePopulationElement = document.querySelector(
+  'span.average-population',
+);
+const totalPopulationValue = countriesPopulationElements.reduce(
+  (prev, next) => {
+    return prev + +next.innerHTML.replace(/,/g, '');
+  },
+  0,
+);
+const averagePopulationValue =
+  totalPopulationValue / countriesPopulationElements.length;
+
+totalPopulationElement.innerHTML = totalPopulationValue.toLocaleString('en-US');
+
+averagePopulationElement.innerHTML =
+  averagePopulationValue.toLocaleString('en-US');
