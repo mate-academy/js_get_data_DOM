eb78a1d548d20542da611bee902c8f1aace26f63
Rusłan Karaniewski <karane88vka@gmail.com>
2024-06-14 19:38:53 +0200

    Solution to the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const populations = [...document.querySelectorAll('.population')].map((el) => {
+  return +el.innerText.replace(/,/g, '');
+});
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const toFormated = (number) => {
+  return number.toLocaleString('en-US');
+};
+
+totalPopulation.innerText = toFormated(populations.reduce((a, b) => a + b));
+
+averagePopulation.innerText = toFormated(
+  populations.reduce((a, b) => a + b) / 10,
+);
