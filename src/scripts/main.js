f437748c01df5fb708d78fb8f546f37389c566cc
Rusłan Karaniewski <karane88vka@gmail.com>
2024-06-14 19:48:15 +0200

    Fix after PR

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,12 @@ const populations = [...document.querySelectorAll('.population')].map((el) => {
 });
 const totalPopulationElement = document.querySelector('.total-population');
 const averagePopulationElement = document.querySelector('.average-population');
-const totalPopulation = populations.reduce((a, b) => a + b);
-const averagePopulation = totalPopulation / 10;
-const toFormated = (number) => {
+const totalPopulation = populations.reduce((a, b) => a + b, 0);
+const averagePopulation = totalPopulation / populations.length;
+const toFormatted = (number) => {
   return number.toLocaleString('en-US');
 };
 
-totalPopulationElement.innerText = toFormated(totalPopulation);
+totalPopulationElement.innerText = toFormatted(totalPopulation);
 
-averagePopulationElement.innerText = toFormated(averagePopulation);
+averagePopulationElement.innerText = toFormatted(averagePopulation);
