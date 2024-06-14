db3ce5737bc3e6393722165637deb61c55a0a580
Rusłan Karaniewski <karane88vka@gmail.com>
2024-06-14 19:42:57 +0200

    Small fix before PR

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,14 @@
 const populations = [...document.querySelectorAll('.population')].map((el) => {
   return +el.innerText.replace(/,/g, '');
 });
-const totalPopulation = document.querySelector('.total-population');
-const averagePopulation = document.querySelector('.average-population');
+const totalPopulationElement = document.querySelector('.total-population');
+const averagePopulationElement = document.querySelector('.average-population');
+const totalPopulation = populations.reduce((a, b) => a + b);
+const averagePopulation = totalPopulation / 10;
 const toFormated = (number) => {
   return number.toLocaleString('en-US');
 };
 
-totalPopulation.innerText = toFormated(populations.reduce((a, b) => a + b));
+totalPopulationElement.innerText = toFormated(totalPopulation);
 
-averagePopulation.innerText = toFormated(
-  populations.reduce((a, b) => a + b) / 10,
-);
+averagePopulationElement.innerText = toFormated(averagePopulation);
