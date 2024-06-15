136120ec648f0483bc6c2c3a409305c49df7d91f
Vyacheslav Kokayko <vkokayko@gmail.com>
2024-06-15 12:31:31 +0300

    Updated comments from mentor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const population = [...document.querySelectorAll('.population')];
+const numberOfCountries = population.length;
+
+const total = population
+  .map((el) => el.textContent)
+  .map((el) => el.replaceAll(',', ''))
+  .map((el) => +el)
+  .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+const average = total / numberOfCountries;
+
+totalPopulation.textContent = total.toLocaleString('en-US');
+averagePopulation.textContent = average.toLocaleString('en-US');
