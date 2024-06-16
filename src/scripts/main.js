d4406e014e591b6b1bcc3362dfe2faf939df3f73
Olena <113938017+Alenalenk@users.noreply.github.com>
2024-06-16 09:26:52 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,14 @@
-'use strict';
+const populations = [...document.querySelectorAll('span.population')];
+const totalEl = document.querySelector('span.total-population');
+const averageEl = document.querySelector('span.average-population');
 
-'use strict';
+const total = populations.reduce((prev, span) => {
+  const population = +span.textContent.replaceAll(',', '');
 
-const employees = [...document.querySelectorAll('li')];
-const list = document.querySelector('ul');
-const newList = document.createElement('ul');
+  return prev + population;
+}, 0);
 
-const sortList = (arr) => {
-  return arr.sort((itemA, itemB) => {
-    const salaryA = +itemA.dataset.salary.slice(1).replace(',', '');
-    const salaryB = +itemB.dataset.salary.slice(1).replace(',', '');
+const average = total / populations.length;
 
-    return salaryB - salaryA;
-  });
-};
-
-const getEmployees = (newArr) => {
-  newArr.forEach((element) => {
-    newList.append(element);
-  });
-
-  list.replaceWith(newList);
-};
-
-getEmployees(sortList(employees));
-
+totalEl.textContent = total.toLocaleString('en-US');
+averageEl.textContent = average.toLocaleString('en-US');
