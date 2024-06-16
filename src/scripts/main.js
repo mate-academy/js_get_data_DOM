586c8bec9f74569bd542e8de2d4ea308422afaa1
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-16 19:41:08 +0300

    [Feature] count total and average

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+
+const populationValues =
+  [...populationElements].map((element) => {
+    const value = element.textContent;
+
+    return Number(value.replaceAll(',', ''));
+  }) ?? [];
+
+const total = populationValues.reduce((acc, value) => acc + value, 0);
+
+const average = total / populationValues.length;
+
+const totalSelector = document.querySelector('.total-population');
+const averageSelector = document.querySelector('.average-population');
+
+if (totalSelector) {
+  totalSelector.innerText = total.toLocaleString('en-US');
+}
+
+if (averageSelector) {
+  averageSelector.innerHTML = average.toLocaleString('en-US');
+}
