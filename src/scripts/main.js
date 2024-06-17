594fa2eead00f40f8acb7c6b18f706115048d2c9
Nazarii <Kyzenko.nr@gmail.com>
2024-06-17 10:21:47 +0300

    developPopulatinCommitOne

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write your code here
+const populationCountries = [...document.querySelectorAll('.population')];
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const total = populationCountries.reduce((prev, current) => {
+  const populationCountry = +current.textContent.replaceAll(',', '');
+
+  return prev + populationCountry;
+}, 0);
+
+const average = total / populationCountries.length;
+
+totalPopulation.textContent = total.toLocaleString();
+averagePopulation.textContent = average.toLocaleString();
