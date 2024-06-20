62bc63c752a3c28a50066c0bf52bffa16f9853d6
Pasha Sich <97337742+Kapidastra@users.noreply.github.com>
2024-06-20 20:39:49 +0300

    Added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
-'use strict';
+const allItems = [...document.querySelectorAll('span')];
 
-// write your code here
+const populations = allItems.filter(
+  (element) => element.className === 'population',
+);
+const total = allItems.filter(
+  (element) => element.className === 'total-population',
+)[0];
+const average = allItems.filter(
+  (element) => element.className === 'average-population',
+)[0];
+
+const totalResult = populations
+  .map((element) => element.textContent.replace(/,/g, ''))
+  .reduce((a, b) => Number.parseFloat(a) + Number.parseFloat(b), 0);
+
+const formattedTotal = totalResult.toLocaleString();
+const formattedAverage = (totalResult / populations.length).toLocaleString();
+
+total.textContent = formattedTotal;
+average.textContent = formattedAverage;
