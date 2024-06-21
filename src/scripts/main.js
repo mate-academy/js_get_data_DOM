502a63a81da8fe4f8207da278289efab425b852d
luter <newgglutt@gmail.com>
2024-06-21 18:29:14 +0300

    add new

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+
+const populationNumbers = Array.from(populations).map((pop) => {
+  return parseInt(pop.textContent.replace(/,/g, ''), 10);
+});
+
+const totalPopulaton = populationNumbers.reduce((total, num) => total + num, 0);
+
+const averagePopulation = totalPopulaton / populationNumbers.length;
+
+const formattedTotal = totalPopulaton.toLocaleString();
+const formattedAverage = Math.round(averagePopulation).toLocaleString();
+
+document.querySelector('.total').textContent = formattedTotal;
+document.querySelector('.average').textContent = formattedAverage;
