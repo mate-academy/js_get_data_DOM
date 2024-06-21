f3d74dd959be8add5a6d3a82af52acc42c2ff6f5
Mykhailo Hubko <mixelios@gmail.com>
2024-06-21 16:45:40 +0200

    js get data DOM 1 reduction

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write your code here
+
+const populations = [...document.querySelectorAll('.population')];
+
+const populationValues = populations.map((population) => {
+  return +population.innerHTML.replace(/,/g, '');
+});
+
+const totalPopulation = populationValues.reduce(
+  (acumulator, currentValue) => acumulator + currentValue,
+  0,
+);
+
+const averagePopulation = totalPopulation / populationValues.length;
+
+const totalPopulationSpan = document.querySelector('.total-population');
+const averagePopulationSpan = document.querySelector('.average-population');
+
+totalPopulationSpan.innerHTML = totalPopulation.toLocaleString('en-US');
+
+averagePopulationSpan.innerHTML = averagePopulation.toLocaleString('en-US');
