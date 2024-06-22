8e7d86644a8864be3c9b458ba4bbb7fd3e7a4061
Iryna Popovych <popovicirina40@gmail.com>
2024-06-22 22:16:15 +0100

    add salution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+const spans = document.getElementsByClassName('population');
+let population = [];
+
+for (const span of spans) {
+  population.push(Number(span.innerHTML.split(',').join('')));
+};
+
+const total = population.reduce((acc, curr) => {
+  return acc + curr;
+}, 0);
+
+const avarege = total/10;
+
+const totalPrint = document.getElementsByClassName('total-population');
+totalPrint[0].innerHTML = total;
+
+const averegePrint = document.getElementsByClassName('average-population');
+averegePrint[0].innerHTML = avarege;
+
