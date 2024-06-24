9e9c21020743cd2a904a2f496c3b4654873884cb
Iryna Popovych <popovicirina40@gmail.com>
2024-06-24 22:50:14 +0100

    chenge format

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,21 +2,22 @@
 
 // write your code here
 const spans = document.getElementsByClassName('population');
-let population = [];
+const population = [];
 
 for (const span of spans) {
   population.push(Number(span.innerHTML.split(',').join('')));
-};
+}
 
 const total = population.reduce((acc, curr) => {
   return acc + curr;
 }, 0);
 
-const avarege = total/10;
+const avarege = total / 10;
 
 const totalPrint = document.getElementsByClassName('total-population');
-totalPrint[0].innerHTML = total;
+
+totalPrint[0].innerHTML = total.toLocaleString('en-US');
 
 const averegePrint = document.getElementsByClassName('average-population');
-averegePrint[0].innerHTML = avarege;
 
+averegePrint[0].innerHTML = avarege.toLocaleString('en-US');
