e4f9c29edbfecd5c689e78dda84acd9eab16f5f1
Roman Perepelkin <perepelkinrs@gmail.com>
2024-06-25 12:12:39 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('[class="population"]')];
+let totalPopulation = 0;
+
+for (const country of population) {
+  totalPopulation += Number(country.innerHTML.split(',').join(''));
+}
+
+document.querySelector('[class="total-population"]').innerHTML =
+  totalPopulation.toLocaleString('En-US');
+
+document.querySelector('[class="average-population"]').innerHTML = (
+  totalPopulation / population.length
+).toLocaleString('En-US');
