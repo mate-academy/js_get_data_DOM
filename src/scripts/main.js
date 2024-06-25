8fde3799ef85ab1e9cc15a9b77bcb0b591573725
Roman Perepelkin <perepelkinrs@gmail.com>
2024-06-25 12:29:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const population = [...document.querySelectorAll('[class="population"]')];
+const population = [...document.querySelectorAll('.population')];
 let totalPopulation = 0;
 
 for (const country of population) {
   totalPopulation += Number(country.innerHTML.split(',').join(''));
 }
 
-document.querySelector('[class="total-population"]').innerHTML =
+document.querySelector('.total-population').innerHTML =
   totalPopulation.toLocaleString('En-US');
 
-document.querySelector('[class="average-population"]').innerHTML = (
+document.querySelector('.average-population').innerHTML = (
   totalPopulation / population.length
 ).toLocaleString('En-US');
