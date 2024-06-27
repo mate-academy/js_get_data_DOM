eb951bf965f3be40d34b18698fb250ec3f874dfa
MacBook Pro 13 <macbookpro13@MacBook-Pro-MacBook.local>
2024-06-27 11:35:55 +0300

    add test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const allSpanPopulation = document.querySelectorAll('.population');
+const total = document.getElementById('total-population');
+const avg = document.getElementById('average-population');
+
+const render = () => {
+  const populations = Array.from(allSpanPopulation).map(
+    (span) => +span.textContent.replaceAll(',', ''),
+  );
+
+  const totalPopulation = populations.reduce((acc, curr) => acc + curr, 0);
+
+  const averagePopulation = totalPopulation / populations.length;
+
+  total.textContent = `${totalPopulation.toLocaleString('en-US')}`;
+  avg.textContent = `${averagePopulation.toLocaleString('en-US')}`;
+};
+
+document.addEventListener('DOMContentLoaded', render);
