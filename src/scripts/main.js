562551a57d56c742c64fbdf3236d1c5e1a81251f
Mykola Bilenkiy <nikolosknv@gmail.com>
2024-06-28 13:44:11 +0300

    make task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+const populationElements = document.querySelectorAll('.population');
+const populations = [];
+
+for (const element of populationElements) {
+  const number = parseInt(element.textContent.replace(/,/g, ''));
+
+  if (!isNaN(number)) {
+    populations.push(number);
+  }
+}
+
+const totalPopulation = populations.reduce((acc, num) => acc + num, 0);
+const averagePopulation = totalPopulation / populations.length;
+
+const formatedTotal = totalPopulation.toLocaleString('en-US');
+const formatedAverage = averagePopulation.toLocaleString('en-US');
+
+document.querySelector('.total-population').textContent = formatedTotal;
+document.querySelector('.average-population').textContent = formatedAverage;
