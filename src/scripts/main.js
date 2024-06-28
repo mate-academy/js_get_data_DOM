4df77ee1b8c8273e9397ae466e557a134ea92267
Heorhii Savostikov <georgesavostikov@gmail.com>
2024-06-28 19:25:18 +0300

    Added some changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,5 +6,8 @@ const allRigth = data.map((text) => +text.textContent.split(',').join(''));
 const total = allRigth.reduce((sum, elem) => sum + elem, 0);
 const average = total / allRigth.length;
 
-document.querySelector('span.average-population').textContent = average;
-document.querySelector('span.total-population').textContent = total;
+document.querySelector('span.average-population').textContent =
+  average.toLocaleString('en-US');
+
+document.querySelector('span.total-population').textContent =
+  total.toLocaleString('en-US');
