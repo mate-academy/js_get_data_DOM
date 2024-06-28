2ee14aacd5c5f81c6584a629682f17c9abe9fd55
Heorhii Savostikov <georgesavostikov@gmail.com>
2024-06-28 19:21:12 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const data = [...document.querySelectorAll('span.population')];
+const allRigth = data.map((text) => +text.textContent.split(',').join(''));
+
+const total = allRigth.reduce((sum, elem) => sum + elem, 0);
+const average = total / allRigth.length;
+
+document.querySelector('span.average-population').textContent = average;
+document.querySelector('span.total-population').textContent = total;
