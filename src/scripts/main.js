c4b96a4588f65cd4873684c1380b8f17d41c485a
KromchenkovOleksandr <khrom31@gmail.com>
2024-06-29 21:10:33 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+const x = [...document.querySelectorAll('span.population')];
+let sum = 0;
+
+for (let i = 0; i < x.length; i++) {
+  const textContent = x[i].textContent.replace(/,/g, '');
+
+  if (!isNaN(textContent)) {
+    sum += +textContent;
+  }
+}
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.textContent = Intl.NumberFormat('en-us').format(sum);
+
+averagePopulation.textContent = Intl.NumberFormat('en-us').format(
+  sum / x.length,
+);
