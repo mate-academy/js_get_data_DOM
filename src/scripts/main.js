f05ad05de6bbf82371f869232c4b45eb5731a05c
KromchenkovOleksandr <khrom31@gmail.com>
2024-06-30 13:51:03 +0300

    Add reduce

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,21 +2,16 @@
 
 // write your code here
 const x = [...document.querySelectorAll('span.population')];
-let sum = 0;
-
-for (let i = 0; i < x.length; i++) {
-  const textContent = x[i].textContent.replace(/,/g, '');
-
-  if (!isNaN(textContent)) {
-    sum += +textContent;
-  }
-}
+const res = x.reduce(
+  (sum, popul) => sum + parseInt(popul.textContent.replace(/,/g, '')),
+  0,
+);
 
 const totalPopulation = document.querySelector('.total-population');
 const averagePopulation = document.querySelector('.average-population');
 
-totalPopulation.textContent = Intl.NumberFormat('en-us').format(sum);
+totalPopulation.textContent = Intl.NumberFormat('en-us').format(res);
 
 averagePopulation.textContent = Intl.NumberFormat('en-us').format(
-  sum / x.length,
+  res / x.length,
 );
