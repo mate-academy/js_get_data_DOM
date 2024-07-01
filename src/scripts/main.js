42692bf9665033b1ead26471290e6ec81dbc8c79
BoGoBooM <MyPikcha@gmail.com>
2024-07-01 23:32:19 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const totalSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+
+const population = [...document.querySelectorAll('.population')];
+const totalPopulation = population.reduce(
+  (total, el) => total + Number(el.innerHTML.replaceAll(',', '')),
+  0,
+);
+const averagePopulation = Math.round(totalPopulation / population.length);
+
+totalSpan.innerHTML = totalPopulation.toLocaleString();
+averageSpan.innerHTML = averagePopulation.toLocaleString();
