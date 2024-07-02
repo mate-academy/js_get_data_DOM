c6754e907ece6bc46fb48b014dc19e327c4a105d
Ivan <fanmetalheavy@gmail.com>
2024-07-02 19:17:59 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write your code here
+function getValue(item) {
+  return +item.split(',').join('');
+}
+
+const population = [
+  ...document.documentElement.getElementsByClassName('population'),
+];
+
+const totalPopulation = population.reduce(
+  (acc, item) => acc + getValue(item.innerText),
+  0,
+);
+
+const total = [
+  ...document.documentElement.getElementsByClassName('total-population'),
+];
+
+total[0].replaceWith(totalPopulation.toLocaleString('en-US'));
+
+const average = [
+  ...document.documentElement.getElementsByClassName('average-population'),
+];
+
+average[0].replaceWith(
+  (totalPopulation / population.length).toLocaleString('en-US'),
+);
