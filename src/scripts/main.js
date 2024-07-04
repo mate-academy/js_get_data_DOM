e9152112cdbe4bee52a51391424f5bbefb736e1b
Yehor Gordiienko <egordienko319@gmail.com>
2024-07-04 23:51:53 +0300

    add toLocalString

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,15 @@
 const elements = document.getElementsByClassName('population');
 let allPopulationNumber = 0;
 
-for (const key of elements ){
+for (const key of elements) {
   allPopulationNumber += +key.innerHTML.replaceAll(',', '');
 }
 
 const averagePopulation = allPopulationNumber / elements.length;
 
-let classTotalPopulation = document.querySelector('.total-population');
-let classAveragePopulation = document.querySelector('.average-population');
+const classTotalPopulation = document.querySelector('.total-population');
+const classAveragePopulation = document.querySelector('.average-population');
 
-classTotalPopulation.innerHTML = allPopulationNumber;
+classTotalPopulation.innerHTML = allPopulationNumber.toLocaleString('en-US');
 
-classAveragePopulation.innerHTML = averagePopulation;
-
-console.log(averagePopulation);
-console.log(allPopulationNumber);
+classAveragePopulation.innerHTML = averagePopulation.toLocaleString('en-US');
