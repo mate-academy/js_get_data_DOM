99da48808b97f5edc917bed0b4139182a6ed6379
Kirill <ovoo1339@gmail.com>
2024-07-04 10:45:15 +0300

    work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write your code here
+
+let total = 0;
+let average = 0;
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const ELEMENTS = document.querySelectorAll('.population');
+
+ELEMENTS.forEach((element) => {
+  const value = Number(element.textContent.split(',').join(''));
+
+  if (!isNaN(value)) {
+    total += value;
+  }
+});
+
+average = total / ELEMENTS.length;
+
+totalPopulation.textContent = total.toLocaleString('en-US');
+averagePopulation.textContent = average.toLocaleString('en-US');
