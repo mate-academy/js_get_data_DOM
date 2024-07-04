4eabce6ff9959e8a2a385c2e295613b76b3953de
Yehor Gordiienko <egordienko319@gmail.com>
2024-07-04 22:00:56 +0300

    new commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const elements = document.getElementsByClassName('population');
+let allPopulationNumber = 0;
+
+for (const key of elements ){
+  allPopulationNumber += +key.innerHTML.replaceAll(',', '');
+}
+
+const averagePopulation = allPopulationNumber / elements.length;
+
+let classTotalPopulation = document.getElementsByClassName('total-population');
+classTotalPopulation.innerHTML = allPopulationNumber;
+
+let classAveragePopulation = document.getElementsByClassName('average-population');
+classAveragePopulation.innerHTML = averagePopulation;
+
+console.log(averagePopulation);
+console.log(allPopulationNumber);
