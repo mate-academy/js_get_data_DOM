2ede843703be6f6a0cda4cce83132bfcb88343b0
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-07-05 22:53:11 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('span.population');
+const population = [...populationElements].map(
+  (el) => +el.textContent.replaceAll(',', ''),
+);
+const sum = population.reduce((prev, number) => number + prev, 0);
+
+const totalPopulation = document.querySelectorAll('span.total-population')[0];
+const average = document.querySelectorAll('span.average-population')[0];
+
+totalPopulation.textContent = sum.toLocaleString('en-US');
+
+average.textContent = (sum / population.length).toLocaleString('en-US');
