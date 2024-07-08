155e684569353eb1fbcd4ce40bac941e84ec32ae
Oleksandra Kuziura <radioday2014@gmail.com>
2024-07-08 20:21:30 +0300

    calculate total sum and average

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
+// array of numbers of populations
+const  populations = Array.from(document.querySelectorAll('.population'));
 
-// write your code here
+// function to count total populations
+const totalSum = (array) => {
+  let result = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    result += parseInt(array[i].textContent.replaceAll(',',''));
+  }
+
+  return result;
+}
+
+//variables to define total and average sum of populations
+const total = totalSum(populations);
+const average = total / populations.length;
+//replace 'calculate it' with final result
+document.querySelector('.total-population').innerText = total.toLocaleString('en-US').replaceAll('.',',');
+document.querySelector('.average-population').innerText = average.toLocaleString('en-US').replaceAll('.',',');
+
