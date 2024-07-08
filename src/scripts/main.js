3907e9b86b2aaab0dea2d2b10a843946efd42103
Oleksandra Kuziura <radioday2014@gmail.com>
2024-07-08 21:22:10 +0300

    calculate total sum and average

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,26 @@
 'use strict';
+
 // array of numbers of populations
-const  populations = Array.from(document.querySelectorAll('.population'));
+const populations = Array.from(document.querySelectorAll('.population'));
 
 // function to count total populations
 const totalSum = (array) => {
   let result = 0;
 
   for (let i = 0; i < array.length; i++) {
-    result += parseInt(array[i].textContent.replaceAll(',',''));
+    result += parseInt(array[i].textContent.replaceAll(',', ''));
   }
 
   return result;
-}
+};
 
-//variables to define total and average sum of populations
+// variables to define total and average sum of populations
 const total = totalSum(populations);
 const average = total / populations.length;
-//replace 'calculate it' with final result
-document.querySelector('.total-population').innerText = total.toLocaleString('en-US').replaceAll('.',',');
-document.querySelector('.average-population').innerText = average.toLocaleString('en-US').replaceAll('.',',');
 
+// replace 'calculate it' with final result
+document.querySelector('.total-population').innerText = total
+  .toLocaleString('en-US');
+
+document.querySelector('.average-population').innerText = average
+  .toLocaleString('en-US');
