e6bba55463f51da19bf54d34e9a87c9aaa8137b0
Olha Rypich <olha.rypich@gmail.com>
2024-07-09 18:32:56 +0300

    js_get_data_DOM-solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const stats = [...document.querySelectorAll('.population')].map(
+  (el) => +el.innerText.split(',').join(''),
+);
+
+const totalSum = stats.reduce((acc, el) => acc + el, 0);
+const total = totalSum.toLocaleString('en-US');
+const average = (totalSum / stats.length).toLocaleString('en-US');
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.innerText = total;
+averagePopulation.innerText = average;
