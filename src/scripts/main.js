ffdc51b2de4d48a6bc97258e740a4addd9bce119
my-git-username <118355685+my-git-username@users.noreply.github.com>
2024-07-09 14:14:33 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('span.population')];
+const total = [];
+
+for (const el of population) {
+  const text = el.textContent;
+  const number = text.replace(/,/g, '');
+
+  total.push(number);
+}
+
+const sum = total.reduce((acc, el) => Number(acc) + Number(el));
+const averege = sum / 10;
+
+const textSum = document.querySelector('span.total-population');
+const textAverege = document.querySelector('span.average-population');
+
+textSum.textContent = sum.toLocaleString('en-US');
+textAverege.textContent = averege.toLocaleString('en-US');
