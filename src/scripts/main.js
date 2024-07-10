370a72e53f7ba376d49c1b9e58ec5d6cf6c843d3
Ren <buher1994@gmail.com>
2024-07-10 18:18:23 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const populationSpan = document.querySelectorAll('span.population');
+const totalPopulation = document.querySelectorAll('total-population');
+const averagePopulation = document.querySelectorAll('average-population');
+
+let numberOfPopulation = 0;
+
+populationSpan.forEach = (span) => {
+  numberOfPopulation += Number(span.textContent.replaceAll(',', ''));
+};
+
+const averagePeople = numberOfPopulation / populationSpan.length;
+
+totalPopulation.textContent = numberOfPopulation.toLocaleString('en-US');
+averagePopulation.textContent = averagePeople.toLocaleString('en-US');
