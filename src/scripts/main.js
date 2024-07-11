fe598356c6ac36eb142954dafa81cd24701babf5
valost <valerieostrovska@yahoo.com>
2024-07-11 17:41:31 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const populationSpanArr = [...document.querySelectorAll('.population')];
+const populationNumArr = [];
+
+let populationTotal = 0;
+
+for (let i = 0; i < populationSpanArr.length; i++) {
+  const spanText = populationSpanArr[i].textContent;
+  const populationNum = parseInt(spanText.replace(/,/g, ''), 10);
+
+  populationNumArr.push(populationNum);
+
+  populationTotal += populationNum;
+}
+
+const populationAverage = populationTotal / populationNumArr.length;
+
+const totalPopulationSpan = document.querySelector('.total-population');
+const averagePopulationSpan = document.querySelector('.average-population');
+
+totalPopulationSpan.textContent = populationTotal.toLocaleString('en-US');
+averagePopulationSpan.textContent = populationAverage.toLocaleString('en-US');
