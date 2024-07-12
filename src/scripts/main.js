7e3da8133fff308b608f2f7d4079353540614964
Oleksii Shpak <willwildred@gmail.com>
2024-07-12 13:47:43 -0600

    implemented solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const data = [...document.querySelectorAll('.population')];
+const dataArr = data.map((el) => parseInt(el.textContent.split(',').join('')));
+const totalPopulation = dataArr.reduce((total, n) => total + n);
+const averagePopulation = (totalPopulation / dataArr.length).toLocaleString(
+  'en-US',
+);
+
+document.querySelector('.total-population').innerHTML =
+  totalPopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population').innerHTML = averagePopulation;
