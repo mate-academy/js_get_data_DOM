ca4c132e82a785c0ce16faae9a592beaf1f0b61a
mrsvolodya <vovamursky@gmail.com>
2024-07-13 19:43:19 +0300

    localeString

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,15 @@
 'use strict';
+
 const numbers = document.querySelectorAll('.population');
 const list = [];
+
 numbers.forEach((item) => list.push(item.textContent));
+
 const total = list.reduce((acc, val) => acc + +val.replaceAll(',', ''), 0);
 const average = total / numbers.length;
 
-document.querySelector('.total-population').textContent = total
-  .toString()
-  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-document.querySelector('.average-population').textContent = average
-  .toString()
-  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+document.querySelector('.total-population').textContent =
+  total.toLocaleString();
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString();
