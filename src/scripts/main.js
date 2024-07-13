2b2f2202b3b792b05a1c97c393d22788e1eae1c2
mrsvolodya <vovamursky@gmail.com>
2024-07-13 19:07:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
+const numbers = document.querySelectorAll('.population');
+const list = [];
+numbers.forEach((item) => list.push(item.textContent));
+const total = list.reduce((acc, val) => acc + +val.replaceAll(',', ''), 0);
+const average = total / numbers.length;
 
-// write your code here
+document.querySelector('.total-population').textContent = total
+  .toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+document.querySelector('.average-population').textContent = average
+  .toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
