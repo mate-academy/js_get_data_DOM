5338d8f3718d074d168edf4ec1d00e61db1de134
Denys Khvashchenko <khvashchenkodenys.gmail.com>
2024-07-16 11:33:51 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const numbers = Array.from(spans)
+  .map((span) => span.textContent.split(',').join(''))
+  .map(Number);
+
+const sum = numbers.reduce((acc, number) => acc + number, 0);
+const average = sum / spans.length;
+
+function thousand(num) {
+  return num.toLocaleString('en-US');
+}
+
+totalPopulation.textContent = thousand(sum);
+averagePopulation.textContent = thousand(average);
