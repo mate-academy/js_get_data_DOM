49efa6b99d0be3dc8fa7e18ca4865aa5c5fdfbd9
Misha Remeslo <misha.remeslol@gmail.com>
2024-07-17 20:41:41 +0300

    Work out total and average

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('span.population');
+const totalPopulation = document.querySelector('span.total-population');
+const averagePopulation = document.querySelector('span.average-population');
+const populationNumbers = [];
+
+population.forEach((element) => {
+  const populationText = element.textContent;
+  const populationNumber = parseInt(populationText.split(',').join(''));
+
+  if (!isNaN(populationNumber)) {
+    populationNumbers.push(populationNumber);
+  }
+});
+
+let total = 0;
+
+populationNumbers.forEach((populationNumber) => {
+  total += populationNumber;
+});
+
+totalPopulation.textContent = total.toString();
+
+let average = 0;
+
+if (populationNumbers.length > 0) {
+  average = total / populationNumbers.length;
+}
+
+averagePopulation.textContent = average.toString();
