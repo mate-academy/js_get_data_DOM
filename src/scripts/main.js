b2c3a83bc1e564294d0964644e870ee79411136d
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-17 03:01:12 +0300

    improve solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,16 @@
 'use strict';
 
-let total = 0;
+const populations = [];
 
-document
-  .querySelectorAll('.population')
-  .forEach((el) => (total += +el.textContent.split(',').join('')));
+document.querySelectorAll('.population').forEach((el) => {
+  populations.push(+el.textContent.replaceAll(',', ''));
+});
+
+const totalPopulation = populations.reduce((prev, cur) => prev + cur);
 
 document.querySelector('.total-population').textContent =
-  total.toLocaleString('en-US');
+  totalPopulation.toLocaleString('en-US');
 
 document.querySelector('.average-population').textContent = (
-  total / 10
+  totalPopulation / populations.length
 ).toLocaleString('en-US');
