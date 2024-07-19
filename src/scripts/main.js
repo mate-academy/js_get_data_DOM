6e8f6647c497d6b3a4e641e83cc5e122b8b7a9a6
Katya <evil.666.buny@gmail.com>
2024-07-19 17:19:39 +0300

    script added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const populationElements = document.querySelectorAll('.population');
+
+  const populations = Array.from(populationElements).map((el) => {
+    const populationText = el.textContent.replace(/,/g, ''); // Видалити коми
+
+    return parseInt(populationText, 10);
+  });
+
+  const totalPopulation = populations.reduce((acc, num) => acc + num, 0);
+  const averagePopulation = totalPopulation / populations.length;
+
+  const formattedTotal = totalPopulation.toLocaleString();
+  const formattedAverage = averagePopulation.toLocaleString(undefined, {
+    maximumFractionDigits: 0,
+  });
+
+  document.querySelector('.total-population').textContent = formattedTotal;
+  document.querySelector('.average-population').textContent = formattedAverage;
+});
