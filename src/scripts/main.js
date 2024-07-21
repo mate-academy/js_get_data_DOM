a98cda4e3786e3027c1a350b9125e90bd8a4ca54
Nadinnnka <tymoshyk02@gmail.com>
2024-07-21 10:15:51 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populations = [...document.querySelectorAll('.population')].map(
+  (el) => +el.textContent.replaceAll(',', ''),
+);
+
+const totalPopulation = populations.reduce((sum, num) => sum + num, 0);
+const averagePopulation = totalPopulation / populations.length;
+
+document.querySelector('.total-population').textContent =
+  addSeparetor(totalPopulation);
+
+document.querySelector('.average-population').textContent =
+  addSeparetor(averagePopulation);
+
+function addSeparetor(num) {
+  return num.toLocaleString('en-US');
+}
