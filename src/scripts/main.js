566ea89d3d3809ad0f3b21cb5328ed9d0c173970
svyatoslav <svyatoslavik9@gmail.com>
2024-07-22 17:46:40 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
-'use strict';
+'use strict'
 
-// write your code here
+// get data dom
+const populations = [...document.querySelectorAll('.population')]
+const totalPopulation = document.querySelector('.total-population');
+const avaragePopulation = document.querySelector('.average-population');
+
+const getParsedData = (data) => {
+  const parsed = data.textContent;
+  const result = +(parsed.replaceAll(',', ''));
+
+  return result
+}
+
+const getSum = (data) => {
+  let result = 0
+
+
+  for (const population of data) {
+    result += getParsedData(population);
+  }
+
+  return result;
+}
+
+
+const getDataFromPopulations = () => {
+  const sum = getSum(populations) || 0;
+  const avarage = sum / populations.length;
+
+  const formatedSum = sum.toLocaleString('en-US');
+  const formatedAvarage = avarage.toLocaleString('en-US');
+
+  totalPopulation.textContent = formatedSum;
+  avaragePopulation.textContent = formatedAvarage;
+}
+
+getDataFromPopulations()
