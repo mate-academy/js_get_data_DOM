efbf7218174fc806c0eb8cb21c31ae5bb23fd082
Your Name <ruslankurman13@gmail.com>
2024-07-22 13:24:03 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const populationSpans = document.querySelectorAll('span.population');
+  const populationNumber = Array.from(populationSpans)
+    .map((span) => {
+      const text = span.textContent;
+
+      const numberText = text.replace(/,/g, '');
+
+      const number = parseFloat(numberText);
+
+      if (!isNaN(number)) {
+        return number;
+      } else {
+        return null;
+      }
+    })
+    .filter((num) => num !== null);
+  const totalPopulation = populationNumber.reduce((sum, num) => sum + num, 0);
+
+  const averagePopulation = totalPopulation / populationNumber.length;
+  const totalPopulationSpan = document.querySelector('span.total-population');
+  const averagePopulationSpan = document.querySelector(
+    'span.average-population',
+  );
+  const options = { style: 'decimal', useGrouping: true };
+
+  if (totalPopulationSpan) {
+    totalPopulationSpan.textContent = totalPopulation.toLocaleString(
+      'en-US',
+      options,
+    );
+  }
+
+  if (averagePopulationSpan) {
+    averagePopulationSpan.textContent = averagePopulation.toLocaleString(
+      'en-US',
+      options,
+    );
+  }
+});
