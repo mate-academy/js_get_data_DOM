e37da5ce80d855fe3a616ce5147aa7944cad2841
Roman Bekh <coldshein@gmail.com>
2024-07-23 15:08:11 +0300

    solved task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write your code here
+const collection = document.querySelectorAll('.population');
+const population = [...collection].map((item) => {
+  item.innerText.split(',').join('');
+});
+
+const totalPopulation = population.reduce((initial, item) => {
+  return +initial + +item;
+}, 0);
+const avgPopulation = totalPopulation / population.length;
+
+const avgPopulationElement = document.querySelector('.average-population');
+const totalPopulationElement = document.querySelector('.total-population');
+
+avgPopulationElement.innerText = avgPopulation.toLocaleString('en-US');
+totalPopulationElement.innerText = totalPopulation.toLocaleString('en-US');
