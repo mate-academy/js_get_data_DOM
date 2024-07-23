616a96a9ab44f4d7dd74bd8b6703a54b95578f66
Timofii Ustilou <timoshaustilov@gmail.com>
2024-07-23 17:27:50 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const populations = Array.from(
+  document.getElementsByClassName('population'),
+).map((x) => +x.textContent.replaceAll(',', ''));
+const summ = populations.reduce((acc, curr) => acc + curr);
+
+document.getElementsByClassName('total-population')[0].textContent =
+  summ.toLocaleString('en-US');
+
+document.getElementsByClassName('average-population')[0].textContent =
+  Math.round(summ / populations.length).toLocaleString('en-US');
