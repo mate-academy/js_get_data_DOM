1f266d0594704c421a174501375ff408dca7649f
Korolko Anastasiia <korolkonasta@gmail.com>
2024-07-24 12:37:53 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+function formatNumber(number) {
+  return number.toLocaleString('en-US');
+}
+
+const spanElements = document.querySelectorAll('span.population');
+
+let totalValue = 0;
+let count = 0;
+
+spanElements.forEach((span) => {
+  const textContent = span.textContent;
+
+  const cleanedText = textContent.split(',').join('');
+
+  const populationNumber = Number(cleanedText);
+
+  if (!isNaN(populationNumber)) {
+    totalValue += populationNumber;
+    count++;
+  }
+});
+
+const averageValue = totalValue / count;
+
+const averageSpan = document.querySelector('span.average-population');
+const totalSpan = document.querySelector('span.total-population');
+
+averageSpan.textContent = formatNumber(averageValue);
+totalSpan.textContent = formatNumber(totalValue);
