28f5dfd7f03e849632cdb31258a75b6fa0ebb366
May <miaqu138@gmail.com>
2024-07-30 23:14:01 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const populationNumber = document.querySelectorAll('.population');
+let total = 0;
+let count = 0;
+
+for (const elem of populationNumber) {
+  const number = parseFloat(elem.textContent.replace(/,/g, ''));
+
+  if (!isNaN(number)) {
+    total += number;
+    count++;
+  }
+}
+
+const average = total / count;
+
+const totalElem = document.querySelector('.total-population');
+const averageElem = document.querySelector('.average-population');
+
+totalElem.innerHTML = total.toLocaleString('en-US');
+averageElem.innerHTML = average.toLocaleString('en-US');
