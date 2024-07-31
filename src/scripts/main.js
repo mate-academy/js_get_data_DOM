c5a896365c3baef46eb0708c8d1d9b04fbb88711
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-07-31 09:46:25 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const populationSpans = document.getElementsByClassName('population');
+const populations = [];
+let totalPopulation = 0;
+
+for (let i = 0; i < populationSpans.length; i++) {
+  const populationText = populationSpans[i].textContent.replace(/,/g, '');
+  const populationNumber = Number(populationText);
+
+  if (!isNaN(populationNumber)) {
+    populations.push(populationNumber);
+    totalPopulation += populationNumber;
+  }
+}
+
+const averagePopulation = totalPopulation / populations.length;
+
+const total = document.querySelector('.total-population');
+
+total.textContent = totalPopulation.toLocaleString();
+
+const average = document.querySelector('.average-population');
+
+average.textContent = averagePopulation.toLocaleString();
