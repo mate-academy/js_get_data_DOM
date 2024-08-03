2e465006bc8e84af8939df6c67bc97c9a2987d5b
Maksim Atanasov <atanasovmaksim56@gmail.com>
2024-08-03 17:23:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const items = document.getElementsByClassName('population');
+
+let totalSum = 0;
+
+for (const item of items) {
+  totalSum += Number(item.innerHTML.split(',').join(''));
+}
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.innerHTML = totalSum;
+averagePopulation.innerHTML = Math.round(totalSum / items.length);
