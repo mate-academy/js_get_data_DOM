852f537c08ce538dc1b772e81e359a2f745eafd6
Maksym Prokopovych <prokopovych2006@gmail.com>
2024-08-05 13:31:53 +0300

    create-prg

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write your code here
+const populationOfElements = document.querySelectorAll('.population');
+let finalPopulation = 0;
+
+populationOfElements.forEach((element) => {
+  finalPopulation += parseInt(element.textContent.replace(/,/g, ''));
+});
+
+const averagePopulation = finalPopulation / populationOfElements.length;
+
+document.querySelector('.total-population').textContent =
+  finalPopulation.toLocaleString();
+
+document.querySelector('.average-population').textContent =
+  averagePopulation.toLocaleString();
