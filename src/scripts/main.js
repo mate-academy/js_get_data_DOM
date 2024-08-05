f44e9c4e00b6d66649addb8d615e4161e52b3d34
Maria <maria.pozniak2013@gmail.com>
2024-08-05 16:09:47 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+
+let populations = [...populationElements].map((element) => element.textContent);
+
+populations = populations.map((element) =>
+  parseInt(element.replace(/,/g, ''), 10),
+);
+
+const length = populations.length;
+
+let totalPopulation = 0;
+
+populations.forEach((element) => {
+  totalPopulation += element;
+});
+
+let averagePopulation = totalPopulation / length;
+
+totalPopulation = totalPopulation.toLocaleString('en-US');
+averagePopulation = averagePopulation.toLocaleString('en-US');
+
+const totalPopulationElem = document.querySelector('.total-population');
+
+totalPopulationElem.textContent = totalPopulation;
+
+const averagePopulationElem = document.querySelector('.average-population');
+
+averagePopulationElem.textContent = averagePopulation;
