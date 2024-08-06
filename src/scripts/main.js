f34c13461804a97eb8830982c535b224a71d3fa4
yursprava <yursprava@gmail.com>
2024-08-05 21:15:26 -0400

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const collection = document.getElementsByClassName("population");
+
+let total = 0;
+
+for (const country of collection) {
+  total += Number(country.textContent.split(',').join(''));
+}
+
+const average = total / collection.length;
+
+const totalElement = document.getElementsByClassName("total-population");
+
+totalElement[0].textContent = total.toLocaleString('en-US');
+
+const averageElement = document.getElementsByClassName("average-population");
+
+averageElement[0].textContent = average.toLocaleString('en-US');
