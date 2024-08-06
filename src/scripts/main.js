a8d77ab9c2f7b6a0544d64009e4de9495c7ffeb6
Mainaiev <farid.mainaiev@proton.me>
2024-08-06 03:25:39 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+
+const numbers = document.querySelectorAll('.population');
+const values = [...numbers].map((e) => e.textContent.trim().replace(/,/g, ''));
+const totalRes = document.querySelector('.total-population');
+const averageRes = document.querySelector('.average-population');
+
+let result = 0;
+const arrLength = values.length;
+
+for (let i = 0; i < arrLength; i++) {
+  const num = Number(values[i]);
+
+  result += num;
+}
+
+const average = result / arrLength;
+
+totalRes.innerText = result.toLocaleString();
+averageRes.innerText = average.toLocaleString();
