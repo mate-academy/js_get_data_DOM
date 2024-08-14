aa406ea6e88d5c7ed60f5384495b2d2476094215
Koshmak Nataliia <koshmaknatali@gmail.com>
2024-08-13 19:39:07 -0600

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const data = [...document.getElementsByClassName('population')];
+let total = 0;
+
+data.forEach((population) => {
+  total += +population.textContent.replace(/,/g, '');
+});
+
+const average = total / data.length;
+
+document.querySelector('.average-population').innerText =
+  average.toLocaleString('en-US');
+
+document.querySelector('.total-population').innerText =
+  total.toLocaleString('en-US');
