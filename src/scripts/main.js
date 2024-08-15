d827179634fa488e418b1087d58a13828807adc7
Vlad Zahorovskyi <zagorovskii12@gmail.com>
2024-08-15 19:47:30 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const populationElements = document.getElementsByClassName('population');
+
+let totalPopulation = 0;
+const populations = [];
+
+for (let i = 0; i < populationElements.length; i++) {
+  const populationSplit = populationElements[i].textContent.split(',');
+  const population = populationSplit.join('');
+
+  populations.push(population);
+  totalPopulation += parseFloat(population);
+}
+
+const averagePopulation = totalPopulation / populations.length;
+
+const formattedTotalPopulation = totalPopulation.toLocaleString('en-US');
+const formattedAveragePopulation = averagePopulation.toLocaleString('en-US');
+
+document.querySelector('.total-population').textContent =
+  formattedTotalPopulation;
+
+document.querySelector('.average-population').textContent =
+  formattedAveragePopulation;
