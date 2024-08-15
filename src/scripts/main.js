6e4df6ac1a4582fc2af81bfa593b2d0284c1aa24
Zyrak Nur <noorzirak7@gmail.com>
2024-08-15 23:55:00 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+function convertToNumber(str) {
+  return +str.replaceAll(',', '');
+}
+
+const populations = [...document.querySelectorAll('.population')];
+
+const convertedPopulations = populations.map((population) => {
+  return convertToNumber(population.textContent);
+});
+
+const totalPopulation = convertedPopulations.reduce((sum, p) => {
+  return sum + p;
+}, 0);
+
+const averagePopulation = totalPopulation / convertedPopulations.length;
+
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+
+totalElement.textContent = totalPopulation;
+averageElement.textContent = averagePopulation;
