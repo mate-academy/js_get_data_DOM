d7bd2e6ac58e2860b74f86690c01dd0bab9eae73
Vlad Zahorovskyi <zagorovskii12@gmail.com>
2024-08-15 18:13:21 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const populationElements = document.getElementsByClassName('population');
+
+let totalPopulation = 0;
+const populations = [];
+
+for (let i = 0; i < populationElements.length; i++) {
+  const population = parseInt(
+    populationElements[i].textContent.replace(',', ''),
+  );
+
+  if (!isNaN(population)) {
+    populations.push(population);
+    totalPopulation += population;
+  }
+}
+
+const averagePopulation = totalPopulation / populations.length;
+
+const formattedTotalPopulation = totalPopulation.toLocaleString('en-US');
+const formattedAveragePopulation = averagePopulation.toLocaleString('en-US');
+
+document.querySelector('.total-population').textContent =
+  formattedTotalPopulation;
+
+document.querySelector('.average-population').textContent =
+  formattedAveragePopulation;
