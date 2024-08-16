baaf15321717fa2902fb37d71f419de2c4e46554
Burchak Dmitry <burchak.dmitry@icloud.com>
2024-08-16 14:15:32 +0300

    delete function && added method split

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// write your code here
 const population =
   [...document.querySelectorAll('.population')]
     .map((el) => el.textContent)
@@ -13,52 +12,52 @@ const averagePopulation = (
   Math.round(totalPopulation / population.length) + ''
 ).split('');
 
-totalPopulation.split('');
-
 const total = document.querySelector('.total-population');
 const average = document.querySelector('.average-population');
 
-total.textContent = addComaElements(totalPopulation, totalPopulation.length);
-
-average.textContent = addComaElements(
-  averagePopulation,
-  averagePopulation.length,
-);
-
-// #region Function Роздліляю елементи масиву на коми
-function addComaElements(array, length) {
-  if (array !== undefined && length !== 0) {
-    let newArray = [...array];
-
-    if (length % 2 === 0) {
-      newArray[0] += ',';
-
-      for (let i = 1; i < newArray.length; i++) {
-        if (i % 3 === 0) {
-          newArray[i] += ',';
-        }
+total.textContent = totalPopulation
+  .split('')
+  .map((el, index, array) => {
+    if (array.length % 2 === 0) {
+      if (index === 1) {
+        return ',' + el;
       }
 
-      if (newArray[newArray.length - 1].includes('')) {
-        newArray[newArray.length - 1] =
-          parseInt(newArray[newArray.length - 1]) + '';
+      if (index % 3 === 0) {
+        return el + ',';
       }
     } else {
-      newArray = newArray.map((item, index) => {
-        if ((index + 1) % 3 === 0) {
-          return item + ',';
-        }
-
-        return item;
-      });
-
-      if (newArray[newArray.length - 1].includes('')) {
-        newArray[newArray.length - 1] =
-          parseInt(newArray[newArray.length - 1]) + '';
+      if ((index + 1) % 3 === 0) {
+        return el + ',';
       }
     }
 
-    return newArray.join('');
-  }
-}
-// #endregion
+    return el;
+  })
+  .join('')
+  .split(',')
+  .filter((el) => el !== '')
+  .join(',');
+
+average.textContent = averagePopulation
+  .map((el, index, array) => {
+    if (array.length % 2 === 0) {
+      if (index === 1) {
+        return ',' + el;
+      }
+
+      if (index % 3 === 0) {
+        return el + ',';
+      }
+    } else {
+      if ((index + 1) % 3 === 0) {
+        return el + ',';
+      }
+    }
+
+    return el;
+  })
+  .join('')
+  .split(',')
+  .filter((el) => el !== '')
+  .join(',');
