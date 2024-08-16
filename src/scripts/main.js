a2b43e56bd91f8e9874047e34d32f86fd1125d76
Oleksandr <dedikad@gmail.com>
2024-08-16 11:56:36 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+function convertToNum (str) {
+  return +str.replaceAll(',', '');
+}
+
+const population = [...document.querySelectorAll('.population')];
+
+const convertedPopulation = population.map((populations) => {
+  return convertToNum(populations.textContent);
+});
+
+const totalPopulation = convertedPopulation.reduce((sum, p) => {
+  return sum + p;
+}, 0)
+
+const avgPopulation = totalPopulation / convertedPopulation.length;
+
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+
+totalElement.textContent = totalPopulation;
+averageElement.textContent = avgPopulation;
