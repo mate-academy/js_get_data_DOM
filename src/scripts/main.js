3f4f1b65fbcfff867b0ff906573bec6e42a5e553
Stas <staskapica93@gmail.com>
2024-08-19 20:13:22 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const ArrayOfSelectorPopulation = [...document.querySelectorAll('.population')];
+const total = document.querySelector('.total');
+const average = document.querySelector('.average');
+
+let newTotal = 0;
+let newAverage = 0;
+
+ArrayOfSelectorPopulation.forEach((selector) => {
+  const NUMBER = Number(selector.textContent.replaceAll(',', ''));
+
+  newTotal += NUMBER;
+});
+
+newAverage = (newTotal / 10).toLocaleString('en-US');
+newTotal = newTotal.toLocaleString('en-US');
+
+total.textContent = `Total: ${newTotal}`;
+average.textContent = `Average: ${newAverage}`;
