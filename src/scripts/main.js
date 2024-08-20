238026b47d61d5e2e94ee9edb1006c86c78968fd
Ruslan Ogerenko <ruslanogerenko@gmail.com>
2024-08-20 12:56:02 +0300

    initial state

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,3 @@
 'use strict';
 
-const allPopulations = Array.from(document.querySelectorAll('.population')).map(
-  (el) => +el.textContent.replace(/,/g, ''),
-);
-
-const totalPopulationValue = allPopulations.reduce((acc, item) => acc + item);
-const averagePopulationValue = totalPopulationValue / allPopulations.length;
-
-document.querySelector('.total-population').textContent =
-  totalPopulationValue.toLocaleString('en-US');
-
-document.querySelector('.average-population').textContent =
-  averagePopulationValue.toLocaleString('en-US');
+// write your code here
