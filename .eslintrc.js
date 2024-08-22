598ae195822591941c0d996281de405a8dfdefc5
sinner1993 <sergio1993goga@gmail.com>
2024-08-22 19:11:17 +0200

    attempt to fix checks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,3 @@
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 2020, // or latest, to support ES6+ features
-    sourceType: 'module', // Allows for the use of imports
-  },
-  env: {
-    es6: true, // Adds all ECMAScript 6 globals and automatically sets the ecmaVersion parser option to 6
-    browser: true, // Enables browser globals like `window` and `document`
-    node: true, // Enables Node.js global variables and Node.js scoping
-  },
-  rules: {
-    // Add custom rules here
-  },
+  extends: '@mate-academy/eslint-config',
 };
