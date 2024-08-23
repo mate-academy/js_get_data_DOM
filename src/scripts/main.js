0f3a64a2d99d095f02e460d897c6d38ee991241b
sinner1993 <sergio1993goga@gmail.com>
2024-08-23 19:52:37 +0200

    12345t

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const averagePopulation = document.querySelector('.average-population');
+const total = document.querySelector('.total-population');
+
+const sum = [...population].map((el) => {
+  return Number(el.textContent.split(',').join(''));
+});
+
+const reduced = sum.reduce((acu, curr) => acu + curr, 0);
+
+total.textContent = reduced.toLocaleString('en-US');
+
+const calculateAveragePop = reduced / sum.length;
+
+averagePopulation.textContent = calculateAveragePop.toLocaleString('en-US');
