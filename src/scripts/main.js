c7175cf72d0aad30e4e1a557e0f0ba5ef2a262ca
Tetyana Chuhai <chugaytatyana@gmail.&git config --global user.email chugaytatyana@gmail.>
2024-08-24 23:11:40 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const allPopulations = [...document.querySelectorAll('.population')].map(
+  (el) => +el.textContent.replaceAll(',', ''),
+);
+
+const totalValue = allPopulations.reduce((acc, el) => acc + el, 0);
+const averageValue = totalValue / allPopulations.length;
+
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+total.textContent = totalValue.toLocaleString('en-US');
+average.textContent = averageValue.toLocaleString('en-US');
