fcf45f3a29cf41c5ce6bdf659ef4f08776c5f46f
Maksym Mohyla <greenparksquad@gmail.com>
2024-08-25 14:27:32 +0300

    get-data-dom task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populationList = document.querySelectorAll('.population');
+const populationNumbers = [];
+
+populationList.forEach((element) => {
+  const populationNumber = Number(element.textContent.replace(/\D/g, ''));
+
+  populationNumbers.push(populationNumber);
+});
+
+const sum = populationNumbers.reduce((a, b) => a + b);
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+total.textContent = sum.toLocaleString('en-US');
+
+average.textContent = (sum / populationNumbers.length).toLocaleString('en-US');
