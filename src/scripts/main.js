5f0cae0a9819cf58df4f03df2eddda37f7bafa33
Anna Agerone <olesnevych12@gmail.com>
2024-08-31 18:04:08 -0500

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populationClass = document.querySelectorAll('.population');
+
+const values = Object.values(populationClass);
+
+const populationCount = values.map(
+  (value) => +value.textContent.replaceAll(',', ''),
+);
+
+const total = populationCount.reduce((acc, count) => acc + count, 0);
+const average = total / populationCount.length;
+
+const getTotal = total.toLocaleString('en-US');
+const getAverage = average.toLocaleString('en-US');
+
+document.querySelector('.total-population').textContent = getTotal;
+document.querySelector('.average-population').textContent = getAverage;
