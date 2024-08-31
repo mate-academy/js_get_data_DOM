d40a9c41729c58e00f6074233ada392ca5921400
Valeria Chubarova <chubarova.valeria99@gmail.com>
2024-08-31 21:11:37 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('span.population');
+
+const populationArr = [];
+
+let total = 0;
+let countries = 0;
+
+population.forEach((span) => {
+  const amount = +span.textContent.replaceAll(',', '');
+
+  populationArr.push(amount);
+
+  total += amount;
+  countries++;
+});
+
+const average = total / countries;
+
+const formattedTotal = total.toLocaleString('en-US');
+const formattedAverage = average.toLocaleString('en-US');
+
+document.querySelector('span.total-population').textContent = formattedTotal;
+
+document.querySelector('span.average-population').textContent =
+  formattedAverage;
