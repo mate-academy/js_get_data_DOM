395555ef77c7483ca2a11e09c8c0da002b0b9389
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-09-01 09:10:56 +0300

    add task soultion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+let totalValue = 0;
+
+for (const population of populations) {
+  population.innerHTML = population.innerHTML.replaceAll(',', '');
+
+  totalValue += +population.innerHTML;
+}
+
+const avarageValue = totalValue / populations.length;
+
+totalPopulation.innerHTML = totalValue.toLocaleString('en-US');
+averagePopulation.innerHTML = avarageValue.toLocaleString('en-US');
