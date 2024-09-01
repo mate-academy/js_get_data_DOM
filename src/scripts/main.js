1f1accc1a3a3e52f682e62b9e06f287ad1848cb7
alex_motalex <tokyopac@gmail.com>
2024-09-01 15:57:01 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const populationElements = document.getElementsByClassName('population');
+
+let totalPopulationCount = null;
+
+for (const elem of populationElements) {
+  const population = Number(elem.innerHTML.replace(/,/g, ''));
+
+  totalPopulationCount += population;
+}
+
+const averagePopulationCount = totalPopulationCount / populationElements.length;
+
+const formattedTotalPopulation = totalPopulationCount.toLocaleString('en-US');
+
+const formattedAveragePopulation =
+  averagePopulationCount.toLocaleString('en-US');
+
+document.querySelector('.total-population').innerHTML =
+  formattedTotalPopulation;
+
+document.querySelector('.average-population').innerHTML =
+  formattedAveragePopulation;
