6fe22b24997b2870c34f214d29722e30b37dfc0f
Kamil <kamilbykowski99@gmail.com>
2024-09-04 16:00:40 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const docPopulationArray = document.querySelectorAll('.population');
+
+const docTotal = document.querySelector('.total-population');
+
+const docAverage = document.querySelector('.average-population');
+
+const populationArray = [];
+
+docPopulationArray.forEach((item) => populationArray.push(item.textContent));
+
+const populationNumbers = populationArray.map(
+  (item) => +item.replaceAll(',', ''),
+);
+
+const totalPopulation = populationNumbers.reduce((total, curr) => total + curr);
+
+const averagePopulation = totalPopulation / populationArray.length;
+
+docTotal.textContent = totalPopulation.toLocaleString('en-US');
+docAverage.textContent = averagePopulation.toLocaleString('en-US');
