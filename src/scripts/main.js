fc40157c228c2c7e848791fa901455d9174af80f
Vika <rudaviktoria0@gmail.com>
2024-09-05 11:20:24 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
 // write your code here
+const allElementSpan = document.querySelectorAll('span.population');
+
+let totalValue = 0;
+let count = 0;
+
+for (let i = 0; i < allElementSpan.length; i++) {
+  // eslint-disable-next-line no-unused-vars
+  const value = allElementSpan[i].textContent;
+}
+
+// eslint-disable-next-line no-undef
+if (!isNaN(value)) {
+  // eslint-disable-next-line no-unused-expressions, no-undef
+  value = +value;
+
+  // eslint-disable-next-line no-undef
+  totalValue += value;
+  count++;
+}
+
+const avarageValue = totalValue / count;
+
+const formattedTotal = totalValue.toLocaleString('en-US');
+const formattedAvarage = avarageValue.toLocaleString('en-Us');
+
+// eslint-disable-next-line max-len
+document.querySelector('span.avarage-population').textContent =
+  formattedAvarage;
+document.querySelector('span.total-population').textContent = formattedTotal;
+
+// eslint-disable-next-line no-console
+console.log('Total Value: ', formattedTotal);
+
+// eslint-disable-next-line no-console
+console.log('Avarage Value: ', formattedAvarage);
