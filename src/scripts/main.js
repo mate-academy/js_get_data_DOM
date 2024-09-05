35d2fa712410d2e803f5bb724cdff0964c306bf8
Anastasiia Cagle <anastasiiacagle@gmail.com>
2024-09-05 10:27:27 +0300

    solve

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write your code here
+
+const populationTextsArr = Array.from(
+  document.querySelectorAll('span.population'),
+);
+
+const populationNumbersArr = populationTextsArr.map((span) =>
+  Number(span.textContent.replace(/,/g, '')),
+);
+
+const total = populationNumbersArr.reduce(
+  (sum, currentValue) => sum + currentValue,
+  0,
+);
+
+const average = total / populationNumbersArr.length;
+
+const totalPopulation = document.querySelector('span.total-population');
+
+const averagePopulation = document.querySelector('span.average-population');
+
+totalPopulation.textContent = total.toLocaleString('en-US');
+averagePopulation.textContent = average.toLocaleString('en-US');
