1ef0aaa6e57d10fbba954c10cfd634a414919eee
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-08 18:28:35 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+const list = document.querySelector('.list');
+const items = list.querySelectorAll('.list__item');
+
+function calculateTotal() {
+  let totalPopulation = 0;
+
+  items.forEach((item) => {
+    const population = item.querySelector('.population').textContent;
+
+    totalPopulation += +population.split(',').join('');
+  });
+  total.textContent = totalPopulation;
+}
+
+function calculateAverage() {
+  let averagePopulation = 0;
+  let totalPopulation = 0;
+
+  items.forEach((item) => {
+    const population = item.querySelector('.population').textContent;
+
+    totalPopulation += +population.split(',').join('');
+  });
+  averagePopulation = Math.floor(totalPopulation / items.length);
+  average.textContent = averagePopulation;
+}
+
+calculateTotal();
+calculateAverage();
