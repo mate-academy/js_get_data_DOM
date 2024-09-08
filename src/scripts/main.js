ca98754f1ec607df1e6ba33b06ff8ebcaf8c5a85
Vika <rudaviktoria0@gmail.com>
2024-09-08 18:50:32 +0300

    Update 0809

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,14 +7,10 @@ let totalValue = 0;
 let count = 0;
 
 for (let i = 0; i < allElementSpan.length; i++) {
-
   let value = allElementSpan[i].textContent;
 
-
   if (!isNaN(value)) {
-
     value = +value;
-
 
     totalValue += value;
     count++;
@@ -26,13 +22,6 @@ const avarageValue = totalValue / count;
 const formattedTotal = totalValue.toLocaleString('en-US');
 const formattedAvarage = avarageValue.toLocaleString('en-Us');
 
-
 document.querySelector('span.avarage-population').textContent =
   formattedAvarage;
 document.querySelector('span.total-population').textContent = formattedTotal;
-
-
-console.log('Total Value: ', formattedTotal);
-
-
-console.log('Avarage Value: ', formattedAvarage);
