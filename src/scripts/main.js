9d00a26c7063c3c8605227f8042084553289520b
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2024-09-08 18:46:05 +0300

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const arr = Array.from(document.querySelectorAll('span.population')).map(
+  (el) => {
+    return parseInt(el.innerText.split(',').join(''));
+  },
+);
+
+const total = arr.reduce((sum, el) => sum + el, 0);
+
+document.querySelector('span.total-population').innerText =
+  total.toLocaleString('en-US');
+
+document.querySelector('span.average-population').innerText = (
+  total / arr.length
+).toLocaleString('en-US');
