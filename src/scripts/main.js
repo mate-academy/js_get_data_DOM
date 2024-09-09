b96c7d29d485997b2c0b7d26703b53ef77de544a
Anastasiia Tatarchenko <neveykina.nastya@gmail.com>
2024-09-09 13:25:46 +0200

    task_done_js_get_data_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')];
+const totalTag = document.querySelector('.total');
+const averageTag = document.querySelector('.average');
+
+const valuesArray = [];
+
+population.forEach((county) => {
+  const value = parseInt(county.textContent.replaceAll(',', ''));
+
+  valuesArray.push(value);
+});
+
+const total = valuesArray.reduce((acc, curr) => acc + curr);
+const average = total / valuesArray.length;
+
+function preparePopulation(param) {
+  const prepared = ('' + param).split('').reverse();
+  let res = '';
+
+  for (let i = 0; i < prepared.length; i++) {
+    if (i !== 0 && i % 3 === 0) {
+      res += ',';
+    }
+    res += prepared[i];
+  }
+
+  return res.split('').reverse().join('');
+}
+
+totalTag.textContent = `Total: ${preparePopulation(total)}`;
+averageTag.textContent = `Average: ${preparePopulation(average)}`;
