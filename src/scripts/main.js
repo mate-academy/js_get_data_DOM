2308b01f8c0c8e3b7baa9435a9d6bfa5b3ba8d9e
Viktoriia Dereza <viktoriiadereza7@gmail.com>
2024-09-17 16:44:55 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', function () {
+  const populations = document.querySelectorAll('.population');
+
+  const populationNumbers = Array.from(populations).map((population) => {
+    return parseInt(population.textContent.replace(/,/g, ''), 10);
+  });
+
+  const totalPopulation = populationNumbers.reduce(
+    (sum, population) => sum + population,
+    0,
+  );
+
+  const averagePopulation = Math.round(
+    totalPopulation / populationNumbers.length,
+  );
+
+  document.querySelector('.total-population').textContent =
+    totalPopulation.toLocaleString('en-US');
+
+  document.querySelector('.average-population').textContent =
+    averagePopulation.toLocaleString('en-US');
+});
