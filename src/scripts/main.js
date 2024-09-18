29633f915025d6ad1e3265ce45a973a5c5447f66
vshvanska <valeriiashvanska@gmail.com>
2024-09-18 10:37:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write your code here
+function getPopulation() {
+  let population = [...document.querySelectorAll('.population')].map(
+    (el) => el.textContent,
+  );
+
+  population = population.map((el) => parseInt(el.replace(/,/g, ''), 10));
+
+  return population;
+}
+
+function getTotalPopulation() {
+  const population = getPopulation();
+
+  return population.reduce((sum, x) => sum + x, 0);
+}
+
+function getAveragePopulation() {
+  const population = getPopulation();
+
+  return population.reduce((sum, x) => sum + x, 0) / population.length;
+}
+
+document.querySelector('.average-population').textContent =
+  getAveragePopulation().toLocaleString('en-US');
+
+document.querySelector('.total-population').textContent =
+  getTotalPopulation().toLocaleString('en-US');
