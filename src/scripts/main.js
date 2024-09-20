1f39b41da7089eda9d022e0bccd32dc8f7250976
Kseniia Karpenko <ksushakarpenko635@gmail.com>
2024-09-20 15:03:55 +0200

    largest countries

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+let spanEl = Array.from(document.getElementsByClassName('population'))
+
+spanEl = spanEl.map((el) => {
+  const element = el.textContent.split(',').join('');
+
+  return Number(element);
+});
+
+let totalValue = 0;
+let count = 0;
+
+spanEl.forEach((elem) => {
+  totalValue += elem;
+  count++;
+});
+
+const averageValue = totalValue / count;
+
+const calculateTV = document.querySelector('.total-population');
+const calculateAV = document.querySelector('.average-population');
+
+calculateTV.textContent = totalValue.toLocaleString();
+calculateAV.textContent = averageValue.toLocaleString();
