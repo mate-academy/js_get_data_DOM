824e43fd37ddb99b143fca5540a8f563237bfd36
Mykhailo Lapchynskyi <mykhailolapchynskyi@Dikaas-MacBook-Pro.local>
2024-09-24 14:28:21 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write your code here
+const elements = [...document.querySelectorAll('.population')];
+
+const totalPopulationArr = elements
+  .map((elem) => elem.innerText)
+  .map((elem) => elem.split(',').join(''))
+  .map((elem) => Number(elem));
+
+const total = totalPopulationArr.reduce((prev, curr) => prev + curr, 0);
+const average = total / totalPopulationArr.length;
+
+const totalElement = document.querySelector('.total-population');
+
+totalElement.innerText = total.toLocaleString();
+
+const averageElement = document.querySelector('.average-population');
+
+averageElement.innerText = average.toLocaleString();
