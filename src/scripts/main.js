cf0cc0e0e7da3080be47ecfa6f5f2660ab16f9fe
Ivan Dovzhanyn <ivan.dovzhanyn@gmail.com>
2024-09-24 21:04:04 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const people = document.querySelectorAll('span.population');
+const nombersOfPopulation = [];
+
+people.forEach((person) => {
+  const text = person.textContent;
+  const number = parseInt(text.replace(/,/g, ''));
+
+  nombersOfPopulation.push(number);
+});
+
+const total = nombersOfPopulation.reduce((sum, current) => sum + current, 0);
+const average = total / nombersOfPopulation.length;
+
+const totalPopulation = document.querySelector('span.total-population');
+const averagePopulation = document.querySelector('span.average-population');
+
+totalPopulation.textContent = total.toLocaleString();
+averagePopulation.textContent = average.toLocaleString();
