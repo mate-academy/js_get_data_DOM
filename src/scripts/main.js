b5c7f22f6a304138b3155013f89f4a082b666073
associate2coder <olatsko@gmail.com>
2024-09-25 20:44:28 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const nodes = document.querySelectorAll('.population').values();
+const values = [...nodes].map((node) => +node.textContent.replaceAll(',', ''));
+
+const total = values.reduce((num1, num2) => num1 + num2);
+const average = Math.round(total / values.length);
+
+const totalField = document.querySelector('.total');
+const averageField = document.querySelector('.average');
+
+totalField.innerText = 'Total: ' + total.toLocaleString('en-US');
+averageField.innerText = 'Average: ' + average.toLocaleString('en-US');
