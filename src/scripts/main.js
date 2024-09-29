7120fa37705cf1a33ebd7b364b6e6921f9890c09
SerhiiSemennikov <semennik@hotmail.com>
2024-09-29 11:00:53 +0200

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write your code here
+
+const elements = [...document.querySelectorAll('.population')].map(
+  (el) => el.textContent,
+);
+
+/* const population1 = elements.map((el) => el.replaceAll(',', ''));
+const population2 = population1.map((el) => Number(el));
+const population3 = population2.reduce((el, sum) => el + sum, 0);
+population=population3.map(el =>el.toSring())
+document.querySelector('average-population').textContent = "population"; */
+const all = elements
+  .map((el) => Number(el.replaceAll(/,/g, '')))
+  .reduce((sum, x) => sum + x, 0);
+
+document.querySelector('.total-population').textContent =
+  all.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent = (
+  all / elements.length
+).toLocaleString('en-US');
