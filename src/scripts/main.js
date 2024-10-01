efd55c0047da8ff34905a11e245481863d350bae
dmytrochernega <dimadneprfdb@gmail.coma>
2024-10-01 19:36:29 +0300

    created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+// const { getVersion } = require("jest");
+
+const populationText = document.querySelectorAll('.population');
+const arrSum = [];
+
+for (const el of populationText) {
+  arrSum.push(+el.textContent.split(',').join(''));
+}
+
+const totalSum = arrSum.reduce((a, b) => a + b, 0);
+const averageSum = totalSum / arrSum.length;
+
+const averageValue = averageSum.toLocaleString('en-US');
+const totatValue = totalSum.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent = averageValue;
+document.querySelector('.total-population').textContent = totatValue;
