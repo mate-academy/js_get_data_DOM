447e47ce292ca6a2aad792d2e5767b8bec5bc73f
Bohdan Horiunov <b.horiunov@gmail.com>
2024-10-03 19:43:17 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write your code here
+const populationElements = document.querySelectorAll('.population');
+
+const populations = [];
+
+populationElements.forEach((el) => {
+  const population = parseInt(el.textContent.replace(/,/g, ''));
+
+  populations.push(population);
+});
+
+const totalPopulation = populations.reduce((sum, num) => sum + num, 0);
+const averagePopulation = totalPopulation / populations.length;
+
+const formatNumber = (num) => num.toLocaleString('en-US');
+
+document.querySelector('.total-population').textContent =
+  formatNumber(totalPopulation);
+
+document.querySelector('.average-population').textContent =
+  formatNumber(averagePopulation);
