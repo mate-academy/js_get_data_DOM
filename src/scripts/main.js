7597870b6c22cd803d20dd92a8361784a0d2259a
NazarHaida <nazar.haida.knm.2020@lpnu.ua>
2024-10-04 21:23:27 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const populations  = document.querySelectorAll('.population')
+const totalPopulation = document.querySelector('.total-population')
+const averagePopulations = document.querySelector('.average-population')
+const populationsArr  = [];
+
+populations.forEach(el => { populationsArr.push(parseInt(el.innerHTML.replace(/,/g, '')))})
+
+const populationSum = populationsArr.reduce((val1, val2) => val1 + val2, 0)
+totalPopulation.innerHTML = populationSum.toLocaleString()
+
+const populationAvg = Math.round(populationSum/populationsArr.length)
+averagePopulations.innerHTML = populationAvg.toLocaleString()
