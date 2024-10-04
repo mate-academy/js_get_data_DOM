4106c0c980e1d2901a98532d3f4c8ed10c122352
Taras Niklevych <niklevychtaras@gmail.com>
2024-10-04 11:26:28 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const popElementsArr = [...document.querySelectorAll('.population')];
+
+const populationArr = [];
+
+popElementsArr.map((el) =>
+  // eslint-disable-next-line prettier/prettier
+  populationArr.push(parseInt(el.innerHTML.replace(/,/g, ''))));
+
+const totalPopulationResult = document.querySelector('.total-population');
+const averagePopulationResult = document.querySelector('.average-population');
+
+const populationSum = populationArr.reduce((accumulator, curentValue) => {
+  return accumulator + curentValue;
+}, 0);
+
+const populationAverage = Math.round(populationSum / populationArr.length);
+
+totalPopulationResult.innerHTML = populationSum.toLocaleString('en-US');
+averagePopulationResult.innerHTML = populationAverage.toLocaleString('en-US');
