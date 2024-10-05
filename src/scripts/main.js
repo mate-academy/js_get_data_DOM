09ad1042973fe121893788b8d89db31b84745e6c
Ivan Paiareli <ipayareli@gmail.com>
2024-10-05 15:19:28 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const popCol = document.querySelectorAll('.population');
+const popNum = [...popCol].map((i) => +i.textContent.split(',').join(''));
+
+function calculateTotal(arr) {
+  return arr.reduce((sum, item) => sum + item, 0);
+}
+
+function calculateAverage(arr) {
+  return calculateTotal(arr) / arr.length;
+}
+
+const total = calculateTotal(popNum);
+const average = calculateAverage(popNum);
+
+const totalPopulation = document.querySelector('.total-population');
+
+totalPopulation.innerHTML = total.toLocaleString('en-EN');
+
+const averagePopulation = document.querySelector('.average-population');
+
+averagePopulation.innerHTML = Math.round(average).toLocaleString('en-EN');
