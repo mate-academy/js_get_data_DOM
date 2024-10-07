dde52ad878cc4ca477bc2167da21ef8b0c2ce73f
Igor <igor.olshanskyi@gmail.com>
2024-10-07 12:50:35 +0300

    test_1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+const population = [...document.querySelector(`.population`)];
+const countCountries = population.length();
+
+population.forEach((item, i) => {
+  item[i] = +item.split(',').join('');
+});
+
+const total = population.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  0,
+);
+const average = total / countCountries;
+const averagePopulation = document.querySelector('.average-population');
+const totalPopulation = document.querySelector('.total-population');
+
+averagePopulation.removeAttribute();
+totalPopulation.removeAttribute();
+totalPopulation.insertAdjacentElement(total);
+averagePopulation.insertAdjacentElement(average);
