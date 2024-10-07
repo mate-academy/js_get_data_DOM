ba62c267dfb9209c4f6a19974de3e60b48a1a6db
Ilona Gogol <ilonka.gogol@gmail.com>
2024-10-07 21:55:14 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write your code here
+const populations = [...document.querySelectorAll('.population')];
+
+const newPopul = populations.map((el) => {
+  return parseInt(el.textContent.replace(/,/g, ''), 10);
+});
+
+const totalCount = newPopul.reduce((el, count) => count + el, 0);
+const average = totalCount / newPopul.length;
+
+document.querySelector('.total-population').textContent =
+  totalCount.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  Math.round(average).toLocaleString('en-US');
