a6716a07aafc21ff8dc7a834b64634b0db46da53
Igor <igor.olshanskyi@gmail.com>
2024-10-07 14:54:32 +0300

    test_2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,19 +4,19 @@
 const population = [...document.querySelector(`.population`)];
 const countCountries = population.length();
 
-population.forEach((item, i) => {
-  item[i] = +item.split(',').join('');
+population.forEach((item) => {
+  return parseInt(item.textContent.split(',').join(''));
 });
 
 const total = population.reduce(
   (accumulator, currentValue) => accumulator + currentValue,
   0,
 );
-const average = total / countCountries;
-const averagePopulation = document.querySelector('.average-population');
-const totalPopulation = document.querySelector('.total-population');
 
-averagePopulation.removeAttribute();
-totalPopulation.removeAttribute();
-totalPopulation.insertAdjacentElement(total);
-averagePopulation.insertAdjacentElement(average);
+const average = total / countCountries;
+
+document.getElementsByClassName(average - population).textContent =
+  average.toLocaleString('en-US');
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString('en-US');
