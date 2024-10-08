4a038786ff5574e0f9e33bebc0a57b6d0ef3909c
Your_Name Your_Surname <your@email>
2024-10-08 09:42:30 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const collection = [...document.querySelectorAll('.population')];
+
+const toNumbers = collection.map((el) => {
+  return parseInt(el.textContent.replace(/,/g, ''));
+});
+
+const totalSum = toNumbers.reduce((sum, el) => sum + el, 0);
+
+const average = Math.round(totalSum / toNumbers.length);
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString();
+
+document.querySelector('.total-population').textContent =
+  totalSum.toLocaleString();
