3c36a0d2ef54c6988375299526fb6844d427e556
Nataliia Bolsunova <nataliia.bolsunova@develops.today>
2024-10-09 00:11:01 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const populationElements = document.querySelectorAll('span.population');
+
+  const populations = Array.from(populationElements).map((el) => {
+    const populationText = el.textContent;
+
+    return Number(populationText.replace(/,/g, ''));
+  });
+
+  const totalPopulation = populations.reduce(
+    (sum, population) => sum + population,
+    0,
+  );
+  const averagePopulation = totalPopulation / populations.length;
+  const formatNumber = (num) => num.toLocaleString('en-US');
+
+  document.querySelector('span.average-population').textContent = formatNumber(
+    Math.round(averagePopulation),
+  );
+
+  document.querySelector('span.total-population').textContent =
+    formatNumber(totalPopulation);
+});
