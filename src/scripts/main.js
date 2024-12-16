389e3de0cb04ab6f69520f8c6ff9092e63cd1919
Tania Kolesnik <kolesnik@gmail.com>
2024-12-16 15:28:29 +0000

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const values = [...document.querySelectorAll('span.population')].map(
+  (e) => parseInt(e.textContent.replaceAll(',', '')),
+  10,
+);
+
+const total = values.reduce((prev, value) => prev + value);
+const average = total / values.length;
+
+document.querySelector('span.average-population').textContent =
+  average.toLocaleString();
+
+document.querySelector('span.total-population').textContent =
+  total.toLocaleString();
