747796a5db89c417633d7a14e8309f86e86cd630
Volodymyr Vynnyk <vinnikvvv@gmail.com>
2025-01-02 11:12:14 +0200

    add feature Population Calculation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const populationValues = [...document.querySelectorAll('.population')].map(
+  (value) => +value.textContent.split(',').join(''),
+);
+
+const totalPopulation = Math.round(
+  populationValues.reduce((total, current) => total + current, 0),
+);
+
+const averagePopulation = Math.round(totalPopulation / populationValues.length);
+
+document.querySelector('.total-population').textContent =
+  totalPopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  averagePopulation.toLocaleString('en-US');
