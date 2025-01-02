5811e5250056ff7a3b1dc6dd016b01dd8ad4762f
Veronica Shyian <veronica.shyian@gmail.com>
2025-01-02 15:35:41 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const populationTags = [...document.querySelectorAll('span.population')];
+const populationAmounts = populationTags.map(
+  (tag) => +tag.textContent.replaceAll(',', ''),
+);
+const totalAmount = populationAmounts.reduce(
+  (total, amount) => total + amount,
+  0,
+);
+const countriesAmount = populationAmounts.length || 1;
+const averageAmount = Math.round(totalAmount / countriesAmount);
+const totalAmountTag = document.querySelector('span.total-population');
+const averageAmountTag = document.querySelector('span.average-population');
+
+totalAmountTag.textContent = totalAmount.toLocaleString('en-US');
+averageAmountTag.textContent = averageAmount.toLocaleString('en-US');
