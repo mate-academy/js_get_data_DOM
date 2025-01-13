a8ca4f81ba2747643675e6b222bd5778a532264f
ArtemHryb <artem.mushroom.gh@gmail.com>
2025-01-13 14:40:29 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const countries = document.getElementsByClassName('population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+let sum = 0;
+
+for (let i = 0; i < countries.length; i++) {
+  let parsedCountry = 0
+  parsedCountry = parseInt(countries[i].innerHTML.replace(/,/g, ''));
+
+  sum += parsedCountry;
+}
+const numberFormatter = new Intl.NumberFormat('en-US');
+total.textContent = numberFormatter.format(sum);
+average.textContent = numberFormatter.format(Math.round(sum / countries.length));
