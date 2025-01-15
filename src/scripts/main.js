44427d6061ccdf10333914d561b7453a0905f98a
Viktoriia <vikapazyuk@gmail.com>
2025-01-15 16:42:05 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const populations = Array.from(document.querySelectorAll('span.population'))
+
+  .map((el) => parseInt(el.textContent.replace(/,/g, ''), 10));
+
+const totalPopulation = populations.reduce(
+  (total, current) => total + current,
+  0,
+);
+const avgPopulation = totalPopulation / populations.length;
+
+const formatWithCommas = (num) =>
+  num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+document.querySelector('span.total-population').textContent =
+  formatWithCommas(totalPopulation);
+
+document.querySelector('span.average-population').textContent =
+  formatWithCommas(Math.round(avgPopulation));
