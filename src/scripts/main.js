5924aa7baec2a206fb577192821061d004f8ab32
artur.manuilenko <artur.manuilenko@yalantis.net>
2025-01-17 19:38:16 +0200

    so;ution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write your code here
+const populationArr = document.querySelectorAll('.population');
+
+const population = [...populationArr].map((p) => {
+  return Number(p.innerText.replaceAll(',', ''));
+});
+
+const total = population.reduce((acc, i) => acc + i, 0);
+const average = Math.ceil(total / population.length);
+
+const totalLocaleString = total.toLocaleString('en-US');
+const averageLocaleString = average.toLocaleString('en-US');
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.textContent = totalLocaleString;
+averagePopulation.textContent = averageLocaleString;
