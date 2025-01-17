bce0f247da742ece8ee7c3b8436bc0f42aa09422
Oleh Mysko <olegmysko1996@gmail.com>
2025-01-17 13:53:29 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const people = document.querySelectorAll('.population');
+let sum = 0;
+let count = 0;
+
+for (const man of people) {
+  const text = man.textContent.trim();
+  const sanitizedText = text.replace(/,/g, '');
+  const number = parseInt(sanitizedText, 10);
+
+  if (!isNaN(number)) {
+    sum += number;
+    count++;
+  }
+}
+
+const average = sum / count;
+
+const formattedSum = sum.toLocaleString('en-US');
+const formattedAverage = average.toLocaleString('en-US', {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const totalPeople = document.querySelector('.total-population');
+
+if (totalPeople) {
+  totalPeople.textContent = `${formattedSum}`;
+}
+
+const averagePeople = document.querySelector('.average-population');
+
+if (averagePeople) {
+  averagePeople.textContent = `${formattedAverage}`;
+}
