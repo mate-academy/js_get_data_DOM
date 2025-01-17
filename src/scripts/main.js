9defb682728ea639fe88979d2aa68de1f54dae9c
Oleg Orykhivskiy <olegkoolll@gmail.com>
2025-01-17 17:30:07 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('span.population');
+
+const population = Array.from(populationElements).map((element) => {
+  const number = parseInt(element.textContent.replace(/,/g, ''));
+
+  return number;
+});
+
+const totalPopulation = population.reduce((acc, curr) => acc + curr, 0);
+const averagePopulation = Math.round(totalPopulation / population.length);
+
+const formatNumber = (num) => num.toLocaleString('en-US');
+
+// eslint-disable-next-line max-len
+// const averageElement = document.querySelector('.average');
+// const totalElement = document.querySelector(`.total`);
+
+// averageElement.firstChild.textContent = formatNumber(averagePopulation);
+
+// totalElement.firstChild.textContent = formatNumber(totalPopulation);
+
+const totalElement = document.querySelector('.total .total-population');
+const averageElement = document.querySelector('.average .average-population');
+
+totalElement.textContent = formatNumber(totalPopulation);
+averageElement.textContent = formatNumber(averagePopulation);
