263313a820e5c4450d9e6ea83293f5bfebcbe282
Alina Isakova <adorosh062@gmail.com>
2025-01-18 18:30:42 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('span.population');
+
+const populations = [...spans];
+
+let total = 0;
+
+for (const elem of populations) {
+  const newElem = +elem.innerHTML.split(',').join('');
+
+  total += newElem;
+}
+
+const average = total / populations.length;
+
+function formatNumber(num) {
+  return num.toLocaleString('en-US');
+}
+
+const totalData = document.querySelector('.total-population');
+const averageData = document.querySelector('.average-population');
+
+totalData.innerHTML = formatNumber(total);
+averageData.innerHTML = formatNumber(average);
