499a653bc1ce977ae1a938744a46db03871d4070
Yaroslava <semydayaroslava@icloud.com>
2025-01-19 19:33:32 -0500

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const populationElements = document.querySelectorAll('.population');
+
+  const populations = Array.from(populationElements).map((popElem) => {
+    return parseInt(popElem.textContent.replace(/,/g, ''), 10);
+  });
+
+  const totalPopulation = populations.reduce(
+    (total, population) => total + population,
+    0,
+  );
+
+  const averagePopulation = totalPopulation / populations.length;
+
+  const formatNumber = (num) => {
+    return num.toLocaleString('en-US');
+  };
+
+  const totalPopulationElement = document.querySelector('.total-population');
+  const averagePopulationElement = document.querySelector(
+    '.average-population',
+  );
+
+  totalPopulationElement.textContent = formatNumber(totalPopulation);
+
+  averagePopulationElement.textContent = formatNumber(
+    averagePopulation.toFixed(0),
+  );
+});
