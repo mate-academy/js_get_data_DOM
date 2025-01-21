11f7b08ab2e36d639cb8876165b4f3b67b0b14c8
Uusuff <90629840+Uusuff@users.noreply.github.com>
2025-01-21 17:38:22 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
+const COUNTRIES_BY_POPULATION = document.getElementsByClassName('population');
+const TOTAL_POPULATION = document.querySelector(".total-population")
+const AVERAGE_POPULATION = document.querySelector('.average-population');
+let total = 0;
+const formattedNumber = num => num.toLocaleString("en-US");
 
-// write your code here
+for (let index = 0; index < COUNTRIES_BY_POPULATION.length; index++) {
+  const numberWithCommas = COUNTRIES_BY_POPULATION[index].innerHTML;
+  const numberWithoutCommas = numberWithCommas.replace(/,/g, "");
+
+  total += Number(numberWithoutCommas);
+}
+
+TOTAL_POPULATION.innerHTML = formattedNumber(total);
+AVERAGE_POPULATION.innerHTML = formattedNumber(Math.ceil(total / COUNTRIES_BY_POPULATION.length));
