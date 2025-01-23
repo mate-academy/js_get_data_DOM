66064eb74a14c96bbe6c26de2aad131019875f52
Che <rererdom@gmail.com>
2025-01-23 14:37:02 +0000

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', function () {
+  const populationElements = document.querySelectorAll('.population');
+
+  const populations = Array.from(populationElements).map((el) => {
+    return Number(el.textContent.replace(/,/g, ''));
+  });
+  const totalPopulation = populations.reduce((sum, el) => sum + el, 0);
+
+  const averagePopulation = totalPopulation / populations.length;
+
+  function formatNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
+  document.querySelector('.total-population').textContent =
+    formatNumber(totalPopulation);
+
+  document.querySelector('.average-population').textContent = formatNumber(
+    Math.round(averagePopulation),
+  );
+});
