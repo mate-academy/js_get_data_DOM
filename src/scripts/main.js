24dc21cc9901b0d1c683b2f15f648a6f7a6c1c04
YourGitHubUsername <YourGitHubEmail>
2025-01-27 23:55:08 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write your code here
+
+const populations = document.querySelectorAll('.population');
+
+let totalPopulation = 0;
+let average = 0;
+
+populations.forEach((element) => {
+  const populationText = element.innerText;
+  const cleanedText = populationText.replace(/[^0-9.-]+/g, '');
+  const populationNumber = parseInt(cleanedText);
+
+  totalPopulation += populationNumber;
+
+  average = Math.round(totalPopulation / populations.length);
+});
+
+const totalElement = document.querySelector('.total-population');
+
+totalElement.innerText = totalPopulation.toLocaleString();
+
+const averageElement = document.querySelector('.average-population');
+
+averageElement.innerText = average.toLocaleString();
