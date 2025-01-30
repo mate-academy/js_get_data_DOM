d0ab97589e5d86426b536cd977ecd17a60db5597
Ivan Lavruk <ilavr8725@gmail.com>
2025-01-30 16:59:54 +0100

    get data dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const formatNumber = (num) =>
+  num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+const population = [...document.querySelectorAll('.population')].map(
+  (totalArray) => +totalArray.textContent.split(',').join(''),
+);
+
+const totalPopulation = population.reduce((acc, el) => acc + el, 0);
+
+document.querySelector('.total-population').textContent =
+  formatNumber(totalPopulation);
+
+document.querySelector('.average-population').textContent = formatNumber(
+  Math.round(totalPopulation / population.length),
+);
