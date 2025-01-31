d61aa6244ec2529e7871fc3e17aab89e5d3e57e0
Volodymyr Suskyi <suskyivolodymyr@gmail.com>
2025-01-31 14:31:48 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const countries = document.querySelectorAll('span.population');
+let total = 0;
+const countriesCount = countries.length;
+
+for (const element of countries) {
+  total += +element.textContent.split(',').join('');
+}
+
+const average = Math.floor(total / countriesCount);
+
+const averageSpan = document.querySelector('span.average-population');
+const totalSpan = document.querySelector('span.total-population');
+
+averageSpan.textContent = average
+  .toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+totalSpan.textContent = total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
