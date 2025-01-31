141a65c4ff31a17f5711b30bea0f32845546d4d4
Вася <pankulich654g@mail.com>
2025-01-31 22:30:29 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const allTextDate = document.querySelectorAll('span.population');
+let totalNumber = 0;
+let average = 0;
+
+for (const element of allTextDate) {
+  const part = element.textContent.split(',');
+  let date = '';
+
+  for (let i = 0; i < part.length; i++) {
+    date += part[i];
+  }
+
+  totalNumber += Number(date);
+}
+
+const totalPopulation = document.querySelector('span.total-population');
+const averagePopulation = document.querySelector('span.average-population');
+
+average = Math.round(totalNumber / allTextDate.length);
+
+totalPopulation.textContent = Intl.NumberFormat('en-US').format(totalNumber);
+
+averagePopulation.textContent = Intl.NumberFormat('en-US').format(average);
