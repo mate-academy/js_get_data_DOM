81fe5845dcf416b1d0a596033ce8696e10ba1d2b
Iryna <irinabolbat98@gmail.com>
2025-02-02 17:23:39 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
+/* eslint-disable prettier/prettier */
 'use strict';
 
-// write your code here
+const allPopulation = document.querySelectorAll('.population');
+// eslint-disable-next-line max-len
+const popuations = Array.from(allPopulation).map((el) => Number(el.textContent.replace(/,/g, '')));
+
+const total = popuations.reduce((a, b) => a + b, 0);
+const average = Math.round(total / popuations.length);
+
+const formatNum = (num) => num.toLocaleString();
+
+document.querySelector('.total-population').textContent = formatNum(total);
+document.querySelector('.average-population').textContent = formatNum(average);
