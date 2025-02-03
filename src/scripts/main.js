6019f654a51ae5f5c8961844bc2887d719a1c927
Vladyslav Tsarenko <vaaaltaaasaaar@gmail.com>
2025-02-03 11:16:00 -0600

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')].map(
+  (elem) => +elem.textContent.replace(/\D/g, ''),
+);
+
+const totalPopulation = population.reduce((a, b) => a + b, 0);
+
+document.querySelector('.total-population').textContent =
+  totalPopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent = Math.floor(
+  totalPopulation / population.length,
+)
+  .toLocaleString('en-US')
+  .replace('.', ',');
