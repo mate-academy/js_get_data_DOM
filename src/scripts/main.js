cac63af61085aaeb55204b48823b049cd175b96b
unknown <Olesyk08@gmail.com>
2025-02-06 11:45:54 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const information = [];
+
+document.querySelectorAll('.population').forEach((population) => {
+  // eslint-disable-next-line use-isnan
+  if (parseInt(population.textContent) !== NaN) {
+    const number = Number(population.textContent.trim().replaceAll(',', ''));
+
+    information.push(number);
+  }
+});
+
+const total = information.reduce((sum, x) => sum + x, 0);
+const average = total / information.length;
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.textContent = total.toLocaleString();
+averagePopulation.textContent = average.toLocaleString();
