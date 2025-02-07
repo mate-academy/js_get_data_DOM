77cf43bf86662ecc6bd37db5509784fc5e1b38c3
VadimKudrenko <48049362+VadimKudrenko@users.noreply.github.com>
2025-02-07 11:25:09 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const populationByCountry = [...document.querySelectorAll('.population')];
+
+const totalPopulationAmount = populationByCountry.reduce((prev, item) => {
+  return prev + Number(item.innerText.replaceAll(',', ''));
+}, 0);
+
+const averagePopulation = totalPopulationAmount / populationByCountry.length;
+
+document.querySelector('.total-population').innerHTML =
+  `${totalPopulationAmount.toLocaleString('en-US')}`;
+
+document.querySelector('.average-population').innerHTML =
+  `${Math.round(averagePopulation).toLocaleString('en-US')}`;
