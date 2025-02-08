b8acb4e84a6ddbcc2e69c5d53d2285af0d536d1a
nnalinn <alina.vysotskayaaa@gmail.com>
2025-02-08 23:06:01 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const totalSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+
+let sum = 0;
+const count = populations.length;
+
+populations.forEach((item) => {
+  sum += parseInt(item.textContent.replace(/,/g, ''), 10);
+});
+
+const average = sum / count;
+
+const formatNumber = (item) => item.toLocaleString();
+
+totalSpan.textContent = formatNumber(sum);
+averageSpan.textContent = formatNumber(Math.round(average));
