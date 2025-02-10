6928c8669eae5fa0f5fa6f7c8495652682a390ec
RVDotsenko <docenkoromaha@gmail.com>
2025-02-10 21:39:49 +0300

    Total & Average population counted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const countriesPopulation = [...document.getElementsByClassName('population')];
+const totalTag = document.querySelector('.total-population');
+const averageTag = document.querySelector('.average-population');
+
+const totalPopulation = countriesPopulation.reduce((acc, element) => {
+  return acc + parseInt(element.innerHTML.replace(/,/g, ''));
+}, 0);
+
+totalTag.innerHTML = totalPopulation.toLocaleString();
+
+averageTag.innerHTML = Math.floor(
+  totalPopulation / countriesPopulation.length,
+).toLocaleString();
