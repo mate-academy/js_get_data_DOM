2ea15cd34edb8bc75a138a875734aa38c25ec38f
Dima Komisarov <komissarov854@gmail.com>
2025-02-11 08:29:13 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+
+const population = Array.from(populationElements).map((element) => {
+  const line = element.textContent.trim();
+  const number = Number(line.replace(/,/g, ''));
+
+  return isNaN(number) ? 0 : number;
+});
+
+const totalPopulation = population.reduce((sum, value) => sum + value, 0);
+const avaregePopulation = totalPopulation / population.length;
+
+document.querySelector('.total-population').textContent =
+  totalPopulation.toLocaleString();
+
+document.querySelector('.average-population').textContent =
+  avaregePopulation.toLocaleString();
