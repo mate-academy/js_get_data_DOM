05b37f0a82643aa55800f651cfb9e0974da67fb8
Dariya-Goncharovskaya <dariyagoncharovskaya@gmail.com>
2025-02-17 09:20:28 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', function () {
+  const populationElements = document.querySelectorAll('.population');
+  const totalPopulationElement = document.querySelector('.total-population');
+  const averagePopulationElement = document.querySelector(
+    '.average-population',
+  );
+
+  let totalPopulation = 0;
+  let count = 0;
+
+  populationElements.forEach((element) => {
+    const population = parseInt(element.textContent.replace(/,/g, ''), 10);
+
+    if (!isNaN(population)) {
+      totalPopulation += population;
+      count++;
+    }
+  });
+
+  const averagePopulation = Math.floor(totalPopulation / count);
+
+  totalPopulationElement.textContent = totalPopulation.toLocaleString();
+  averagePopulationElement.textContent = averagePopulation.toLocaleString();
+});
