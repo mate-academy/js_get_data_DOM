b2b210e628e9aa18a1ce1fdbe112e2e55257ad45
Ivan Babiuk <whatifitryfirst@gmail.com>
2025-02-17 16:07:21 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+window.onload = function () {
+  const populationElements = document.querySelectorAll('.population');
+
+  const populations = Array.from(populationElements).map((el) => {
+    const populationText = el.textContent.replace(/,/g, '');
+
+    return parseInt(populationText, 10);
+  });
+
+  const totalPopulation = populations.reduce((acc, val) => acc + val, 0);
+
+  const averagePopultaion = totalPopulation / populations.length;
+
+  const formatNumber = (number) => {
+    return number.toLocaleString('uk-UA');
+  };
+
+  document.querySelector('.total-population').textContent =
+    formatNumber(totalPopulation);
+
+  document.querySelector('.average-population').textContent = formatNumber(
+    Math.round(averagePopultaion),
+  );
+};
