32bc5f64d4fd9a84f637919655c92725def89b1a
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-02-21 00:42:13 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')];
+
+const totalPopulation = population.reduce(
+  (acc, elem) => acc + Number(elem.innerHTML.replaceAll(',', '')),
+  0,
+);
+
+document.querySelector('.total-population').innerHTML =
+  totalPopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population').innerHTML = Math.trunc(
+  totalPopulation / population.length,
+).toLocaleString('en-US');
