1f3db067732f507edde01864e6b1ca22e2584b8b
Nazar Shpylovyi <shpiloviy.nazar@ukr.net>
2025-02-20 15:53:22 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
-'use strict';
+const populationSpans = document.querySelectorAll('.population');
 
-// write your code here
+let totalPopulation = 0;
+
+populationSpans.forEach((span) => {
+  const populationString = span.textContent.replace(/,/g, '');
+  const populationNumber = parseInt(populationString, 10);
+
+  totalPopulation += populationNumber;
+});
+
+const averagePopulation = totalPopulation / populationSpans.length;
+
+const formattedTotal = totalPopulation.toLocaleString();
+const formattedAverage = averagePopulation.toLocaleString();
+
+document.querySelector('.total-population').textContent = formattedTotal;
+document.querySelector('.average-population').textContent = formattedAverage;
