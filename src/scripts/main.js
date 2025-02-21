889721a3f44a5c6d5aeff5de04ddc66388ba3d5d
Serhii Mamchur <kovaniysvit@gmail.com>
2025-02-21 10:55:19 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const allPopulations = document.querySelectorAll('.population');
+const totalPopulation = document.getElementsByClassName('total-population');
+const averagePopul = document.getElementsByClassName('average-population');
+
+const total = Array.from(allPopulations)
+  .map((population) => +population.textContent.replaceAll(',', ''))
+  .reduce((acc, num) => acc + num);
+
+const avarege = total / Array.from(allPopulations).length;
+
+totalPopulation[0].textContent = total.toLocaleString('en-US');
+averagePopul[0].textContent = avarege.toLocaleString('en-US');
