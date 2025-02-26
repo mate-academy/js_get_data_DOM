391cc6cc94a4a0c0e66f5c1fed374d577bdb4215
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-02-26 19:33:58 +0100

    getDataDom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', function () {
+  const populations = document.querySelectorAll('span.population');
+  let totalPopulation = 0;
+  let count = 0;
+
+  populations.forEach((pop) => {
+    const populationText = pop.textContent.trim();
+    const populationNumber = parseInt(populationText.replace(/,/g, ''), 10);
+
+    if (!isNaN(populationNumber)) {
+      totalPopulation += populationNumber;
+      count++;
+    }
+  });
+
+  const averagePopulation = totalPopulation / count;
+
+  const formatNumber = (num) => {
+    return num.toLocaleString('en-US');
+  };
+
+  document.querySelector('.average-population').textContent = formatNumber(
+    Math.round(averagePopulation),
+  );
+
+  document.querySelector('.total-population').textContent =
+    formatNumber(totalPopulation);
+});
