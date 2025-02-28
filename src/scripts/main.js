a0e0d1c7fa0cd44a97c675771355a7fe3d29aa70
Alex Marcovych <alexmarcovych@gmail.com>
2025-02-28 16:31:21 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const popEls = document.querySelectorAll('span.population');
+
+  const populations = [...popEls]
+    .map((el) => el.textContent.replace(/,/g, ''))
+    .map(Number)
+    .filter((num) => !isNaN(num));
+
+  if (!populations.length) {
+    return;
+  }
+
+  const total = populations.reduce((sum, num) => sum + num, 0);
+  const average = Math.round(total / populations.length);
+  const form = (num) => num.toLocaleString('en-US');
+
+  document.querySelector('span.total-population').textContent = form(total);
+  document.querySelector('span.average-population').textContent = form(average);
+});
