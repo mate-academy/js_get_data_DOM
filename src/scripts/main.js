c9a6ce12dbf0a748fb460333f6c5945844d04523
Ivan <tushchukivan@gmail.com>
2025-03-01 15:44:33 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
+const populationAll = document.querySelectorAll('span.population');
 
-// write your code here
+const population = Array.from(populationAll)
+.map(el => el.textContent.replace(/,/g, ''))
+.map(Number)
+.filter(num => !isNaN(num));
+
+const totalPopulation = population.reduce((acc, num) => acc + num, 0);
+const averagePopulation = Math.round(totalPopulation / population.length);
+
+const formatNumber = num => num.toLocaleString();
+
+// Замінюємо текст у відповідних span
+const totalSpan = document.querySelector('span.total-population');
+const averageSpan = document.querySelector('span.average-population');
+if (totalSpan) totalSpan.textContent = formatNumber(totalPopulation);
+if (averageSpan) averageSpan.textContent = formatNumber(averagePopulation);
+
