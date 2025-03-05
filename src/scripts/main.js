5e36b054e3a3cbd275c9f0d43abf5f8f9d3bfd31
kami2693 <kamikaze2693@gmail.com>
2025-03-05 14:12:09 +0100

    solution?

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const allPopulation = [...document.querySelectorAll('.population')];
+const valuePopulation = Array.from(allPopulation).map((el) => el.innerText);
+let averagePopulation = 0;
+let totalPopulation = 0;
+
+for (let i = 0; i < valuePopulation.length; i++) {
+  const toNumber = +valuePopulation[i].replace(/,/g, '');
+
+  totalPopulation += toNumber;
+}
+
+averagePopulation = Math.floor(totalPopulation / valuePopulation.length);
+
+totalPopulation = totalPopulation.toLocaleString('en-US');
+averagePopulation = averagePopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population').innerText = averagePopulation;
+document.querySelector('.total-population').innerText = totalPopulation;
