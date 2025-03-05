4a1b1e7f243d33ce6deb98ac1a0786ba5e262a49
Ihor Kvasha <kvashaihor@gmail.com>
2025-03-05 01:58:42 -0800

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
+const populationSpans = document.querySelectorAll('span.population');
+
+let populationSum = 0;
+
+for (const span of populationSpans) {
+  const spanComasRemoved = parseInt(span.textContent.replaceAll(',', ''));
+
+  populationSum += spanComasRemoved;
+}
+
+const averageSpan = Math.round(populationSum / populationSpans.length);
+
+document.querySelector('span.total-population').textContent = populationSum;
+document.querySelector('span.average-population').textContent = averageSpan;
+
 // write your code here
