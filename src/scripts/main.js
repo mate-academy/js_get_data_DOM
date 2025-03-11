6d69e9fde55555a2be6419d2a6a5ac87aad86c5a
Anna Onyshchenko <auonishchenko.com>
2025-03-11 22:08:59 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const populations = Array.from(document.querySelectorAll('span.population'))
+    .map((span) => Number(span.textContent.replace(/\D/g, '')))
+    .filter((num) => !isNaN(num));
+
+  const totalPopulation = populations.reduce((sum, num) => sum + num, 0);
+
+  let averagePopulation = 0;
+
+  if (populations.length > 0) {
+    averagePopulation = Math.ceil(totalPopulation / populations.length);
+  }
+
+  function formatNumber(num) {
+    return num.toLocaleString();
+  }
+
+  const averageSpan = document.querySelector('span.average-population');
+  const totalSpan = document.querySelector('span.total-population');
+
+  if (averageSpan) {
+    averageSpan.textContent = formatNumber(averagePopulation);
+  }
+
+  if (totalSpan) {
+    totalSpan.textContent = formatNumber(totalPopulation);
+  }
+});
