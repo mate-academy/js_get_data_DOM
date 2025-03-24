052bb23678fbd1c110fbbd07060f3c390451e3a8
Yurii Chain <yuriichaiun@gmail.com>
2025-03-25 01:54:29 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const spans = [...document.querySelectorAll('.population')];
+
+const populationNumber = spans.map(
+  (elem) => +elem.textContent.split(',').join(''),
+);
+
+const totalPopulation = populationNumber.reduce((accum, num) => accum + num, 0);
+const avgpopulation = Math.floor(totalPopulation / populationNumber.length);
+
+document.querySelector('.total-population').textContent =
+  totalPopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  avgpopulation.toLocaleString('en-US');
