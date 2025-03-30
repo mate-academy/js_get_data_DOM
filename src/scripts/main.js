1bb09d158b58bbf7fafbbf5408eadcd45a866230
Yurii Bobuskyi <y.bobuskyi@gmail.com>
2025-03-30 23:31:46 +0300

    add population data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const populationList = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const populationArray = [];
+
+populationList.forEach((item) => {
+  populationArray.push(Number(item.textContent.replaceAll(',', '')));
+});
+
+const totalData = populationArray.reduce((accum, current) => {
+  return accum + current;
+}, 0);
+
+const averageData = totalData / populationArray.length;
+
+totalPopulation.textContent = totalData.toLocaleString();
+averagePopulation.textContent = averageData.toLocaleString();
