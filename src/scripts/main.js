a338a606f038d26db154bc6c58cbd7ac68be24be
cementix <bodakondrackij@gmail.com>
2025-03-31 17:30:54 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+
+const nums = [];
+
+for (const span of spans) {
+  const num = +span.innerText.split(',').join('');
+
+  nums.push(num);
+}
+
+const total = nums.reduce((acc, num) => acc + num, 0);
+const average = Math.floor(total / nums.length);
+
+document.querySelector('.total-population').innerText =
+  total.toLocaleString('en-US');
+
+document.querySelector('.average-population').innerText =
+  average.toLocaleString('en-US');
