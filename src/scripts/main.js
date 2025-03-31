c0e9d76cf112a43ea984f3151d274cb27d2243fd
Galyna Maikovych <gmaik.com@gmail.com>
2025-03-31 18:07:13 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const elements = [...document.querySelectorAll('.population')];
+let sum = 0;
+const quantity = elements.length;
+
+for (let i = 0; i < quantity; i++) {
+  sum += parseInt(elements[i].textContent.split(',').join(''));
+}
+
+let sumAv = Math.round(sum / quantity);
+
+sumAv = sumAv.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+total.textContent = sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+average.textContent = sumAv.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
