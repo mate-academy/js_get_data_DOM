6750530e8c0be01602d558d52d97ed9e7186070d
in7264 <in7264@gmail.com>
2025-04-01 16:05:07 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+
+let sum = 0;
+let count = 0;
+let average = 0;
+
+populations.forEach((element) => {
+  const cleanedValue = element.textContent.replace(/[^\d.]/g, '');
+
+  const populationValue = parseFloat(cleanedValue);
+
+  if (!isNaN(populationValue)) {
+    sum += populationValue;
+    count++;
+  }
+});
+
+if (count > 0) {
+  average = sum / count;
+}
+
+const averageElement = document.querySelector('.average-population');
+const totalElement = document.querySelector('.total-population');
+
+if (totalElement) {
+  totalElement.textContent = `${sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, `,`)}`;
+}
+
+if (averageElement) {
+  averageElement.textContent = `${average.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, `,`)}`;
+}
