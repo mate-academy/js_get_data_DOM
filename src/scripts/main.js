449d6d8f2b433e03724eca224414dc5f606254af
Roman Shevchyshyn <romanshevchyshyn1998@gmail.com>
2025-04-16 16:25:20 +0200

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const populationElements = [...document.querySelectorAll('.population')];
+
+const populations = populationElements.map(
+  (elem) => +elem.textContent.replace(/,/g, ''),
+);
+
+const total = populations.reduce((sum, num) => sum + num, 0);
+
+const average = Math.round(total / populations.length);
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString();
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString();
