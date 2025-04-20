68aa79f1524336f6e3a81ea9b0b224a777a652c8
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-04-21 02:06:52 +0300

    done task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+
+const numbers = Array.from(spans).map((span) => {
+  return parseInt(span.textContent.replace(/\D/g, ''), 10);
+});
+
+let sum = 0;
+
+numbers.forEach((number) => {
+  sum += number;
+});
+
+const average = sum / numbers.length;
+
+const sumSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+
+sumSpan.textContent = sum;
+averageSpan.textContent = average;
