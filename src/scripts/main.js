c917641592902d7fa5813f33b05cad6a173e8b5d
Dmytro Kovtun <kovtundima2910@gmail.com>
2025-04-26 13:30:13 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+const populationValues = [];
+
+document.querySelectorAll('span.population').forEach((node) => {
+  const text = node.textContent;
+
+  const convertedText = Number(text.split(',').join(''));
+
+  if (isNaN(convertedText)) {
+    return;
+  }
+
+  populationValues.push(convertedText);
+});
+
+function getTotal(values) {
+  let sum = 0;
+
+  for (let i = 0; i < values.length; i++) {
+    sum = sum + values[i];
+  }
+
+  return sum;
+}
+
+const total = getTotal(populationValues);
+const average = Math.ceil(total / populationValues.length);
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString();
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString();
