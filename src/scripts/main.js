f29dee350cf49262efa2adf1e266e87f71661f0e
Viktor Pastushenko <viktor.pastushenko@udelphi.com>
2025-05-06 16:12:39 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const populations = [...document.getElementsByClassName('population')];
+const total = populations.reduce((sum, item) => {
+  const itemPopulation = parseInt(item.innerText.replace(/,/g, ''));
+
+  return sum + (isNaN(itemPopulation) ? 0 : itemPopulation);
+}, 0);
+
+const fn = (className, n) =>
+  (document.getElementsByClassName(className)[0].innerText =
+    n.toLocaleString());
+
+fn('total-population', total);
+fn('average-population', Math.round(total / populations.length));
