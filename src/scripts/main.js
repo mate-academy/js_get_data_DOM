14348d780486f118065db8b6c3ad236a3fb358ac
Diana <berezandiana24@gmail.com>
2025-05-09 23:10:02 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const populationData = [];
+
+population.forEach((item) => {
+  const itemText = item.textContent;
+
+  const itemNumber = Number(itemText.replaceAll(',', ''));
+
+  populationData.push(itemNumber);
+});
+
+const totalPopulation = populationData.reduce((acc, item) => acc + item, 0);
+
+const averagePopulation = totalPopulation / populationData.length;
+
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+
+totalElement.textContent = totalPopulation.toLocaleString('en-US');
+averageElement.textContent = averagePopulation.toLocaleString('en-US');
