a61cadd24abaa5b624819b90a769ac4f7af98f44
Taras <taras.holotiuk@gmail.com>
2025-05-21 15:49:27 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const collection = document.getElementsByClassName('population');
+const population = [];
+let total = 0;
+let average = 0;
+
+for (let i = 0; i < collection.length; i++) {
+  const num = collection[i].innerText.replace(/,/g, '');
+
+  population.push(num);
+}
+
+total = population.reduce((acc, curr) => {
+  return acc + parseInt(curr);
+}, 0);
+average = total / population.length;
+
+document.getElementsByClassName('total-population')[0].innerText = total;
+document.getElementsByClassName('average-population')[0].innerText = average;
