4347eb39455f7a83acb7dcc37510e442fc0f41bb
Orest Vaheman <ovaheman@gmail.com>
2025-05-24 16:48:17 +0300

    replace total/average

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const data = document.querySelectorAll('.population');
+
+const popNumbers = [];
+
+for (const text of data) {
+  popNumbers.push(text.textContent);
+}
+
+const numbersArray = popNumbers.map((element) => +element.split(',').join(''));
+const total = numbersArray.reduce((acc, curr) => acc + curr, 0);
+const average = Math.ceil(total / numbersArray.length);
+const formatAverage = average.toLocaleString('en-US');
+const formatTotal = total.toLocaleString('en-US');
+
+const averagePop = document.querySelector('.average-population');
+const totalPop = document.querySelector('.total-population');
+
+averagePop.textContent = formatAverage;
+totalPop.textContent = formatTotal;
