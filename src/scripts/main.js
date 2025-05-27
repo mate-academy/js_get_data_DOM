480fc8c5ad2cb38c18ecdd9f224b531f3425aa73
Mariia Kapustkina <kapustkina08@mail.com>
2025-05-27 23:52:31 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
+/* eslint-disable max-len */
 'use strict';
 
-// write your code here
+const populationsSpan = document.querySelectorAll('.population');
+// eslint-disable-next-line prettier/prettier
+const populationsArray = Array.from(populationsSpan).map((span) => Number(span.textContent.split(',').join('')));
+const total = populationsArray.reduce((acc, num) => acc + num, 0);
+const average = total / populationsArray.length;
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString();
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString();
