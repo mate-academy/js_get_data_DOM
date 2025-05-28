1fb901131ff3d468b3dc73044696574b0ff840f8
Daria <darialys2510@gmail.com>
2025-05-28 16:13:17 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
+const populationElements = document.getElementsByClassName('population');
+
+const valuesOfPopulation = Array.from(populationElements).map((el) => {
+  Number(el.textContent.replaceAll(',', ''));
+});
+
+const sumOfValues = valuesOfPopulation.reduce((sum, num) => sum + num, 0);
+
+const average = Math.floor(sumOfValues / valuesOfPopulation.length);
+
+document.querySelector('.total-population').textContent =
+  `${addComas(sumOfValues)}`;
+
+document.querySelector('.average-population').textContent =
+  `${addComas(average)}`;
+
+function addComas(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 // write your code here
