23a392de78dca7fe3f5a9c94469e9bc9b33154f6
Taras <luchakt2@gmail.com>
2025-05-28 10:42:22 +0300

    finished work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+window.addEventListener('DOMContentLoaded', () => {
+  averageAndTotalValue();
+});
+
+function averageAndTotalValue() {
+  const population = document.querySelectorAll('.population');
+  const value = Array.from(population).map((pop) => pop.innerText);
+  const num = value.map((val) => Number(val.replaceAll(',', '')));
+  const totalSum = num.reduce((acc, n) => acc + n, 0);
+  const average = totalSum / num.length;
+
+  const totalPopulation = document.getElementsByClassName('total-population');
+
+  totalPopulation[0].innerText = totalSum.toLocaleString();
+
+  const averagePopulation =
+    document.getElementsByClassName('average-population');
+
+  averagePopulation[0].innerText = average.toLocaleString();
+}
