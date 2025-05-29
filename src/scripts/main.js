b5e6224a8246f7fbc61d5ce0485ba586b267c1d8
Eleonora Stasyuk <1990applesweet@gmail.com>
2025-05-29 21:23:40 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
 // write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const population = document.querySelectorAll('.population');
+  let totalPopulation = 0;
+  let populationCount = 0;
+
+  population.forEach((span) => {
+    const populationString = span.textContent;
+    const cleanedPopulationString = populationString.replace(/,/g, '');
+    const populationNumber = parseInt(cleanedPopulationString, 10);
+
+    if (!isNaN(populationNumber)) {
+      totalPopulation += populationNumber;
+      populationCount++;
+    }
+  });
+
+  const averagePopulation = totalPopulation / populationCount;
+
+  const formattedTotalPopulation = totalPopulation.toLocaleString('en-US');
+  const formattedAveragePopulation = averagePopulation.toLocaleString('en-US', {
+    maximumFractionDigits: 0,
+  });
+
+  const totalPopulationSpan = document.querySelector('.total-population');
+  const averagePopulationSpan = document.querySelector('.average-population');
+
+  if (totalPopulationSpan) {
+    totalPopulationSpan.textContent = formattedTotalPopulation;
+  }
+
+  if (averagePopulationSpan) {
+    averagePopulationSpan.textContent = formattedAveragePopulation;
+  }
+});
