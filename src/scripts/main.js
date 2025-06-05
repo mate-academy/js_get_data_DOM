326e38e53572fa057ad4c3bddd820ad3b8a0a44f
Sofiia13 <sophiyastanish@gmail.com>
2025-06-05 16:54:50 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+let sum = 0;
+
+const population = document.querySelectorAll('.population');
+
+population.forEach((span) => {
+  sum += parseFloat(span.textContent.replace(/,/g, ''));
+});
+
+const list = document.querySelector('.list');
+const items = list.querySelectorAll('li');
+
+const average = sum / items.length;
+
+document.querySelector('.total-population').textContent =
+  sum.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString('en-US');
