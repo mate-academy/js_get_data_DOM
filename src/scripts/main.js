92d58c84632b5f779296e1b530ce63b711b0af1b
Whslv <vladon19977@gmail.com>
2025-06-05 17:45:53 -0400

    Create js for total and average amount

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const populationElement = [...document.querySelectorAll('.population')];
+const takeText = populationElement.map((el) => el.textContent);
+const toNumber = takeText.map((str) => parseFloat(str.replaceAll(',', '')));
+const total = toNumber.reduce((sum, n) => sum + n, 0);
+const average = Math.round(total / toNumber.length);
+
+const toSameStyle = function (num) {
+  return num.toLocaleString();
+};
+
+document.querySelector('.total-population').textContent =
+  `${toSameStyle(total)}`;
+
+document.querySelector('.average-population').textContent =
+  `${toSameStyle(average)}`;
