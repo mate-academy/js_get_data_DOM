ad88420c23db824e9f4efb5daad4dfe5791b5d6c
Oleksii <oleksii.t.volkov@gmail.com>
2025-06-20 04:03:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write your code here
+const spansWitPopulation = Array.from(
+  document.querySelectorAll('span.population'),
+);
+
+const totalPopulation = spansWitPopulation.reduce((sum, element) => {
+  return sum + parseInt(element.textContent.replace(/,/g, ''));
+}, 0);
+
+const averagePopulation = totalPopulation / spansWitPopulation.length;
+
+function toStringWithCommas(number) {
+  return number.toLocaleString('en-US', { maximumFractionDigits: 0 });
+}
+
+const totalPopulationSpan = document.querySelector('.total-population');
+
+totalPopulationSpan.textContent = toStringWithCommas(totalPopulation);
+
+const averagePopulationSpan = document.querySelector('.average-population');
+
+averagePopulationSpan.textContent = toStringWithCommas(averagePopulation);
