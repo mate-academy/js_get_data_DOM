0fbfeb04c35327948598c2d8c8f60b1fcddae002
Adam <ad.kamieniarz@gmail.com>
2025-06-23 18:52:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('span.population');
+
+let sumOfPopulation = 0;
+
+for (let i = 0; i < populations.length; i++) {
+  const str = populations[i].innerHTML;
+  const arr = str.split(',');
+  const population = +arr.join('');
+
+  sumOfPopulation += population;
+}
+
+sumOfPopulation = '' + sumOfPopulation;
+
+const total = sumOfPopulation.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+let averagePopulation = Math.round(sumOfPopulation / populations.length);
+
+averagePopulation = '' + averagePopulation;
+
+const avrPopulation = averagePopulation.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+const spanTotalPopulation = document.querySelector('span.total-population');
+
+spanTotalPopulation.innerHTML = total;
+
+const spanAvrPopulation = document.querySelector('span.average-population');
+
+spanAvrPopulation.innerHTML = avrPopulation;
