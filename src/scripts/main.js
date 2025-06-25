5d3e382d483a4eeb16dff3d3e6232c20e94a5aa9
Alina Yamchuk <alina.utc@gmail.com>
2025-06-25 13:35:53 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const countries = document.getElementsByClassName("population");
+Number(countries);
+
+let sum = 0;
+
+for (let i = 0; i < countries.length; i++) {
+  const text = countries[i].textContent.replace(/,/g, '');
+  const number = Number(text);
+
+  if (isNaN(number)) {
+    sum += number;
+  }
+}
+const average = sum / countries.length;
+
+const formattedSum = sum.toLocaleString();
+const formattedAverage = Math.round(average).toLocaleString();
+
+document.getElementsByClassName("total-population").textContent = formattedSum;
+document.getElementsByClassName("average-population").textContent = formattedAverage;
+
