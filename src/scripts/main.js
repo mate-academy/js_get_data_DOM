ff9d075041a60b5b72f883c76e6812eb71a22f44
tymchukmr <184266140+tymchukmr@users.noreply.github.com.>
2025-06-26 11:35:18 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const populationElem = document.querySelectorAll('.population');
+
+const populations = [];
+
+populationElem.forEach((el) => {
+  const number = parseInt(el.textContent.replace(/,/g, ''), 10);
+
+  if (!isNaN(number)) {
+    populations.push(number);
+  }
+});
+
+const total = populations.reduce((sum, num) => sum + num, 0);
+const average = total / populations.length;
+
+const formattedTotal = total.toLocaleString('en-US');
+const formattedAverage = Math.round(average).toLocaleString('en-US');
+
+document.querySelector('.total-population').textContent = formattedTotal;
+document.querySelector('.average-population').textContent = formattedAverage;
