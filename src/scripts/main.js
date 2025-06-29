8f56ca61df0558d477da31594d17bc906464c7cd
Artur Shatskov <artur.shackov77@gmail.com>
2025-06-29 14:16:37 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const countries = Array.from(document.getElementsByClassName('population')).map(
+  (country) => Number(country.textContent.replaceAll(',', '')),
+);
+
+const total = countries.reduce((sum, country) => country + sum, 0);
+const average = Math.round(total / countries.length);
+
+const totalPopulation = document.querySelectorAll('span.total-population');
+
+totalPopulation.forEach((el) => (el.textContent = format(total)));
+
+const averagePopulation = document.querySelectorAll('span.average-population');
+
+averagePopulation.forEach((el) => (el.textContent = format(average)));
+
+function format(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
