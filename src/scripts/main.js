560d21ec0942b3b1908ba49b5cb4cb4a3a3f09b9
Drsgon <oksanaleschenko.ol@gmail.com>
2025-07-07 19:29:23 +0300

    add get_data_dom solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const population = document.querySelectorAll('.population');
+  const total = document.querySelector('.total-population');
+  const average = document.querySelector('.average-population');
+  const populationCountries = [];
+
+  populationCountries.push(...population);
+
+  let totalPopulation = 0;
+  let averageCount = 0;
+
+  population.forEach((country) => {
+    const cleanedText = country.textContent.replace(/,/g, '').trim();
+
+    totalPopulation += Number(cleanedText);
+  });
+
+  total.textContent = `${totalPopulation.toLocaleString('en-US')}`;
+  averageCount = Math.round(totalPopulation / population.length);
+  average.textContent = `${averageCount.toLocaleString('en-US')}`;
+});
