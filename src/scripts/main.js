5f3e1298e2697fb406308c972ae428f5a8053c5b
Pavlo Zhabort <zhabortp@gmail.com>
2025-07-09 12:31:26 +0300

    finished task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write your code here
+
+function formatNumber(number) {
+  return number.toLocaleString('en-US');
+}
+
+const populationElements = document.querySelectorAll('.population');
+
+const populations = Array.from(populationElements)
+  .map(el => el.textContent.replace(/,/g, '')) 
+  .map(Number)                                
+  .filter(num => !isNaN(num));                 
+
+const total = populations.reduce((sum, num) => sum + num, 0);
+const average = total / populations.length;
+
+document.querySelector('.total-population').textContent = formatNumber(total);
+document.querySelector('.average-population').textContent = formatNumber(Math.round(average));
+ent.querySelector('.average-population').textContent = formatNumber(Math.round(average));
