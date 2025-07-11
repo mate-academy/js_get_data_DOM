f10fe379e7253a261ec6137af9fa0e2633e43dcd
Pavlo <marlana08022022@gmail.com>
2025-07-11 17:02:24 +0300

    add total and average

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const populationElem = document.querySelectorAll('.population');
+
+let populations = [];
+
+populationElem.forEach((item) => {
+  populations.push(item.textContent);
+});
+
+populations = populations.map((str) => Number(str.replace(/\D/g, '')));
+
+let total = populations.reduce((sum, num) => sum + num, 0);
+
+let average = Math.floor(total / 9);
+
+total = total.toLocaleString('en-US');
+
+average = average.toLocaleString('en-US');
+
+document.querySelector('.total-population').textContent = `${total}`;
+document.querySelector('.average-population').textContent = `${average}`;
