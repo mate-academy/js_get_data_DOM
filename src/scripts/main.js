351ea91f6fda1272b177a3a1a52afce26b4c35f1
Nazarii Korol <nazarkorol3684@gmail.com>
2025-07-15 18:23:29 +0300

    Solved task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const populationElements = [...document.querySelectorAll('.population')];
+const populationsData = populationElements.map((element) => {
+  return Number(element.textContent.replaceAll(',', ''));
+});
+const populationsTotal = populationsData.reduce(
+  (acc, current) => acc + current,
+  0,
+);
+const populationsAvg = Math.round(populationsTotal / populationsData.length);
+
+document.querySelector('.total-population').textContent =
+  populationsTotal.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  populationsAvg.toLocaleString('en-US');
