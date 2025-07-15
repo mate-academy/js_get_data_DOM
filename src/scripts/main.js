5fdef335483f97485e7b3749151c288ef6a0598e
Alya <alinataranik03@gmail.com>
2025-07-15 18:12:00 +0300

    add task solution 15.07.25

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+let total = 0;
+
+for (const key of spans) {
+  const text = key.textContent;
+  const number = Number(text);
+
+  total += number;
+}
+
+const average = total / spans.length;
+
+const totalForm = document.querySelector('.total-population');
+const averageForm = document.querySelector('.average-population');
+
+totalForm.textContent = total.toLocaleString();
+averageForm.textContent = average.toLocaleString();
