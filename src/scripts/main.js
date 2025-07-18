04cfdadf2c716386b0b95f7119be6add39c6abae
Denys Herych <fojjof2@gmail.com>
2025-07-18 11:40:03 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
 'use strict';
 
-// write your code here
+const dates = document.getElementsByTagName('span');
+const totalPopulationSpan = document.getElementsByClassName('total-population');
+const averagePopulationSpan =
+  document.getElementsByClassName('average-population');
+let totalPopulation = 0;
+
+for (const data of dates) {
+  if (data.classList.contains('population')) {
+    const dataParseNumber = Number(data.textContent.replaceAll(',', ''));
+
+    totalPopulation += dataParseNumber;
+  }
+}
+
+const averagePopulation = String(
+  Math.ceil(totalPopulation / (dates.length - 2)),
+);
+const averagePopulationArr = averagePopulation.split('');
+const totalPopulationArr = String(totalPopulation).split('');
+let count = 0;
+
+if (averagePopulationArr.length >= 10) {
+  averagePopulationArr.splice(1, 0, ',');
+}
+
+for (let i = 0; i < averagePopulationArr.length - 1; i++) {
+  if (count === 3) {
+    averagePopulationArr.splice(i, 0, ',');
+    i++;
+    count = 0;
+  }
+  count++;
+}
+
+count = 0;
+
+if (totalPopulationArr.length >= 10) {
+  totalPopulationArr.splice(1, 0, ',');
+}
+
+for (let i = 2; i < totalPopulationArr.length - 1; i++) {
+  if (count === 3) {
+    totalPopulationArr.splice(i, 0, ',');
+    i++;
+    count = 0;
+  }
+  count++;
+}
+
+totalPopulationSpan[0].textContent = totalPopulationArr.join('');
+averagePopulationSpan[0].textContent = averagePopulationArr.join('');
