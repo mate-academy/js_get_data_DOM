6b65014ea1c1025b062f341d1e938de888edcff2
Eksonurit <fizervitalij897@gmail.com>
2025-07-26 21:46:43 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const dataPopulations = document.querySelectorAll('.population');
+
+const totalPopulation = Array.from(dataPopulations).reduce((prev, element) => {
+  const newText = element.textContent.replaceAll(',', '');
+
+  return prev + parseFloat(newText);
+}, 0);
+
+const averagePopulation = totalPopulation / Array.from(dataPopulations).length;
+
+const formatedTotalPopulation = totalPopulation.toLocaleString('en-US');
+const formatedAveragePopulation = averagePopulation.toLocaleString('en-US');
+
+const averageSpan = document.querySelector('.average-population');
+const totalSpan = document.querySelector('.total-population');
+
+averageSpan.textContent = formatedAveragePopulation;
+totalSpan.textContent = formatedTotalPopulation;
