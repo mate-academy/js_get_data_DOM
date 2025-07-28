8130ddb472567fddc1d6ef297c5a8c3aef9b4d9e
Dariia Hryshchukova <dariagrisukova@gmail.com>
2025-07-28 21:30:18 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
+/* eslint-disable function-paren-newline */
 
 // write your code here
+const spans = document.querySelectorAll('.population');
+
+let cleanSpans = Array.from(spans);
+
+cleanSpans = cleanSpans.map((span) =>
+  Number(span.innerText.replaceAll(',', '')),
+);
+
+const total = cleanSpans.reduce((result, num) => {
+  return result + num;
+}, 0);
+
+const average = Math.round(total / cleanSpans.length);
+
+document.querySelector('.average-population').innerText =
+  average.toLocaleString();
+
+document.querySelector('.total-population').innerText = total.toLocaleString();
