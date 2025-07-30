419bfaee4001378b4243181f4f3100b23ccde563
CwaR228 <sosok2gt@gmail.com>
2025-07-30 15:08:01 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
+/* eslint-disable */
 'use strict';
 
-// write your code here
+const  population = [...document.querySelectorAll('.population')];
+
+const populationSum = population.reduce((accumulator, currentValue) => accumulator + +currentValue.innerHTML.replaceAll(',', ''), 0);
+
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+total.innerHTML = populationSum.toLocaleString('en-US');
+average.innerHTML = Math.ceil(populationSum / population.length).toLocaleString('en-US');
+
