076baf777343e0861fc5ce949304d88d2826f66d
Maxim <makson202007@gmail.com>
2025-08-05 10:01:13 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write your code here
+const populationCollection = document.querySelectorAll('span.population');
+
+let averageAmount = 0;
+let total = 0;
+
+populationCollection.forEach((population) => {
+  const data = population.innerHTML.replaceAll(',', '');
+
+  if (+data || +data === 0) {
+    averageAmount++;
+
+    total += +data;
+  }
+});
+
+const average = Math.floor(total / averageAmount);
+
+const averageElement = document.querySelector('span.average-population');
+const totalElement = document.querySelector('span.total-population');
+
+averageElement.innerText = average.toLocaleString('en-US');
+totalElement.innerText = total.toLocaleString('en-US');
