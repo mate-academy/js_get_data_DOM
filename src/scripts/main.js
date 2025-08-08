bec52c0bc656dac30a9489af1a713a88ce21bf68
gOFFa <n4v4rm0re666@gmail.com>
2025-08-08 23:27:06 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const population = document.getElementsByClassName('population');
+let total = 0;
+
+for (const country of population) {
+  const value = Number(country.innerText.replace(/,/g, ''));
+
+  total += value;
+}
+
+const average = Math.floor(total / population.length).toLocaleString();
+
+document.querySelector('.total-population').innerText = total.toLocaleString();
+document.querySelector('.average-population').innerText = average;
