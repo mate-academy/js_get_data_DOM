00bdf58455924d18ceb1517167b60ff52d128373
Olena Tatyuk <tatyukolena@gmail.com>
2025-08-12 19:48:04 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const population = document.getElementsByClassName('population');
+
+const numbers = Array.from(population).map((element) => {
+  const elem = element.textContent;
+  const repl = elem.replaceAll(/[, ]/g, '');
+
+  return Number(repl);
+});
+
+const total = numbers.reduce((sum, number) => sum + number, 0);
+const average = Math.round(total / population.length);
+
+const avg = document.getElementsByClassName('average-population')[0];
+
+avg.innerHTML = average.toLocaleString('en-US');
+
+const totl = document.getElementsByClassName('total-population')[0];
+
+totl.innerHTML = total.toLocaleString('en-US');
