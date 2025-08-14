53bb57b5edb71e0c726329156877d7d803f5cc93
Matvey Turluk <matychencia.ua@gmail.com>
2025-08-14 16:49:35 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
 // write your code here
+const populations = document.querySelectorAll('.population');
+
+const values = Array.from(populations).map((el) => el.textContent);
+
+let sum = 0;
+
+for (const value of values) {
+  sum += +value.split(',').join('');
+}
+
+const averageValue = Math.floor(sum / values.length);
+
+const totalPopulation = document.querySelector('.total-population');
+const averPopulation = document.querySelector('.average-population');
+
+// const totalResult = '';
+// const averResult = '';
+
+function getPopulation(val) {
+  const value = val.toString();
+  const result = [];
+  let pie = '';
+
+  for (let i = value.length - 1; i >= 0; i--) {
+    pie += value[i];
+
+    if (i === 0 && pie.length < 3) {
+      result.push(pie);
+    } else if (pie.length === 3) {
+      result.push(pie);
+      pie = '';
+    }
+  }
+  // console.log(result);
+
+  return result.join(',').split('').reverse().join('');
+}
+
+// console.log(sum);
+// console.log(averageValue);
+
+totalPopulation.textContent = getPopulation(sum);
+averPopulation.textContent = getPopulation(averageValue);
