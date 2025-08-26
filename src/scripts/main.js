b1666d76cb9969d67eba4000b9e955e1d6886eaf
Vasyliev Vadym <protocol222333@gmail.com>
2025-08-26 15:43:42 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const populations = [...document.querySelectorAll('.population')].map((el) =>
+  Number(el.textContent.split(',').join('')));
+
+const totalPopulation = populations.reduce((pre, current) => pre + current, 0);
+
+const avgPopulation = Math.round(totalPopulation / populations.length);
+
+document.querySelector('.total-population').textContent =
+  totalPopulation.toLocaleString();
+
+document.querySelector('.average-population').textContent =
+  avgPopulation.toLocaleString();
