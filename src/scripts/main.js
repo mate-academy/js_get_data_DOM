6d9f8216267bb16f8c61236622c780f334979d61
Marta Osikiewicz <marta.osikiewicz@gmail.com>
2025-08-26 18:18:59 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population'); // wszystkie spany
+const numbers = Array.from(spans).map((span) => {
+  return Number(span.textContent.replace(/,/g, ''));
+});
+const total = numbers.reduce(
+  (accumulator, current) => accumulator + current,
+  0,
+);
+
+const average = Math.round(total / numbers.length);
+
+const span1 = document.querySelector('.total-population');
+
+span1.textContent = total;
+
+const span2 = document.querySelector('.average-population');
+
+span2.textContent = average;
