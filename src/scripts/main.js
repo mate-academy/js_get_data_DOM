4e3346d60a9ea478d7b7eea37c4bdf08b0febe25
Luna <yanka16247@mail.com>
2025-09-02 20:48:41 +0300

    implement population calculation in main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,18 @@
 let total = 0;
 let average = 0;
 const population = [...document.querySelectorAll('.population')];
+const populationCount = [];
 
 for (const elem of population) {
-  total += Number(elem.textContent.replaceAll(',', ''));
+  const populationOfCountry = Number(elem.textContent.replaceAll(',', ''));
+
+  if (Number.isFinite(populationOfCountry)) {
+    total += populationOfCountry;
+    populationCount.push(populationOfCountry);
+  }
 }
 
-average = total / population.length;
+average = populationCount.length > 0 ? total / populationCount.length : 0;
 
 document.querySelector('.total-population').textContent =
   total.toLocaleString('en-US');
