2fc09af5f6952e61bfe91570c66909b6dfe40299
Luna <yanka16247@mail.com>
2025-09-02 20:30:07 +0300

    feat: implement population calculation in main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
+let total = 0;
+let average = 0;
+const population = [...document.querySelectorAll('.population')];
+
+for (const elem of population) {
+  total += Number(elem.textContent.replaceAll(',', ''));
+}
+
+average = total / population.length;
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  Math.round(average).toLocaleString('en-US');
+
 // write your code here
