bde691f702d9a606fe556d2cc6155a3d77a8b11f
Artem Arseniev <144947394+Aarseniev@users.noreply.github.com>
2025-09-02 04:03:24 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+const populationData = [];
+
+for (let i = 0; i < populationElements.length; i++) {
+  const parsedPopulation = Number(
+    populationElements[i].textContent.replace(/,/g, ''),
+  );
+
+  populationData.push(parsedPopulation);
+}
+
+const totalPopulation = populationData.reduce(
+  (populationSum, currentNumber) => {
+    return populationSum + currentNumber;
+  },
+  0,
+);
+
+const formattedTotalPopulation = totalPopulation.toLocaleString('en-US');
+const totalPopulationElement = document.querySelector('.total-population');
+const averagePopulationElement = document.querySelector('.average-population');
+
+totalPopulationElement.textContent = formattedTotalPopulation;
+
+averagePopulationElement.textContent = Math.round(
+  totalPopulation / populationData.length,
+).toLocaleString('en-US');
