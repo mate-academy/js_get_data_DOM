e46d92ed5d458248405bc81e7235511e57b44a73
Nazariy <nazariy.holovach2007@gmail.com>
2025-09-10 13:21:05 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const countriesPopulation = [...document.getElementsByClassName('population')];
+const populationArr = countriesPopulation.map((country) => {
+  return Number(country.textContent.replace(/,/g, ''));
+});
+
+const totalElement = document.querySelector('.total-population');
+const avgElement = document.querySelector('.average-population');
+
+const total = populationArr.reduce((a, b) => a + b, 0);
+const avg = Math.round(total / populationArr.length);
+
+totalElement.textContent = total.toLocaleString();
+avgElement.textContent = avg.toLocaleString();
