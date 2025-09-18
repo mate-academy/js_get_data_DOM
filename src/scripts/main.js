79c673f9730c46ea521c113014d7df2009a2feb3
kovalchukp <you@example.comp.kovalchuk@icloud.com>
2025-09-17 23:29:10 -0600

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const populationSpans = document.querySelectorAll('span.population');
+
+  const population = Array.from(populationSpans)
+    .map((span) => span.textContent.replace(/,/g, ''))
+    .map((numStr) => Number(numStr))
+    .filter((num) => !isNaN(num));
+  const total = population.reduce((acc, item) => acc + item, 0);
+  const average = total / population.length;
+
+  const formatter = new Intl.NumberFormat();
+
+  document.querySelector('span.total-population').textContent =
+    formatter.format(total);
+
+  document.querySelector('span.average-population').textContent =
+    formatter.format(Math.round(average));
+});
