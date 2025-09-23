41f0f3dc75cc8e763f16dec9791c30fe6c28f6cc
Vasya <rusnakvasa3@gmail.com>
2025-09-23 19:22:39 +0300

    Fix lint issues and format code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const alSpan = document.querySelectorAll('.population');
+
+const populations = Array.from(alSpan).map((el) => {
+  return Number(el.textContent.replaceAll(',', ''));
+});
+
+const total = populations.reduce((acc, el) => acc + el, 0);
+const average = total / populations.length;
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString('en-US');
