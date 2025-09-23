d95b9fd4fcba8cabc835173697e50e29eb997fae
Iryna <Yurchak.Iryna2017@gmail.com>
2025-09-23 15:02:49 +0300

    newSolution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')].map((el) => {
+  Number(el.textContent.replace(/,/g, ''));
+});
+
+let total = 0;
+
+for (let i = 0; i < population.length; i++) {
+  total += population[i];
+}
+
+const average = Math.round(total / population.length);
+
+const resultTotal = document.querySelector('.total-population');
+
+resultTotal.textContent = total.toLocaleString();
+
+const resultAverage = document.querySelector('.average-population');
+
+resultAverage.textContent = average.toLocaleString();
