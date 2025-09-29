23bc692097e94869a6d950a05b382a03be818382
SheFing <shevchukm2002@gmail.com>
2025-09-29 16:27:46 +0300

    attemp 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+function chunkString(str) {
+  const remainder = str.length % 3;
+
+  const head = remainder ? [str.slice(0, remainder)] : [];
+
+  const tail = str.slice(remainder).match(/.{1,3}/g) || [];
+
+  return [...head, ...tail].join(',');
+}
+
+const populationArr = [...document.querySelectorAll('.population')].reduce(
+  (acc, i) => {
+    acc.push(+i.textContent.split(',').join(''));
+
+    return acc;
+  },
+  [],
+);
+
+document.querySelector('.total-population').textContent = chunkString(
+  '' + populationArr.reduce((acc, i) => acc + i),
+);
+
+document.querySelector('.average-population').textContent = chunkString(
+  '' +
+    Math.round(
+      populationArr.reduce((acc, i) => acc + i, 0) / populationArr.length,
+    ),
+);
