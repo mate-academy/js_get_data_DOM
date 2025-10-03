8da81d6baa4d0e75ce7cf15f5660554c9eea64e8
Bohdan Pidhrebelnyi <bodiiiiic@gmail.com>
2025-10-03 11:34:33 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const span = [...document.querySelectorAll('.population')];
+
+const sum = span
+  .map((el) => Number(el.innerText.trim().replace(/,/g, '')))
+  .filter((num) => !isNaN(num))
+  .reduce((acc, num) => acc + num, 0);
+
+const totalPopulation = document.querySelector('.total-population');
+
+totalPopulation.innerHTML = sum.toLocaleString('en-US');
+
+const average = Math.round(sum / span.length);
+
+const averagePopulation = document.querySelector('.average-population');
+
+averagePopulation.innerHTML = average.toLocaleString('en-US');
