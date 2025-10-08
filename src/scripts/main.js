449f70cbfd18c45ffbdd351ed81b74ee1061ef08
Dima <dima2003strik@gmail.com>
2025-10-09 01:51:44 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 // write your code here
+const populationes = [...document.querySelectorAll('.population')];
+
+let total = 0;
+
+for (const population of populationes) {
+  const num = population.innerText.split('');
+  let n = [];
+
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] === ',') {
+      continue;
+    }
+
+    n.push(num[i]);
+  }
+  n = n.join('');
+
+  total += Number(n);
+}
+
+const average = Math.round(total / populationes.length);
+
+const formattedTotal = total.toLocaleString('en-US');
+const formattedAverage = average.toLocaleString('en-US');
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.innerText = formattedTotal;
+averagePopulation.innerText = formattedAverage;
