e8e8c4eea07099c2669ddd2624b2efaa37fefd9e
Andriy <a.tupko98@gmail.com>
2025-10-09 14:03:45 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+const populations = document.querySelectorAll('span.population');
+const allText = Array.from(populations).map((span) => span.textContent);
+
+let total = 0;
+
+for (const i of allText) {
+  const num = Number(i.replace(/,/g, ''));
+
+  total += num;
+}
+
+const average = total / allText.length;
+
+document.querySelector('span.total-population').textContent = total;
+
+document.querySelector('span.average-population').textContent = average;
