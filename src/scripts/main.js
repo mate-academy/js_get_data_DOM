38ee6209e9c5728054307a18570d756f3bd08467
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2025-10-14 19:49:39 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const data = document.querySelectorAll('.population');
+
+const total = Array.from(data).reduce((acc, el) => {
+  const cleanNumber = el.textContent.replace(/,/g, '').trim();
+
+  return acc + Number(cleanNumber);
+}, 0);
+
+const countOfCountries = document.querySelectorAll('.list__item').length;
+
+const average = total / countOfCountries;
+
+const tagTotal = document.querySelector('.total-population');
+const tagAverage = document.querySelector('.average-population');
+
+tagTotal.textContent = total.toLocaleString();
+
+tagAverage.textContent = average.toLocaleString(undefined, {
+  maximumFractionDigits: 0,
+});
