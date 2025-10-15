2dafe680d238ae7d07d8e886522728a1b929e71e
wzfxc <maksohrimenko1@gmail.com>
2025-10-15 18:49:25 +0300

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const populationArray = document.querySelectorAll('.population');
+
+let totalAmount = 0;
+
+populationArray.forEach((number) => {
+  let amount = number.textContent.split(',').join('');
+
+  amount = parseInt(amount);
+
+  if (Number.isFinite(amount)) {
+    totalAmount += amount;
+  }
+});
+
+const averageAmount = totalAmount / populationArray.length;
+
+const formattedTotal = totalAmount.toLocaleString('en-US');
+const formattedAverage = averageAmount.toLocaleString('en-US');
+
+document.querySelector('.total-population').textContent = formattedTotal;
+document.querySelector('.average-population').textContent = formattedAverage;
