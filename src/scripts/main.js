1e965157e8eb747780669f365e2cc2ee8cc13ccb
Viktor-1995 <madebyvityok@ukr.net>
2025-10-23 13:06:57 +0200

    1 solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write your code here
+const populationArray = [...document.getElementsByClassName('population')];
+const populationNumbers = [];
+
+populationArray.forEach((country) => {
+  populationNumbers.push(parseInt(country.outerText.split(',').join('')));
+});
+
+const initVal = 0;
+const total = populationNumbers.reduce((acc, val) => acc + val, initVal);
+const average = total / populationNumbers.length;
+
+document.getElementsByClassName('total-population')[0].outerText = total;
+document.getElementsByClassName('average-population')[0].outerText = average;
