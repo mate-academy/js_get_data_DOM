3b5f36c5a364649193e2c28c8d21a40918c560a0
Viktor-1995 <madebyvityok@ukr.net>
2025-10-23 13:18:55 +0200

    1.1 solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,19 @@ const populationArray = [...document.getElementsByClassName('population')];
 const populationNumbers = [];
 
 populationArray.forEach((country) => {
-  populationNumbers.push(parseInt(country.outerText.split(',').join('')));
+  const value = country.outerText.split(',').join('');
+
+  if (!isNaN(value) && !isNaN(parseFloat(value))) {
+    populationNumbers.push(parseInt(value));
+  }
 });
 
 const initVal = 0;
 const total = populationNumbers.reduce((acc, val) => acc + val, initVal);
 const average = total / populationNumbers.length;
 
-document.getElementsByClassName('total-population')[0].outerText = total;
-document.getElementsByClassName('average-population')[0].outerText = average;
+document.getElementsByClassName('total-population')[0].outerText =
+  total.toLocaleString();
+
+document.getElementsByClassName('average-population')[0].outerText =
+  average.toLocaleString();
