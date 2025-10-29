4a8a3337d8bcddc65687cafaa89b88b03bd3f51f
Oleh Prokopenko <olehprokopenko1@gmail.com>
2025-10-29 16:30:10 +0200

    correct mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,24 @@
 
 const totalPopulation = document.querySelector('.total-population');
 const averagePopulation = document.querySelector('.average-population');
+
 const populationOfCountries = document.querySelectorAll('.population');
+
 const arrOfPopulationOfCountries = [...populationOfCountries].map((index) => {
-  return index.textContent.replaceAll(',', '');
+  const clean = index.textContent.replaceAll(',', '').trim();
+  const num = Number(clean);
+
+  if (!Number.isNaN(num) && clean !== '') {
+    return num;
+  } else {
+    return 0;
+  }
 });
 
 const totalAmount = arrOfPopulationOfCountries.reduce((acc, i) => {
-  return acc + Number(i);
+  return acc + i;
 }, 0);
+
 const averageValue = totalAmount / arrOfPopulationOfCountries.length;
 
 const totalAmountStr = totalAmount.toLocaleString();
