1fcecb19c6442e5979080d694dd49f8c54cfd227
Oleh Prokopenko <olehprokopenko1@gmail.com>
2025-10-29 22:41:35 +0200

    correct mistakes 02

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,16 +5,7 @@ const averagePopulation = document.querySelector('.average-population');
 
 const populationOfCountries = document.querySelectorAll('.population');
 
-const arrOfPopulationOfCountries = [...populationOfCountries].map((index) => {
-  const clean = index.textContent.replaceAll(',', '').trim();
-  const num = Number(clean);
-
-  if (!Number.isNaN(num) && clean !== '') {
-    return num;
-  } else {
-    return 0;
-  }
-});
+const arrOfPopulationOfCountries = validation(populationOfCountries);
 
 const totalAmount = arrOfPopulationOfCountries.reduce((acc, i) => {
   return acc + i;
@@ -27,3 +18,22 @@ const averageValueStr = averageValue.toLocaleString();
 
 totalPopulation.textContent = totalAmountStr;
 averagePopulation.textContent = averageValueStr;
+
+function validation(data) {
+  if (data.length === 0) {
+    return [];
+  }
+
+  const dataArr = [...data].map((index) => {
+    const clean = index.textContent.replaceAll(',', '').trim();
+    const num = Number(clean);
+
+    if (!Number.isNaN(num) && clean !== '') {
+      return num;
+    } else {
+      return 0;
+    }
+  });
+
+  return dataArr;
+}
