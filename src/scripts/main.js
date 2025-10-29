f572463f3a6b786859bba881c4898d0ea1dff224
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2025-10-29 16:35:26 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write your code here
+const populationElenents = document.querySelectorAll('span.population');
+const countsList = [];
+const countriesCount = populationElenents.length;
+
+for (const count of populationElenents) {
+  countsList.push(Number(count.innerText.replaceAll(',', '')));
+}
+
+const total = countsList.reduce((sum, num) => sum + num, 0);
+const average = total / countriesCount;
+
+function fixComa(num) {
+  let result = '';
+
+  let counter = 0;
+
+  for (let i = num.length - 1; i >= 0; i--) {
+    result += num[i];
+    counter++;
+
+    if (counter % 3 === 0 && i !== 0) {
+      result += ',';
+    }
+  }
+
+  result = result.split('').reverse().join('');
+
+  return result;
+}
+
+const totalString = total.toString();
+const totalResult = fixComa(totalString);
+
+const averageString = average.toString().split('.')[0];
+const averageResult = fixComa(averageString);
+
+document.querySelector('span.total-population').replaceWith(totalResult);
+
+document.querySelector('span.average-population').replaceWith(averageResult);
