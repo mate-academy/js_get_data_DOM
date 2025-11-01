3a8bcfef99aadb49f4981f3c293f3ae86c582bbd
Ievgen Petryk <jakeshad123@gmail.com>
2025-11-01 13:47:37 +0200

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write your code here
+const elements = [...document.querySelectorAll('.population')];
+
+const totalPop = elements.reduce((total, el) => {
+  const text = el.textContent.trim();
+
+  let s = '';
+
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== ',' && text[i] !== ' ') {
+      s += text[i];
+    }
+  }
+
+  const n = Number(s);
+
+  if (!isNaN(n)) {
+    return total + n;
+  } else {
+    return total;
+  }
+}, 0);
+
+const averagePop = Math.round(totalPop / elements.length);
+
+const newTotalPop = new Intl.NumberFormat('en-US').format(totalPop);
+const newAveragePop = new Intl.NumberFormat('en-US').format(averagePop);
+
+document.querySelector('.total-population').textContent = newTotalPop;
+document.querySelector('.average-population').textContent = newAveragePop;
