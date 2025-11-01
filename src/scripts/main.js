020cb75098fa53b2566fa9c8e5f29d8b636c7346
Anastasia <ahinata1717@gmail.com>
2025-11-01 17:12:47 +0200

    added a validation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,10 @@
 const elements = [...document.querySelectorAll('.population')];
 
 const text = elements.map((el) => el.textContent);
-const numbers = text.map((t) => Number(t.replaceAll(',', '')));
+const numbers = text
+  .filter((el) => typeof el === 'number')
+  .map((t) => Number(t.replaceAll(',', '')));
+
 const sum = numbers.reduce((acc, num) => acc + num);
 const average = (sum / numbers.length).toLocaleString('en-US');
 const formattedSum = sum.toLocaleString('en-US');
