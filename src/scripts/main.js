647585e2d44f419884198865d5512e04896a2be6
Anastasia <ahinata1717@gmail.com>
2025-11-01 17:07:05 +0200

    implemented population calculation logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const elements = [...document.querySelectorAll('.population')];
+
+const text = elements.map((el) => el.textContent);
+const numbers = text.map((t) => Number(t.replaceAll(',', '')));
+const sum = numbers.reduce((acc, num) => acc + num);
+const average = (sum / numbers.length).toLocaleString('en-US');
+const formattedSum = sum.toLocaleString('en-US');
+
+const totalPopulationElement = document.querySelector('.total-population');
+const avaregePopulationElement = document.querySelector('.average-population');
+
+totalPopulationElement.textContent = formattedSum;
+avaregePopulationElement.textContent = average;
