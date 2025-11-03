d41c2842f1346dc6beb8245b0f687030f5ccce4a
Mariia Alefirenko <mari.alefir@gmail.com>
2025-11-03 16:31:52 +0200

    feat: get data from DOM and calculate average

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write your code here
+const populationElements = document.querySelectorAll('.population');
+const populations = Array.from(populationElements)
+  .map((el) => el.textContent.replace(/,/g, ''))
+  .map(Number)
+  .filter((num) => !isNaN(num));
+const total = populations.reduce((sum, el) => sum + el, 0);
+const average = total / populations.length;
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString('en-US');
