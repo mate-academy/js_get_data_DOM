8ee0612f2535877772b228553fb5be4cad27d1ac
Yarovskiy Andrii <yarovskiy@gmail.com>
2025-11-09 20:09:56 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const spansList = document.querySelectorAll('span.population');
+let sum = 0;
+
+for (const i of spansList) {
+  let tmp = i.textContent.replace(/,/g, '');
+
+  tmp = parseInt(tmp);
+  sum += tmp;
+}
+
+const total = document.querySelector('span.total-population');
+
+total.textContent = sum.toLocaleString('en-US');
+
+const countCountries = spansList.length;
+
+const ever = document.querySelector('span.average-population');
+
+ever.textContent = (sum / countCountries).toLocaleString('en-US');
