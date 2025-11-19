31c6f8757d42c4a1e32805b05c9370554f979208
horbenkoOleksandr <horbenko.oleksandr.v@gmail.com>
2025-11-20 01:26:24 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const spanElement = document.querySelectorAll('span.population');
+const spanElementTotalPopulation = document.querySelector(
+  'span.total-population',
+);
+const spanElementAveragePopulation = document.querySelector(
+  'span.average-population',
+);
+
+const getSpanElements = [...spanElement];
+
+const totalCountryPopulation = getSpanElements.reduce(
+  (initialCountry, nextCountry) => {
+    const valueNextCountry = nextCountry.innerHTML;
+    const cleanSymbol = valueNextCountry.replace(/[^\d]/g, '');
+
+    return initialCountry + Number(cleanSymbol);
+  },
+  0,
+);
+
+const averageValuePopulation = totalCountryPopulation / getSpanElements.length;
+
+spanElementTotalPopulation.innerHTML =
+  totalCountryPopulation.toLocaleString('en-US');
+
+spanElementAveragePopulation.innerHTML = Math.floor(
+  averageValuePopulation,
+).toLocaleString('en-US');
