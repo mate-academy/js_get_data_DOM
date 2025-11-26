dcfb56a21f4ccd6ee45360de053304bf604b3c8b
Stas <oleksii.stanislav@student.uzhnu.edu.ua>
2025-11-26 18:48:35 +0200

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write your code here
+const population = document.querySelectorAll('.population');
+const avaragePopulation = document.querySelector('.average-population');
+const totalPopulation = document.querySelector('.total-population');
+
+const arr = [...population];
+const result = arr.map((item) => parseInt(item.textContent.replace(/,/g, '')));
+
+const total = result.reduce((acc, rec) => acc + rec, 0);
+const avarage = total / result.length;
+
+totalPopulation.textContent = total.toLocaleString('en-US');
+avaragePopulation.textContent = avarage.toLocaleString('en-US');
