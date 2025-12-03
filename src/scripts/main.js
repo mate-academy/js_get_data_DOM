2de2d03c6d53b783acf1b248e19df30b411c1250
Inna Haichenia <gajchenja2424@ukr.net>
2025-12-03 11:41:28 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const populationSpans = document.querySelectorAll('.population');
+
+const cleanNumbersArray = [...populationSpans].map((span) => {
+  const cleanStr = span.textContent.trim().replaceAll(',', '');
+
+  return +cleanStr;
+});
+
+let totalPopulation = 0;
+
+for (let i = 0; i < cleanNumbersArray.length; i++) {
+  totalPopulation += cleanNumbersArray[i];
+}
+
+const averagePopulation = totalPopulation / cleanNumbersArray.length;
+
+function formatNumberWithCommas(number) {
+  return number.toLocaleString('en-US', { maximumFractionDigits: 0 });
+}
+
+const formattedResult1 = formatNumberWithCommas(totalPopulation);
+const formattedResult2 = formatNumberWithCommas(averagePopulation);
+
+const totalSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+
+totalSpan.textContent = formattedResult1;
+averageSpan.textContent = formattedResult2;
