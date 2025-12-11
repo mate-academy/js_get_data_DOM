e2d0d9281c2c91921befcd7d14beac71036fbb3a
Yaroslav Ivasenko <yarik.dock@gmail.com>
2025-12-11 14:23:42 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const avarageButton = document.querySelector('.average-population');
+const totalPopulation = document.querySelector('.total-population');
+const numbers = [...document.querySelectorAll('.population')].map((el) =>
+  Number(el.textContent.replaceAll(',', ''))
+);
+
+totalPopulation.addEventListener('click', () => {
+  calculateTotal();
+});
+
+avarageButton.addEventListener('click', () => {
+  calculateAvarage();
+});
+
+function calculateTotal() {
+  const counter = numbers.reduce((sum, n) => sum + n, 0);
+  const formattedCounter = counter.toLocaleString('en-US');
+
+  totalPopulation.textContent = formattedCounter;
+}
+
+function calculateAvarage() {
+  const counter = numbers.reduce((sum, n) => sum + n, 0);
+  const avarageNumber = counter / numbers.length;
+  const formattedCounter = avarageNumber.toLocaleString('en-US');
+
+  totalPopulation.textContent = formattedCounter;
+}
