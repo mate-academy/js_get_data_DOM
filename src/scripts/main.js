01f6fb55157fe13040a17027b538c78ebe962a2e
Yaroslav Ivasenko <yarik.dock@gmail.com>
2025-12-11 14:51:38 +0200

    updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,23 @@
 'use strict';
 
-const avarageButton = document.querySelector('.average-population');
+const average = document.querySelector('.average-population');
 const totalPopulation = document.querySelector('.total-population');
 const numbers = [...document.querySelectorAll('.population')].map((el) =>
   // eslint-disable-next-line prettier/prettier
   Number(el.textContent.replaceAll(',', '')));
+const counter = numbers.reduce((sum, n) => sum + n, 0);
 
 function calculateTotal() {
-  const counter = numbers.reduce((sum, n) => sum + n, 0);
   const formattedCounter = counter.toLocaleString('en-US');
 
   totalPopulation.textContent = formattedCounter;
 }
 
 function calculateAvarage() {
-  const counter = numbers.reduce((sum, n) => sum + n, 0);
   const avarageNumber = counter / numbers.length;
   const formattedCounter = avarageNumber.toLocaleString('en-US');
 
-  avarageButton.textContent = formattedCounter;
+  average.textContent = formattedCounter;
 }
 
 calculateTotal();
