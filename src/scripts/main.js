5f985d4581e999f11fd9e9e32cfb687dba0e2e8c
Aleks Pashkevych <alekspashkevych@gmail.com>
2025-12-18 18:47:40 +0200

    the first version of homework

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const populationTag = document.querySelector('span.population');
+
+const populationString = populationTag.textContent;
+const tmp = populationString.split(',');
+
+const totalPopulation = Number(tmp.join(''));
+
+const countries = document.querySelectorAll('li');
+const numberOfCountries = countries.length;
+
+const average = totalPopulation / numberOfCountries;
+
+const totalTag = document.querySelector('.total-population');
+const averagePopulationTag = document.querySelector('.average-population');
+const outputTotal = new Intl.NumberFormat('en-US').format(totalPopulation);
+const outputAverage = new Intl.NumberFormat('en-US').format(average.toFixed(0));
+
+totalTag.textContent = outputTotal;
+averagePopulationTag.textContent = outputAverage;
