896edf13f7d81f2e0f330ad399058fb40ceac791
Aleks Pashkevych <alekspashkevych@gmail.com>
2025-12-18 19:12:57 +0200

    changed calculation to all countries

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,24 @@
 'use strict';
 
-const populationTag = document.querySelector('span.population');
+const populationTag = document.querySelectorAll('span.population');
 
-const populationString = populationTag.textContent;
-const tmp = populationString.split(',');
+let population = 0;
 
-const totalPopulation = Number(tmp.join(''));
+populationTag.forEach((el) => {
+  const tmp = el.textContent.split(',');
+  const countryPopulation = Number(tmp.join(''));
+
+  population += countryPopulation;
+});
 
 const countries = document.querySelectorAll('li');
 const numberOfCountries = countries.length;
 
-const average = totalPopulation / numberOfCountries;
+const average = population / numberOfCountries;
 
 const totalTag = document.querySelector('.total-population');
 const averagePopulationTag = document.querySelector('.average-population');
-const outputTotal = new Intl.NumberFormat('en-US').format(totalPopulation);
+const outputTotal = new Intl.NumberFormat('en-US').format(population);
 const outputAverage = new Intl.NumberFormat('en-US').format(average.toFixed(0));
 
 totalTag.textContent = outputTotal;
