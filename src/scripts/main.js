9d258dcaadc953e0531309f8a0832996cc40c2b4
Mikitjuk Pavlo <mikitjukpavlo@mail.com>
2025-12-20 10:27:58 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
+/* eslint-disable function-paren-newline */
 'use strict';
 
-// write your code here
+const populations = [...document.querySelectorAll('.population')].map((el) =>
+  Number(el.textContent.split(',').join('')),
+);
+
+const totalPopulation = populations.reduce((pre, current) => pre + current, 0);
+
+const avgPopulation = Math.round(totalPopulation / populations.length);
+
+document.querySelector('.total-population').textContent =
+  totalPopulation.toLocaleString();
+
+document.querySelector('.average-population').textContent =
+  avgPopulation.toLocaleString();
