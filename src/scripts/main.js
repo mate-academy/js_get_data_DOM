52f55dd31576c80ce56e3d33c5349726295c405e
Yegor <egor598000@gmail.com>
2025-12-22 23:19:32 +0100

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write your code here
+const countries = document.querySelectorAll('.population');
+
+function separator(number) {
+  const result = [];
+  const arr = Array.from(number + '');
+  let count = 0;
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (count === 3) {
+      result.unshift(',');
+      count = 0;
+    }
+
+    result.unshift(arr[i]);
+    count++;
+  }
+
+  return result.join('');
+}
+
+function countTotal(arr) {
+  let total = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    total += Number.parseInt(arr[i].textContent.split(',').join(''));
+  }
+
+  return total;
+}
+
+function countAverage(arr) {
+  return Math.round(countTotal(arr) / arr.length);
+}
+
+document.querySelector('.total-population').textContent = separator(
+  countTotal(countries),
+);
+
+document.querySelector('.average-population').textContent = separator(
+  countAverage(countries),
+);
