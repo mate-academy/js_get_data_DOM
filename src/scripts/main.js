ea130538c77c3f6763843a70a1eb7c98c49902a1
Evgenu <bogdanovevgenij87@gmail.com>
2025-12-22 18:20:53 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const allSpan = document.getElementsByClassName('population');
+
+const numberSpan = [...allSpan].map((item) => {
+  return Number(item.textContent.trim().replace(/,/g, ''));
+});
+
+const total = numberSpan.reduce((a, b) => {
+  return a + b;
+}, 0);
+
+const average = total / numberSpan.length;
+
+document.getElementsByClassName('total-population')[0].textContent =
+  total.toLocaleString('en-US');
+
+document.getElementsByClassName('average-population')[0].textContent =
+  Math.round(average.toLocaleString('en-US'));
