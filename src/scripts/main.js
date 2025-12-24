53e2a8bc66906fe83353f5cae36f75e7c5df3374
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2025-12-24 16:46:28 +0100

    solution for task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const spanList = document.querySelectorAll('.population');
+const populationList = [...spanList].map((span) =>
+  Number(span.textContent.replace(/,/g, '')),
+);
+
+const sumPopulation = populationList.reduce((total, value) => total + value, 0);
+
+const averagePopulation = Math.floor(sumPopulation / populationList.length);
+
+document.querySelector('.total-population').textContent =
+  sumPopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  averagePopulation.toLocaleString('en-US');
