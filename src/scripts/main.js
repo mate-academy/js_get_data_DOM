97bdcc3085ba3e2f1f6234c66e71e00ef6320b13
Dmytro Krylov <dimononk@gmail.com>
2026-01-07 10:30:10 +0200

    complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const allPopulations = document.querySelectorAll('.population');
+let totalPopulation = 0;
+
+allPopulations.forEach((element) => {
+  const population = +element.textContent.split(',').join('');
+
+  if (!isNaN(population)) {
+    totalPopulation += population;
+  }
+});
+
+const averagePopulation = Math.round(totalPopulation / allPopulations.length);
+
+const totalEl = document.querySelector('.total-population');
+const averageEl = document.querySelector('.average-population');
+
+totalEl.textContent = totalPopulation.toLocaleString('en-US');
+averageEl.textContent = averagePopulation.toLocaleString('en-US');
