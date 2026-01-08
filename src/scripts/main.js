5d8b631676e9c26355cf06f309ef36bd549c91b2
DevT <dev.tymofiichuk@gmail.com>
2026-01-08 13:23:01 +0200

    Top 10 largest countries

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const allPopulations = [...document.querySelectorAll('.population')];
+let total = 0;
+let count = 0;
+
+allPopulations.forEach((el) => {
+  total += +el.textContent.replace(/\D/g, '');
+  count++;
+});
+
+document.querySelector('.total-population').innerHTML = total.toLocaleString();
+
+document.querySelector('.average-population').innerHTML = Math.round(
+  total / count,
+).toLocaleString();
