41ed3b324b17425e3710d8c23165f478b54d7152
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2026-01-10 20:27:11 +0200

    done Top 10 largest countries task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+let total = 0;
+
+for (const item of populations) {
+  const cleanedText = item.textContent.replaceAll(',', '');
+  const num = Number(cleanedText);
+
+  if (!isNaN(num)) {
+    total += num;
+  }
+}
+
+const average = Math.round(total / populations.length);
+
+totalPopulation.textContent = total.toLocaleString('en-US');
+averagePopulation.textContent = average.toLocaleString('en-US');
