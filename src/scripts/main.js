2e22c22906464558a9af67651b89ccb43938c574
Andriy <andriit35@gmail.com>
2026-01-22 10:12:30 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write your code here
+const populationArray = document.querySelectorAll('.population');
+let total = 0;
+let count = 0;
+
+populationArray.forEach((element) => {
+  const population = parseInt(element.textContent.replace(/,/g, ''), 10);
+
+  total += population;
+  count++;
+});
+
+const average = Math.round(total / count);
+
+document.querySelector('.total-population').textContent =
+  toCurrectFormat(total);
+
+document.querySelector('.average-population').textContent =
+  toCurrectFormat(average);
+
+function toCurrectFormat(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
