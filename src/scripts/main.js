265a6d1e4800522e8f2f99c77807e3f019ea26c7
Andriy <andriit35@gmail.com>
2026-01-22 10:29:16 +0200

    solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,20 +6,20 @@ let total = 0;
 let count = 0;
 
 populationArray.forEach((element) => {
-  const population = parseInt(element.textContent.replace(/,/g, ''), 10);
+  const population = parseInt(element.textContent.replace(/,/g, ''), 10) || 0;
 
   total += population;
   count++;
 });
 
-const average = Math.round(total / count);
+const average = Math.round(total / count) || 0;
 
 document.querySelector('.total-population').textContent =
-  toCurrectFormat(total);
+  toCorrectFormat(total);
 
 document.querySelector('.average-population').textContent =
-  toCurrectFormat(average);
+  toCorrectFormat(average);
 
-function toCurrectFormat(number) {
+function toCorrectFormat(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
