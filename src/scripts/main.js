cc0da75a3e0a5b75e7d82e64f7bf9adbe7d4c532
Taras Borshch <tarasborshch4@gmail.com>
2026-01-30 13:11:07 +0200

    Add DOM functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+const populationNodes = document.querySelectorAll('.population');
+
+const populations = Array.from(populationNodes).map((node) => {
+  const value = node.textContent.replace(/,/g, '');
+  return parseInt(value, 10);
+});
+
+const total = populations.reduce((total, value) => total + value, 0);
+const average = Math.round(total / populations.length);
+
+totalElement.textContent = total.toLocaleString();
+averageElement.textContent = average.toLocaleString();
