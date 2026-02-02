92b287585c1876c3963bb04bd79d75a670417c17
freelinex <freelinex7@gmail.com>
2026-02-02 23:04:01 +0200

    Task Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const populations = [...document.querySelectorAll('.population')];
+const maxPopulation = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+const numbers = populations.map((el) => {
+  const text = el.textContent;
+
+  return Number(text.replace(/\D/g, ''));
+});
+
+const maxPopulationNumber = numbers.reduce((acc, cur) => acc + cur, 0);
+
+maxPopulation.textContent = maxPopulationNumber.toLocaleString('en-US');
+
+const averagePopulationNum = Math.round(maxPopulationNumber / numbers.length);
+
+average.textContent = averagePopulationNum.toLocaleString('en-US');
