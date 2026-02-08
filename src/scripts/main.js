4c0664b0f439a32a33297380a40d77b02fc17340
Yevhen Serdiuk <yevhen.srdk@gmail.com>
2026-02-08 18:35:52 +0200

    Added number check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,17 +2,23 @@
 
 const population = document.querySelectorAll('.population');
 let totalPopulationCount = 0;
+let isNumberCount = 0;
 
 for (const item of population) {
-  totalPopulationCount += Number(item.textContent.replace(/,/g, ''));
+  const value = Number(item.textContent.replace(/,/g, ''));
+
+  if (Number.isFinite(value)) {
+    totalPopulationCount += value;
+    isNumberCount++;
+  }
 }
 
 const averagePopulationResult = Math.round(
-  totalPopulationCount / population.length,
+  totalPopulationCount / isNumberCount,
 );
 
 const totalPopulation = document.querySelector('.total-population');
-const avaragePopulation = document.querySelector('.average-population');
+const averagePopulation = document.querySelector('.average-population');
 
 totalPopulation.textContent = totalPopulationCount.toLocaleString('en-US');
-avaragePopulation.textContent = averagePopulationResult.toLocaleString('en-US');
+averagePopulation.textContent = averagePopulationResult.toLocaleString('en-US');
