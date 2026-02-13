ea12127670c2a6f78ab3e33c75d5a4e496f051f7
Yaroslav Grom <gromyarik3@gmail.com>
2026-02-13 21:27:21 +0100

    solutions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+// 1. Отримуємо всі елементи
+const populationSpans = document.querySelectorAll('.population');
+
+const populations = Array.from(populationSpans).map((span) => {
+  const text = span.innerText.replace(/,/g, '');
+
+  return Number(text);
+});
+
+const total = populations.reduce((sum, val) => sum + val, 0);
+const average = total / populations.length;
+
+document.querySelector('.total-population').innerText = total.toLocaleString();
+
+document.querySelector('.average-population').innerText =
+  Math.round(average).toLocaleString();
