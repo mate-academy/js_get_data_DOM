c6048b8113ecb921c1d5c2a1ca34ca60e1daba62
naviailpach <ivan.chaplya13@gmail.com>
2026-02-15 21:08:43 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+const numbers = [];
+
+spans.forEach((span) => {
+  const clean = span.textContent.replace(/\D/g, '');
+
+  if (clean !== '') {
+    numbers.push(+clean);
+  }
+});
+
+const total = numbers.reduce((sum, num) => sum + num, 0);
+const average = numbers.length > 0 ? total / numbers.length : 0;
+const formattedTotal = total.toLocaleString('en-US').replace(/,/g, ' ');
+const formattedAverage = Math.round(average)
+  .toLocaleString('en-US')
+  .replace(/,/g, ' ');
+
+const totalSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+
+totalSpan.textContent = formattedTotal;
+averageSpan.textContent = formattedAverage;
