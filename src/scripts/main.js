38dec4fa53914cca1bc565c86420f76698e89a42
TiFAu <turchin.anton2@gmail.com>
2026-02-20 01:20:58 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const spanPopulation = [...document.querySelectorAll('.population')];
+const averagePopulation = document.querySelector('.average-population');
+const totalPopulation = document.querySelector('.total-population');
+const total = spanPopulation.reduce(
+  (sum, n) => sum + Number(n.innerHTML.split(',').join('')),
+  0,
+);
+const average = Math.round(total / spanPopulation.length);
+
+function convertNumbers(number) {
+  const resultNumber = [];
+  const numberToString = String(number);
+
+  for (let i = numberToString.length; i > 0; i -= 3) {
+    if (i < 3) {
+      resultNumber.unshift(numberToString.slice(0, i));
+
+      break;
+    }
+    resultNumber.unshift(numberToString.slice(i - 3, i));
+  }
+
+  return resultNumber.join(',');
+}
+
+averagePopulation.outerHTML = convertNumbers(average);
+totalPopulation.outerHTML = convertNumbers(total);
