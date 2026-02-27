835fdc3844ff6ff2b0bb85bc76ff470ba13425d8
Dmytro Halieba <dmytro.halyeba@gmail.com>
2026-02-27 22:51:13 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable max-len */
 'use strict';
 
-// write your code here
+let totalPopulation = 0;
+
+const population = document.getElementsByClassName('population');
+
+for (let i = 0; i < population.length; i++) {
+  const populationValue = parseInt(population[i].textContent.split(',').join(''));
+
+  totalPopulation += populationValue;
+}
+
+document.getElementsByClassName('total-population')[0].textContent =
+  totalPopulation.toLocaleString();
+
+document.getElementsByClassName('average-population')[0].textContent = (
+  totalPopulation / population.length
+).toLocaleString();
