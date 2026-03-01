7b19cda13fcb7917d521f9e37f37a770fccfe7ca
LiliiaVol <volanska.lilya@gmail.com>
2026-03-01 02:28:10 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,7 +9,7 @@ const getTotalSum = (expectedResult) => {
   population.forEach((populationContent) => {
     let num = '';
 
-    for (let i = 0; populationContent.textContent.length >= i; i++) {
+    for (let i = 0; populationContent.textContent.length > i; i++) {
       if (
         Number(populationContent.textContent[i]) ||
         Number(populationContent.textContent[i]) === 0
@@ -21,40 +21,15 @@ const getTotalSum = (expectedResult) => {
     total += Number(num);
   });
 
-  const arrayOfNum = total.toString().split('');
-
-  for (let i = arrayOfNum.length - 1; i >= 0; i--) {
-    if (i % 3 === 0 && i !== arrayOfNum.length - 1) {
-      result += ',';
-      result += arrayOfNum[i];
-    } else {
-      result += arrayOfNum[i];
-    }
-  }
-
-  result = result.split('').reverse().join('');
+  result = total.toLocaleString();
 
   if (expectedResult === 'total') {
     return result;
   }
 
-  result = result.split(',').join('');
-  result = Math.round(Number(result) / population.length);
+  result = Math.round(Number(total) / population.length);
 
-  const arrayOfNumAvarage = result.toString().split('');
-
-  result = '';
-
-  for (let i = arrayOfNumAvarage.length - 1; i >= 0; i--) {
-    if (i === 5 || i === 2) {
-      result += ',';
-      result += arrayOfNumAvarage[i];
-    } else {
-      result += arrayOfNumAvarage[i];
-    }
-  }
-
-  result = result.split('').reverse().join('');
+  result = result.toLocaleString();
 
   return result;
 };
