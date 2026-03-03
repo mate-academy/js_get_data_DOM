b9fd419fd8f2228be82a43cf82b07223602a9a7b
Pavlo Polyvoda <pvlpeapod@gmail.com>
2026-03-03 12:26:14 +0200

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write your code here
+const populationList = document.querySelectorAll('.population');
+const averagePopulation = document.querySelector('.average-population');
+const totalPopulation = document.querySelector('.total-population');
+
+const populationArray = [];
+
+for (const populationItem of populationList) {
+  const populationValue = populationItem.textContent.split(',').join('');
+
+  populationArray.push(populationValue);
+}
+
+const sumPopNum = populationArray.reduce((sum, population) => {
+  return sum + Number(population);
+}, 0);
+
+const averagePopNum = (sumPopNum / populationArray.length).toFixed(2);
+
+function strFormatNum(number) {
+  const numberItems = [];
+
+  let currentNumber = number;
+
+
+  while (currentNumber / 1000 > 0) {
+    if (currentNumber !== parseInt(currentNumber)) {
+      numberItems.unshift((currentNumber % 1000).toFixed(2));
+    } else {
+      numberItems.unshift(currentNumber % 1000);
+    }
+
+    currentNumber = parseInt(currentNumber / 1000);
+  }
+
+  return numberItems.join(',');
+}
+
+totalPopulation.textContent = strFormatNum(sumPopNum);
+
+averagePopulation.textContent = strFormatNum(averagePopNum);
