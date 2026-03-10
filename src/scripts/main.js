ef06dbbe7361b93d5736610e123312e1593d8a90
Oksana Kyreieva <senyakir@gmail.com>
2026-03-10 17:50:05 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const populationNumbers = [];
+
+population.forEach((item) => {
+  let text = item.textContent;
+
+  text = text.replaceAll(',', '');
+
+  const number = Number(text);
+
+  if (isNaN(number)) {
+    return;
+  }
+
+  populationNumbers.push(number);
+});
+
+const totalPopulation = populationNumbers.reduce((acc, curr) => acc + curr, 0);
+const averagePopulation = totalPopulation / populationNumbers.length;
+
+const totalFormatted = totalPopulation.toLocaleString('en-US');
+const averageFormatted = Math.round(averagePopulation).toLocaleString('en-US');
+
+document.querySelector('.total-population').textContent = totalFormatted;
+document.querySelector('.average-population').textContent = averageFormatted;
