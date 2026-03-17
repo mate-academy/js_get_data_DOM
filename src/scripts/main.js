f0b4e0f532f8b928ddf5c0e4d0dbd554371f495e
Katrysya <katrysyar@gmail.com>
2026-03-17 20:18:40 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const extractedText = document.querySelectorAll('.population');
+const allPopulation = [];
+
+for (let i = 0; i < extractedText.length; i++) {
+  const string = extractedText[i].textContent;
+
+  const editedStr = string.replaceAll(',', '');
+
+  allPopulation.push(Number(editedStr));
+}
+
+const total = allPopulation.reduce((el1, el2) => el1 + el2);
+const avarage = total / allPopulation.length;
+
+const newAvarage = document.querySelector('.average-population');
+
+newAvarage.textContent = avarage.toLocaleString('en-US');
+
+const newTotal = document.querySelector('.total-population');
+
+newTotal.textContent = total.toLocaleString('en-US');
