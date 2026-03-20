add47016146d15ce5dfee08c0a0c5bc939c3db80
Yurii Leniv <supusman32@gmail.com>
2026-03-20 12:53:24 +0200

    fdgdfgf

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const inside = document.querySelector('.container');
+const t = inside.querySelector('.list');
+const list = t.querySelectorAll('li span');
+const p = Array.from(list);
+
+const totalSum = p.reduce((acc, curr) => {
+  const value = Number(curr.textContent);
+
+  return acc + value;
+}, 0);
+
+const average = totalSum / list.length;
+
+const l = inside.querySelector('.total');
+const u = inside.querySelector('.average');
+
+l.textContent = totalSum;
+u.textContent = average;
