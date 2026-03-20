acc54a1ae70ed0717c3720057d84176435e807e0
Yurii Leniv <supusman32@gmail.com>
2026-03-20 17:48:18 +0200

    sdfdhjutjhd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,23 @@
 'use strict';
 
-const inside = document.querySelector('.container');
-const t = inside.querySelector('.list');
-const list = t.querySelectorAll('li span');
+const list = document.querySelectorAll(
+  '.container .list .list__item .population',
+);
 const p = Array.from(list);
 
-const totalSum = p.reduce((acc, curr) => {
-  const value = Number(curr.textContent);
+let totalSum = p.reduce((acc, curr) => {
+  const value = Number(curr.textContent.split(',').join(''));
 
   return acc + value;
 }, 0);
 
-const average = totalSum / list.length;
+let average = totalSum / list.length;
 
-const l = inside.querySelector('.total');
-const u = inside.querySelector('.average');
+totalSum = totalSum.toLocaleString('en-US');
+average = average.toLocaleString('en-US');
 
-l.textContent = totalSum;
-u.textContent = average;
+const t = document.querySelector('.container .total-population');
+const k = document.querySelector('.container .average-population');
+
+t.textContent = totalSum;
+k.textContent = average;
