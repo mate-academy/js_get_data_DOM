7011566090d70e374aaaad53f133c266fa0acb13
Anastasiia Riadnova <a.ryadnova01@gmail.com>
2026-03-23 19:46:38 +0100

    add task solution v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+
+const nums = [];
+
+for (const span of spans) {
+  const raw = span.textContent.trim();
+  const cleaned = raw.replace(/\s|,/g, '');
+  const num = Number(cleaned);
+
+  if (Number.isFinite(num)) {
+    nums.push(num);
+  }
+}
+
+const total = nums.reduce((acc, v) => acc + v, 0);
+
+let average = 0;
+
+if (nums.length > 0) {
+  average = total / nums.length;
+}
+
+const formattedTotal = Math.round(total).toLocaleString('en-US');
+const formattedAverage = Math.round(average).toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent = formattedAverage;
+document.querySelector('.total-population').textContent = formattedTotal;
