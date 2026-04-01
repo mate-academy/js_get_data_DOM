fbf3f8c440ef2ca1d7ab7be04924cebf34978fa4
Banderos14 <anton.shyshenko@gmail.com>
2026-04-01 13:27:08 +0200

    feat: task_solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const span = document.querySelectorAll('.population');
+
+const result = [];
+
+span.forEach((item) => {
+  result.push(Number(item.textContent.replaceAll(',', '')));
+});
+
+let total = 0;
+
+result.forEach((num) => {
+  total += num;
+});
+
+const average = total / result.length;
+
+const averagePopulation = document.querySelector('.average-population');
+
+averagePopulation.textContent = Math.round(average).toLocaleString();
+
+const totalPopulation = document.querySelector('.total-population');
+
+totalPopulation.textContent = total.toLocaleString();
