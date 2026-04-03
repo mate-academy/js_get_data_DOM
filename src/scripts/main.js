d986baca906b87adee637df1ecbf2cdaa14a8a8b
Georgiy Veshchev <ghora2008@gmail.com>
2026-04-03 15:26:38 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const populationSpans = document.querySelectorAll('.population');
+const totalSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+
+let total = 0;
+const count = populationSpans.length;
+
+populationSpans.forEach((span) => {
+  const text = span.textContent;
+  const cleanText = text.replace(/,/g, '');
+  const num = Number(cleanText);
+
+  if (!isNaN(num)) {
+    total += num;
+  }
+});
+
+const average = total / count;
+
+totalSpan.textContent = total.toLocaleString('en-US');
+averageSpan.textContent = Math.round(average).toLocaleString('en-US');
