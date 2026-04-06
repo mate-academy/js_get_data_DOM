230c43cb9c6150ba3f3c940a42d936297ce3be5a
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-04-06 13:39:22 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const values = document.getElementsByClassName('population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+function CalculateTotalPopulation() {
+  // eslint-disable-next-line no-shadow
+  let total = 0;
+
+  for (const population of values) {
+    const textValue = population.textContent.replaceAll(',', '');
+    const value = Number(textValue);
+
+    total += value;
+  }
+
+  return total;
+}
+
+function calculateAveragePopulation(totalValue, populations) {
+  const sourceCount = populations.length;
+
+  return totalValue / sourceCount;
+}
+
+const total = CalculateTotalPopulation();
+const average = calculateAveragePopulation(total, values);
+
+totalPopulation.textContent = total.toLocaleString('en-US');
+
+averagePopulation.textContent = average.toLocaleString('en-US');
