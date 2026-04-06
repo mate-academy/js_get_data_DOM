06acf900087c3f1cd3abc2d14f2aeeb5f8586725
Tymur <timurradkevic84@mail.com>
2026-04-06 12:48:21 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,9 +7,7 @@ for (const el of populationArr) {
   if (isNaN(el.textContent.replaceAll(',', ''))) {
     throw new Error('Population element not a number');
   }
-}
 
-for (const el of populationArr) {
   sumPopul += Number(el.textContent.replaceAll(',', ''));
 }
 
@@ -18,5 +16,5 @@ const avgPopul = Math.round(sumPopul / populationArr.length);
 const total = document.querySelector('span.total-population');
 const average = document.querySelector('span.average-population');
 
-total.replaceWith(sumPopul.toLocaleString());
-average.replaceWith(avgPopul.toLocaleString());
+total.textContent = sumPopul.toLocaleString();
+average.textContent = avgPopul.toLocaleString();
