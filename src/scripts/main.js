b03560d6325c01b3c2eaa694ab21457eee48d24e
Tymur <timurradkevic84@mail.com>
2026-04-06 12:27:59 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const populationArr = [...document.querySelectorAll('span.population')];
+let sumPopul = 0;
+
+for (const el of populationArr) {
+  if (isNaN(el.textContent.replaceAll(',', ''))) {
+    throw new Error('Population element not a number');
+  }
+}
+
+for (const el of populationArr) {
+  sumPopul += Number(el.textContent.replaceAll(',', ''));
+}
+
+const avgPopul = Math.round(sumPopul / populationArr.length);
+
+const total = document.querySelector('span.total-population');
+const average = document.querySelector('span.average-population');
+
+total.replaceWith(sumPopul.toLocaleString());
+average.replaceWith(avgPopul.toLocaleString());
