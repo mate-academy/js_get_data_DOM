35affce7030fd1489393b7a9c708804c5a41a135
Diana Komarchuk <dianakocmarchuk2508@gmail.com>
2026-04-07 14:43:20 +0300

    version 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const nodes = document.querySelectorAll('.population');
+const texts = Array.from(nodes).map((el) => el.textContent.trim());
+const cleaned = texts.map((el) => el.replace(/,/g, ''));
+
+const nums = cleaned.map((s) => Number(s)).filter((n) => !Number.isNaN(n));
+
+if (nums.length > 0) {
+  const total = nums.reduce((sum, a) => sum + a, 0);
+  const average = total / nums.length;
+
+  const formatter = new Intl.NumberFormat('en-US');
+
+  const formattedTotal = formatter.format(total);
+  const formattedAverage = formatter.format(Math.round(average));
+
+  const avgPl = document.querySelector('.average-population');
+  const ttlPl = document.querySelector('.total-population');
+
+  avgPl.textContent = formattedAverage;
+  ttlPl.textContent = formattedTotal;
+}
