046529f0737c3b9b0eea1ae0cc2bd4724c305ac3
yevkrasnenkov <krasnenkov.evgen@gmail.com>
2026-04-07 18:01:12 -0400

    calc ave / total

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const spaned = document.body.querySelector('span');
+const pop = spaned.getElementsByClassName('population');
+const clazz = document.getElementsByClassName('population');
+
+let total = 0;
+let i = 0;
+Array.from(clazz).forEach(element => {
+    const text = element.innerText;
+    const num = Number(text.replace(/,/g, ''));
+    total += num;
+    i++;
+
+});
+const average = Math.floor(total / i);
+
+const totalElement = document.querySelector('.total-population');
+totalElement.innerText = total.toLocaleString();
+const averageElement = document.querySelector('.average-population');
+averageElement.innerText = average.toLocaleString();
