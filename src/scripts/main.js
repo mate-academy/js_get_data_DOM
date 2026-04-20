89a83ca57723d73837cdd25552236a3bfe39c942
linska <linskatanya@gmail.com>
2026-04-20 17:26:00 +0300

    feat: calculate average and total value-based to parsed numbers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const elementList = document.querySelectorAll('.list .population');
+
+let count = 0;
+
+const total = [...elementList].reduce((acc, item) => {
+  const text = item.textContent.replace(/,/g, '');
+  const num = Number(text);
+
+  if (Number.isNaN(num)) {
+    return acc;
+  }
+  count++;
+
+  return acc + num;
+}, 0);
+const average = total / (count || 1);
+
+const formattedTotal = total.toLocaleString('en-US');
+const formattedAverage = Math.round(average).toLocaleString('en-US');
+
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+
+totalElement.innerHTML = formattedTotal;
+averageElement.innerHTML = formattedAverage;
