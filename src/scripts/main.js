2ef0f84fa1b1ce1116a9bb1a68140e86261c735f
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-04-28 13:55:29 +0300

    finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const populationOfAllCountries = Array.from(
+  document.querySelectorAll('.population'),
+);
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const getTotalPopulation = populationOfAllCountries.reduce(
+  (acc, population) => {
+    const convertText = Number(population.textContent.split(',').join(''));
+
+    return acc + convertText;
+  },
+  0,
+);
+
+const getAverage = Math.ceil(
+  getTotalPopulation / populationOfAllCountries.length,
+);
+
+totalPopulation.textContent = getTotalPopulation.toLocaleString('en-US');
+averagePopulation.textContent = getAverage.toLocaleString('en-US');
