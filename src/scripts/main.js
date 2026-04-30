8f67b891a539838b6587d2ac2f4fb8ce032ae7b1
Олена Чернишова <elena.chernyshova01@gmail.com>
2026-04-30 15:58:55 +0300

    Fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write your code here
+const populationElements = document.querySelectorAll('.population');
+
+const populations = Array.from(populationElements, (el) => {
+  const number = Number(el.textContent.replace(/,/g, ''));
+
+  return number;
+});
+
+const total = populations.reduce((sum, num) => sum + num, 0);
+
+const average = total / populations.length;
+
+const formatNumber = (num) => num.toLocaleString('en-US');
+
+const totalEl = document.querySelector('.total-population');
+
+const averageEl = document.querySelector('.average-population');
+
+totalEl.textContent = formatNumber(total);
+averageEl.textContent = formatNumber(Math.round(average));
