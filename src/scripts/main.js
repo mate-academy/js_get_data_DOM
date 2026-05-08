26e6187f56bb38e18dace1375e387baf4025b90f
Damian Kurzydlo <duchozywia2024@gmail.com>
2026-05-08 15:31:08 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write your code here
+
+const countriesPopulation = document.querySelectorAll('.population');
+const totalPopulationEl = document.querySelector('.total-population');
+const averagePopulationEl = document.querySelector('.average-population');
+
+let totalPopulation = 0;
+
+countriesPopulation.forEach((countryPopulation) => {
+  totalPopulation += Number(countryPopulation.innerText.replaceAll(',', ''));
+});
+
+const averagePopulation = totalPopulation / countriesPopulation.length;
+
+totalPopulationEl.innerText = totalPopulation.toLocaleString();
+averagePopulationEl.innerText = averagePopulation.toLocaleString('en-GB', {
+  maximumFractionDigits: 0,
+});
