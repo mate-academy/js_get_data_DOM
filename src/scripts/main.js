1565f60119a9d6f8f8e37ba05ef62dfefce04c44
Damian Kurzydlo <duchozywia2024@gmail.com>
2026-05-09 09:22:41 +0100

    add validation for population values

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,12 +9,19 @@ const averagePopulationEl = document.querySelector('.average-population');
 let totalPopulation = 0;
 
 countriesPopulation.forEach((countryPopulation) => {
-  totalPopulation += Number(countryPopulation.innerText.replaceAll(',', ''));
+  const parsedPopulation = Number(
+    countryPopulation.innerText.replaceAll(',', ''),
+  );
+
+  if (!isNaN(parsedPopulation)) {
+    totalPopulation += parsedPopulation;
+  }
 });
 
 const averagePopulation = totalPopulation / countriesPopulation.length;
 
 totalPopulationEl.innerText = totalPopulation.toLocaleString();
+
 averagePopulationEl.innerText = averagePopulation.toLocaleString('en-GB', {
   maximumFractionDigits: 0,
 });
