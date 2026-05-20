df9b3e15491512aafedeabc069bcb1dde57b28f6
sqw <bogdanfust@gmail.com>
2026-05-20 21:12:31 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
-'use strict';
+const populations = document.querySelectorAll('.population');
 
-// write your code here
+let total = 0;
+
+populations.forEach((span) => {
+  const number = Number(span.textContent.replaceAll(',', ''));
+
+  total += number;
+});
+
+const average = total / populations.length;
+
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+
+totalElement.textContent = total;
+averageElement.textContent = average;
