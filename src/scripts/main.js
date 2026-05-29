43259f6a09ddd6f87463a839e064140338625e71
Yaroslav Volkov <olkov1979164@gmail.com>
2026-05-29 15:42:07 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+
+let total = 0;
+const count = populationElements.length;
+
+populationElements.forEach((el) => {
+  const clearNumber = Number(el.textContent.replace(/,/g, ''));
+
+  if (!isNaN(clearNumber)) {
+    total += clearNumber;
+  }
+});
+
+const average = total / count;
+
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+
+totalElement.textContent = total.toLocaleString();
+averageElement.textContent = average.toLocaleString();
