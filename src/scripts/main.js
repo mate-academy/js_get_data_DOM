29773e088240c4e4efcb4dedab480e2523a7a94b
Mykyta <f1xggofficial@gmail.com>
2026-06-01 15:51:17 +0200

    solved tasl

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+
+const populationElements = document.querySelectorAll(".population");
+
+const populations = Array.from(populationElements).map(el =>
+  Number(el.textContent.replace(/,/g, ''))
+);
+
+const total = populations.reduce((sum, n) => sum +n,0);
+const average = total / populations.length;
+
+document.querySelector(".total-population").textContent = total.toLocaleString();
+document.querySelector(".average-population").textContent = Math.round(average).toLocaleString();
+
