965b6866f4a561129e567cd4b8a6c9f8d2a43496
Mykyta <f1xggofficial@gmail.com>
2026-06-01 16:00:56 +0200

    2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,11 @@
 
 const populationElements = document.querySelectorAll(".population");
 
-const populations = Array.from(populationElements).map(el =>
-  Number(el.textContent.replace(/,/g, ''))
-);
+const populations = Array.from(populationElements).map(el => {
+  const num = Number(el.textContent.replace(/,/g, ''));
+
+  return Number.isNaN(num) ? 0 : num;
+});
 
 const total = populations.reduce((sum, n) => sum +n,0);
 const average = total / populations.length;
