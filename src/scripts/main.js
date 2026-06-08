25d63422d97c77fa8534722114af6ff6b5abf8f0
interpunktus <240490702+interpunktus@users.noreply.github.com>
2026-06-08 21:30:03 +0300

    try1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const allFromSpanPopulation = [
+  ...document.querySelectorAll('span.population'),
+].map((span) => Number(span.textContent.replace(/,/g, '')));
+
+const total = allFromSpanPopulation.reduce((acc, num) => acc + num, 0);
+const average = Math.round(total / allFromSpanPopulation.length);
+
+function formatNumber(num) {
+  const str = String(num);
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (i > 0 && (str.length - i) % 3 === 0) {
+      result += ',';
+    }
+    result += str[i];
+  }
+
+  return result;
+}
+
+document.querySelector('span.total-population').textContent =
+  formatNumber(total);
+
+document.querySelector('span.average-population').textContent =
+  formatNumber(average);
