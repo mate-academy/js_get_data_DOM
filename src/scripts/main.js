3a56fa1d16763471719dde6b1617df7620b9c20c
Tendeeer <leifisisidj747@gmail.com>
2026-06-14 15:24:55 +0300

    add total and average

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+
+let total = 0;
+
+populationElements.forEach((element) => {
+  const text = element.textContent;
+  const number = Number(text.split(',').join(''));
+
+  total += number;
+});
+
+const average = total / populationElements.length;
+
+const formattedTotal = total.toLocaleString();
+const formattedAverage = average.toLocaleString();
+
+const totalElement = document.querySelector('.total-population');
+
+totalElement.textContent = formattedTotal;
+
+const averageElement = document.querySelector('.average-population');
+
+averageElement.textContent = formattedAverage;
