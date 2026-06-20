cc7aa09b0fbc4145470a6a9688bed344e8c1dc51
Vitalii <vtysiachnyk@gmail.com>
2026-06-20 22:21:48 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,21 +2,20 @@
 
 const allPopulationSpans = document.querySelectorAll('.population');
 let totalPopulation = 0;
+let validCount = 0;
 
 for (const spanEl of allPopulationSpans) {
   const parsedValue = spanEl.textContent;
+  const currentNumber = Number(parsedValue.split(',').join(''));
 
-  if (Number.isNaN(parsedValue)) {
-    const currentNumber = Number(parsedValue.split(',').join(''));
-
+  if (Number.isNaN(currentNumber)) {
     totalPopulation += currentNumber;
+    validCount++;
   }
 }
 
 const average =
-  allPopulationSpans.length > 0
-    ? totalPopulation / allPopulationSpans.length
-    : 0;
+  allPopulationSpans.length > 0 ? totalPopulation / validCount : 0;
 
 const formatter = new Intl.NumberFormat('en-US');
 const formattedAverage = formatter.format(average);
