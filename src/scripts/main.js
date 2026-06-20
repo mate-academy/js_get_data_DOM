e245234a9099217ce0a25c093aedb83d9ada8ec2
Vitalii <vtysiachnyk@gmail.com>
2026-06-20 22:06:06 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const allPopulationSpans = document.querySelectorAll('.population');
+let totalPopulation = 0;
+
+for (const spanEl of allPopulationSpans) {
+  const currentNumber = Number(spanEl.textContent.split(',').join(''));
+
+  totalPopulation += currentNumber;
+}
+
+const average =
+  allPopulationSpans.length > 0
+    ? totalPopulation / allPopulationSpans.length
+    : 0;
+
+const formatter = new Intl.NumberFormat('en-US');
+const formattedAverage = formatter.format(average);
+const formattedTotal = formatter.format(totalPopulation);
+
+const averageNode = document.querySelector('.average-population');
+const totalNode = document.querySelector('.total-population');
+
+averageNode.textContent = formattedAverage;
+totalNode.textContent = formattedTotal;
