4c3bd41edfccff2c4e17c53ad0b35acc0c578ebc
Stanislav Mosakov <ayfonsky@gmail.com>
2026-06-22 19:13:24 +0300

    completed the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const spanDataByPopulation = document.querySelectorAll('span.population');
+const populations = Array.from(spanDataByPopulation).map((span) => {
+  const str = span.textContent;
+  const withoutCommas = str.replace(/,/g, '');
+  const num = Number(withoutCommas);
+
+  return num;
+});
+
+const total = populations.reduce((acc, value) => acc + value, 0);
+const average = Math.floor(total / populations.length);
+
+const totalPopulation = document.querySelector('span.total-population');
+const averagePopulation = document.querySelector('span.average-population');
+
+totalPopulation.textContent = total.toLocaleString('en-US');
+averagePopulation.textContent = average.toLocaleString('en-US');
