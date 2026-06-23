faa5c333a47e60d0b1ae58571298b75ec129fe50
Olexander <parfenov4589@gmail.com>
2026-06-23 14:10:45 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const populate = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+let sum = 0;
+
+for (let i = 0; i < populate.length; i++) {
+  populate[i].textContent = populate[i].textContent.split(',').join('');
+  sum += parseInt(populate[i].textContent);
+}
+
+const avg = sum / populate.length;
+
+total.textContent = sum;
+average.textContent = avg;
