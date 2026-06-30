016ce4c437594d8abc703261973359da14b04bab
Skoulskiy <yaskevychilya@gmail.com>
2026-06-30 11:36:36 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write your code here
+const populationElement = [...document.querySelectorAll('.population')];
+const totalElement = document.querySelector('.total-population');
+const avarageElement = document.querySelector('.average-population');
+
+const populations = populationElement.map((el) => {
+  return Number(el.textContent.replace(/,/g, ''));
+});
+
+const total = populations.reduce((acc, value) => acc + value, 0);
+
+totalElement.textContent = `${total.toLocaleString('en-US')}`;
+
+const avarage = Math.floor(total / populationElement.length);
+
+avarageElement.textContent = `${avarage.toLocaleString('en-US')}`;
