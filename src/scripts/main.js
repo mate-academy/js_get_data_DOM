d41b000a04e34e428ab50977bc0cf85c29ccfeda
Oleksandr Stepaniuk <o.step4niuk@gmail.com>
2026-07-09 00:28:29 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+
+let totalPopulation = 0;
+
+populations.forEach((population) => {
+  const populationNumber = Number(population.textContent.replaceAll(',', ''));
+
+  totalPopulation += populationNumber;
+});
+
+const averagePopulation = totalPopulation / populations.length;
+
+const averagePopulationElement = document.querySelector('.average-population');
+
+averagePopulationElement.textContent = averagePopulation.toLocaleString();
+
+const totalPopulationElement = document.querySelector('.total-population');
+
+totalPopulationElement.textContent = totalPopulation.toLocaleString();
