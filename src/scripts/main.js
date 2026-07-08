319b8c57e6f9def4fe1d93e086bcec49fc20267d
Nik200309 <nikitapidlypnij@gmail.com>
2026-07-08 21:43:51 +0300

    Added solution, second attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,18 @@
-
 'use strict';
 
 // write your code here
 const populationSpans = document.querySelectorAll('.population');
 
-const populationNumbers = [...populationSpans].map((span) => {
-  const text = span.textContent;
-  const cleanedText = text.replace(/,/g, '');
+const populationNumbers = [...populationSpans]
+  .map((span) => {
+    const text = span.textContent;
+    const cleanedText = text.replace(/,/g, '');
 
-  if (!isNaN(cleanedText) && cleanedText !== '') {
-    return Number(cleanedText);
-  }
-});
+    if (!isNaN(cleanedText) && cleanedText !== '') {
+      return Number(cleanedText);
+    }
+  })
+  .filter(Number.isFinite);
 
 const total = populationNumbers.reduce((sum, num) => sum + num, 0);
 const average = Math.round(total / populationNumbers.length);
