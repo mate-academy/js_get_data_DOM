09add579333c4681c5cbc2edf1f8c43be6f1adcd
reddev111 <300704yb@gmail.com>
2026-07-10 15:56:15 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+const populations = document.querySelectorAll('.population');
+
+const popList = [...populations].map((item) => {
+  return Number(item.textContent.split(',').join(''));
+});
+
+const total = popList.reduce((tot, num) => tot + num, 0);
+const average = Math.round(total / popList.length);
+
+const totalElement = document.querySelector('.total-population');
+
+totalElement.textContent = total.toLocaleString();
+
+const averageElement = document.querySelector('.average-population');
+
+averageElement.textContent = average.toLocaleString();
