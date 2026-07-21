762590dd37ca732a3fc4c92ce280a7a1def816bf
Ol. <holmate@dontsp.am>
2026-07-22 00:13:14 +0300

    mirror fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,13 @@
 const populations = Array.from(document.querySelectorAll('.population'));
 const total = document.querySelector('.total-population');
 const average = document.querySelector('.average-population');
-const numbers = populations.map((el) => {
-  return +el.textContent.replaceAll(',', '');
-});
+const numbers = populations
+  .map((el) => +el.textContent.replaceAll(',', ''))
+  .filter((num) => !Number.isNaN(num));
 const totalNumber = numbers.reduce((acc, num) => acc + num, 0);
 
 total.textContent = totalNumber.toLocaleString('en-US');
 
 average.textContent = Math.round(totalNumber / numbers.length).toLocaleString(
-  'eu-US',
+  'en-US',
 );
