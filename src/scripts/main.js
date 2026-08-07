49e95888d154c3725cd60f1b7d399ec433815a85
Sonya <cuka9955@gmail.com>
2026-08-07 09:50:31 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const elements = document.querySelectorAll('.population');
+
+const numbers = Array.from(elements).map((element) => {
+  const cleanString = element.textContent.replace(/,/g, '');
+
+  const number = Number(cleanString);
+
+  return isNaN(number) ? 0 : number;
+});
+
+const total = numbers.reduce((tot, curr) => curr + tot, 0);
+const average = total / numbers.length;
+
+const formatter = new Intl.NumberFormat('en-US');
+const formatterTotal = formatter.format(total);
+const formatterAverage = formatter.format(average);
+
+const averageSpan = document.querySelector('.average-population');
+const totalSpan = document.querySelector('.total-population');
+
+if (averageSpan) {
+  averageSpan.textContent = formatterAverage;
+}
+
+if (totalSpan) {
+  totalSpan.textContent = formatterTotal;
+}
