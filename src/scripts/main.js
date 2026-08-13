87728e19571e6432ab803b73a3cfe0ba1e4bd6ef
Natalia Onufrienko <natalia_anamalia@icloud.com>
2026-08-13 10:10:47 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write your code here
+
+const populationElements = document.querySelectorAll('.population');
+const averageElement = document.querySelector('.average-population');
+const totalElement = document.querySelector('.total-population');
+
+const populations = [];
+
+for (const element of populationElements) {
+  const value = Number(element.textContent.replace(/,/g, ''));
+
+  if (!Number.isNaN(value)) {
+    populations.push(value);
+  }
+}
+
+let total = 0;
+
+for (const population of populations) {
+  total += population;
+}
+
+const average = total / populations.length;
+
+averageElement.textContent = Math.round(average).toLocaleString();
+totalElement.textContent = total.toLocaleString();
