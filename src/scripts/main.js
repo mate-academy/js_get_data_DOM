37568d24f7b0f73d2e4a2c1d1da25fae3c29404d
Artroy666 <xandertroy666@gmail.com>
2026-08-13 15:56:54 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populations = Array.from(
+  document.querySelectorAll('.population'),
+  (span) => Number(span.textContent.replace(/,/g, '').trim()),
+).filter(Number.isFinite);
+
+const totalPopulation = populations.reduce(
+  (sum, population) => sum + population,
+  0,
+);
+const averagePopulation = totalPopulation / populations.length;
+
+document.querySelector('.average-population').textContent =
+  averagePopulation.toLocaleString('en-US');
+
+document.querySelector('.total-population').textContent =
+  totalPopulation.toLocaleString('en-US');
