7456cdbd4c34b2cef698f43af0359219ccb93ac2
polina3107 <polinadavidenko3107@gmail.com>
2026-08-14 22:25:16 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const textData = [...document.querySelectorAll('span')];
+
+textData.length = textData.length - 2;
+
+const numbers = textData.map((num) => {
+  return +num.innerText.replaceAll(',', '');
+});
+
+const total = numbers.reduce((prev, num) => prev + num, 0);
+const totalR = addComa(total);
+
+const average = Math.round(total / numbers.length);
+const averageR = addComa(average);
+
+const totalSpan = document.querySelector('.total-population');
+const averageSpan = document.querySelector('.average-population');
+
+totalSpan.textContent = totalR;
+averageSpan.textContent = averageR;
+
+function addComa(number) {
+  const arr = number.toString().split('');
+
+  for (let i = arr.length - 4; i >= 0; i -= 3) {
+    arr[i] = arr[i] + ',';
+  }
+
+  return arr.join('');
+}
