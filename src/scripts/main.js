49d0336ea2d698d4d56b37660917ac7f509076a7
Vitalii <vitaliy.doronin4@gmail.com>
2026-08-18 13:00:39 +0300

    Add a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const numberOfPop = [...document.body.querySelectorAll('.population')].map(
+  (item) => +item.textContent.split(',').join(''),
+);
+const total = numberOfPop.reduce((sum, item) => sum + item, 0);
+const average = Math.round(total / numberOfPop.length);
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString('en-US');
