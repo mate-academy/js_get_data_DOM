5a9b03c65f0693074b26e00691567ba82611a437
Anton Floreskul <floreskula830@gmail.com>
2026-08-19 12:18:55 +0300

    add wront values filter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,11 +8,13 @@ const itemsElement = listElement.querySelectorAll('.list__item');
 const totalPopElement = containerElement.querySelector('.total-population');
 const averagePopElement = containerElement.querySelector('.average-population');
 
-const populations = [...itemsElement].map((element) => {
-  const populationsText = element.querySelector('.population').textContent;
+const populations = [...itemsElement]
+  .map((element) => {
+    const populationsText = element.querySelector('.population').textContent;
 
-  return Number(populationsText.replace(/,/g, ''));
-});
+    return Number(populationsText.replace(/,/g, ''));
+  })
+  .filter((value) => Number.isFinite(value));
 
 const total = populations.reduce((sum, num) => sum + num, 0);
 const average = total / populations.length;
