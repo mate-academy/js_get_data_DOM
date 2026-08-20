4eaf9bdf3016fb368b9956ae026b978c455feaa3
ALex <sasha.hivuk@gmail.com>
2026-08-20 13:36:53 +0300

    10 countrys

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+const listCountrys = document.querySelectorAll('.population');
+let count = 0;
+
+for (const country of listCountrys) {
+  count += Number(country.textContent.replaceAll(',', ''));
+}
+
+const midle = count / listCountrys.length;
+
+const niceCount = count.toLocaleString();
+const niceMidle = midle.toLocaleString();
+
+const population = document.querySelector('.average-population');
+
+population.textContent = niceMidle;
+
+const total = document.querySelector('.total-population');
+
+total.textContent = niceCount;
