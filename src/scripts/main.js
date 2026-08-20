ad4fe0913ca6c245148f778a82b608902f98d8e1
Sumar Illya <illasumar002@gmail.com>
2026-08-20 21:32:28 +0300

    Tasks complited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const array = document.querySelectorAll('.population');
+
+const totalPopulation = Array.from(array).reduce(
+  (acc, span) => acc + parseFloat(span.textContent.replaceAll(',', '')),
+  0,
+);
+
+document.querySelector('.total-population').textContent =
+  totalPopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent = Math.round(
+  totalPopulation / array.length,
+).toLocaleString('en-US');
