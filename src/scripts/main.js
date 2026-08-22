cd0082d6695a0573a3cbe665686c4de7968fd1af
Your Name <you@example.com>
2026-08-22 15:34:12 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write your code here
+const listOfElements = document.querySelectorAll('.population');
+const arrayOfElements = [];
+
+for (let i = 0; i < listOfElements.length; i++) {
+  const el = Number(listOfElements[i].innerText.replaceAll(',', ''));
+
+  arrayOfElements.push(el);
+}
+
+let totalPopulation = 0;
+let avaragePopulation = 0;
+
+for (let i = 0; i < arrayOfElements.length; i++) {
+  totalPopulation += arrayOfElements[i];
+  avaragePopulation = Math.floor(totalPopulation / arrayOfElements.length);
+}
+
+const totalPopulationString = backToStringFormat(totalPopulation);
+const avaragePopulationString = backToStringFormat(avaragePopulation);
+
+const totalPopulationValue = document.querySelector('.total-population');
+
+totalPopulationValue.innerText = totalPopulationString;
+
+const avaragePopulationValue = document.querySelector('.average-population');
+
+avaragePopulationValue.innerText = avaragePopulationString;
+
+function backToStringFormat(from) {
+  const str = from.toString();
+  let result = '';
+
+  for (let i = str.length; i >= 0; i = i - 3) {
+    result = str.substring(i - 3, i) + result;
+
+    if (i > 3) {
+      result = ',' + result;
+    }
+  }
+
+  return result;
+}
