8f6701e670372bc1f31787d6287d754ffce2abd6
Yevhenii Dmytriienko <dmitrik24042007@gmail.com>
2026-08-22 16:01:00 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const populationElements = [...document.querySelectorAll('.population')];
+
+const population = populationElements.map((item) => {
+  let text = item.textContent;
+
+  text = text.replaceAll(',', '');
+
+  const countPopulation = Number(text);
+
+  return countPopulation;
+});
+
+const totalPopulation = population.reduce((sum, num) => sum + num, 0);
+const avaragePopulation = Math.round(totalPopulation / population.length);
+
+const elemTotalCalc = document.querySelector('.total-population');
+const elemAvgCalc = document.querySelector('.average-population');
+
+elemTotalCalc.textContent = totalPopulation.toLocaleString('en-US');
+elemAvgCalc.textContent = avaragePopulation.toLocaleString('en-US');
