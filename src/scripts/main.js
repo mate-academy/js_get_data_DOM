06575cf24dbfb01a71a23458cfb06420f1df0e6c
AchekcSs_ <achekcss@gmail.com>
2026-08-23 13:38:33 +0300

    feat: calculate total and average population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+const totalPopulationElement = document.querySelector('.total-population');
+const averagePopulationElement = document.querySelector('.average-population');
+
+let totalPopulation = 0;
+let averagePopulation = 0;
+
+for (const population of populationElements) {
+  totalPopulation += +population.textContent.split(',').join('');
+}
+
+averagePopulation = totalPopulation / populationElements.length;
+
+totalPopulationElement.textContent = totalPopulation.toLocaleString('en-US');
+
+averagePopulationElement.textContent =
+  averagePopulation.toLocaleString('en-US');
