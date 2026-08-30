f686a661db315208f12e6e851b060bb8a1048ac3
Ruslan Sydorenko <letovletov111@gmail.com>
2026-08-30 18:13:50 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const populationClasses = document.querySelectorAll('span.population');
+const allText = Array.from(populationClasses).map((el) => el.textContent);
+const textIntoNumber = allText.map((elem) => Number(elem.replaceAll(',', '')));
+const totalSum = textIntoNumber.reduce((count, elem) => count + elem, 0);
+const formattedSum = totalSum.toLocaleString('en-US');
+const average = (totalSum / textIntoNumber.length).toLocaleString('en-US');
+
+const totalPopulation = document.querySelector('span.total-population');
+const averagePopulation = document.querySelector('span.average-population');
+
+totalPopulation.textContent = formattedSum;
+averagePopulation.textContent = average;
