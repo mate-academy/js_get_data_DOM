3e19ff169dedd23116bd15894bb1be2b6033636f
Mykola Averchenko <kolya.averchenko@gmail.com>
2026-09-08 21:43:39 +0300

    add solution in main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write your code here
+const populations = [...document.querySelectorAll('.population')].map(
+  (span) => +span.textContent.replaceAll(',', ''),
+);
+
+const total = populations.reduce((sum, val) => sum + val);
+const average = Math.round(total / populations.length);
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString('en-us');
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString('en-us');
