138ea8cf32a6910670ca09eb45e71bc0e6c46155
Veronika Nychyporuk <veronikanychyporuk@gmail.com>
2026-09-14 23:43:58 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const allPopulationElements = document.querySelectorAll('.population');
+const totalPopulationElement = document.querySelector('.total-population');
+const averagePopulationElement = document.querySelector('.average-population');
+
+let totalPopulation = 0;
+
+allPopulationElements.forEach((population) => {
+  const populationNumber = parseInt(population.textContent.replaceAll(',', ''));
+
+  totalPopulation += populationNumber;
+});
+
+totalPopulationElement.textContent = formateNumber(totalPopulation);
+
+averagePopulationElement.textContent = formateNumber(
+  Math.round(totalPopulation / allPopulationElements.length),
+);
+
+function formateNumber(num) {
+  let number = num;
+  const numberParts = [];
+
+  for (let i = Math.ceil(String(num).length / 3) - 1; i >= 0; i--) {
+    numberParts[i] = number % 1000;
+    number = Math.trunc(number / 1000);
+  }
+
+  return numberParts.join(',');
+}
