08534a12052a88980eafbc47ec4a9fe36036ef42
Vlad <rebellqwe@gmail.com>
2026-09-14 20:08:40 +0300

    data task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const populationCountries = document.querySelectorAll('.population');
+const populations = Array.from(populationCountries).map((el) => {
+  return parseInt(el.textContent.replace(/,/g, ''));
+});
+const total = populations.reduce((sum, num) => sum + num, 0);
+const average = total / populations.length;
+
+const averageSpan = document.querySelector('.average-population');
+const totalSpan = document.querySelector('.total-population');
+
+averageSpan.textContent = Math.round(average).toLocaleString();
+totalSpan.textContent = total.toLocaleString();
