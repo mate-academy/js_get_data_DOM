8145e5358012cea87a93548acbc21fe4018a329b
Vlad <rebellqwe@gmail.com>
2026-09-14 20:58:32 +0300

    data task fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,9 @@
 'use strict';
 
 const populationCountries = document.querySelectorAll('.population');
-const populations = Array.from(populationCountries).map((el) => {
-  const value = parseInt(el.textContent.replace(/,/g, ''), 10);
-
-  if (!el || isNaN(value)) {
-    return 0; // або null
-  } else {
-    return value;
-  }
-});
+const populations = Array.from(populationCountries)
+  .map((el) => parseInt(el.textContent.replace(/,/g, ''), 10))
+  .filter((value) => Number.isFinite(value));
 const total = populations.reduce((sum, num) => sum + num, 0);
 const average = total / populations.length;
 
