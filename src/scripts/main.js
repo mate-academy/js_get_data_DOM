64c46ae30c357ead6b422541c677788339e24731
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2026-09-15 12:44:49 +0300

    Changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,14 @@
 const populationElements = document.querySelectorAll('.population');
 
 const populations = Array.from(populationElements)
-  .map((el) => Number(el.textContent.replace(/,/g, '')))
+  .map((element) => Number(element.textContent.replace(/,/g, '')))
   .filter(Number.isFinite);
 
 const total = populations.reduce((sum, population) => sum + population, 0);
-const average = total / populations.length;
+const average = populations.length ? total / populations.length : 0;
 
-document.querySelector('.total-population').textContent = total;
-document.querySelector('.average-population').textContent = average;
+document.querySelector('.total-population').textContent =
+  total.toLocaleString();
+
+document.querySelector('.average-population').textContent =
+  Math.round(average).toLocaleString();
