c1256381522a9a8aed5dc5435ba33225c4d13e56
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2026-09-15 12:41:05 +0300

    Completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+
+const populations = Array.from(populationElements)
+  .map((el) => Number(el.textContent.replace(/,/g, '')))
+  .filter(Number.isFinite);
+
+const total = populations.reduce((sum, population) => sum + population, 0);
+const average = total / populations.length;
+
+document.querySelector('.total-population').textContent = total;
+document.querySelector('.average-population').textContent = average;
